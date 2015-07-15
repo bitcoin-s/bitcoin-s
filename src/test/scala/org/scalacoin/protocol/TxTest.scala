235ece91116a6a1a7c5f2eba7313dfa1bf14fa08
Chris Stewart <stewart.chris1234@gmail.com>
2015-07-15 11:47:46 -0500

    Adding transaction case classes

---
+++ src/test/scala/org/scalacoin/protocol/TxTest.scala
@@ -0,0 +1,14 @@
+package org.scalacoin.protocol
+
+import org.scalatest.FlatSpec
+import org.scalatest.MustMatchers
+
+/**
+ * Created by chris on 7/14/15.
+ */
+class TxTest extends FlatSpec with MustMatchers {
+
+  
+
+
+}
