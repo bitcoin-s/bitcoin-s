f9f9ec79c3a7b6a2e9996a9d6c1396cc30f7eba0
Chris Stewart <stewart.chris1234@gmail.com>
2015-07-26 18:49:59 -0500

    Added test for transaction version

--- src/main/scala/org/scalacoin/protocol/Tx.scala
+++ src/main/scala/org/scalacoin/protocol/Tx.scala
@@ -4,7 +4,27 @@ package org.scalacoin.protocol
 /**
  * Created by chris on 7/14/15.
  */
-case class Tx(version : Long, txInCount : VarInt, txIn : Seq[TxIn], txOutCount : VarInt, txOut : TxOut, lockTime : Long)
+
+
+trait Tx {
+  def version : Long
+  def txInCount : VarInt
+  def txIn : Seq[TxIn]
+  def txOutCount : VarInt
+  def txOut : TxOut
+  def lockTime : Long
+}
+
+case class NetworkTx(serialization : String ) extends Tx {
+  require(!serialization.contains(" "), "Your network transaction contains whitespace")
+  override def version = java.lang.Long.parseLong(serialization.slice(0,8),16)
+  override def txInCount : VarInt = VarInt("")
+  override def txIn : Seq[TxIn] = Seq()
+  override def txOutCount : VarInt = VarInt("")
+  override def txOut : TxOut = TxOut(1,VarInt(""), Seq())
+  override def lockTime : Long = 0
+
+}
 
 
 case class TxIn(prevousOutput : OutPoint, scriptLength : VarInt, scriptSignature : Seq[Char], sequence : Long)
