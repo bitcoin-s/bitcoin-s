c5eed4174126a29764f851cfb078c28cc58e784c
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-21 17:16:30 -0600

    Adding build files

---
+++ project/Build.scala
@@ -0,0 +1,24 @@
+import sbt._
+import Keys._ 
+object ScalaCoinBuild extends Build {
+
+  val appName = "scalacoin"
+  val appV = "0.0.1" 
+  val scalaV = "2.11.4"
+  val organization = "org.scalacoin"
+  val slf4jV = "1.7.7"
+  val appDependencies = Seq(
+    "org.scalatest" % "scalatest_2.11" % "2.2.0",
+    "org.bitcoinj" % "bitcoinj-core" % "0.13.3",
+    "org.slf4j" % "slf4j-api" % slf4jV % "provided",
+    "io.spray" %%  "spray-json" % "1.3.0" withSources() withJavadoc()
+  ) 
+  
+  val main = Project(appName, file(".")).enablePlugins().settings(
+    version := appV,
+    scalaVersion := scalaV,
+    resolvers += Resolver.sonatypeRepo("releases"),  
+    libraryDependencies ++= appDependencies
+  )
+} 
+
