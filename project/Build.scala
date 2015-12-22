2a1eb915c6de431b1c60778afa4c2e56a1166284
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-21 19:03:43 -0600

    Excluding slf4j-api inside of bitcoinj for Build

--- project/Build.scala
+++ project/Build.scala
@@ -6,11 +6,11 @@ object ScalaCoinBuild extends Build {
   val appV = "0.0.1" 
   val scalaV = "2.11.4"
   val organization = "org.scalacoin"
-  val slf4jV = "1.7.7"
+  val slf4jV = "1.7.5"
   val appDependencies = Seq(
     "org.scalatest" % "scalatest_2.11" % "2.2.0",
-    "org.bitcoinj" % "bitcoinj-core" % "0.13.3",
-    "org.slf4j" % "slf4j-api" % slf4jV % "provided",
+    ("org.bitcoinj" % "bitcoinj-core" % "0.13.3").exclude("org.slf4j", "slf4j-api"),
+    "org.slf4j" % "slf4j-api" % slf4jV /*% "provided"*/,
     "io.spray" %%  "spray-json" % "1.3.0" withSources() withJavadoc()
   ) 
   
