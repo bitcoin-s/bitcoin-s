03d4e2f5e1b0d3ec315c970a7636ce6610b73cc6
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-26 21:11:17 -0600

    Adding case classes for implmentations of Inputs, Outputs and Outpoints

--- src/main/scala/org/scalacoin/protocol/transaction/TransactionInput.scala
+++ src/main/scala/org/scalacoin/protocol/transaction/TransactionInput.scala
@@ -6,8 +6,11 @@ import org.scalacoin.protocol.VarInt
  * Created by chris on 12/26/15.
  */
 trait TransactionInput {
-  def previousOutput : OutPoint
+  def previousOutput : TransactionOutPoint
   def scriptLength : VarInt
   def scriptSignature : Seq[Char]
   def sequence : Long
 }
+
+case class TransactionInputImpl(previousOutput : TransactionOutPoint, scriptLength : VarInt,
+  scriptSignature : Seq[Char], sequence : Long) extends TransactionInput
--- src/main/scala/org/scalacoin/protocol/transaction/TransactionOutPoint.scala
+++ src/main/scala/org/scalacoin/protocol/transaction/TransactionOutPoint.scala
@@ -7,3 +7,5 @@ trait TransactionOutPoint {
   def hash : Seq[Char]
   def index : Long
 }
+
+case class TransactionOutPointImpl(hash : Seq[Char], index : Long) extends TransactionOutPoint
