10f9397e43a7c44b90e683802d401df566312bef
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-26 21:03:43 -0600

    Factoring out transaction components into their own scala files

---
+++ src/main/scala/org/scalacoin/protocol/transaction/TransactionOutput.scala
@@ -0,0 +1,15 @@
+package org.scalacoin.protocol.transaction
+
+import org.scalacoin.currency.Satoshis
+import org.scalacoin.protocol.VarInt
+
+/**
+ * Created by chris on 12/26/15.
+ */
+trait TransactionOutput {
+
+  def value : Satoshis
+  def pkScriptLength : VarInt
+  def pkScript : Seq[Char]
+
+}
