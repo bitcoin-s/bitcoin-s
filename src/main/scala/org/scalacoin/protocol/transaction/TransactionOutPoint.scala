98acc89e037713634aea97bec3dbaeec4d7de41c
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-27 19:43:13 -0600

    Adding marshaller for TransactionOutPoint

--- src/main/scala/org/scalacoin/protocol/transaction/TransactionOutPoint.scala
+++ src/main/scala/org/scalacoin/protocol/transaction/TransactionOutPoint.scala
@@ -4,8 +4,8 @@ package org.scalacoin.protocol.transaction
  * Created by chris on 12/26/15.
  */
 trait TransactionOutPoint {
-  def hash : Seq[String]
-  def index : Long
+  def txId : String
+  def vout : Int
 }
 
-case class TransactionOutPointImpl(hash : Seq[String], index : Long) extends TransactionOutPoint
+case class TransactionOutPointImpl(txId : String, vout : Int) extends TransactionOutPoint
