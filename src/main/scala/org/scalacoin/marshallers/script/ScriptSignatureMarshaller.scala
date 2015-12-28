b135ca887457ed5bb48491c17f6b59376b011654
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-27 20:17:34 -0600

    Adding TransactionInput marshaller

--- src/main/scala/org/scalacoin/marshallers/script/ScriptSignatureMarshaller.scala
+++ src/main/scala/org/scalacoin/marshallers/script/ScriptSignatureMarshaller.scala
@@ -1,12 +1,31 @@
 package org.scalacoin.marshallers.script
 
-import org.scalacoin.protocol.script.ScriptSignatureImpl
-import spray.json.DefaultJsonProtocol
+import org.scalacoin.protocol.script.{ScriptSignature, ScriptSignatureImpl}
+import spray.json._
 
 /**
  * Created by chris on 12/27/15.
  */
 object ScriptSignatureMarshaller extends DefaultJsonProtocol {
+  val scriptSigKey = "scriptSig"
+  val asmKey = "asm"
+  val hexKey = "hex"
+  implicit object ScriptSignatureFormatter extends RootJsonFormat[ScriptSignature] {
 
-  implicit val scriptSignatureMarshaller = jsonFormat2(ScriptSignatureImpl.apply _)
+    override def read(value : JsValue) : ScriptSignature = {
+      val obj = value.asJsObject
+      val asm = obj.fields(asmKey)
+      val hex = obj.fields(hexKey)
+      ScriptSignatureImpl(asm.convertTo[String], hex.convertTo[String])
+    }
+
+    override def write(scriptSig : ScriptSignature) : JsValue = {
+      val m : Map[String,JsValue] = Map(
+        asmKey -> JsString(scriptSig.asm),
+        hexKey -> JsString(scriptSig.hex)
+      )
+
+      JsObject(m)
+    }
+  }
 }
