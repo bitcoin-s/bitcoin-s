a062ac8e14be68e53329641e0c726269e063cb85
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-06 15:01:42 -0600

    Implementing OP_HASH160, OP_DUP and adding in some util functions

---
+++ src/main/scala/org/scalacoin/script/interpreter/ScriptInterpreter.scala
@@ -0,0 +1,16 @@
+package org.scalacoin.script.interpreter
+
+import org.scalacoin.script.ScriptOperation
+import org.scalacoin.script.stack.OP_DUP
+
+import scala.collection.immutable.Stack
+
+/**
+ * Created by chris on 1/6/16.
+ */
+trait ScriptInterpreter {
+
+
+
+
+}
---
+++ src/test/scala/org/scalacoin/script/interpreter/ScriptInterpreterTest.scala
@@ -0,0 +1,13 @@
+package org.scalacoin.script.interpreter
+
+import org.scalacoin.script.stack.OP_DUP
+import org.scalatest.{MustMatchers, FlatSpec}
+
+/**
+ * Created by chris on 1/6/16.
+ */
+class ScriptInterpreterTest extends FlatSpec with MustMatchers with ScriptInterpreter {
+
+
+
+}
