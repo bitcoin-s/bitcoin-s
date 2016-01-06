8fc4aad147a998c0c554e3a183c313c0ac9f3584
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-06 15:30:42 -0600

    Defining bitwise operations

---
+++ src/main/scala/org/scalacoin/script/bitwise/BitwiseOperations.scala
@@ -0,0 +1,22 @@
+package org.scalacoin.script.bitwise
+
+import org.scalacoin.script.ScriptOperation
+
+/**
+ * Created by chris on 1/6/16.
+ */
+sealed trait BitwiseOperation extends ScriptOperation
+
+/**
+ * Returns 1 if the inputs are exactly equal, 0 otherwise.
+ */
+case object OP_EQUAL extends ScriptOperation {
+  override def opCode = 135
+}
+
+/**
+ * Same as OP_EQUAL, but runs OP_VERIFY afterward.
+ */
+case object OP_EQUALVERIFY extends ScriptOperation {
+  override def opCode = 136
+}
--- src/test/scala/org/scalacoin/script/crypto/CryptoInterpreterTest.scala
+++ src/test/scala/org/scalacoin/script/crypto/CryptoInterpreterTest.scala
@@ -13,6 +13,21 @@ class CryptoInterpreterTest extends FlatSpec with MustMatchers with CryptoInterp
     val (newStack,newScript) = hash160(stack,script)
     newStack.head must be ("5238C71458E464D9FF90299ABCA4A1D7B9CB76AB".toLowerCase)
     newScript.size must be(0)
+  }
 
+  it must "fail to evaluate OP_HASH160 when the stack is empty" in {
+    intercept[IllegalArgumentException] {
+      val stack = List()
+      val script = List(OP_HASH160)
+      val (newStack,newScript) = hash160(stack,script)
+    }
+  }
+
+  it must "fail to evaluate OP_HASH160 when the script stack is empty" in {
+    intercept[IllegalArgumentException] {
+      val stack = List("02218AD6CDC632E7AE7D04472374311CEBBBBF0AB540D2D08C3400BB844C654231".toLowerCase)
+      val script = List()
+      val (newStack,newScript) = hash160(stack,script)
+    }
   }
 }
