3df808a82bd1dfdb0daacd6870291eac106055d6
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-06 19:44:57 -0600

    Implementing the OP_EQUALVERIFY operator

--- src/main/scala/org/scalacoin/script/bitwise/BitwiseInterpreter.scala
+++ src/main/scala/org/scalacoin/script/bitwise/BitwiseInterpreter.scala
@@ -1,12 +1,19 @@
 package org.scalacoin.script.bitwise
 
 import org.scalacoin.script.ScriptOperation
+import org.scalacoin.script.control.{OP_VERIFY, ControlOperationsInterpreter}
 
 /**
  * Created by chris on 1/6/16.
  */
-trait BitwiseInterpreter  {
+trait BitwiseInterpreter extends ControlOperationsInterpreter  {
 
+  /**
+   * 	Returns 1 if the inputs are exactly equal, 0 otherwise.
+   * @param stack
+   * @param script
+   * @return
+   */
   def equal(stack : List[String], script : List[ScriptOperation]) : (List[String], List[ScriptOperation]) = {
     require(stack.size > 1, "Stack size must be 2 or more to compare the top two values")
     require(script.headOption.isDefined && script.head == OP_EQUAL, "Script operation must be OP_EQUAL")
@@ -17,5 +24,21 @@ trait BitwiseInterpreter  {
 
   }
 
-  def equalVerify(stack : Seq[String], script : List[ScriptOperation]) : (Seq[String], Seq[ScriptOperation]) = ???
+  /**
+   * Same as OP_EQUAL, but runs OP_VERIFY afterward.
+   * @param stack
+   * @param script
+   * @return
+   */
+  def equalVerify(stack : List[String], script : List[ScriptOperation]) : Boolean = {
+    require(stack.size > 1, "Stack size must be 2 or more to compare the top two values")
+    require(script.headOption.isDefined && script.head == OP_EQUALVERIFY, "Script operation must be OP_EQUALVERIFY")
+    //first replace OP_EQUALVERIFY with OP_EQUAL and OP_VERIFY
+    val simpleScript = OP_EQUAL :: OP_VERIFY :: script.tail
+    val (newStack,newScript) = equal(stack,simpleScript)
+    val result : Boolean = verify(newStack,newScript)
+    result
+  }
+
+
 }
--- src/test/scala/org/scalacoin/script/bitwise/BitwiseInterpreterTset.scala
+++ src/test/scala/org/scalacoin/script/bitwise/BitwiseInterpreterTset.scala
@@ -6,15 +6,53 @@ import org.scalatest.{MustMatchers, FlatSpec}
  * Created by chris on 1/6/16.
  */
 class BitwiseInterpreterTest extends FlatSpec with MustMatchers with BitwiseInterpreter {
+  private val pubKeyHash = "5238C71458E464D9FF90299ABCA4A1D7B9CB76AB".toLowerCase
 
   "BitwiseInterpreter" must "evaluate OP_EQUAL" in {
-    val pubKeyHash = "5238C71458E464D9FF90299ABCA4A1D7B9CB76AB".toLowerCase
     val stack = List(pubKeyHash, pubKeyHash)
     val script = List(OP_EQUAL)
 
     val (newStack,newScript) = equal(stack,script)
-
     newStack.head.toInt must be (1)
+  }
+
+  it must "throw an exception for OP_EQUAL when we don't have enough items on the stack" in {
+    intercept[IllegalArgumentException] {
+      equal(List(), List(OP_EQUAL))
+    }
+  }
+
+  it must "throw an exception for OP_EQUAL when we don't have enough items on the script stack" in {
+    intercept[IllegalArgumentException] {
+      equal(List(pubKeyHash), List())
+    }
+  }
+
+  it must "evaulate OP_EQUALVERIFY to true given two of the same pub keys" in {
+    val stack = List(pubKeyHash, pubKeyHash)
+    val script = List(OP_EQUALVERIFY)
+    val result = equalVerify(stack,script)
+    result must be (true)
+  }
+
+  it must "evaluate OP_EQUALVERIFY to false given two different pub keys" in {
+    val uniquePubKey = pubKeyHash +"0"
+    val stack = List(pubKeyHash,uniquePubKey)
+    val script = List(OP_EQUALVERIFY)
+    val result = equalVerify(stack,script)
+    result must be (false)
+  }
+
+  it must "throw an exception for OP_EQUALVERIFY when we don't have enough args in stack" in {
+    intercept[IllegalArgumentException] {
+      equalVerify(List(),List(OP_EQUALVERIFY))
+    }
+  }
+
+  it must "throw an exception for OP_EQUALVERIFY when we don't have enough args in script stack" in {
+    intercept[IllegalArgumentException] {
+      equalVerify(List(pubKeyHash),List())
+    }
   }
 
 }
