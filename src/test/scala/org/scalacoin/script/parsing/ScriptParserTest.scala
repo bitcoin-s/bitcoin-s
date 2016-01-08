71f6dd2ad9dc3ef7ef795b684abe79e8c08b6a29
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-07 22:07:03 -0600

    fixing compliation issue, adding unit tests for parsing

--- src/test/scala/org/scalacoin/script/parsing/ScriptParserTest.scala
+++ src/test/scala/org/scalacoin/script/parsing/ScriptParserTest.scala
@@ -10,7 +10,12 @@ class ScriptParserTest extends FlatSpec with MustMatchers with ScriptParser {
 
 
   "ScriptParser" must "parse an input script" in {
-    val parsedInput = parse(TestUtil.p2khInputScriptNotParsed)
+    val parsedInput = parse(TestUtil.p2pkhInputScriptNotParsed)
     parsedInput must be (TestUtil.p2pkhInputScript)
+  }
+
+  it must "parse an output script" in {
+    val parsedOutput = parse(TestUtil.p2pkhOutputScriptNotParsed)
+    parsedOutput must be (TestUtil.p2pkhOutputScript)
   }
 }
