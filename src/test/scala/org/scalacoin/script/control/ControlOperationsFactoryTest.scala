7e5bc6e839c6d22401f84d9838d9393ddc87cd41
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-08 10:12:32 -0600

    Adding control operations factory

---
+++ src/test/scala/org/scalacoin/script/control/ControlOperationsFactoryTest.scala
@@ -0,0 +1,20 @@
+package org.scalacoin.script.control
+
+import org.scalatest.{MustMatchers, FlatSpec}
+
+/**
+ * Created by chris on 1/8/16.
+ */
+class ControlOperationsFactoryTest extends FlatSpec with MustMatchers with ControlOperationsFactory {
+
+  "ControlOperationsFactory" must "match a string with a control operation" in {
+    fromString("OP_ELSE") must be (Some(OP_ELSE))
+    fromString("OP_ENDIF") must be (Some(OP_ENDIF))
+    fromString("OP_IF") must be (Some(OP_IF))
+    fromString("OP_NOP") must be (Some(OP_NOP))
+    fromString("OP_NOTIF") must be (Some(OP_NOTIF))
+    fromString("OP_RETURN") must be (Some(OP_RETURN))
+    fromString("OP_VERIFY") must be (Some(OP_VERIFY))
+    fromString("RANDOM") must be (None)
+  }
+}
