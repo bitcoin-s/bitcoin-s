ef85d18a052e8e402dedd33748812b4e729b513a
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-08 10:16:22 -0600

    Adding Bitwise operations factory

--- src/main/scala/org/scalacoin/script/bitwise/BitwiseOperations.scala
+++ src/main/scala/org/scalacoin/script/bitwise/BitwiseOperations.scala
@@ -10,13 +10,13 @@ sealed trait BitwiseOperation extends ScriptOperation
 /**
  * Returns 1 if the inputs are exactly equal, 0 otherwise.
  */
-case object OP_EQUAL extends ScriptOperation {
+case object OP_EQUAL extends BitwiseOperation {
   override def opCode = 135
 }
 
 /**
  * Same as OP_EQUAL, but runs OP_VERIFY afterward.
  */
-case object OP_EQUALVERIFY extends ScriptOperation {
+case object OP_EQUALVERIFY extends BitwiseOperation {
   override def opCode = 136
 }
---
+++ src/main/scala/org/scalacoin/script/bitwise/BitwiseOperationsFactory.scala
@@ -0,0 +1,10 @@
+package org.scalacoin.script.bitwise
+
+import org.scalacoin.script.ScriptOperationFactory
+
+/**
+ * Created by chris on 1/8/16.
+ */
+trait BitwiseOperationsFactory extends ScriptOperationFactory[BitwiseOperation] {
+  override def operations = Seq(OP_EQUAL, OP_EQUALVERIFY)
+}
