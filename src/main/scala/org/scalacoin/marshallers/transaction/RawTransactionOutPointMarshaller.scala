84982d8b950464f3b240ca8cdb0eab8873e9ec1a
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-11 14:39:51 -0600

    Adding ability to serialize and unserialize raw outpoints

---
+++ src/main/scala/org/scalacoin/marshallers/transaction/RawTransactionOutPointMarshaller.scala
@@ -0,0 +1,30 @@
+package org.scalacoin.marshallers.transaction
+
+import java.nio.ByteBuffer
+
+import org.scalacoin.marshallers.RawBitcoinSerializer
+import org.scalacoin.protocol.transaction.{TransactionOutPointImpl, TransactionOutPoint}
+import org.scalacoin.util.ScalacoinUtil
+
+/**
+ * Source for serialization
+ * https://bitcoin.org/en/developer-reference#outpoint
+ *
+ */
+object RawTransactionOutPointMarshaller extends RawBitcoinSerializer[TransactionOutPoint] {
+
+
+  def read(str : String) : TransactionOutPoint = {
+    val bytes = ScalacoinUtil.decodeHex(str)
+    val txId : List[Byte] = bytes.slice(0,16)
+    val index : BigInt = BigInt(bytes.slice(16, bytes.size).toArray)
+    TransactionOutPointImpl(ScalacoinUtil.encodeHex(txId), index.toInt)
+  }
+
+  def write(outPoint : TransactionOutPoint) : String = {
+    val indexBytes : List[Byte] = List(0x00,0x00,0x00,outPoint.vout.toByte)
+    //first 32 bytes are txId, last 4 bytes are the index
+    outPoint.txId + ScalacoinUtil.encodeHex(indexBytes)
+  }
+
+}
