ee47f852120423e62d52f79dfebf94ff3e2884af
TomMcCabe <btmccabejr@gmail.com>
2016-01-11 11:21:01 -0600

    added memory pool info marshaller

---
+++ src/main/scala/org/scalacoin/protocol/blockchain/MemPoolInfo.scala
@@ -0,0 +1,11 @@
+package org.scalacoin.protocol.blockchain
+
+/**
+ * Created by Tom on 1/11/2016.
+ */
+trait MemPoolInfo {
+  def size : Int
+  def bytes : Int
+}
+
+case class MemPoolInfoImpl(size : Int, bytes : Int) extends MemPoolInfo
---
+++ src/test/scala/org/scalacoin/marshallers/blockchain/MemPoolInfoMarshallerTest.scala
@@ -0,0 +1,26 @@
+package org.scalacoin.marshallers.blockchain
+
+import org.scalacoin.protocol.blockchain.MemPoolInfo
+import org.scalatest.{MustMatchers, FlatSpec}
+import spray.json._
+
+/**
+ * Created by Tom on 1/11/2016.
+ */
+class MemPoolInfoMarshallerTest extends FlatSpec with MustMatchers {
+  val str =
+    """
+      |{
+      |    "size" : 270,
+      |    "bytes" : 295151
+      |}
+    """.stripMargin
+
+  val json = str.parseJson
+
+  "MemPoolInfo" must "parse node's tx memory pool" in {
+    val mempool : MemPoolInfo = MemPoolInfoMarshaller.MemPoolInfoFormatter.read(json)
+      mempool.size must be (270)
+      mempool.bytes must be (295151)
+  }
+}
