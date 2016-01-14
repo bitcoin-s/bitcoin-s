51a821066adb4d2ccc2fa70496ff492be71e5d33
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-14 10:14:51 -0600

    Adding logging capability

--- src/main/scala/org/scalacoin/marshallers/script/ScriptSignatureMarshaller.scala
+++ src/main/scala/org/scalacoin/marshallers/script/ScriptSignatureMarshaller.scala
@@ -1,6 +1,7 @@
 package org.scalacoin.marshallers.script
 
 import org.scalacoin.protocol.script.{ScriptSignature, ScriptSignatureImpl}
+import org.slf4j.LoggerFactory
 import spray.json._
 
 /**
@@ -10,9 +11,11 @@ object ScriptSignatureMarshaller extends DefaultJsonProtocol {
   val scriptSigKey = "scriptSig"
   val asmKey = "asm"
   val hexKey = "hex"
+  private lazy val logger = LoggerFactory.getLogger(this.getClass().toString)
   implicit object ScriptSignatureFormatter extends RootJsonFormat[ScriptSignature] {
 
     override def read(value : JsValue) : ScriptSignature = {
+      logger.debug(this.getClass().toString + " is marshalling json value: " + value)
       val obj = value.asJsObject
       val asm = obj.fields(asmKey)
       val hex = obj.fields(hexKey)
