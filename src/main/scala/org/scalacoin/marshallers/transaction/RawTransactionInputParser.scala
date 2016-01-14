6afe7dcbaca74495adbad7a90224836d394edf73
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-14 10:56:41 -0600

    Fixing bug in outpoint parser, wasn't correctly parsing vout numbers

--- src/main/scala/org/scalacoin/marshallers/transaction/RawTransactionInputParser.scala
+++ src/main/scala/org/scalacoin/marshallers/transaction/RawTransactionInputParser.scala
@@ -5,6 +5,7 @@ import org.scalacoin.marshallers.script.RawScriptSignatureParser
 import org.scalacoin.protocol.script.ScriptSignature
 import org.scalacoin.protocol.transaction.{TransactionInputImpl, TransactionOutPoint, TransactionInput}
 import org.scalacoin.util.ScalacoinUtil
+import org.slf4j.LoggerFactory
 
 import scala.annotation.tailrec
 
@@ -13,6 +14,8 @@ import scala.annotation.tailrec
  * https://bitcoin.org/en/developer-reference#txin
  */
 trait RawTransactionInputParser extends RawBitcoinSerializer[Seq[TransactionInput]] {
+
+  private lazy val logger = LoggerFactory.getLogger(this.getClass().toString())
 
   override def read(bytes : List[Byte]) : Seq[TransactionInput] = {
     require(bytes.size > 0, "You passed in an empty list to read")
@@ -26,13 +29,20 @@ trait RawTransactionInputParser extends RawBitcoinSerializer[Seq[TransactionInpu
         val scriptSigSize = bytes(outPointBytesSize).toInt
         val scriptSigBytes = bytes.slice(outPointBytesSize, outPointBytesSize + scriptSigSize + 1)
         val scriptSig : ScriptSignature = RawScriptSignatureParser.read(scriptSigBytes)
-        val sequenceBytes = bytes.slice(outPointBytesSize + scriptSigSize + 1 , outPointBytesSize + scriptSigSize + 6)
+
+        val sequenceBytesSize = 4
+        val lastInputByte = outPointBytesSize + scriptSigSize + sequenceBytesSize + 1
+        val sequenceBytes = bytes.slice(outPointBytesSize + scriptSigSize + 1,lastInputByte)
         val sequenceNumberHex : String = ScalacoinUtil.encodeHex(sequenceBytes)
         val sequenceNumber : Long = BigInt(sequenceNumberHex,16).toLong
         val txInput = TransactionInputImpl(outPoint, scriptSig,sequenceNumber)
+
         val newAccum =  txInput :: accum
-        val bytesToBeParsed = bytes.slice(scriptSigSize + 5, bytes.size)
-        loop(bytesToBeParsed, newAccum, inputsLeftToParse - 1)
+        val bytesToBeParsed = bytes.slice(lastInputByte, bytes.size)
+        val inputsLeft = inputsLeftToParse - 1
+        logger.debug("Parsed tx input: " + txInput)
+        logger.debug("Inputs to be parsed " +  inputsLeft)
+        loop(bytesToBeParsed, newAccum,inputsLeft)
       } else accum
     }
 
@@ -41,3 +51,5 @@ trait RawTransactionInputParser extends RawBitcoinSerializer[Seq[TransactionInpu
 
   override def write(inputs : Seq[TransactionInput]) = ???
 }
+
+object RawTransactionInputParser extends RawTransactionInputParser
--- src/test/scala/org/scalacoin/marshallers/transaction/RawTransactionOutPointParserTest.scala
+++ src/test/scala/org/scalacoin/marshallers/transaction/RawTransactionOutPointParserTest.scala
@@ -1,6 +1,7 @@
 package org.scalacoin.marshallers.transaction
 
 import org.scalacoin.protocol.transaction.TransactionOutPointImpl
+import org.scalacoin.util.ScalacoinUtil
 import org.scalatest.{FlatSpec, MustMatchers}
 
 /**
@@ -15,6 +16,13 @@ class RawTransactionOutPointParserTest extends FlatSpec with MustMatchers  {
     outPoint.vout must be (0)
   }
 
+
+  it must "parse a large vout for an outpoint" in {
+    val outPointLargeVout = "df80e3e6eba7dcd4650281d3c13f140dafbb823a7227a78eb6ee9f6cedd04001340000006b"
+    val outPoint = RawTransactionOutPointParser.read(outPointLargeVout)
+    outPoint.vout must be (52)
+    outPoint.txId must be ("0140d0ed6c9feeb68ea727723a82bbaf0d143fc1d3810265d4dca7ebe6e380df")
+  }
   it must "write a TransactionOutPoint to a serialized format" in {
     val outPoint = RawTransactionOutPointParser.read(rawOutPoint)
     val actualSerialization = RawTransactionOutPointParser.write(outPoint)
