87ff7f90be39ae7acc3ecaeee42454184f9d93e4
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-25 08:03:36 -0600

    Adding arithmetic operations to ScriptNumbers

--- src/main/scala/org/scalacoin/script/constant/ScriptNumber.scala
+++ src/main/scala/org/scalacoin/script/constant/ScriptNumber.scala
@@ -5,7 +5,11 @@ import org.scalacoin.script.ScriptOperationFactory
 /**
  * Created by chris on 1/9/16.
  */
-trait ScriptNumber extends ScriptOperation
+trait ScriptNumber extends ScriptOperation {
+  def + (that : ScriptNumber) : ScriptNumber = ScriptNumberImpl(opCode + that.opCode)
+  def - (that : ScriptNumber) : ScriptNumber = ScriptNumberImpl(opCode - that.opCode)
+  def * (that : ScriptNumber) : ScriptNumber = ScriptNumberImpl(opCode * that.opCode)
+}
 
 case class ScriptNumberImpl(num : Int) extends ScriptNumber {
   //see the 'Constants; section in https://en.bitcoin.it/wiki/Script
---
+++ src/test/scala/org/scalacoin/script/constant/ScriptNumberTest.scala
@@ -0,0 +1,38 @@
+package org.scalacoin.script.constant
+
+import org.scalatest.{FlatSpec, MustMatchers}
+
+/**
+ * Created by chris on 1/25/16.
+ */
+class ScriptNumberTest extends FlatSpec with MustMatchers {
+
+  val zero = ScriptNumberImpl(0)
+  val one = ScriptNumberImpl(1)
+  val ten = ScriptNumberImpl(10)
+  "ScriptNumber" must "derive the correct hex value from a script number" in {
+    ScriptNumberImpl(1).hex must be ("01")
+
+    ScriptNumberImpl(8).hex must be ("08")
+  }
+
+  it must "add two script numbers correctly" in {
+    (zero + zero) must be (zero)
+    (one + zero) must be (one)
+    (one + ten) must be (ScriptNumberImpl(11))
+  }
+
+  it must "subtract two script numbers correctly" in {
+    (zero - zero) must equal (zero)
+    (one - zero) must equal (one)
+    (ten - one) must equal (ScriptNumberImpl(9))
+  }
+
+  it must "multiply two script numbers correctly" in {
+    (zero * zero) must equal (zero)
+    (one * zero) must equal (zero)
+    (ten * one) must equal (ten)
+    (ten * ScriptNumberImpl(5)) must equal (ScriptNumberImpl(50))
+  }
+
+}
