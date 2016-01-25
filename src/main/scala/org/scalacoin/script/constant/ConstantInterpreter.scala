ea517312aba342135f9e5f6ba959d80f3d038b15
Chris Stewart <stewart.chris1234@gmail.com>
2016-01-25 08:03:36 -0600

    Adding arithmetic operations to ScriptNumbers

--- src/main/scala/org/scalacoin/script/constant/ConstantInterpreter.scala
+++ src/main/scala/org/scalacoin/script/constant/ConstantInterpreter.scala
@@ -1,12 +1,14 @@
 package org.scalacoin.script.constant
 
 import org.scalacoin.util.ScalacoinUtil
+import org.slf4j.LoggerFactory
 
 /**
  * Created by chris on 1/24/16.
  */
 trait ConstantInterpreter {
 
+  private def logger = LoggerFactory.getLogger(this.getClass())
   /**
    * The next byte contains the number of bytes to be pushed onto the stack.
    * @param stack
@@ -32,7 +34,9 @@ trait ConstantInterpreter {
   def opPushData2(stack : List[ScriptToken], script : List[ScriptToken]) : (List[ScriptToken],List[ScriptToken]) = {
     require(script.headOption.isDefined && script.head == OP_PUSHDATA2, "Top of script stack must be OP_PUSHDATA2")
     val numberOfBytesToBePushed : String = script.slice(1,3).map(_.hex).mkString
+    logger.debug("Number of bytes to be pushed: " + numberOfBytesToBePushed)
     val numberOfBytes : Int = Integer.parseInt(numberOfBytesToBePushed,16)
+    logger.debug("Parsed hex: " + numberOfBytes)
     val slicedScript = script.slice(3,script.size)
     val (newStack,newScript) = opPushData(stack,slicedScript,numberOfBytes)
     (newStack,newScript)
