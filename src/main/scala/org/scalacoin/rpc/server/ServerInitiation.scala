452295dbf6480ff26188ee2f51d6d01f2f6e636c
Tom McCabe <btmccabejr@gmail.com>
2016-02-02 19:55:35 -0600

    shorten server initiation code

--- src/main/scala/org/scalacoin/rpc/server/ServerInitiation.scala
+++ src/main/scala/org/scalacoin/rpc/server/ServerInitiation.scala
@@ -5,28 +5,8 @@ import sys.process._
 /**
  * Created by Tom on 1/15/2016.
  */
-class ServerInitiation {
-  /**
-   * Starts a bitcoin server on the main network
-   * @return
-   */
-  def bitcoinMain = Process("bitcoind").run
+class ServerInitiation(server : String) {
 
-  /**
-   * Starts a bitcoin server on the test network
-   * https://bitcoin.org/en/developer-examples#testnet
-   * @return
-   */
-  def bitcoinTestNet = Process("bitcoind -testnet").run
-
-  /**
-   * Starts a bitcoin server on the regression test mode, which allows you to create new private block chains.
-   * For more see: https://bitcoin.org/en/developer-examples#regtest-mode
-   * @return
-   */
-  def bitcoinRegTest = Process("bitcoind -regtest").run
-
-  def initiateServer(server : String) = Process(server).run
-
+  Process(server).run
 
 }
