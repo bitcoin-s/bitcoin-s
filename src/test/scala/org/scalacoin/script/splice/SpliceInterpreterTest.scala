24b79f88b753c8cc843cdb3edf5d480479f40bf7
Chris Stewart <stewart.chris1234@gmail.com>
2016-02-06 14:17:46 -0600

    Finished implementing OP_SIZE script operation

--- src/test/scala/org/scalacoin/script/splice/SpliceInterpreterTest.scala
+++ src/test/scala/org/scalacoin/script/splice/SpliceInterpreterTest.scala
@@ -37,7 +37,14 @@ class SpliceInterpreterTest extends FlatSpec with MustMatchers with SpliceInterp
     val newProgram = opSize(program)
     newProgram.stack must be (List(ScriptNumberImpl(2),ScriptConstantImpl("80")))
     newProgram.script.isEmpty must be (true)
+  }
 
-
+  it must "evaluate an OP_SIZE correctly with a negative number" in {
+    val stack = List(ScriptNumberImpl(-1))
+    val script = List(OP_SIZE)
+    val program = ScriptProgramImpl(stack,script,TestUtil.transaction,List())
+    val newProgram = opSize(program)
+    newProgram.stack must be (List(ScriptNumberImpl(1),ScriptNumberImpl(-1)))
+    newProgram.script.isEmpty must be (true)
   }
 }
