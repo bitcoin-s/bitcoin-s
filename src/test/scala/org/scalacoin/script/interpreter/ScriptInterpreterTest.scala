0a8b7eaca1315caeef4412af21c1d340c541e38e
Chris Stewart <stewart.chris1234@gmail.com>
2016-02-06 10:12:30 -0600

    Uncommenting SpliceInterpreter test

--- src/test/scala/org/scalacoin/script/interpreter/ScriptInterpreterTest.scala
+++ src/test/scala/org/scalacoin/script/interpreter/ScriptInterpreterTest.scala
@@ -54,13 +54,13 @@ class ScriptInterpreterTest extends FlatSpec with MustMatchers with ScriptInterp
     val source = scala.io.Source.fromFile("src/test/scala/org/scalacoin/script/interpreter/script_valid.json")
 
       //use this to represent a single test case from script_valid.json
-/*    val lines =
+    val lines =
     """
       |
       |[["128", "SIZE 2 EQUAL", "P2SH,STRICTENC"]]
-    """.stripMargin*/
+    """.stripMargin
 
-    val lines = try source.getLines.filterNot(_.isEmpty).map(_.trim) mkString "\n" finally source.close()
+    //val lines = try source.getLines.filterNot(_.isEmpty).map(_.trim) mkString "\n" finally source.close()
     val json = lines.parseJson
     val testCasesOpt : Seq[Option[CoreTestCase]] = json.convertTo[Seq[Option[CoreTestCase]]]
     val testCases : Seq[CoreTestCase] = testCasesOpt.flatten
--- src/test/scala/org/scalacoin/script/splice/SpliceInterpreterTest.scala
+++ src/test/scala/org/scalacoin/script/splice/SpliceInterpreterTest.scala
@@ -21,12 +21,12 @@ class SpliceInterpreterTest extends FlatSpec with MustMatchers with SpliceInterp
 
   }
 
-/*  it must "evaluate an OP_SIZE correctly with something of size 2 bytes" in {
+  it must "evaluate an OP_SIZE correctly with something of size 2 bytes" in {
     val stack = List(ScriptConstantImpl("80"))
     val script = List(OP_SIZE)
     val program = ScriptProgramImpl(stack,script,TestUtil.transaction,List())
     val newProgram = opSize(program)
     newProgram.stack must be (List(ScriptNumberImpl(2)),ScriptConstantImpl("80"))
     newProgram.script.isEmpty must be (true)
-  }*/
+  }
 }
