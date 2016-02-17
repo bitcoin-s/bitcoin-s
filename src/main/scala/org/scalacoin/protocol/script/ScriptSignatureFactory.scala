ce847b207c2bd7fd83842647958838206fb55d02
Chris Stewart <stewart.chris1234@gmail.com>
2016-02-17 15:17:55 -0600

    Finding a script signature and it's hash type is working for p2pkh scripts

--- src/main/scala/org/scalacoin/protocol/script/ScriptSignatureFactory.scala
+++ src/main/scala/org/scalacoin/protocol/script/ScriptSignatureFactory.scala
@@ -1,6 +1,6 @@
 package org.scalacoin.protocol.script
 
-import org.scalacoin.marshallers.script.ScriptParser
+import org.scalacoin.marshallers.script.{RawScriptSignatureParser, ScriptParser}
 import org.scalacoin.script.constant.ScriptToken
 import org.scalacoin.util.ScalacoinUtil
 
@@ -33,8 +33,8 @@ object ScriptSignatureFactory {
    * @return
    */
   def factory(bytes : Seq[Byte]) : ScriptSignature = {
-    val asm = ScriptParser.parse(bytes)
-    ScriptSignatureImpl(asm,ScalacoinUtil.encodeHex(bytes))
+    val scriptSig : ScriptSignature = RawScriptSignatureParser.read(bytes.toList)
+    scriptSig
   }
 
 }
--- src/test/scala/org/scalacoin/protocol/script/ScriptSignatureTest.scala
+++ src/test/scala/org/scalacoin/protocol/script/ScriptSignatureTest.scala
@@ -10,27 +10,18 @@ import org.scalatest.{FlatSpec, MustMatchers}
 class ScriptSignatureTest extends FlatSpec with MustMatchers {
 
   "ScriptSignature" must "find the digital signature for the transaction inside of the script signature" in {
-    //from this question
-    //https://bitcoin.stackexchange.com/questions/37125/how-are-sighash-flags-encoded-into-a-signature
-    val scriptSigHex = "304402206e3729f021476102a06ea453cea0a26cb9c096cca641efc4229c1111ed3a96fd022037dce1456a93f53d3e868c789b1b750a48a4c1110cd5b7049779b5f4f3c8b6200103ff1104b46b2141df1948dd0df2223720a3a471ec57404cace47063843a699a0f"
-
-    val scriptSig = ScriptSignatureFactory.factory(scriptSigHex)
-
-    scriptSig.signature must be ("304402206e3729f021476102a06ea453cea0a26cb9c096cca641efc4229c1111ed3a96fd022037dce1456a93f53d3e868c789b1b750a48a4c1110cd5b7049779b5f4f3c8b62001")
+    val scriptSig = ScriptSignatureFactory.factory(TestUtil.rawScriptSig)
+    scriptSig.signature.head.hex must be ("3045022100ad8e961fe3c22b2647d92b078f4c0cf81b3106ea5bf8b900ab8646aa4430216f022071d4edc2b5588be20ac4c2d07edd8ed069e10b2402d3dce2d3b835ccd075f28301")
   }
 
-/*   it must "derive the signature hash type from the signature" in {
-    val signatureHex = "47304402206e3729f021476102a06ea453cea0a26cb9c096cca641efc4229c1111ed3a96fd022037dce1456a93f53d3e868c789b1b750a48a4c1110cd5b7049779b5f4f3c8b62001"
+   it must "derive the signature hash type from the signature" in {
 
-    val scriptSig = ScriptSignatureFactory.factory(signatureHex)
-
-
-    scriptSig.hashType must be (SIGHASH_ALL)
+    TestUtil.scriptSig.hashType must be (SIGHASH_ALL)
   }
 
 
 
-  it must "parse a p2pkh input script and derive it's hash type" in {
+  /*it must "parse a p2pkh input script and derive it's hash type" in {
     val scriptSig = ScriptSignatureFactory.factory(TestUtil.p2pkhInputScript)
     scriptSig.hashType must be (SIGHASH_ALL)
   }*/
