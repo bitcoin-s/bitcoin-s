6bbcfa8cff7b77ee087dd1cf5e56e64ca834d2f6
Chris Stewart <stewart.chris1234@gmail.com>
2016-02-17 15:34:14 -0600

    Finding script signatures and their hash types working for p2sh script sigs

--- src/main/scala/org/scalacoin/protocol/script/ScriptSignature.scala
+++ src/main/scala/org/scalacoin/protocol/script/ScriptSignature.scala
@@ -1,7 +1,7 @@
 package org.scalacoin.protocol.script
 
 import org.scalacoin.marshallers.transaction.TransactionElement
-import org.scalacoin.script.constant.ScriptToken
+import org.scalacoin.script.constant.{OP_0, ScriptToken}
 import org.scalacoin.script.crypto.{HashType, HashTypeFactory}
 import org.scalacoin.util.ScalacoinUtil
 import org.slf4j.LoggerFactory
@@ -15,11 +15,17 @@ trait ScriptSignature extends TransactionElement {
   def asm : Seq[ScriptToken]
   def hex : String
 
-  def signature : Seq[ScriptToken]  = Seq(asm(1))
+  def signature : ScriptToken  = {
+    if (asm.headOption.isDefined && asm.head == OP_0) {
+      //must be p2sh because of bug that forces p2sh scripts
+      //to begin with OP_0
+      asm(2)
+    } else asm(1)
+  }
   def hashType : HashType = {
-    require(HashTypeFactory.fromByte(signature.head.bytes.last).isDefined,
-      "Hash type could not be read for this scriptSig: " + signature.head.hex)
-    HashTypeFactory.fromByte(signature.head.bytes.last).get
+    require(HashTypeFactory.fromByte(signature.bytes.last).isDefined,
+      "Hash type could not be read for this scriptSig: " + signature.hex)
+    HashTypeFactory.fromByte(signature.bytes.last).get
   }
 
 }
