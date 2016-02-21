0f9a87f971b26bb1c1adddd3ad0c5dd78b0c5937
Chris Stewart <stewart.chris1234@gmail.com>
2016-02-21 16:16:41 -0600

    implementing first parts of serializing a transaction to be signed by an ECKey

--- src/main/scala/org/scalacoin/crypto/TransactionSignatureSerializer.scala
+++ src/main/scala/org/scalacoin/crypto/TransactionSignatureSerializer.scala
@@ -26,7 +26,7 @@ trait TransactionSignatureSerializer extends RawBitcoinSerializerHelper {
    * @param script
    * @return
    */
-  def serializeScriptCode(script : Seq[ScriptToken]) : String = removeOpCodeSeparators(script)
+  def serializeScriptCode(script : Seq[ScriptToken]) : List[Byte] = removeOpCodeSeparators(script)
 
 
   def serializeInput(input : TransactionInput, nType : Int, nVersion : Int) : String = ???
@@ -56,11 +56,32 @@ trait TransactionSignatureSerializer extends RawBitcoinSerializerHelper {
   }
 
   def serialize(inputIndex : Int, nType : Int, script : ScriptPubKey, hashType : HashType) : String = {
-    //remove signatures from all inputs because we cannot sign existing signatures
+    // Clear input scripts in preparation for signing. If we're signing a fresh
+    // transaction that step isn't very helpful, but it doesn't add much cost relative to the actual
+    // EC math so we'll do it anyway.
     val txWithInputSigsRemoved = for {
       input <- spendingTransaction.inputs
     } yield input.factory(ScriptSignatureFactory.empty)
-    ???
+
+
+    // This step has no purpose beyond being synchronized with Bitcoin Core's bugs. OP_CODESEPARATOR
+    // is a legacy holdover from a previous, broken design of executing scripts that shipped in Bitcoin 0.1.
+    // It was seriously flawed and would have let anyone take anyone elses money. Later versions switched to
+    // the design we use today where scripts are executed independently but share a stack. This left the
+    // OP_CODESEPARATOR instruction having no purpose as it was only meant to be used internally, not actually
+    // ever put into scripts. Deleting OP_CODESEPARATOR is a step that should never be required but if we don't
+    // do it, we could split off the main chain.
+    val scriptWithOpCodeSeparatorsRemoved : List[Byte] = serializeScriptCode(script.asm)
+
+    val inputToSign = spendingTransaction.inputs(inputIndex)
+
+    // Set the input to the script of its output. Bitcoin Core does this but the step has no obvious purpose as
+    // the signature covers the hash of the prevout transaction which obviously includes the output script
+    // already. Perhaps it felt safer to him in some way, or is another leftover from how the code was written.
+    val inputWithConnectedScript = inputToSign.factory(script)
+
+    //check the hash type of
+
   }
 
   /**
@@ -68,12 +89,12 @@ trait TransactionSignatureSerializer extends RawBitcoinSerializerHelper {
    * format
    * @return
    */
-  def removeOpCodeSeparators(script : Seq[ScriptToken]) : String = {
+  def removeOpCodeSeparators(script : Seq[ScriptToken]) : List[Byte] = {
     val scriptWithoutOpCodeSeparators : String = script.filterNot(_ == OP_CODESEPARATOR).map(_.hex).mkString
     val scriptWithoutOpCodeSeparatorSize = addPrecedingZero((scriptWithoutOpCodeSeparators.size / 2).toHexString)
     val expectedScript : ScriptPubKey = ScriptPubKeyFactory.factory(
       scriptWithoutOpCodeSeparatorSize + scriptWithoutOpCodeSeparators)
-    expectedScript.hex
+    expectedScript.bytes
   }
 }
 
