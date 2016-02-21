b786ebfb9bbd75003d17378be198eaf7fbc21b53
Chris Stewart <stewart.chris1234@gmail.com>
2016-02-21 17:05:48 -0600

    Adding functionality to TransactionFactory to update inputs and outputs

--- src/main/scala/org/scalacoin/protocol/transaction/TransactionFactory.scala
+++ src/main/scala/org/scalacoin/protocol/transaction/TransactionFactory.scala
@@ -3,10 +3,22 @@ package org.scalacoin.protocol.transaction
 /**
  * Created by chris on 2/21/16.
  */
+
 trait TransactionFactory { this : Transaction =>
 
-  def factory(outputs : Seq[TransactionOutput]) = {
-    TransactionImpl(version,inputs,outputs,lockTime)
+
+  def factory(updatedOutputs : UpdateTransactionOutputs) : Transaction = {
+    TransactionImpl(version,inputs,updatedOutputs.outputs,lockTime)
   }
 
+  def factory(updatedInputs : UpdateTransactionInputs) = {
+    TransactionImpl(version,updatedInputs.inputs,outputs,lockTime)
+  }
+
+
 }
+
+sealed trait TransactionFactoryHelper
+case class UpdateTransactionOutputs(outputs : Seq[TransactionOutput]) extends TransactionFactoryHelper
+case class UpdateTransactionInputs(inputs : Seq[TransactionInput]) extends TransactionFactoryHelper
+
