f4ec6b4caa2aa236a9db47158be58dd4bff825c6
Chris Stewart <stewart.chris1234@gmail.com>
2016-03-07 14:49:05 -0600

    Creating product types for scriptPubKeys

--- src/main/scala/org/scalacoin/protocol/script/ScriptPubKeyFactory.scala
+++ src/main/scala/org/scalacoin/protocol/script/ScriptPubKeyFactory.scala
@@ -3,7 +3,7 @@ package org.scalacoin.protocol.script
 import org.scalacoin.marshallers.script.{RawScriptPubKeyParser, ScriptParser}
 import org.scalacoin.protocol.{NonStandard, MultiSignature, P2SH, P2PKH}
 import org.scalacoin.script.bitwise.{OP_EQUAL, OP_EQUALVERIFY}
-import org.scalacoin.script.constant.{ScriptConstantImpl, BytesToPushOntoStackImpl, ScriptToken}
+import org.scalacoin.script.constant.{ScriptConstant, ScriptConstantImpl, BytesToPushOntoStackImpl, ScriptToken}
 import org.scalacoin.script.crypto.{OP_CHECKMULTISIG, OP_CHECKSIG, OP_HASH160}
 import org.scalacoin.script.stack.OP_DUP
 import org.scalacoin.util.{BitcoinScriptUtil, BitcoinSUtil, Factory, ScalacoinUtil}
@@ -41,10 +41,14 @@ trait ScriptPubKeyFactory extends Factory[ScriptPubKey] {
   def fromAsm(asm : Seq[ScriptToken]) : ScriptPubKey = {
     val scriptPubKeyHex = BitcoinScriptUtil.asmToHex(asm)
     asm match {
-      case List(OP_DUP, OP_HASH160, BytesToPushOntoStackImpl(x), ScriptConstantImpl(pubKeyHash), OP_EQUALVERIFY, OP_CHECKSIG) => P2PKHScriptPubKey(scriptPubKeyHex)
-      case List(OP_HASH160, BytesToPushOntoStackImpl(x), ScriptConstantImpl(scriptHash), OP_EQUAL) => P2SHScriptPubKey(scriptPubKeyHex)
+      case List(OP_DUP, OP_HASH160, BytesToPushOntoStackImpl(x), ScriptConstantImpl(pubKeyHash), OP_EQUALVERIFY, OP_CHECKSIG) =>
+        P2PKHScriptPubKeyImpl(scriptPubKeyHex)
+      case List(OP_HASH160, BytesToPushOntoStackImpl(x), ScriptConstantImpl(scriptHash), OP_EQUAL) =>
+        P2SHScriptPubKeyImpl(scriptPubKeyHex)
+      case List(x : ScriptConstant, OP_CHECKSIG) => P2PKScriptPubKeyImpl(scriptPubKeyHex)
       //TODO: make this more robust, this isn't the pattern that multsignature scriptPubKeys follow
-      case _ if (asm.size > 0 && asm.last == OP_CHECKMULTISIG) => MultiSignatureScriptPubKey(scriptPubKeyHex)
+      case _ if (asm.size > 0 && asm.last == OP_CHECKMULTISIG) =>
+        MultiSignatureScriptPubKeyImpl(scriptPubKeyHex)
       case _ => ScriptPubKeyImpl(scriptPubKeyHex)
     }
   }
