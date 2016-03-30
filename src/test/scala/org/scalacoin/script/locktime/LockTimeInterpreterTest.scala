3279e5c45f2da0d2973789a9857411e3684f1864
Chris Stewart <stewart.chris1234@gmail.com>
2016-03-30 12:45:31 -0500

    Adding test cases to LockTimeInterpreter

--- src/test/scala/org/scalacoin/script/locktime/LockTimeInterpreterTest.scala
+++ src/test/scala/org/scalacoin/script/locktime/LockTimeInterpreterTest.scala
@@ -1,7 +1,8 @@
 package org.scalacoin.script.locktime
 
+import org.scalacoin.protocol.transaction.{UpdateTransactionInputs, TransactionInputFactory, TransactionFactory}
 import org.scalacoin.script.ScriptProgramFactory
-import org.scalacoin.script.constant.OP_0
+import org.scalacoin.script.constant.{ScriptNumberImpl, OP_0}
 import org.scalacoin.util.TestUtil
 import org.scalatest.{MustMatchers, FlatSpec}
 
@@ -25,4 +26,65 @@ class LockTimeInterpreterTest extends FlatSpec with MustMatchers with LockTimeIn
     val newProgram = opCheckLockTimeVerify(program)
     newProgram.isValid must be (false)
   }
+
+  it must "mark the transaction as invalid if the stack top is negative" in {
+    val stack = Seq(ScriptNumberImpl(-1))
+    val script = Seq(OP_CHECKLOCKTIMEVERIFY)
+    val txInputAdjustedSequenceNumber = TransactionInputFactory.factory(TestUtil.transaction.inputs(0),0)
+    val txAdjustedSequenceNumber = TransactionFactory.factory(TestUtil.transaction,UpdateTransactionInputs(Seq(txInputAdjustedSequenceNumber)))
+    val adjustedLockTimeTx = TransactionFactory.factory(txAdjustedSequenceNumber,0)
+    val baseProgram = ScriptProgramFactory.factory(adjustedLockTimeTx,TestUtil.testProgram.scriptPubKey,TestUtil.testProgram.inputIndex,TestUtil.testProgram.flags)
+    val program = ScriptProgramFactory.factory(baseProgram,stack,script)
+    val newProgram = opCheckLockTimeVerify(program)
+    newProgram.isValid must be (false)
+  }
+
+  it must "mark the transaction as invalid if the locktime on the tx is < 500000000 && stack top is >= 500000000" in {
+    val stack = Seq(ScriptNumberImpl(500000000))
+    val script = Seq(OP_CHECKLOCKTIMEVERIFY)
+    val txInputAdjustedSequenceNumber = TransactionInputFactory.factory(TestUtil.transaction.inputs(0),0)
+    val txAdjustedSequenceNumber = TransactionFactory.factory(TestUtil.transaction,UpdateTransactionInputs(Seq(txInputAdjustedSequenceNumber)))
+    val adjustedLockTimeTx = TransactionFactory.factory(txAdjustedSequenceNumber,0)
+    val baseProgram = ScriptProgramFactory.factory(adjustedLockTimeTx,TestUtil.testProgram.scriptPubKey,TestUtil.testProgram.inputIndex,TestUtil.testProgram.flags)
+    val program = ScriptProgramFactory.factory(baseProgram,stack,script)
+    val newProgram = opCheckLockTimeVerify(program)
+    newProgram.isValid must be (false)
+  }
+
+  it must "mark the transaction as invalid if the locktime on the tx is >= 500000000 && stack top is < 500000000" in {
+    val stack = Seq(ScriptNumberImpl(499999999))
+    val script = Seq(OP_CHECKLOCKTIMEVERIFY)
+    val txInputAdjustedSequenceNumber = TransactionInputFactory.factory(TestUtil.transaction.inputs(0),0)
+    val txAdjustedSequenceNumber = TransactionFactory.factory(TestUtil.transaction,UpdateTransactionInputs(Seq(txInputAdjustedSequenceNumber)))
+    val adjustedLockTimeTx = TransactionFactory.factory(txAdjustedSequenceNumber,500000000)
+    val baseProgram = ScriptProgramFactory.factory(adjustedLockTimeTx,TestUtil.testProgram.scriptPubKey,TestUtil.testProgram.inputIndex,TestUtil.testProgram.flags)
+    val program = ScriptProgramFactory.factory(baseProgram,stack,script)
+    val newProgram = opCheckLockTimeVerify(program)
+    newProgram.isValid must be (false)
+  }
+
+  it must "mark the transaction as valid if the locktime on the tx is < 500000000 && stack top is < 500000000" in {
+    val stack = Seq(ScriptNumberImpl(499999999))
+    val script = Seq(OP_CHECKLOCKTIMEVERIFY)
+    val txInputAdjustedSequenceNumber = TransactionInputFactory.factory(TestUtil.transaction.inputs(0),0)
+    val txAdjustedSequenceNumber = TransactionFactory.factory(TestUtil.transaction,UpdateTransactionInputs(Seq(txInputAdjustedSequenceNumber)))
+    val adjustedLockTimeTx = TransactionFactory.factory(txAdjustedSequenceNumber,0)
+    val baseProgram = ScriptProgramFactory.factory(adjustedLockTimeTx,TestUtil.testProgram.scriptPubKey,TestUtil.testProgram.inputIndex,TestUtil.testProgram.flags)
+    val program = ScriptProgramFactory.factory(baseProgram,stack,script)
+    val newProgram = opCheckLockTimeVerify(program)
+    newProgram.isValid must be (true)
+  }
+
+  it must "mark the transaction as valid if the locktime on the tx is >= 500000000 && stack top is >= 500000000" in {
+    val stack = Seq(ScriptNumberImpl(500000000))
+    val script = Seq(OP_CHECKLOCKTIMEVERIFY)
+    val txInputAdjustedSequenceNumber = TransactionInputFactory.factory(TestUtil.transaction.inputs(0),0)
+    val txAdjustedSequenceNumber = TransactionFactory.factory(TestUtil.transaction,UpdateTransactionInputs(Seq(txInputAdjustedSequenceNumber)))
+    val adjustedLockTimeTx = TransactionFactory.factory(txAdjustedSequenceNumber,500000000)
+    val baseProgram = ScriptProgramFactory.factory(adjustedLockTimeTx,TestUtil.testProgram.scriptPubKey,TestUtil.testProgram.inputIndex,TestUtil.testProgram.flags)
+    val program = ScriptProgramFactory.factory(baseProgram,stack,script)
+    val newProgram = opCheckLockTimeVerify(program)
+    newProgram.isValid must be (true)
+  }
 }
+
