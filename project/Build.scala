29de3cf433338c7edd5c1b9466bae9fab6ca1663
Chris Stewart <stewart.chris1234@gmail.com>
2016-04-08 08:37:42 -0500

    bumping scala version trying to get continous integration working

--- project/Build.scala
+++ project/Build.scala
@@ -4,13 +4,15 @@ object ScalaCoinBuild extends Build {
 
   val appName = "scalacoin"
   val appV = "0.0.1" 
-  val scalaV = "2.11.4"
+  val scalaV = "2.11.7"
   val organization = "org.scalacoin"
   val slf4jV = "1.7.5"
   val logbackV = "1.0.13"
   val spongyCastleV = "1.51.0.0"
+  val scalaTestV = "2.2.6"
   val appDependencies = Seq(
-    "org.scalatest" % "scalatest_2.11" % "2.2.0",
+    "org.scalactic" %% "scalactic" % scalaTestV, 
+    "org.scalatest" %% "scalatest" % scalaTestV % "test",
     ("org.bitcoinj" % "bitcoinj-core" % "0.13.4").exclude("org.slf4j", "slf4j-api"),
     "org.slf4j" % "slf4j-api" % slf4jV,
     "io.spray" %%  "spray-json" % "1.3.0" withSources() withJavadoc(), 
