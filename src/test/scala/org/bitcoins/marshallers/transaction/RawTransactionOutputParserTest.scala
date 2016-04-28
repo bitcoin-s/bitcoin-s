Christewart/scalacoin

Refactoring the implementation tyeps for ScriptNumber & ScriptConstant into their factories

src/test/scala/org/scalacoin/marshallers/transaction/RawTransactionOutputParserTest.scala
@@ -3,7 +3,7 @@ package org.scalacoin.marshallers.transaction
 import org.scalacoin.currency.{Satoshis, CurrencyUnits, Bitcoins}
 import org.scalacoin.protocol.transaction.TransactionOutput
 import org.scalacoin.script.bitwise.OP_EQUAL
-import org.scalacoin.script.constant.{BytesToPushOntoStackImpl, ScriptConstantImpl}
+import org.scalacoin.script.constant.{ScriptConstant, BytesToPushOntoStackImpl}
 import org.scalacoin.script.crypto.OP_HASH160
 import org.scalatest.{MustMatchers, FlatSpec}
 
@@ -23,8 +23,8 @@ class RawTransactionOutputParserTest extends FlatSpec with MustMatchers with Raw
     val secondOutput = txOutput(1)
     firstOutput.value must be (CurrencyUnits.toSatoshis(Bitcoins(0.0002)))
     secondOutput.value must be (CurrencyUnits.toSatoshis(Bitcoins(0.02981145)))
-    firstOutput.scriptPubKey.asm must be (Seq(OP_HASH160, BytesToPushOntoStackImpl(20),ScriptConstantImpl("eda8ae08b5c9f973f49543e90a7c292367b3337c"), OP_EQUAL))
-    secondOutput.scriptPubKey.asm must be (Seq(OP_HASH160,BytesToPushOntoStackImpl(20), ScriptConstantImpl("be2319b9060429692ebeffaa3be38497dc5380c8"), OP_EQUAL))
+    firstOutput.scriptPubKey.asm must be (Seq(OP_HASH160, BytesToPushOntoStackImpl(20),ScriptConstant("eda8ae08b5c9f973f49543e90a7c292367b3337c"), OP_EQUAL))
+    secondOutput.scriptPubKey.asm must be (Seq(OP_HASH160,BytesToPushOntoStackImpl(20), ScriptConstant("be2319b9060429692ebeffaa3be38497dc5380c8"), OP_EQUAL))
   }
 
   it must "seralialize a transaction output" in {

src/test/scala/org/scalacoin/protocol/script/P2SHScriptSignatureTest.scala
@@ -1,7 +1,7 @@
 package org.scalacoin.protocol.script
 
 import org.scalacoin.crypto.{ECFactory}
-import org.scalacoin.script.constant.{OP_0, BytesToPushOntoStackImpl, ScriptConstantImpl}
+import org.scalacoin.script.constant.{ScriptConstant, OP_0, BytesToPushOntoStackImpl}
 import org.scalacoin.util.TestUtil
 import org.scalatest.{FlatSpec, MustMatchers}
 
@@ -31,9 +31,9 @@ class P2SHScriptSignatureTest extends FlatSpec with MustMatchers {
     }
 
     p2shScriptSig.scriptSignatureNoRedeemScript.asm must be (Seq(
-      OP_0, BytesToPushOntoStackImpl(71), ScriptConstantImpl("304402207d764cb90c9fd84b74d33a47cf3a0ffead9ded98333776becd6acd32c4426dac02203905a0d064e7f53d07793e86136571b6e4f700c1cfb888174e84d78638335b8101"),
+      OP_0, BytesToPushOntoStackImpl(71), ScriptConstant("304402207d764cb90c9fd84b74d33a47cf3a0ffead9ded98333776becd6acd32c4426dac02203905a0d064e7f53d07793e86136571b6e4f700c1cfb888174e84d78638335b8101"),
       BytesToPushOntoStackImpl(72),
-      ScriptConstantImpl("3045022100906aaca39f022acd8b7a38fd2f92aca9e9f35cfeaee69a6f13e1d083ae18222602204c9ed96fc6c4de56fd85c679fc59c16ee1ccc80c42563b86174e1a506fc007c801")
+      ScriptConstant("3045022100906aaca39f022acd8b7a38fd2f92aca9e9f35cfeaee69a6f13e1d083ae18222602204c9ed96fc6c4de56fd85c679fc59c16ee1ccc80c42563b86174e1a506fc007c801")
     ))
   }
 }

src/test/scala/org/scalacoin/script/locktime/LockTimeInterpreterTest.scala
@@ -3,7 +3,7 @@ package org.scalacoin.script.locktime
 import org.scalacoin.protocol.transaction.{TransactionInput, Transaction, UpdateTransactionInputs}
 import org.scalacoin.script.result.{ScriptErrorUnsatisfiedLocktime, ScriptErrorNegativeLockTime, ScriptErrorInvalidStackOperation}
 import org.scalacoin.script.{ExecutionInProgressScriptProgram, ExecutedScriptProgram, PreExecutionScriptProgram, ScriptProgram}
-import org.scalacoin.script.constant.{ScriptNumber, ScriptNumberImpl, OP_0}
+import org.scalacoin.script.constant.{ScriptNumber, OP_0}
 import org.scalacoin.util.{ScriptProgramTestUtil, TestUtil}
 import org.scalatest.{MustMatchers, FlatSpec}
 

src/test/scala/org/scalacoin/util/BitcoinJConversions.scala
@@ -10,7 +10,7 @@ import org.scalacoin.crypto.ECPublicKey
 import org.scalacoin.protocol.script.{UpdateScriptPubKeyAsm, ScriptPubKey}
 import org.scalacoin.protocol.transaction.{TransactionOutput, Transaction}
 import org.scalacoin.script.ScriptOperationFactory
-import org.scalacoin.script.constant.{ScriptConstantImpl, ScriptToken}
+import org.scalacoin.script.constant.{ScriptToken}
 import org.slf4j.LoggerFactory
 import scala.collection.JavaConversions._
 /**

bitcoin-s/bitcoin-s

src/test/scala/org/bitcoins/marshallers/transaction/RawTransactionOutputParserTest.scala
@@ -1,11 +1,12 @@
 package org.bitcoins.marshallers.transaction
 
-import org.bitcoins.currency.{Satoshis, CurrencyUnits, Bitcoins}
+
+import org.bitcoins.currency.{Bitcoins, CurrencyUnits, Satoshis}
 import org.bitcoins.protocol.transaction.TransactionOutput
 import org.bitcoins.script.bitwise.OP_EQUAL
-import org.bitcoins.script.constant.{BytesToPushOntoStackImpl, ScriptConstantImpl}
+import org.bitcoins.script.constant.{BytesToPushOntoStackImpl, ScriptConstant}
 import org.bitcoins.script.crypto.OP_HASH160
-import org.scalatest.{MustMatchers, FlatSpec}
+import org.scalatest.{FlatSpec, MustMatchers}
 
 /**
  * Created by chris on 1/11/16.
@@ -23,8 +24,8 @@ class RawTransactionOutputParserTest extends FlatSpec with MustMatchers with Raw
     val secondOutput = txOutput(1)
     firstOutput.value must be (CurrencyUnits.toSatoshis(Bitcoins(0.0002)))
     secondOutput.value must be (CurrencyUnits.toSatoshis(Bitcoins(0.02981145)))
-    firstOutput.scriptPubKey.asm must be (Seq(OP_HASH160, BytesToPushOntoStackImpl(20),ScriptConstantImpl("eda8ae08b5c9f973f49543e90a7c292367b3337c"), OP_EQUAL))
-    secondOutput.scriptPubKey.asm must be (Seq(OP_HASH160,BytesToPushOntoStackImpl(20), ScriptConstantImpl("be2319b9060429692ebeffaa3be38497dc5380c8"), OP_EQUAL))
+    firstOutput.scriptPubKey.asm must be (Seq(OP_HASH160, BytesToPushOntoStackImpl(20),ScriptConstant("eda8ae08b5c9f973f49543e90a7c292367b3337c"), OP_EQUAL))
+    secondOutput.scriptPubKey.asm must be (Seq(OP_HASH160,BytesToPushOntoStackImpl(20), ScriptConstant("be2319b9060429692ebeffaa3be38497dc5380c8"), OP_EQUAL))
   }
 
   it must "seralialize a transaction output" in {

src/test/scala/org/bitcoins/protocol/script/P2SHScriptSignatureTest.scala
@@ -1,7 +1,8 @@
 package org.bitcoins.protocol.script
 
-import org.bitcoins.crypto.{ECFactory}
-import org.bitcoins.script.constant.{OP_0, BytesToPushOntoStackImpl, ScriptConstantImpl}
+
+import org.bitcoins.crypto.ECFactory
+import org.bitcoins.script.constant.{BytesToPushOntoStackImpl, OP_0, ScriptConstant}
 import org.bitcoins.util.TestUtil
 import org.scalatest.{FlatSpec, MustMatchers}
 
@@ -31,9 +32,9 @@ class P2SHScriptSignatureTest extends FlatSpec with MustMatchers {
     }
 
     p2shScriptSig.scriptSignatureNoRedeemScript.asm must be (Seq(
-      OP_0, BytesToPushOntoStackImpl(71), ScriptConstantImpl("304402207d764cb90c9fd84b74d33a47cf3a0ffead9ded98333776becd6acd32c4426dac02203905a0d064e7f53d07793e86136571b6e4f700c1cfb888174e84d78638335b8101"),
+      OP_0, BytesToPushOntoStackImpl(71), ScriptConstant("304402207d764cb90c9fd84b74d33a47cf3a0ffead9ded98333776becd6acd32c4426dac02203905a0d064e7f53d07793e86136571b6e4f700c1cfb888174e84d78638335b8101"),
       BytesToPushOntoStackImpl(72),
-      ScriptConstantImpl("3045022100906aaca39f022acd8b7a38fd2f92aca9e9f35cfeaee69a6f13e1d083ae18222602204c9ed96fc6c4de56fd85c679fc59c16ee1ccc80c42563b86174e1a506fc007c801")
+      ScriptConstant("3045022100906aaca39f022acd8b7a38fd2f92aca9e9f35cfeaee69a6f13e1d083ae18222602204c9ed96fc6c4de56fd85c679fc59c16ee1ccc80c42563b86174e1a506fc007c801")
     ))
   }
 }

src/test/scala/org/bitcoins/script/locktime/LockTimeInterpreterTest.scala
@@ -1,9 +1,10 @@
 package org.bitcoins.script.locktime
 
+
 import org.bitcoins.protocol.transaction.{TransactionInput, Transaction, UpdateTransactionInputs}
 import org.bitcoins.script.error.{ScriptErrorNegativeLockTime, ScriptErrorUnsatisfiedLocktime, ScriptErrorInvalidStackOperation}
 import org.bitcoins.script.{ExecutionInProgressScriptProgram, ExecutedScriptProgram, PreExecutionScriptProgram, ScriptProgram}
-import org.bitcoins.script.constant.{ScriptNumber, ScriptNumberImpl, OP_0}
+import org.bitcoins.script.constant.{ScriptNumber, OP_0}
 import org.bitcoins.util.{ScriptProgramTestUtil, TestUtil}
 import org.scalatest.{MustMatchers, FlatSpec}
 

src/test/scala/org/bitcoins/util/BitcoinJConversions.scala
@@ -10,7 +10,7 @@ import org.bitcoins.crypto.ECPublicKey
 import org.bitcoins.protocol.script.{UpdateScriptPubKeyAsm, ScriptPubKey}
 import org.bitcoins.protocol.transaction.{TransactionOutput, Transaction}
 import org.bitcoins.script.ScriptOperationFactory
-import org.bitcoins.script.constant.{ScriptConstantImpl, ScriptToken}
+import org.bitcoins.script.constant.{ ScriptToken}
 import org.slf4j.LoggerFactory
 import scala.collection.JavaConversions._
 /**