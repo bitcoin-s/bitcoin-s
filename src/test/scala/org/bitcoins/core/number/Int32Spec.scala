cbbf70b83775420f426b1f5b1df350b8a55657dd
Chris Stewart <stewart.chris1234@gmail.com>
2016-06-21 09:53:03 -0500

    Refactoring encodeHex for numbers to encode numbers to ALWAYS have the maximum bytes in the number type. For instance a long will ALWAYS have 16 characters when encoded to hex

--- src/test/scala/org/bitcoins/core/number/Int32Spec.scala
+++ src/test/scala/org/bitcoins/core/number/Int32Spec.scala
@@ -1,10 +1,53 @@
 package org.bitcoins.core.number
 
-import org.scalacheck.Properties
+import org.scalacheck.{Prop, Properties}
+
+import scala.util.Try
 
 /**
   * Created by chris on 6/21/16.
   */
 class Int32Spec extends Properties("Int32Spec") {
 
+
+  property("Serialization symmetry") =
+    Prop.forAll(NumberGenerator.int32s) { int32: Int32 =>
+      Int32(int32.hex) == int32
+
+    }
+  property("Additive identity") =
+    Prop.forAll(NumberGenerator.int32s)  { int32 : Int32 =>
+      int32 + Int32.zero == int32
+    }
+
+  property("Add two arbitrary int32s") =
+    Prop.forAll(NumberGenerator.int32s, NumberGenerator.int32s) { (num1 : Int32, num2: Int32) =>
+      val result = num1.underlying + num2.underlying
+      if (result <= Int32.max.underlying) num1 + num2 == Int32(result)
+      else if (result <= Int64.max.underlying) num1 + num2 == Int64(result)
+      else Try(num1 + num2).isFailure
+    }
+
+  property("Add a int32 and a int64") =
+    Prop.forAll(NumberGenerator.int32s, NumberGenerator.int64s) { (int32 : Int32, int64 : Int64) =>
+      val result = int32.underlying + int64.underlying
+      if (result >= Int32.min.underlying && result <= Int32.max.underlying) int32 + int64 == Int32(result.toInt)
+      else if (result >= Int64.min.underlying  && result <= Int64.max.underlying) int32 + int64 == Int64(result)
+      else Try(int32 + int64).isFailure
+    }
+
+
+  property("Subtractive identity") =
+    Prop.forAll(NumberGenerator.int32s) { int32 : Int32 =>
+      int32 - Int32.zero == int32
+    }
+
+  property("Subtract two arbitrary int32s") =
+    Prop.forAll(NumberGenerator.int32s, NumberGenerator.int32s) { (num1 : Int32, num2 : Int32) =>
+      val result = num1.underlying - num2.underlying
+      if (result >= Int32.min.underlying) num1 - num2 == Int32(result)
+      else if (result >= Int64.min.underlying) num1 - num2 == Int64(result)
+      else Try(num1 - num2).isFailure
+    }
+  
 }
