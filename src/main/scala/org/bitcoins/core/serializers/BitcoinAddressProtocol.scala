32afdcf7f220daba822a31f9f9bc08b77fa135d8
Chris Stewart <stewart.chris1234@gmail.com>
2016-08-31 10:31:09 -0500

    Refactoring Address type to have a networkParameters, hash and address value, removing AssetAddress - we can support these later if needed

--- src/main/scala/org/bitcoins/core/serializers/BitcoinAddressProtocol.scala
+++ src/main/scala/org/bitcoins/core/serializers/BitcoinAddressProtocol.scala
@@ -1,6 +1,6 @@
 package org.bitcoins.core.serializers
 
-import org.bitcoins.core.protocol.{AssetAddress, Address, BitcoinAddress}
+import org.bitcoins.core.protocol.{Address, BitcoinAddress}
 import spray.json._
 /**
  * Created by chris on 12/19/15.
@@ -11,7 +11,6 @@ object AddressProtocol extends DefaultJsonProtocol {
     override def read(jsValue: JsValue) = {
       jsValue match {
         case JsString(string) => string match {
-          case s if s(0) == 'a' => AssetAddress(s)
           case s if Seq('1','3','2','m','n').contains(s(0)) => BitcoinAddress(s)
           case _ => throw new RuntimeException("Addresses should always start with 'a' '1' or '3'")
         }
