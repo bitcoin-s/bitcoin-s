df4d63bd0785802fe99bf3874e3896082cd1955d
Chris Stewart <stewart.chris1234@gmail.com>
2016-11-10 12:08:34 -0600

    Adding new ScriptFlags for segwit

--- src/test/scala/org/bitcoins/core/script/interpreter/testprotocol/CoreTestCase.scala
+++ src/test/scala/org/bitcoins/core/script/interpreter/testprotocol/CoreTestCase.scala
@@ -1,6 +1,8 @@
 package org.bitcoins.core.script.interpreter.testprotocol
 
+import org.bitcoins.core.currency.CurrencyUnit
 import org.bitcoins.core.protocol.script.{ScriptPubKey, ScriptSignature}
+import org.bitcoins.core.script.constant.ScriptToken
 import org.bitcoins.core.script.result.ScriptResult
 
 /**
@@ -16,8 +18,9 @@ trait CoreTestCase {
   def expectedResult : ScriptResult
   def comments : String
   def raw : String
+  def witness: Option[(Seq[ScriptToken], CurrencyUnit)]
 }
 
 case class CoreTestCaseImpl(scriptSig : ScriptSignature,
   scriptPubKey: ScriptPubKey, flags : String, expectedResult : ScriptResult,
-  comments : String, raw : String) extends CoreTestCase
+  comments : String, raw : String, witness: Option[(Seq[ScriptToken], CurrencyUnit)]) extends CoreTestCase
