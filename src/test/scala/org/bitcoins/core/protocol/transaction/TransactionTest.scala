68c1616faf48ccd4963099abba1cfa0b1b7c5fc4
Chris Stewart <stewart.chris1234@gmail.com>
2018-03-13 10:57:42 -0500

    Uncommenting tx_invalid.json test cases

--- src/test/scala/org/bitcoins/core/protocol/transaction/TransactionTest.scala
+++ src/test/scala/org/bitcoins/core/protocol/transaction/TransactionTest.scala
@@ -171,11 +171,11 @@ class TransactionTest extends FlatSpec with MustMatchers {
 
     val source = Source.fromURL(getClass.getResource("/tx_invalid.json"))
     //use this to represent a single test case from script_valid.json
-    val lines =
+/*    val lines =
         """
           |[[[["0000000000000000000000000000000000000000000000000000000000000000",-1,"1"]], "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0151ffffffff010000000000000000015100000000", "P2SH"]]
-        """.stripMargin
-    //val lines = try source.getLines.filterNot(_.isEmpty).map(_.trim) mkString "\n" finally source.close()
+        """.stripMargin*/
+    val lines = try source.getLines.filterNot(_.isEmpty).map(_.trim) mkString "\n" finally source.close()
     val json = lines.parseJson
     val testCasesOpt : Seq[Option[CoreTransactionTestCase]] = json.convertTo[Seq[Option[CoreTransactionTestCase]]]
     val testCases : Seq[CoreTransactionTestCase] = testCasesOpt.flatten
