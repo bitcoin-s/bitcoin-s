11f20083475c7459057770662b83b782b084ad28
Chris Stewart <stewart.chris1234@gmail.com>
2018-04-18 07:44:58 -0500

    Adding dummy sign method inside of Sign

--- src/main/scala/org/bitcoins/core/crypto/Sign.scala
+++ src/main/scala/org/bitcoins/core/crypto/Sign.scala
@@ -4,7 +4,7 @@ import scala.concurrent.{ Await, ExecutionContext, Future }
 import scala.concurrent.duration.DurationInt
 
 /**
- * This is meant to be an abstraction for a [[org.bitcoins.core.crypto.ECPrivateKey]],f sometimes we will not
+ * This is meant to be an abstraction for a [[org.bitcoins.core.crypto.ECPrivateKey]], sometimes we will not
  * have direct access to a private key in memory -- for instance if that key is on a hardware device -- so we need to create an
  * abstraction of the signing process. Fundamentally a private key takes in a Seq[Byte] and returns a [[ECDigitalSignature]]
  * That is what this abstraction is meant to represent. If you have a [[ECPrivateKey]] in your application, you can get it's
@@ -34,5 +34,17 @@ object Sign {
 
   def apply(signFunction: Seq[Byte] => Future[ECDigitalSignature], pubKey: ECPublicKey): Sign = {
     SignImpl(signFunction, pubKey)
+  }
+
+  /**
+   * This dummySign function is useful for the case where we do not have the
+   * signFunction available on the same jvm as the place where we are creating the
+   * sign. I can't think of a good way to serialize the signFunction, so it needs to be
+   * optional for now. Maybe we rethink the idea of the signFunction in the future.
+   * the public key is still useful here though because it can be used to match against
+   * a specific private key on another server
+   */
+  def dummySign(publicKey: ECPublicKey): Sign = {
+    SignImpl({ _: Seq[Byte] => Future.successful(EmptyDigitalSignature) }, publicKey)
   }
 }
