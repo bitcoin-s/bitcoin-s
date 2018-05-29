7f0d82c307926a705e25f560afbe0aa48e2e5fd5
Nadav Kohen <nadavk25@gmail.com>
2018-05-29 13:49:26 -0500

    Cleanup of RpcClient, now every call is a single line of code!

--- rpc/src/main/scala/org/bitcoins/rpc/serializers/JsonSerializers.scala
+++ rpc/src/main/scala/org/bitcoins/rpc/serializers/JsonSerializers.scala
@@ -2,11 +2,12 @@ package org.bitcoins.rpc.serializers
 
 import org.bitcoins.core.crypto.DoubleSha256Digest
 import org.bitcoins.core.currency.Bitcoins
-import org.bitcoins.core.number.{ Int32, UInt32 }
+import org.bitcoins.core.number.{Int32, UInt32}
+import org.bitcoins.core.protocol.Address
 import org.bitcoins.core.protocol.blockchain.BlockHeader
 import org.bitcoins.rpc.jsonmodels._
 import org.bitcoins.rpc.serializers.JsonReaders._
-import play.api.libs.json.{ Json, Reads }
+import play.api.libs.json.{Json, Reads}
 
 object JsonSerializers {
   // Internal Types
@@ -15,6 +16,7 @@ object JsonSerializers {
   implicit val blockHeaderReads: Reads[BlockHeader] = BlockHeaderReads
   implicit val int32Reads: Reads[Int32] = Int32Reads
   implicit val uInt32Reads: Reads[UInt32] = UInt32Reads
+  implicit val addressReads: Reads[Address] = AddressReads
 
   // Network Models
   implicit val networkReads: Reads[Network] = Json.reads[Network]
