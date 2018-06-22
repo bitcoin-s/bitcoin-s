04bace39d0a1ec724f066836d7f81709690fb026
nkohen <nadavk25@gmail.com>
2018-06-22 12:59:13 -0500

    Added implementation and test for getBlockTemplate, replaced filter(...).head with find(...).get (performs better), and ran scalafmt

--- rpc/src/main/scala/org/bitcoins/rpc/jsonmodels/OtherResult.scala
+++ rpc/src/main/scala/org/bitcoins/rpc/jsonmodels/OtherResult.scala
@@ -1,11 +1,54 @@
 package org.bitcoins.rpc.jsonmodels
 
-import org.bitcoins.core.crypto.{ECPublicKey, Sha256Hash160Digest}
+import org.bitcoins.core.crypto.{
+  DoubleSha256Digest,
+  ECPublicKey,
+  Sha256Hash160Digest
+}
+import org.bitcoins.core.currency.Satoshis
+import org.bitcoins.core.number.UInt32
 import org.bitcoins.core.protocol.BitcoinAddress
 import org.bitcoins.core.protocol.script.ScriptPubKey
+import org.bitcoins.core.protocol.transaction.Transaction
 import org.bitcoins.core.wallet.fee.BitcoinFeeUnit
+import play.api.libs.json.JsObject
 
 sealed abstract class OtherResult
+
+case class GetBlockTemplateResult(
+    capabilities: Vector[String],
+    version: Int,
+    rules: Vector[String],
+    vbavailable: Map[String, Int], // Is this Int or BigDecimal?
+    vbrequired: Int,
+    previousblockhash: DoubleSha256Digest,
+    transactions: Vector[BlockTransaction],
+    coinbaseaux: Map[String, String],
+    coinbasevalue: Satoshis,
+    longpollid: String, // What is this?
+    coinbasetxn: Option[JsObject],
+    target: String, // What should this be?
+    mintime: UInt32,
+    mutable: Vector[String],
+    noncerange: String,
+    sigoplimit: Int,
+    sizelimit: Int,
+    weightlimit: Int,
+    curtime: UInt32,
+    bits: String, // What should this be?
+    height: Int
+) extends OtherResult
+
+case class BlockTransaction(
+    data: Transaction,
+    txid: DoubleSha256Digest,
+    hash: DoubleSha256Digest,
+    depends: Vector[Int],
+    fee: Satoshis,
+    sigops: Int,
+    weight: Int,
+    required: Option[Boolean]
+) extends OtherResult
 
 case class GetMiningInfoResult(
     blocks: Int,
