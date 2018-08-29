48135ed2e9eb748c0359dd8a28cdd3631040a5dc
nkohen <nadavk25@gmail.com>
2018-08-29 16:42:53 -0500

    Fixed compile errors

--- ecliar-rpc/src/main/scala/org/bitcoins/eclair/rpc/client/RpcClient.scala
+++ ecliar-rpc/src/main/scala/org/bitcoins/eclair/rpc/client/RpcClient.scala
@@ -12,16 +12,11 @@ import play.api.libs.json._
 
 import scala.concurrent.{ ExecutionContext, Future }
 
-class RpcClient(implicit m: ActorMaterializer) {
+class RpcClient()(implicit m: ActorMaterializer) {
   private val resultKey = "result"
   private val errorKey = "error"
   implicit val ec: ExecutionContext = m.executionContext
   private val logger = BitcoinSLogger.logger
-
-  // Errors for Unit return types are caught in RpcClient::checkUnit
-  implicit object UnitReads extends Reads[Unit] {
-    override def reads(json: JsValue): JsResult[Unit] = JsSuccess(Unit)
-  }
 
   case class GetInfoResult(nodeId: String, alias: String, port: Int, chainHash: String, blockHeight: Long)
   implicit val getInfoResultReads: Reads[GetInfoResult] = Json.reads[GetInfoResult]
@@ -50,7 +45,9 @@ class RpcClient(implicit m: ActorMaterializer) {
   sealed trait SendResult
   case class PaymentSucceeded(amountMsat: Long, paymentHash: String, paymentPreimage: String, route: JsArray) extends SendResult
   implicit val paymentSucceededReads: Reads[PaymentSucceeded] = Json.reads[PaymentSucceeded]
-  case class PaymentFailure() extends SendResult
+  implicit val sendResultReads: Reads[SendResult] = Reads[SendResult](_.validate[PaymentSucceeded])
+  /*
+  case class PaymentFailure(???) extends SendResult
   implicit val paymentFailureReads: Reads[PaymentFailure] = Json.reads[PaymentFailure]
   implicit val sendResultReads: Reads[SendResult] = Reads[SendResult] { json =>
     json.validate[PaymentSucceeded] match {
@@ -60,7 +57,7 @@ class RpcClient(implicit m: ActorMaterializer) {
         case err2: JsError => JsError.merge(err1, err2)
       }
     }
-  }
+  }*/
 
   def allChannels: Future[Vector[ChannelDesc]] = {
     eclairCall[Vector[ChannelDesc]]("allchannels")
@@ -142,83 +139,103 @@ class RpcClient(implicit m: ActorMaterializer) {
     eclairCall[Vector[String]]("help")
   }
 
-  private def open(nodeId: String,
-           fundingSatoshis: Long,
-           pushMsat: Long,
-           feerateSatPerByte: Option[Long],
-           channelFlags: Option[Byte]): Future[String] = {
+  private def open(
+    nodeId: String,
+    fundingSatoshis: Long,
+    pushMsat: Option[Long],
+    feerateSatPerByte: Option[Long],
+    channelFlags: Option[Byte]): Future[String] = {
+    val num: Long = pushMsat.getOrElse(0)
+    val pushMsatJson = JsNumber(num)
+
     val params =
       if (feerateSatPerByte.isEmpty) {
         List(
           JsString(nodeId),
           JsNumber(fundingSatoshis),
-          JsNumber(pushMsat)
-        )
+          pushMsatJson)
       } else if (channelFlags.isEmpty) {
         List(
           JsString(nodeId),
           JsNumber(fundingSatoshis),
-          JsNumber(pushMsat),
-          JsNumber(feerateSatPerByte.get)
-        )
+          pushMsatJson,
+          JsNumber(feerateSatPerByte.get))
       } else {
         List(
           JsString(nodeId),
           JsNumber(fundingSatoshis),
-          JsNumber(pushMsat),
+          pushMsatJson,
           JsNumber(feerateSatPerByte.get),
-          JsString(channelFlags.toString)
-        )
+          JsString(channelFlags.toString))
       }
 
     eclairCall[String]("open", params)
   }
 
-  def open(nodeId: String, fundingSatoshis: Long, pushMsat: Long = 0): Future[String] =
-    open(nodeId, fundingSatoshis, pushMsat, None, None)
+  def open(nodeId: String, fundingSatoshis: Long): Future[String] =
+    open(nodeId, fundingSatoshis, None, None, None)
 
-  def open(nodeId: String, fundingSatoshis: Long, pushMsat: Long = 0, feerateSatPerByte: Long): Future[String] =
-    open(nodeId, fundingSatoshis, pushMsat, Some(feerateSatPerByte), None)
+  def open(nodeId: String, fundingSatoshis: Long, pushMsat: Long): Future[String] =
+    open(nodeId, fundingSatoshis, Some(pushMsat), None, None)
 
-  def open(nodeId: String,
-           fundingSatoshis: Long,
-           pushMsat: Long = 0,
-           feerateSatPerByte: Long,
-           channelFlags: Byte): Future[String] =
-    open(nodeId, fundingSatoshis, pushMsat, Some(feerateSatPerByte), Some(channelFlags))
+  def open(nodeId: String, fundingSatoshis: Long, pushMsat: Long, feerateSatPerByte: Long): Future[String] =
+    open(nodeId, fundingSatoshis, Some(pushMsat), Some(feerateSatPerByte), None)
+
+  def open(
+    nodeId: String,
+    fundingSatoshis: Long,
+    pushMsat: Long = 0,
+    feerateSatPerByte: Long,
+    channelFlags: Byte): Future[String] =
+    open(nodeId, fundingSatoshis, Some(pushMsat), Some(feerateSatPerByte), Some(channelFlags))
+
+  def open(
+    nodeId: String,
+    fundingSatoshis: Long,
+    feerateSatPerByte: Long,
+    channelFlags: Byte): Future[String] =
+    open(nodeId, fundingSatoshis, None, Some(feerateSatPerByte), Some(channelFlags))
 
   def peers: Future[Vector[PeerInfo]] = {
     eclairCall[Vector[PeerInfo]]("peers")
   }
 
-  private def receive(description: String, amountMsat: Option[Long], expirySeconds: Option[Long]): Future[String] = {
+  private def receive(description: Option[String], amountMsat: Option[Long], expirySeconds: Option[Long]): Future[String] = {
     val params =
       if (amountMsat.isEmpty) {
-        List(JsString(description))
+        List(JsString(description.getOrElse("")))
       } else if (expirySeconds.isEmpty) {
-        List(JsNumber(amountMsat.get), JsString(description))
+        List(JsNumber(amountMsat.get), JsString(description.getOrElse("")))
       } else {
-        List(JsNumber(amountMsat.get), JsString(description), JsNumber(expirySeconds.get))
+        List(JsNumber(amountMsat.get), JsString(description.getOrElse("")), JsNumber(expirySeconds.get))
       }
 
     eclairCall[String]("receive", params)
   }
 
-  def receive(description: String = ""): Future[String] =
-    receive(description, None, None)
+  def receive(): Future[String] =
+    receive(None, None, None)
 
-  def receive(description: String ="", amountMsat: Long): Future[String] =
-    receive(description, Some(amountMsat), None)
+  def receive(description: String): Future[String] =
+    receive(Some(description), None, None)
 
-  def receive(description: String = "", amountMsat: Long, expirySeconds: Long): Future[String] =
-    receive(description, Some(amountMsat), Some(expirySeconds))
+  def receive(description: String, amountMsat: Long): Future[String] =
+    receive(Some(description), Some(amountMsat), None)
+
+  def receive(description: String, amountMsat: Long, expirySeconds: Long): Future[String] =
+    receive(Some(description), Some(amountMsat), Some(expirySeconds))
+
+  def receive(amountMsat: Long): Future[String] =
+    receive(None, Some(amountMsat), None)
+
+  def receive(amountMsat: Long, expirySeconds: Long): Future[String] =
+    receive(None, Some(amountMsat), Some(expirySeconds))
 
   def send(amountMsat: Long, paymentHash: String, nodeId: String): Future[SendResult] = {
     eclairCall[SendResult]("send", List(
       JsNumber(amountMsat),
       JsString(paymentHash),
-      JsString(nodeId))
-    )
+      JsString(nodeId)))
   }
 
   private def send(invoice: String, amountMsat: Option[Long]): Future[SendResult] = {
@@ -240,8 +257,7 @@ class RpcClient(implicit m: ActorMaterializer) {
     eclairCall[String]("updaterelayfee", List(
       JsString(channelId),
       JsNumber(feeBaseMsat),
-      JsNumber(feeProportionalMillionths))
-    )
+      JsNumber(feeProportionalMillionths)))
   }
 
   // TODO: channelstats, audit, networkfees?
@@ -264,8 +280,6 @@ class RpcClient(implicit m: ActorMaterializer) {
   implicit val rpcErrorReads: Reads[RpcError] = Json.reads[RpcError]
 
   private def parseResult[T](result: JsResult[T], json: JsValue): T = {
-    checkUnitError[T](result, json)
-
     result match {
       case res: JsSuccess[T] => res.value
       case res: JsError =>
@@ -279,19 +293,6 @@ class RpcClient(implicit m: ActorMaterializer) {
             throw new IllegalArgumentException(
               s"Could not parse JsResult: ${(json \ resultKey).get}")
         }
-    }
-  }
-
-  // Catches errors thrown by calls with Unit as the expected return type (which isn't handled by UnitReads)
-  private def checkUnitError[T](result: JsResult[T], json: JsValue): Unit = {
-    if (result == JsSuccess(())) {
-      (json \ errorKey).validate[RpcError] match {
-        case err: JsSuccess[RpcError] =>
-          logger.error(s"Error ${err.value.code}: ${err.value.message}")
-          throw new RuntimeException(
-            s"Error ${err.value.code}: ${err.value.message}")
-        case _: JsError =>
-      }
     }
   }
 
