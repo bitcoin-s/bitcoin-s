77fc23d1fe006c34de586f9e42eb0f774563d46f
nkohen <nadavk25@gmail.com>
2018-08-29 11:54:56 -0500

    Added getinfo functionality

--- ecliar-rpc/src/main/scala/org/bitcoins/eclair/rpc/client/RpcClient.scala
+++ ecliar-rpc/src/main/scala/org/bitcoins/eclair/rpc/client/RpcClient.scala
@@ -13,14 +13,49 @@ import play.api.libs.json._
 import scala.concurrent.{ ExecutionContext, Future }
 
 class RpcClient(implicit m: ActorMaterializer) {
+  private val resultKey = "result"
+  private val errorKey = "error"
   implicit val ec: ExecutionContext = m.executionContext
   private val logger = BitcoinSLogger.logger
 
-  def getInfo: Future[JsValue] = {
-    val request = buildRequest("getinfo", JsArray.empty)
-    val resonseF = sendRequest(request)
+  case class GetInfoResult(nodeId: String, alias: String, port: Int, chainHash: String, blockHeight: Long)
+  implicit val getInfoResultReads: Reads[GetInfoResult] = Json.reads[GetInfoResult]
 
-    resonseF.flatMap(getPayload)
+  def getInfo: Future[GetInfoResult] = {
+    eclairCall[GetInfoResult]("getinfo")
+  }
+
+  private def eclairCall[T](command: String,
+                            parameters: List[JsValue] = List.empty)
+                           (implicit reader: Reads[T]): Future[T] = {
+    val request = buildRequest(command, JsArray(parameters))
+    val responseF = sendRequest(request)
+
+    val payloadF: Future[JsValue] = responseF.flatMap(getPayload)
+
+    payloadF.map { payload =>
+      parseResult((payload \ resultKey).validate[T], payload)
+    }
+  }
+
+  case class RpcError(code: Int, message: String)
+  implicit val rpcErrorReads: Reads[RpcError] = Json.reads[RpcError]
+
+  private def parseResult[T](result: JsResult[T], json: JsValue): T = {
+    result match {
+      case res: JsSuccess[T] => res.value
+      case res: JsError =>
+        (json \ errorKey).validate[RpcError] match {
+          case err: JsSuccess[RpcError] =>
+            logger.error(s"Error ${err.value.code}: ${err.value.message}")
+            throw new RuntimeException(
+              s"Error ${err.value.code}: ${err.value.message}")
+          case _: JsError =>
+            logger.error(JsError.toJson(res).toString())
+            throw new IllegalArgumentException(
+              s"Could not parse JsResult: ${(json \ resultKey).get}")
+        }
+    }
   }
 
   private def getPayload(response: HttpResponse): Future[JsValue] = {
@@ -43,7 +78,7 @@ class RpcClient(implicit m: ActorMaterializer) {
       "params" -> params,
       "id" -> JsString(uuid)))
 
-    val uri = "http://localhost:9736"
+    val uri = "http://localhost:8081"
     val username = "suredbits"
     val password = "abc123"
     HttpRequest(
