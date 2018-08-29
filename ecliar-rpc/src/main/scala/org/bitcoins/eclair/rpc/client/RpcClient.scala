4ccb29a402c0bf5488e4378e5392a8acd613a9c4
nkohen <nadavk25@gmail.com>
2018-08-29 12:25:49 -0500

    Added connect functionality

--- ecliar-rpc/src/main/scala/org/bitcoins/eclair/rpc/client/RpcClient.scala
+++ ecliar-rpc/src/main/scala/org/bitcoins/eclair/rpc/client/RpcClient.scala
@@ -18,16 +18,33 @@ class RpcClient(implicit m: ActorMaterializer) {
   implicit val ec: ExecutionContext = m.executionContext
   private val logger = BitcoinSLogger.logger
 
+  // Errors for Unit return types are caught in RpcClient::checkUnit
+  implicit object UnitReads extends Reads[Unit] {
+    override def reads(json: JsValue): JsResult[Unit] = JsSuccess(Unit)
+  }
+
   case class GetInfoResult(nodeId: String, alias: String, port: Int, chainHash: String, blockHeight: Long)
   implicit val getInfoResultReads: Reads[GetInfoResult] = Json.reads[GetInfoResult]
+
+  def connect(nodeId: String, host: String, port: Int): Future[Unit] = {
+    eclairCall[Unit]("connect", List(JsString(nodeId), JsString(host), JsNumber(port)))
+  }
+
+  def connect(uri: String): Future[Unit] = {
+    eclairCall[Unit]("connect", List(JsString(uri)))
+  }
 
   def getInfo: Future[GetInfoResult] = {
     eclairCall[GetInfoResult]("getinfo")
   }
 
-  private def eclairCall[T](command: String,
-                            parameters: List[JsValue] = List.empty)
-                           (implicit reader: Reads[T]): Future[T] = {
+  def help: Future[List[String]] = {
+    eclairCall[List[String]]("help")
+  }
+
+  private def eclairCall[T](
+    command: String,
+    parameters: List[JsValue] = List.empty)(implicit reader: Reads[T]): Future[T] = {
     val request = buildRequest(command, JsArray(parameters))
     val responseF = sendRequest(request)
 
@@ -42,6 +59,8 @@ class RpcClient(implicit m: ActorMaterializer) {
   implicit val rpcErrorReads: Reads[RpcError] = Json.reads[RpcError]
 
   private def parseResult[T](result: JsResult[T], json: JsValue): T = {
+    checkUnitError[T](result, json)
+
     result match {
       case res: JsSuccess[T] => res.value
       case res: JsError =>
@@ -55,6 +74,19 @@ class RpcClient(implicit m: ActorMaterializer) {
             throw new IllegalArgumentException(
               s"Could not parse JsResult: ${(json \ resultKey).get}")
         }
+    }
+  }
+
+  // Catches errors thrown by calls with Unit as the expected return type (which isn't handled by UnitReads)
+  private def checkUnitError[T](result: JsResult[T], json: JsValue): Unit = {
+    if (result == JsSuccess(())) {
+      (json \ errorKey).validate[RpcError] match {
+        case err: JsSuccess[RpcError] =>
+          logger.error(s"Error ${err.value.code}: ${err.value.message}")
+          throw new RuntimeException(
+            s"Error ${err.value.code}: ${err.value.message}")
+        case _: JsError =>
+      }
     }
   }
 
