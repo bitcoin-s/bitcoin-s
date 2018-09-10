cc198a0a9ee860dc90a731c7fe5d5ec77c1b6366
Tanner R <TannerRogers@outlook.com>
2018-09-10 06:51:25 -0500

    WIP: Implement LnHumanReadablePart (#190)
    * Initial Implementation of LnHumanReadablePart
    
    * Add unit tests and improve deserialization from string
    
    * Refactor LnParams and LnHrp. Add requirements for instantiating LnHrp.
    
    * Clean up and re-organize things
    
    Re-working LnHumanReadablePart.fromString
    
    Fix unnecessary pattern match
    
    Removing test case

---
+++ core/src/main/scala/org/bitcoins/core/protocol/ln/LnHumanReadablePart.scala
@@ -0,0 +1,121 @@
+package org.bitcoins.core.protocol.ln
+
+import org.bitcoins.core.config.NetworkParameters
+import org.bitcoins.core.protocol.ln.LnParams._
+
+import scala.util.matching.Regex
+import scala.util.{ Failure, Success, Try }
+
+sealed abstract class LnHumanReadablePart {
+  require(
+    amount.isEmpty || amount.get.toBigInt > 0,
+    s"Invoice amount must be greater then 0, got $amount")
+  require(
+    amount.isEmpty || amount.get <= LnPolicy.maxAmountMSat,
+    s"Invoice amount must be less than ${LnPolicy.maxAmountMSat}, got $amount")
+
+  def network: LnParams
+
+  def amount: Option[LnCurrencyUnit]
+
+  override def toString: String = {
+    val b = StringBuilder.newBuilder
+    val prefix = network.invoicePrefix.toArray.map(_.toChar).mkString
+    b.append(prefix)
+
+    val amt = amount.map(_.toEncodedString).getOrElse("")
+    b.append(amt)
+
+    b.toString()
+  }
+}
+
+/** Prefix for generating a LN invoice on the Bitcoin MainNet */
+case class lnbc(override val amount: Option[LnCurrencyUnit]) extends LnHumanReadablePart {
+  override def network: LnParams = LnBitcoinMainNet
+}
+
+/** Prefix for generating a LN invoice on the Bitcoin TestNet3 */
+case class lntb(override val amount: Option[LnCurrencyUnit]) extends LnHumanReadablePart {
+  override def network: LnParams = LnBitcoinTestNet
+}
+
+/** Prefix for genearting a LN invoice on the Bitcoin RegTest */
+case class lnbcrt(override val amount: Option[LnCurrencyUnit]) extends LnHumanReadablePart {
+  def network: LnParams = LnBitcoinRegTest
+}
+
+object LnHumanReadablePart {
+
+  def apply(network: NetworkParameters): Option[LnHumanReadablePart] = {
+    val lnNetworkOpt = LnParams.fromNetworkParameters(network)
+    lnNetworkOpt.map(LnHumanReadablePart.fromLnParams(_))
+  }
+
+  def apply(network: NetworkParameters, amount: LnCurrencyUnit): Option[LnHumanReadablePart] = {
+    val lnNetworkOpt = LnParams.fromNetworkParameters(network)
+    lnNetworkOpt.map(ln => LnHumanReadablePart(ln, Some(amount)))
+  }
+
+  def apply(network: LnParams): LnHumanReadablePart = {
+    fromLnParams(network)
+  }
+
+  /**
+   * Will return a [[org.bitcoins.core.protocol.ln.LnHumanReadablePart]]
+   * without a [[LnCurrencyUnit]] encoded in the invoice
+   * @param network
+   * @return
+   */
+  def fromLnParams(network: LnParams): LnHumanReadablePart = {
+    LnHumanReadablePart(network, None)
+  }
+
+  /**
+   * Will return a [[org.bitcoins.core.protocol.ln.LnHumanReadablePart]]
+   * with the provide [[LnCurrencyUnit]] encoded in the invoice
+   * @param network
+   * @param amount
+   * @return
+   */
+  def apply(network: LnParams, amount: Option[LnCurrencyUnit]): LnHumanReadablePart = {
+    network match {
+      case LnParams.LnBitcoinMainNet => lnbc(amount)
+      case LnParams.LnBitcoinTestNet => lntb(amount)
+      case LnParams.LnBitcoinRegTest => lnbcrt(amount)
+    }
+  }
+
+  /**
+   * First two chars MUST be 'ln'
+   * Next chars must be the BIP173 currency prefixes
+   * [[https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md#human-readable-part]]
+   * [[https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Specification]]
+   * @param input
+   * @return
+   */
+  def fromString(input: String): Try[LnHumanReadablePart] = {
+    //Select all of the letters, until we hit a number, as the network
+    val networkPattern: Regex = "^[a-z]*".r
+    val networkStringOpt = networkPattern.findFirstIn(input)
+    val lnParamsOpt = networkStringOpt.flatMap(LnParams.fromPrefixString(_))
+
+    if (lnParamsOpt.isEmpty) {
+      Failure(new IllegalArgumentException(s"Could not parse a valid network prefix, got ${input}"))
+    } else {
+
+      val lnParams = lnParamsOpt.get
+      val prefixSize = lnParams.invoicePrefix.size.toInt
+      val amountString = input.slice(prefixSize, input.size)
+      val amount = LnCurrencyUnits.fromEncodedString(amountString).toOption
+
+      //If we are able to parse something as an amount, but are unable to convert it to a LnCurrencyUnit, we should fail.
+      if (amount.isEmpty && !amountString.isEmpty) {
+        Failure(new IllegalArgumentException(s"Parsed an amount, " +
+          s"but could not convert to a valid currency, got: $amountString"))
+      } else {
+        Success(LnHumanReadablePart(lnParams, amount))
+      }
+    }
+  }
+}
--- core/src/main/scala/org/bitcoins/core/protocol/ln/LnPolicy.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/ln/LnPolicy.scala
@@ -4,6 +4,14 @@ import org.bitcoins.core.number.Int64
 
 sealed abstract class LnPolicy {
 
+  /**
+   * The "amount_msat" field has been artificially limited to a UInt32. This means that the current maximum transaction that can be completed
+   * over the lightning network is 4294967295 MilliSatoshi.
+   * This is a self imposed limit, and is subject to change.
+   * Please see BOLT #2: https://github.com/lightningnetwork/lightning-rfc/blob/master/02-peer-protocol.md#adding-an-htlc-update_add_htlc
+   */
+  lazy val maxAmountMSat: MilliBitcoins = MilliBitcoins(4294967295L)
+
   val milliMultiplier: BigDecimal = BigDecimal(0.001)
   val microMultiplier: BigDecimal = BigDecimal(0.000001)
   val nanoMultiplier: BigDecimal = BigDecimal(0.000000001)
