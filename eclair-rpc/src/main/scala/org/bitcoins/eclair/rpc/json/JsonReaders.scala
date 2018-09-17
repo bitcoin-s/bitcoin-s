bac3f509392821e1f9c462da440279895fad801d
Chris Stewart <stewart.chris1234@gmail.com>
2018-09-17 11:42:25 -0400

    Add NodeId, NodeUri, ChannelId (#196)
    refactor json serializing methods to SerializerUtil, add more types

--- eclair-rpc/src/main/scala/org/bitcoins/eclair/rpc/json/JsonReaders.scala
+++ eclair-rpc/src/main/scala/org/bitcoins/eclair/rpc/json/JsonReaders.scala
@@ -1,8 +1,39 @@
 package org.bitcoins.eclair.rpc.json
 
-import play.api.libs.json.{ Json, Reads }
+import org.bitcoins.core.crypto.{ DoubleSha256Digest, ECDigitalSignature }
+import org.bitcoins.core.protocol.ln.PicoBitcoins
+import org.bitcoins.core.protocol.ln.channel.FundedChannelId
+import org.bitcoins.eclair.rpc.network.{ NodeId, PeerState }
+import org.bitcoins.rpc.serializers.SerializerUtil
+import org.slf4j.LoggerFactory
+import play.api.libs.json._
+
+import scala.util.Try
 
 object JsonReaders {
+
+  import org.bitcoins.rpc.serializers.JsonReaders._
+
+  private val logger = LoggerFactory.getLogger(this.getClass.getSimpleName)
+
+  implicit val picoBitcoins: Reads[PicoBitcoins] = {
+    Reads { jsValue: JsValue =>
+      SerializerUtil.processJsNumberBigInt(PicoBitcoins.apply)(jsValue)
+    }
+  }
+
+  implicit val peerState: Reads[PeerState] = {
+    Reads { jsValue: JsValue =>
+      SerializerUtil.processJsStringOpt(PeerState.fromString)(jsValue)
+    }
+  }
+
+  implicit val nodeId: Reads[NodeId] = {
+    Reads { jsValue: JsValue =>
+      SerializerUtil.processJsString(NodeId.fromHex)(jsValue)
+    }
+
+  }
 
   implicit val getInfoResultReads: Reads[GetInfoResult] = {
     Json.reads[GetInfoResult]
@@ -14,6 +45,12 @@ object JsonReaders {
 
   implicit val nodeInfoReads: Reads[NodeInfo] = {
     Json.reads[NodeInfo]
+  }
+
+  implicit val fundedChannelIdReads: Reads[FundedChannelId] = {
+    Reads { jsValue: JsValue =>
+      SerializerUtil.processJsString(FundedChannelId.fromHex)(jsValue)
+    }
   }
 
   implicit val channelDescReads: Reads[ChannelDesc] = {
@@ -42,5 +79,4 @@ object JsonReaders {
 
   implicit val channelResultReads: Reads[ChannelResult] =
     Json.reads[ChannelResult]
-
 }
