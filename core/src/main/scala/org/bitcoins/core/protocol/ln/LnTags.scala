c6e25d060fbf67d76839648a070676e5b87faa70
Chris Stewart <stewart.chris1234@gmail.com>
2018-09-24 10:18:09 -0400

    Adding bitcoin-s types to the eclair-rpc, fixing bug with decoding numbers, refactoring more things (#204)

--- core/src/main/scala/org/bitcoins/core/protocol/ln/LnTags.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/ln/LnTags.scala
@@ -6,21 +6,20 @@ import org.bitcoins.core.config.{ MainNet, NetworkParameters }
 import org.bitcoins.core.crypto.{ ECPublicKey, Sha256Digest, Sha256Hash160Digest }
 import org.bitcoins.core.number.{ UInt32, UInt5, UInt8 }
 import org.bitcoins.core.protocol._
-import org.bitcoins.core.protocol.ln.LnInvoiceTag.{ PaymentHashTag, encodeNumber }
 import org.bitcoins.core.protocol.ln.routing.LnRoute
+import org.bitcoins.core.protocol.ln.util.LnUtil
 import org.bitcoins.core.protocol.script.{ P2WPKHWitnessSPKV0, P2WSHWitnessSPKV0, WitnessScriptPubKeyV0 }
-import org.bitcoins.core.util.Bech32
+import org.bitcoins.core.util.{ Bech32, BitcoinSUtil }
 import org.slf4j.LoggerFactory
 import scodec.bits.ByteVector
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 /**
  * One of the tagged fields on a Lightning Network invoice
  * [[https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md#tagged-fields]]
  */
-sealed abstract class LnInvoiceTag {
+sealed abstract class LnTag {
 
   def prefix: LnTagPrefix
 
@@ -29,23 +28,20 @@ sealed abstract class LnInvoiceTag {
     UInt5(char.toByte)
   }
 
-  /** The payload for the tag without any meta infromation encoded with it */
+  /** The payload for the tag without any meta information encoded with it */
   def encoded: Vector[UInt5]
 
   def data: Vector[UInt5] = {
-    val len = encodeNumber(encoded.length)
+    val len = LnUtil.createDataLength(encoded)
     prefixUInt5 +: (len ++ encoded)
   }
 
   override def toString: String = {
-    val b = new mutable.StringBuilder
 
     val dataBech32 = Bech32.encode5bitToString(data)
 
-    b.append(prefix.toString)
-    b.append(dataBech32)
+    dataBech32
 
-    b.toString()
   }
 }
 
@@ -54,7 +50,7 @@ sealed abstract class LnInvoiceTag {
  * Refer to BOLT11 for a full list
  * [[https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md#tagged-fields]]
  */
-object LnInvoiceTag {
+object LnTag {
   private val logger = LoggerFactory.getLogger(this.getClass.getName)
 
   /** Fallback address versions */
@@ -91,71 +87,26 @@ object LnInvoiceTag {
         }
       }
 
-      LnInvoiceTag.FallbackAddressTag(address)
+      LnTag.FallbackAddressTag(address)
 
     }
-  }
-
-  /**
-   * The formula for this calculation is as follows:
-   * Take the length of the Bech32 encoded input and divide it by 32.
-   * Take the quotient, and encode this value as Bech32. Take the remainder and encode this value as Bech32.
-   * Append these values to produce a valid Lighting Network data_length field.
-   * Please see Bolt-11 for examples:
-   * https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md#examples
-   */
-  def dataLength(bech32String: String): Vector[UInt5] = {
-    val e = encodeNumber(bech32String.length)
-    e
   }
 
   private def u32ToU5(u32: UInt32): Vector[UInt5] = {
-    val encoded = encodeNumber(u32.toLong)
-    require(encoded.size == 2, s"u32ToU5 ${encoded} ${encoded.size}")
+    val encoded = LnUtil.encodeNumber(u32.toLong)
     encoded
   }
 
-  /** Returns a 5bit bytevector with the encoded number for a ln invoice */
-  @tailrec
-  def encodeNumber(len: Long, accum: Vector[UInt5] = Vector.empty): Vector[UInt5] = {
-    val quotient = len / 32
-    val remainder = UInt5(len % 32)
-    if (quotient >= 32) {
-      encodeNumber(quotient, remainder +: accum)
-    } else {
-      val quo = UInt5.fromByte(quotient.toByte)
-      val v = Vector(quo, remainder)
-      v ++ accum
-    }
-
-  }
-
-  @tailrec
-  def decodeNumber(vector: Vector[UInt5], accum: Long = 0): Long = {
-
-    if (vector.isEmpty) accum
-    else if (vector.size == 1) {
-      decodeNumber(vector.tail, vector.head.toInt + accum)
-    } else {
-      val newAccum = vector.head.toInt * 32 + accum
-      decodeNumber(vector.tail, newAccum)
-    }
-  }
-
-  case class PaymentHashTag(hash: Sha256Digest) extends LnInvoiceTag {
+  case class PaymentHashTag(hash: Sha256Digest) extends LnTag {
 
     override val prefix: LnTagPrefix = LnTagPrefix.PaymentHash
 
     override val encoded: Vector[UInt5] = {
       Bech32.from8bitTo5bit(hash.bytes)
     }
-
-    def fromBytes(vector: ByteVector): PaymentHashTag = {
-      ???
-    }
   }
 
-  case class DescriptionTag(string: String) extends LnInvoiceTag {
+  case class DescriptionTag(string: String) extends LnTag {
     override val prefix: LnTagPrefix = LnTagPrefix.Description
 
     override val encoded: Vector[UInt5] = {
@@ -165,7 +116,7 @@ object LnInvoiceTag {
 
   }
 
-  case class NodeIdTag(pubKey: ECPublicKey) extends LnInvoiceTag {
+  case class NodeIdTag(pubKey: ECPublicKey) extends LnTag {
 
     override val prefix: LnTagPrefix = LnTagPrefix.NodeId
 
@@ -174,7 +125,7 @@ object LnInvoiceTag {
     }
   }
 
-  case class DescriptionHashTag(hash: Sha256Digest) extends LnInvoiceTag {
+  case class DescriptionHashTag(hash: Sha256Digest) extends LnTag {
     override val prefix: LnTagPrefix = LnTagPrefix.DescriptionHash
 
     override val encoded: Vector[UInt5] = {
@@ -183,11 +134,11 @@ object LnInvoiceTag {
   }
 
   /** The amount in seconds until this payment request expires */
-  case class ExpiryTimeTag(u32: UInt32) extends LnInvoiceTag {
+  case class ExpiryTimeTag(u32: UInt32) extends LnTag {
     override val prefix: LnTagPrefix = LnTagPrefix.ExpiryTime
 
     override val encoded: Vector[UInt5] = {
-      u32ToU5(u32)
+      LnUtil.encodeNumber(u32.toLong)
     }
   }
 
@@ -197,7 +148,7 @@ object LnInvoiceTag {
    * This is denominated in blocks
    * [[https://github.com/lightningnetwork/lightning-rfc/blob/master/02-peer-protocol.md#cltv_expiry_delta-selection]]
    */
-  case class MinFinalCltvExpiry(u32: UInt32) extends LnInvoiceTag {
+  case class MinFinalCltvExpiry(u32: UInt32) extends LnTag {
     override val prefix: LnTagPrefix = LnTagPrefix.CltvExpiry
 
     override val encoded: Vector[UInt5] = {
@@ -206,7 +157,7 @@ object LnInvoiceTag {
 
   }
 
-  case class FallbackAddressTag(address: Address) extends LnInvoiceTag {
+  case class FallbackAddressTag(address: Address) extends LnTag {
 
     /** The version of the fallback address is indicated here in BOLT11 */
     def version: UInt8 = {
@@ -227,7 +178,7 @@ object LnInvoiceTag {
     }
   }
 
-  case class RoutingInfo(routes: Vector[LnRoute]) extends LnInvoiceTag {
+  case class RoutingInfo(routes: Vector[LnRoute]) extends LnTag {
 
     override val prefix: LnTagPrefix = LnTagPrefix.RoutingInfo
 
@@ -258,46 +209,47 @@ object LnInvoiceTag {
       val bytes = UInt8.toBytes(Bech32.from5bitTo8bit(u5s))
       val vecRoutes: Vector[LnRoute] = loop(bytes, Vector.empty)
 
-      LnInvoiceTag.RoutingInfo(vecRoutes)
+      LnTag.RoutingInfo(vecRoutes)
 
     }
   }
 
-  def fromLnTagPrefix(prefix: LnTagPrefix, payload: Vector[UInt5]): LnInvoiceTag = {
+  def fromLnTagPrefix(prefix: LnTagPrefix, payload: Vector[UInt5]): LnTag = {
 
     val u8s = Bech32.from5bitTo8bit(payload)
     val bytes = UInt8.toBytes(u8s)
 
-    val tag: LnInvoiceTag = prefix match {
+    val tag: LnTag = prefix match {
       case LnTagPrefix.PaymentHash =>
 
         val hash = Sha256Digest.fromBytes(bytes)
-        LnInvoiceTag.PaymentHashTag(hash)
+        LnTag.PaymentHashTag(hash)
 
       case LnTagPrefix.Description =>
 
         val description = new String(bytes.toArray, Charset.forName("UTF-8"))
-        LnInvoiceTag.DescriptionTag(description)
+        LnTag.DescriptionTag(description)
 
       case LnTagPrefix.DescriptionHash =>
 
         val hash = Sha256Digest.fromBytes(bytes)
-        LnInvoiceTag.DescriptionHashTag(hash)
+        LnTag.DescriptionHashTag(hash)
 
       case LnTagPrefix.NodeId =>
 
         val pubKey = ECPublicKey.fromBytes(bytes)
-        LnInvoiceTag.NodeIdTag(pubKey)
+        LnTag.NodeIdTag(pubKey)
 
       case LnTagPrefix.ExpiryTime =>
 
-        val u32 = UInt32(decodeNumber(payload))
-        LnInvoiceTag.ExpiryTimeTag(u32)
+        val decoded = LnUtil.decodeNumber(payload)
+        val u32 = UInt32(decoded)
+        LnTag.ExpiryTimeTag(u32)
 
       case LnTagPrefix.CltvExpiry =>
 
         val u32 = UInt32.fromBytes(bytes)
-        LnInvoiceTag.MinFinalCltvExpiry(u32)
+        LnTag.MinFinalCltvExpiry(u32)
 
       case LnTagPrefix.FallbackAddress =>
 
