f294c6f194606914f79eba3855f0649535ed5792
Nadav Kohen <nadavk25@gmail.com>
2018-11-08 16:08:30 -0600

    Cleaned up eclair conf (#237)
    * Cleaned up eclair conf
    
    * Added test for bad auth and Reads for LnInvoice

---
+++ eclair-rpc/src/main/scala/org/bitcoins/eclair/rpc/config/ConfigUtil.scala
@@ -0,0 +1,14 @@
+package org.bitcoins.eclair.rpc.config
+
+import com.typesafe.config.Config
+
+import scala.util.{ Failure, Success, Try }
+
+object ConfigUtil {
+  def getIntOrElse(config: Config, path: String, default: Int): Int = {
+    Try(config.getInt(path)) match {
+      case Success(num) => num
+      case Failure(_) => default
+    }
+  }
+}
