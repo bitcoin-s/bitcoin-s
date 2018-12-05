c31507afabac602ccda81bea82a1a67cbe0ee8f0
Torkel Rogstad <torkel@rogstad.io>
2018-12-05 18:56:19 +0100

    Fixes SO error by reversing remowal of `new`

--- core/src/main/scala/org/bitcoins/core/protocol/ln/channel/ChannelId.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/ln/channel/ChannelId.scala
@@ -37,6 +37,6 @@ case class FundedChannelId(bytes: ByteVector) extends ChannelId {
 
 object FundedChannelId extends Factory[FundedChannelId] {
   override def fromBytes(bytes: ByteVector): FundedChannelId = {
-    FundedChannelId(bytes)
+    new FundedChannelId(bytes)
   }
 }
