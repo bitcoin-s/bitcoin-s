2eae4c287189059b997e94ca830bbc0e720b0dbc
Torkel Rogstad <torkel@rogstad.io>
2019-01-20 21:57:16 +0100

    Tunes scoverage (#303)

--- core/src/main/scala/org/bitcoins/core/crypto/HashDigest.scala
+++ core/src/main/scala/org/bitcoins/core/crypto/HashDigest.scala
@@ -30,7 +30,9 @@ sealed abstract class Sha1Digest extends HashDigest {
 
 object Sha1Digest extends Factory[Sha1Digest] {
   private case class Sha1DigestImpl(bytes: ByteVector) extends Sha1Digest {
+    // $COVERAGE-OFF$
     override def toString = s"Sha1DigestImpl($hex)"
+    // $COVERAGE-ON$
   }
   override def fromBytes(bytes: ByteVector): Sha1Digest = Sha1DigestImpl(bytes)
 }
@@ -45,8 +47,10 @@ sealed abstract class Sha256Digest extends HashDigest {
 object Sha256Digest extends Factory[Sha256Digest] {
   private case class Sha256DigestImpl(bytes: ByteVector) extends Sha256Digest {
     require(bytes.length == 32,
+      // $COVERAGE-OFF$
             "Sha256Digest must be 32 bytes in size, got: " + bytes.length)
     override def toString = s"Sha256DigestImpl($hex)"
+    // $COVERAGE-ON$
   }
   override def fromBytes(bytes: ByteVector): Sha256Digest =
     Sha256DigestImpl(bytes)
@@ -63,8 +67,10 @@ object DoubleSha256Digest extends Factory[DoubleSha256Digest] {
   private case class DoubleSha256DigestImpl(bytes: ByteVector)
       extends DoubleSha256Digest {
     require(bytes.length == 32,
+            // $COVERAGE-OFF$
             "DoubleSha256Digest must always be 32 bytes, got: " + bytes.length)
     override def toString = s"DoubleSha256DigestImpl($hex)"
+    // $COVERAGE-ON$
   }
   override def fromBytes(bytes: ByteVector): DoubleSha256Digest =
     DoubleSha256DigestImpl(bytes)
@@ -82,8 +88,10 @@ object RipeMd160Digest extends Factory[RipeMd160Digest] {
   private case class RipeMd160DigestImpl(bytes: ByteVector)
       extends RipeMd160Digest {
     require(bytes.length == 20,
+            // $COVERAGE-OFF$
             "RIPEMD160Digest must always be 20 bytes, got: " + bytes.length)
     override def toString = s"RipeMd160DigestImpl($hex)"
+    // $COVERAGE-ON$
   }
   override def fromBytes(bytes: ByteVector): RipeMd160Digest =
     RipeMd160DigestImpl(bytes)
@@ -100,8 +108,10 @@ object Sha256Hash160Digest extends Factory[Sha256Hash160Digest] {
   private case class Sha256Hash160DigestImpl(bytes: ByteVector)
       extends Sha256Hash160Digest {
     require(bytes.length == 20,
+            // $COVERAGE-OFF$
             "Sha256Hash160Digest must always be 20 bytes, got: " + bytes.length)
     override def toString = s"Sha256Hash160DigestImpl($hex)"
+    // $COVERAGE-ON$
   }
   override def fromBytes(bytes: ByteVector): Sha256Hash160Digest =
     Sha256Hash160DigestImpl(bytes)
