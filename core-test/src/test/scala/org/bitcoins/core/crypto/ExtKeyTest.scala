5d142ede7f6fa5ec42dccd6b4d837ab6d1887240
Torkel Rogstad <torkel@rogstad.io>
2019-02-28 13:13:37 +0100

    BIP39 support (#353)
    * Adds SHA256 hashing of BitVector
    
    * Add BIP39 support

--- core-test/src/test/scala/org/bitcoins/core/crypto/ExtKeyTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/crypto/ExtKeyTest.scala
@@ -1,16 +1,17 @@
 package org.bitcoins.core.crypto
 
+import org.bitcoins.core.crypto.ExtKeyVersion._
 import org.bitcoins.core.number.UInt32
-import org.bitcoins.core.util.BitcoinSUtil
 import org.scalatest.{FlatSpec, MustMatchers}
+import scodec.bits.HexStringSyntax
 
 class ExtKeyTest extends FlatSpec with MustMatchers {
 
   //https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#test-vectors
   "ExtKey" must "pass the test vectors in BIP32" in {
     //master key
-    val seed = BitcoinSUtil.decodeHex("000102030405060708090a0b0c0d0e0f")
-    val masterPriv = ExtPrivateKey(MainNetPriv, Some(seed))
+    val seedBytes = hex"000102030405060708090a0b0c0d0e0f"
+    val masterPriv = ExtPrivateKey(MainNetPriv, Some(seedBytes))
     masterPriv.toString must be(
       "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi")
 
@@ -63,10 +64,10 @@ class ExtKeyTest extends FlatSpec with MustMatchers {
   }
 
   it must "pass test vector 2 in BIP32" in {
-    val seed = BitcoinSUtil.decodeHex(
-      "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542")
+    val seedBytes =
+      hex"fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542"
 
-    val masterPriv = ExtPrivateKey(MainNetPriv, Some(seed))
+    val masterPriv = ExtPrivateKey(MainNetPriv, Some(seedBytes))
     masterPriv.toString must be(
       "xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U")
 
@@ -119,9 +120,10 @@ class ExtKeyTest extends FlatSpec with MustMatchers {
   }
 
   it must "pass test vector 3 in BIP32" in {
-    val seed = BitcoinSUtil.decodeHex(
-      "4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be")
-    val masterPrivKey = ExtPrivateKey(MainNetPriv, Some(seed))
+    val seedBytes =
+      hex"4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be"
+
+    val masterPrivKey = ExtPrivateKey(MainNetPriv, Some(seedBytes))
     masterPrivKey.toString must be(
       "xprv9s21ZrQH143K25QhxbucbDDuQ4naNntJRi4KUfWT7xo4EKsHt2QJDu7KXp1A3u7Bi1j8ph3EGsZ9Xvz9dGuVrtHHs7pXeTzjuxBrCmmhgC6")
 
@@ -143,7 +145,7 @@ class ExtKeyTest extends FlatSpec with MustMatchers {
     //actual priv key 68e5ed2b2c8fc5a6605107d29d074e3d6ccb119c2811007e32f48305176f814c
     val str =
       "xprv9s21ZrQH143K4LCRq4tUZUt3fiTNZr6QTiep3HGzMxtSwfxKAhBmNJJnsmoyWuYZCPC4DNsiVwToHJbxZtq4iEkozBhMzWNTiCH4tzJNjPi"
-    val masterPriv = ExtKey.fromString(str).get.asInstanceOf[ExtPrivateKey]
+    val masterPriv = ExtPrivateKey.fromString(str).get
     val idx = UInt32((1L << 31) - 1)
     val path1 = masterPriv.deriveChildPrivKey(idx).extPublicKey.key
     val path2 = masterPriv.extPublicKey.deriveChildPubKey(idx).get.key
