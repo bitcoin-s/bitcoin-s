11cdb4e449ff6248fc089a7ec6f902081dba1d44
Torkel Rogstad <torkel@rogstad.io>
2019-03-25 13:56:29 +0100

    Update BIP44 classes

--- core/src/main/scala/org/bitcoins/core/crypto/bip44/BIP44Coin.scala
+++ core/src/main/scala/org/bitcoins/core/crypto/bip44/BIP44Coin.scala
@@ -1,5 +1,11 @@
 package org.bitcoins.core.crypto.bip44
 import org.bitcoins.core.crypto.bip32.{BIP32Node, BIP32Path}
+import org.bitcoins.core.protocol.blockchain.{
+  ChainParams,
+  MainNetChainParams,
+  RegTestNetChainParams,
+  TestNetChainParams
+}
 
 /**
   * Represents a
@@ -30,6 +36,18 @@ object BIP44Coin {
     */
   final case object Testnet extends BIP44Coin {
     override val toInt: Int = 1
+  }
+
+  /**
+    * Converts the given chain params into a BIP44 coin type. Treats regtest and
+    * testnet the same.
+    *
+    * @see [[https://github.com/bcoin-org/bcoin/blob/7c64fd845cbae23751558efbe8e078e2ccbfbd30/lib/protocol/networks.js#L838 bcoin]]
+    *     and [[https://github.com/bitcoinj/bitcoinj/blob/bfe2a195b62bcbf1d2e678969e541ebc3656ae17/core/src/main/java/org/bitcoinj/params/RegTestParams.java#L48 BitcoinJ]]
+    */
+  def fromChainParams(chainParams: ChainParams): BIP44Coin = chainParams match {
+    case MainNetChainParams                         => Bitcoin
+    case TestNetChainParams | RegTestNetChainParams => Testnet
   }
 
   def fromInt(int: Int): BIP44Coin =
