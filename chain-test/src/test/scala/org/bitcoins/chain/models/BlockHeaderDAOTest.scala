519cc0c1fe3522fe1af582c37f02342dc7d2266c
Chris Stewart <stewart.chris1234@gmail.com>
2019-04-05 10:57:16 -0500

    Add coverage minimum for chain project (#398)
    * Add coverage minimum for chain project
    
    * Add first Blockchain.connectTip() unit test, switch to a in memory sqlite database for unit tests, starting using fixtures for BlockHeaderDAO in unit tests
    
    * Add tests for ChainHandler.processNewHeader(), ChainHandler.getHeader(), Blockchain.connectTip(). Refactor redundant configurations being passed around excessivly
    
    * Address code review, fix a flaky test in ClientTest.scala

--- chain-test/src/test/scala/org/bitcoins/chain/models/BlockHeaderDAOTest.scala
+++ chain-test/src/test/scala/org/bitcoins/chain/models/BlockHeaderDAOTest.scala
@@ -1,199 +1,166 @@
 package org.bitcoins.chain.models
 
-import org.bitcoins.chain.db.ChainDbManagement
 import org.bitcoins.chain.util.ChainUnitTest
-import org.bitcoins.core.crypto.{DoubleSha256Digest, ECPrivateKey}
-import org.bitcoins.core.number.{Int32, UInt32}
-import org.bitcoins.core.protocol.blockchain.BlockHeader
-import org.bitcoins.testkit.chain.ChainTestUtil
+import org.bitcoins.testkit.chain.BlockHeaderHelper
 
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.Future
 
 /**
   * Created by chris on 9/8/16.
   */
 class BlockHeaderDAOTest extends ChainUnitTest {
 
-  val blockHeaderDAO = BlockHeaderDAO(chainParams =
-                                        ChainTestUtil.regTestChainParams,
-                                      dbConfig = dbConfig)
-  before {
-    //Awaits need to be used to make sure this is fully executed before the next test case starts
-    //TODO: Figure out a way to make this asynchronous
-    Await.result(ChainDbManagement.createHeaderTable(dbConfig), timeout)
-    Await.result(blockHeaderDAO.create(genesisHeader), timeout)
-  }
-
   behavior of "BlockHeaderDAO"
 
-  it should "insert and read the genesis block header back" in {
-    val readF = blockHeaderDAO.read(genesisHeader.hashBE)
+  it should "insert and read the genesis block header back" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val readF = blockHeaderDAO.read(genesisHeaderDb.hashBE)
 
-    val assert1 = readF.map { readHeader =>
-      assert(readHeader.get.blockHeader.hashBE == genesisHeader.hashBE)
-    }
-    val read1F = blockHeaderDAO.getAtHeight(0)
-
-    val assert2 = {
-      read1F.map { headersAtHeight0 =>
-        assert(headersAtHeight0 == List(genesisHeader))
+      val assert1 = readF.map { readHeader =>
+        assert(readHeader.get.blockHeader.hashBE == genesisHeaderDb.hashBE)
       }
-    }
+      val read1F = blockHeaderDAO.getAtHeight(0)
 
-    assert1.flatMap(_ => assert2.map(_ => succeed))
+      val assert2 = {
+        read1F.map { headersAtHeight0 =>
+          assert(headersAtHeight0 == List(genesisHeaderDb))
+        }
+      }
+
+      assert1.flatMap(_ => assert2.map(_ => succeed))
 
   }
 
-  it must "delete a block header in the database" in {
+  it must "delete a block header in the database" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val blockHeader = BlockHeaderHelper.buildNextHeader(genesisHeaderDb)
 
-    val blockHeader = buildHeader(genesisHeader)
-
-    val createdF = blockHeaderDAO.create(blockHeader)
-    //delete the header in the db
-    val deletedF = {
-      createdF.flatMap { _ =>
-        blockHeaderDAO.delete(blockHeader)
+      val createdF = blockHeaderDAO.create(blockHeader)
+      //delete the header in the db
+      val deletedF = {
+        createdF.flatMap { _ =>
+          blockHeaderDAO.delete(blockHeader)
+        }
       }
-    }
 
-    deletedF.flatMap { _ =>
-      blockHeaderDAO
-        .read(blockHeader.blockHeader.hashBE)
-        .map(opt => assert(opt.isEmpty))
-    }
+      deletedF.flatMap { _ =>
+        blockHeaderDAO
+          .read(blockHeader.blockHeader.hashBE)
+          .map(opt => assert(opt.isEmpty))
+      }
 
   }
 
-  it must "retrieve the chain tip saved in the database" in {
+  it must "retrieve the chain tip saved in the database" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val blockHeader = BlockHeaderHelper.buildNextHeader(genesisHeaderDb)
 
-    val blockHeader = buildHeader(genesisHeader)
+      val createdF = blockHeaderDAO.create(blockHeader)
 
-    val createdF = blockHeaderDAO.create(blockHeader)
+      val chainTip1F = createdF.flatMap { _ =>
+        blockHeaderDAO.chainTips
+      }
 
-    val chainTip1F = createdF.flatMap { _ =>
-      blockHeaderDAO.chainTips
-    }
-
-    val assert1F = chainTip1F.map { tips =>
-      assert(tips.length == 1)
-      assert(tips.head.blockHeader.hash == blockHeader.blockHeader.hash)
-    }
-
-    val blockHeader2 = buildHeader(blockHeader)
-
-    //insert another header and make sure that is the new last header
-    assert1F.flatMap { _ =>
-      val created2F = blockHeaderDAO.create(blockHeader2)
-      val chainTip2F = created2F.flatMap(_ => blockHeaderDAO.chainTips)
-
-      chainTip2F.map { tips =>
+      val assert1F = chainTip1F.map { tips =>
         assert(tips.length == 1)
-        assert(tips.head.blockHeader.hash == blockHeader2.blockHeader.hash)
+        assert(tips.head.blockHeader.hash == blockHeader.blockHeader.hash)
       }
-    }
 
-  }
+      val blockHeader2 = BlockHeaderHelper.buildNextHeader(blockHeader)
 
-  it must "return the genesis block when retrieving block headers from an empty database" in {
-    val chainTipsF = blockHeaderDAO.chainTips
-    chainTipsF.map { tips =>
-      assert(tips.headOption == Some(genesisHeader))
-    }
-  }
+      //insert another header and make sure that is the new last header
+      assert1F.flatMap { _ =>
+        val created2F = blockHeaderDAO.create(blockHeader2)
+        val chainTip2F = created2F.flatMap(_ => blockHeaderDAO.chainTips)
 
-  it must "retrieve a block header by height" in {
-    val blockHeader = buildHeader(genesisHeader)
-
-    val createdF = blockHeaderDAO.create(blockHeader)
-
-    val getAtHeightF: Future[Vector[BlockHeaderDb]] = {
-      createdF.flatMap { _ =>
-        blockHeaderDAO.getAtHeight(1)
+        chainTip2F.map { tips =>
+          assert(tips.length == 1)
+          assert(tips.head.blockHeader.hash == blockHeader2.blockHeader.hash)
+        }
       }
-    }
 
-    val assert1F = getAtHeightF.map {
-      case headers =>
-        assert(headers.head == blockHeader)
-        assert(headers.head.height == 1)
-    }
-
-    //create one at height 2
-    val blockHeader2 = buildHeader(blockHeader)
-
-    val created2F = blockHeaderDAO.create(blockHeader2)
-
-    val getAtHeight2F: Future[Vector[BlockHeaderDb]] = {
-      created2F.flatMap(_ => blockHeaderDAO.getAtHeight(2))
-    }
-
-    val assert2F = getAtHeight2F.map { headers =>
-      assert(headers.head == blockHeader2)
-    }
-
-    assert1F.flatMap(_ => assert2F.map(_ => succeed))
   }
 
-  it must "find the height of the longest chain" in {
-    val blockHeader = buildHeader(genesisHeader)
-    val createdF = blockHeaderDAO.create(blockHeader)
-
-    val maxHeightF = createdF.flatMap(_ => blockHeaderDAO.maxHeight)
-
-    val blockHeader2 = buildHeader(blockHeader)
-
-    val created2F = maxHeightF.flatMap(_ => blockHeaderDAO.create(blockHeader2))
-
-    val maxHeight2F = created2F.flatMap(_ => blockHeaderDAO.maxHeight)
-
-    maxHeightF.flatMap { h1 =>
-      maxHeight2F.map { h2 =>
-        assert(h1 == 1)
-        assert(h2 == 2)
-
+  it must "return the genesis block when retrieving block headers from an empty database" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val chainTipsF = blockHeaderDAO.chainTips
+      chainTipsF.map { tips =>
+        assert(tips.headOption == Some(genesisHeaderDb))
       }
-    }
+  }
+
+  it must "retrieve a block header by height" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val blockHeader = BlockHeaderHelper.buildNextHeader(genesisHeaderDb)
+
+      val createdF = blockHeaderDAO.create(blockHeader)
+
+      val getAtHeightF: Future[Vector[BlockHeaderDb]] = {
+        createdF.flatMap { _ =>
+          blockHeaderDAO.getAtHeight(1)
+        }
+      }
+
+      val assert1F = getAtHeightF.map {
+        case headers =>
+          assert(headers.head == blockHeader)
+          assert(headers.head.height == 1)
+      }
+
+      //create one at height 2
+      val blockHeader2 = BlockHeaderHelper.buildNextHeader(blockHeader)
+
+      val created2F = blockHeaderDAO.create(blockHeader2)
+
+      val getAtHeight2F: Future[Vector[BlockHeaderDb]] = {
+        created2F.flatMap(_ => blockHeaderDAO.getAtHeight(2))
+      }
+
+      val assert2F = getAtHeight2F.map { headers =>
+        assert(headers.head == blockHeader2)
+      }
+
+      assert1F.flatMap(_ => assert2F.map(_ => succeed))
+  }
+
+  it must "find the height of the longest chain" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val blockHeader = BlockHeaderHelper.buildNextHeader(genesisHeaderDb)
+      val createdF = blockHeaderDAO.create(blockHeader)
+
+      val maxHeightF = createdF.flatMap(_ => blockHeaderDAO.maxHeight)
+
+      val blockHeader2 = BlockHeaderHelper.buildNextHeader(blockHeader)
+
+      val created2F =
+        maxHeightF.flatMap(_ => blockHeaderDAO.create(blockHeader2))
+
+      val maxHeight2F = created2F.flatMap(_ => blockHeaderDAO.maxHeight)
+
+      maxHeightF.flatMap { h1 =>
+        maxHeight2F.map { h2 =>
+          assert(h1 == 1)
+          assert(h2 == 2)
+
+        }
+      }
 
   }
 
-  it must "find the height of two headers that are competing to be the longest chain" in {
-    val blockHeader = buildHeader(genesisHeader)
-    val createdF = blockHeaderDAO.create(blockHeader)
+  it must "find the height of two headers that are competing to be the longest chain" in withBlockHeaderDAO {
+    blockHeaderDAO: BlockHeaderDAO =>
+      val blockHeader = BlockHeaderHelper.buildNextHeader(genesisHeaderDb)
+      val createdF = blockHeaderDAO.create(blockHeader)
 
-    val blockHeader1 = buildHeader(genesisHeader)
-    val created2F = createdF.flatMap(_ => blockHeaderDAO.create(blockHeader1))
+      val blockHeader1 = BlockHeaderHelper.buildNextHeader(genesisHeaderDb)
+      val created2F = createdF.flatMap(_ => blockHeaderDAO.create(blockHeader1))
 
-    //now make sure they are both at height 1
-    val getHeightF = created2F.flatMap(_ => blockHeaderDAO.getAtHeight(1))
+      //now make sure they are both at height 1
+      val getHeightF = created2F.flatMap(_ => blockHeaderDAO.getAtHeight(1))
 
-    getHeightF.map {
-      case headers =>
-        assert(headers == Seq(blockHeader, blockHeader1))
-    }
+      getHeightF.map {
+        case headers =>
+          assert(headers == Seq(blockHeader, blockHeader1))
+      }
   }
-
-  after {
-    //Awaits need to be used to make sure this is fully executed before the next test case starts
-    //TODO: Figure out a way to make this asynchronous
-    Await.result(ChainDbManagement.dropHeaderTable(dbConfig), timeout)
-  }
-
-  private def buildHeader(prevHeader: BlockHeaderDb): BlockHeaderDb = {
-    val prevHash = prevHeader.blockHeader.previousBlockHash
-    val blockHeader = {
-      BlockHeader(
-        version = Int32.one,
-        previousBlockHash = prevHash,
-        //get random 32 bytes
-        merkleRootHash =
-          DoubleSha256Digest.fromBytes(ECPrivateKey.freshPrivateKey.bytes),
-        time = UInt32.one,
-        nBits = UInt32.one,
-        nonce = UInt32.one
-      )
-    }
-
-    BlockHeaderDbHelper.fromBlockHeader(prevHeader.height + 1, blockHeader)
-  }
-
 }
--- chain-test/src/test/scala/org/bitcoins/chain/util/ChainUnitTest.scala
+++ chain-test/src/test/scala/org/bitcoins/chain/util/ChainUnitTest.scala
@@ -1,30 +1,97 @@
 package org.bitcoins.chain.util
 
-import org.bitcoins.chain.models.BlockHeaderDb
-import org.bitcoins.core.protocol.blockchain.RegTestNetChainParams
+import org.bitcoins.chain.api.ChainApi
+import org.bitcoins.chain.blockchain.{Blockchain, ChainHandler}
+import org.bitcoins.chain.db.ChainDbManagement
+import org.bitcoins.chain.models.{BlockHeaderDAO, BlockHeaderDb}
+import org.bitcoins.core.protocol.blockchain.{
+  ChainParams,
+  RegTestNetChainParams
+}
+import org.bitcoins.core.util.BitcoinSLogger
 import org.bitcoins.db.{DbConfig, UnitTestDbConfig}
 import org.bitcoins.testkit.chain.ChainTestUtil
-import org.scalatest.{
-  AsyncFlatSpec,
-  BeforeAndAfter,
-  BeforeAndAfterAll,
-  MustMatchers
-}
+import org.scalatest._
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 trait ChainUnitTest
     extends AsyncFlatSpec
     with MustMatchers
+    with BitcoinSLogger
     with BeforeAndAfter
     with BeforeAndAfterAll {
 
   val timeout = 10.seconds
   def dbConfig: DbConfig = UnitTestDbConfig
-  val genesisHeader: BlockHeaderDb = ChainTestUtil.regTestGenesisHeaderDb
-  val networkParam: RegTestNetChainParams.type = RegTestNetChainParams
+
+  val genesisHeaderDb: BlockHeaderDb = ChainTestUtil.regTestGenesisHeaderDb
+  val chainParam: ChainParams = RegTestNetChainParams
+
+  lazy val blockHeaderDAO = BlockHeaderDAO(chainParams =
+                                             ChainTestUtil.regTestChainParams,
+                                           dbConfig = dbConfig)
+
+  lazy val blockchain =
+    Blockchain.fromHeaders(Vector(genesisHeaderDb), blockHeaderDAO)
+
+  lazy val chainHandler: ChainHandler = ChainHandler(blockchain)
+  lazy val chainApi: ChainApi = chainHandler
 
   implicit def ec: ExecutionContext =
     scala.concurrent.ExecutionContext.Implicits.global
+
+  /** Fixture that creates a [[org.bitcoins.chain.models.BlockHeaderTable]]
+    * with one row inserted into it, the [[RegTestNetChainParams]]
+    * genesis block
+    * @param test
+    * @return
+    */
+  def withBlockHeaderDAO(
+      test: BlockHeaderDAO => Future[Assertion]): Future[Assertion] = {
+
+    val genesisHeaderF = setupHeaderTableWithGenesisHeader()
+
+    val blockHeaderDAOF = genesisHeaderF.map(_ => blockHeaderDAO)
+    val testExecutionF = blockHeaderDAOF.flatMap(test(_))
+
+    dropHeaderTable(testExecutionF)
+  }
+
+  def withChainHandler(
+      test: ChainHandler => Future[Assertion]): Future[Assertion] = {
+
+    val genesisHeaderF = setupHeaderTableWithGenesisHeader()
+    val chainHandlerF = genesisHeaderF.map(_ => chainHandler)
+
+    val testExecutionF = chainHandlerF.flatMap(test(_))
+
+    dropHeaderTable(testExecutionF)
+  }
+
+  /** Creates the [[org.bitcoins.chain.models.BlockHeaderTable]] and inserts the genesis header */
+  private def setupHeaderTableWithGenesisHeader(): Future[BlockHeaderDb] = {
+    val tableSetupF = ChainDbManagement.createHeaderTable(dbConfig = dbConfig,
+                                                          createIfNotExists =
+                                                            true)
+
+    val genesisHeaderF = tableSetupF.flatMap(_ =>
+      chainHandler.blockchain.blockHeaderDAO.create(genesisHeaderDb))
+
+    genesisHeaderF
+  }
+
+  /** Drops the header table and returns the given Future[Assertion] after the table is dropped */
+  private def dropHeaderTable(
+      testExecutionF: Future[Assertion]): Future[Assertion] = {
+    val dropTableP = Promise[Unit]()
+    testExecutionF.onComplete { _ =>
+      ChainDbManagement.dropHeaderTable(dbConfig).foreach { _ =>
+        dropTableP.success(())
+      }
+    }
+
+    dropTableP.future.flatMap(_ => testExecutionF)
+  }
 }
