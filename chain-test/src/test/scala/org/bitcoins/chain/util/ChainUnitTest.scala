a181779532c46d8bc08cf60fe7a229c86de3ac09
Chris Stewart <stewart.chris1234@gmail.com>
2019-04-09 17:01:28 -0500

    Add integration test between bitcoind <-> zmq <-> bitcoin-s-chain project. Test that we can relay a header from bitcoind over zmq into the bitcoin-s chain project. Redo ZmqConfig to use InetSocketAddress

--- chain-test/src/test/scala/org/bitcoins/chain/util/ChainUnitTest.scala
+++ chain-test/src/test/scala/org/bitcoins/chain/util/ChainUnitTest.scala
@@ -1,5 +1,6 @@
 package org.bitcoins.chain.util
 
+import akka.actor.ActorSystem
 import org.bitcoins.chain.api.ChainApi
 import org.bitcoins.chain.blockchain.{Blockchain, ChainHandler}
 import org.bitcoins.chain.db.ChainDbManagement
@@ -10,14 +11,19 @@ import org.bitcoins.chain.models.{
 }
 import org.bitcoins.core.protocol.blockchain.{
   BlockHeader,
+  Block,
   ChainParams,
   RegTestNetChainParams
 }
 import org.bitcoins.core.util.BitcoinSLogger
 import org.bitcoins.db.{DbConfig, UnitTestDbConfig}
+import org.bitcoins.rpc.client.common.BitcoindRpcClient
 import org.bitcoins.testkit.chain.ChainTestUtil
+import org.bitcoins.testkit.rpc.BitcoindRpcTestUtil
+import org.bitcoins.zmq.ZMQSubscriber
 import org.scalatest._
 import play.api.libs.json.{JsError, JsSuccess, Json}
+import scodec.bits.ByteVector
 
 import scala.annotation.tailrec
 import scala.concurrent.duration.DurationInt
@@ -188,7 +194,76 @@ trait ChainUnitTest
 
     val genesisHeaderF = tableSetupF.flatMap(_ =>
       chainHandler.blockchain.blockHeaderDAO.create(genesisHeaderDb))
-
     genesisHeaderF
+  }
+
+  /** Drops the header table and returns the given Future[Assertion] after the table is dropped */
+  private def dropHeaderTable(
+      testExecutionF: Future[Outcome]): FutureOutcome = {
+    val dropTableP = Promise[Unit]()
+    testExecutionF.onComplete { _ =>
+      ChainDbManagement.dropHeaderTable(dbConfig).foreach { _ =>
+        dropTableP.success(())
+      }
+    }
+
+    val outcomeF = dropTableP.future.flatMap(_ => testExecutionF)
+
+    new FutureOutcome(outcomeF)
+  }
+
+  /** Represents a bitcoind instance paired with a chain handler via zmq */
+  case class BitcoindChainHandler(
+      bitcoindRpc: BitcoindRpcClient,
+      chainHandler: ChainHandler)
+
+  def bitcoindZmqChainHandler(test: BitcoindChainHandler => Future[Assertion])(
+      implicit system: ActorSystem): Future[Assertion] = {
+    val instance = BitcoindRpcTestUtil.instance()
+    val bitcoindF = {
+      val bitcoind = new BitcoindRpcClient(instance)
+      bitcoind.start().map(_ => bitcoind)
+    }
+    val zmqRawBlockUriF = bitcoindF.map(_ => instance.zmqConfig.rawBlock)
+    val f: ChainHandler => Future[Assertion] = { chainHandler: ChainHandler =>
+      val handleRawBlock: ByteVector => Unit = { bytes: ByteVector =>
+        val block = Block.fromBytes(bytes)
+        chainHandler.processHeader(block.blockHeader)
+
+        ()
+      }
+
+      val zmqSubscriberF = zmqRawBlockUriF.map { uriOpt =>
+        val socket = uriOpt.get
+        val z =
+          new ZMQSubscriber(socket, None, None, None, Some(handleRawBlock))
+        z.start()
+        Thread.sleep(1000)
+        z
+      }
+
+      val bitcoindChainHandlerF = for {
+        _ <- zmqSubscriberF
+        bitcoind <- bitcoindF
+      } yield BitcoindChainHandler(bitcoind, chainHandler)
+
+      val testExecutionF = bitcoindChainHandlerF
+        .flatMap(test(_))
+
+      testExecutionF.onComplete { _ =>
+        //kill bitcoind
+        bitcoindChainHandlerF.flatMap { bch =>
+          BitcoindRpcTestUtil.stopServer(bch.bitcoindRpc)
+        }
+
+        //stop zmq
+        zmqSubscriberF.map(_.stop)
+      }
+
+      testExecutionF
+    }
+
+    withChainHandler(f)
+
   }
 }
