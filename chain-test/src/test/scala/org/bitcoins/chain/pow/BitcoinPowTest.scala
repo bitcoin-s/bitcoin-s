028865de74c8a5fcc4ddcd81d996fd428fdd06d6
Nadav Kohen <nadavk25@gmail.com>
2019-04-11 10:39:34 -0500

    Test Fixtures (#403)
    * Working test fixtures
    
    * Removed ChainTestFixture trait in main code

--- chain-test/src/test/scala/org/bitcoins/chain/pow/BitcoinPowTest.scala
+++ chain-test/src/test/scala/org/bitcoins/chain/pow/BitcoinPowTest.scala
@@ -5,12 +5,17 @@ import org.bitcoins.chain.util.ChainUnitTest
 import org.bitcoins.core.protocol.blockchain.MainNetChainParams
 import org.bitcoins.db.UnitTestDbConfig
 import org.bitcoins.testkit.chain.ChainTestUtil
+import org.scalatest.FutureOutcome
 
 class BitcoinPowTest extends ChainUnitTest {
 
+  override type FixtureParam = Unit
+
+  override def withFixture(test: OneArgAsyncTest): FutureOutcome = test(())
+
   behavior of "BitcoinPow"
 
-  it must "NOT calculate a POW change when one is not needed" in {
+  it must "NOT calculate a POW change when one is not needed" in { _ =>
     val chainParams = MainNetChainParams
     val blockHeaderDAO = BlockHeaderDAO(chainParams, UnitTestDbConfig)
     val header1 = ChainTestUtil.ValidPOWChange.blockHeaderDb566494
@@ -24,7 +29,7 @@ class BitcoinPowTest extends ChainUnitTest {
     nextWorkF.map(nextWork => assert(nextWork == header1.nBits))
   }
 
-  it must "calculate a pow change as per the bitcoin network" in {
+  it must "calculate a pow change as per the bitcoin network" in { _ =>
     val firstBlockDb = ChainTestUtil.ValidPOWChange.blockHeaderDb564480
     val currentTipDb = ChainTestUtil.ValidPOWChange.blockHeaderDb566495
     val expectedNextWork = ChainTestUtil.ValidPOWChange.blockHeader566496.nBits
