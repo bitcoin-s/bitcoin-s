48064457d020ce5c9d3453976456aa85c728b601
Torkel Rogstad <torkel@rogstad.io>
2019-05-13 15:01:53 +0200

    Add copy buttons to website code snippets

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -105,7 +105,13 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ["https://buttons.github.io/buttons.js"],
+  scripts: [
+    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+    `${baseUrl}js/code-block-buttons.js`
+  ],
+
+  stylesheets: [`${baseUrl}css/code-block-buttons.css`],
 
   // On page navigation for the current documentation page.
   onPageNav: "separate",
