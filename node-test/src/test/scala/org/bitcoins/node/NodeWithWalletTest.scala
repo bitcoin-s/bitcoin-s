d99c37c970952e5aa8116cdb10d8fc475de11496
Torkel Rogstad <torkel@rogstad.io>
2019-06-04 16:18:26 +0200

    Complete NodeWithWalletTest

--- node-test/src/test/scala/org/bitcoins/node/NodeWithWalletTest.scala
+++ node-test/src/test/scala/org/bitcoins/node/NodeWithWalletTest.scala
@@ -8,10 +8,17 @@ import org.bitcoins.node.config.NodeAppConfig
 import org.bitcoins.node.models.Peer
 import org.scalatest.FutureOutcome
 import org.bitcoins.testkit.wallet.BitcoinSWalletTest
+import org.bitcoins.testkit.BitcoinSAppConfig
 import org.bitcoins.wallet.config.WalletAppConfig
 
 import scala.concurrent.Future
 import org.bitcoins.node.networking.peer.DataMessageHandler
+import scala.concurrent.Promise
+import scala.concurrent.duration._
+import org.scalatest.compatible.Assertion
+import org.scalatest.exceptions.TestFailedException
+import org.bitcoins.core.crypto.DoubleSha256Digest
+import org.bitcoins.rpc.util.AsyncUtil
 
 class NodeWithWalletTest extends BitcoinSWalletTest {
 
@@ -24,14 +31,14 @@ class NodeWithWalletTest extends BitcoinSWalletTest {
     param =>
       val WalletWithBitcoind(wallet, rpc) = param
 
-      implicit val walletConfig: WalletAppConfig = WalletAppConfig()
-      implicit val nodeConfig: NodeAppConfig = NodeAppConfig()
-      implicit val chainConfig: ChainAppConfig = ChainAppConfig()
-
       val chainHandler = {
-        val bhDao = BlockHeaderDAO(chainConfig)
-        ChainHandler(bhDao, chainConfig)
+        val bhDao = BlockHeaderDAO(config)
+        ChainHandler(bhDao, config)
       }
+
+      var txidFromBitcoind: Option[DoubleSha256Digest] = None
+
+      val completionP = Promise[Assertion]
 
       val peer = Peer.fromBitcoind(rpc.instance)
 
@@ -39,21 +46,37 @@ class NodeWithWalletTest extends BitcoinSWalletTest {
         onBlockReceived = { block =>
           logger.error(s"Received a block")
           // for some reason this isn't triggered?
-          fail(s"Received a block! We are only expecting merkle blocks")
+          completionP.failure(
+            new TestFailedException(
+              s"Received a block! We are only expecting merkle blocks",
+              failedCodeStackDepth = 0))
+
         },
         onMerkleBlockReceived =
           block => logger.info(s"Received merkle block: $block"),
-        onTxReceived = tx => logger.info(s"Received TX: $tx"),
+        onTxReceived = { tx =>
+          logger.info(s"Received TX: $tx")
+          if (txidFromBitcoind.contains(tx.txId)) {
+            completionP.success(succeed)
+          }
+        },
       )
 
-      val spv = SpvNode(peer, chainHandler, callbacks = callbacks)
+      /**
+        * This is not ideal, how do we get one implicit value (`config`)
+        * to resolve to multiple implicit parameters?
+        */
+      implicit val nodeConfig: NodeAppConfig = config
+      implicit val chainConfig: ChainAppConfig = config
+      val spv =
+        SpvNode(peer, chainHandler, callbacks = callbacks)
 
       logger.info(
         s"Bitcoind instance has datadir: ${rpc.instance.authCredentials.datadir}")
 
       for {
-        _ <- nodeConfig.initialize()
-        _ = logger.info(s"Node config initialized")
+        _ <- config.initialize()
+        _ = logger.info(s"Initialized")
 
         address <- wallet.getNewAddress()
         info <- wallet.getAddressInfo(address).map {
@@ -64,32 +87,43 @@ class NodeWithWalletTest extends BitcoinSWalletTest {
         _ = logger.info(s"Added pubkey from wallet to SPV node")
 
         started <- spv.start()
+        _ <- spv.sync()
+        bitcoindHash <- rpc.getBestBlockHash
+        _ <- {
+          val isSyncedF =
+            () => spv.chainApi.getBestBlockHash.map(_ == bitcoindHash)
+          AsyncUtil.awaitConditionF(isSyncedF)
+        }
+        _ = logger.info(s"SPV node is synced")
+
         bloom = started.bloomFilter match {
           case Some(b) => b
           case None    => fail(s"Wallet had no bloom filter")
         }
 
-        _ = assert(bloom.contains(info.pubkey.bytes))
-        _ = logger.info(s"Bloom matches pubkey")
+        txid <- rpc.sendToAddress(address, 1.bitcoin).map { tx =>
+          txidFromBitcoind = Some(tx.flip)
+          val delay = 30.seconds
+          val runnable: Runnable = { () =>
+            val msg =
+              s"Did not receive sent transaction within $delay"
+            logger.error(msg)
+            completionP.failure(new TestFailedException(msg, 0))
+          }
 
-        _ = logger.info(s"SPV node started")
-        txid <- rpc.sendToAddress(address, 1.bitcoin)
-        _ = logger.info(s"Sent money to wallet address")
+          actorSystem.scheduler.scheduleOnce(delay, runnable)
+          tx
+        }
+        _ = logger.info(s"bitcoind sent TX $txid")
 
         rpcAddress <- rpc.getNewAddress
-        _ = logger.info(s"About to generate a block")
         block +: _ <- rpc.generateToAddress(blocks = 1, rpcAddress)
+        _ = logger.info(s"Generated a block")
         txInfo <- rpc.getTransaction(txid)
         _ = assert(txInfo.confirmations > 0 && txInfo.blockhash.contains(block))
-        _ = logger.info(s"Generated a block: ${block}")
-        balance <- wallet.getBalance()
-        _ = logger.info(s"wallet balance: $balance")
 
-        _ <- Future {
-          // this is to make the test hang, so you have time to investigate logs and whatnot
-          Thread.sleep(100000)
-        }
-      } yield fail
+        assertion <- completionP.future
+      } yield assertion
 
   }
 }
