6a2f72b3fe0efb91a610046517ede6dd2c1e2dab
Torkel Rogstad <torkel@rogstad.io>
2019-06-12 18:32:51 +0200

    Trezor test vectors
    In this commit we add:
    
    1) A program for dumping a bunch of address
       information from a running Trezor
    2) A test in the wallet module that verifies
       that our addresses line up with the ones
       Trezor generates

---
+++ wallet-test/src/test/scala/org/bitcoins/wallet/TrezorAddressTest.scala
@@ -0,0 +1,266 @@
+package org.bitcoins.wallet
+
+import org.bitcoins.testkit.util.BitcoinSUnitTest
+import org.bitcoins.core.crypto.MnemonicCode
+import scala.io.Source
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import org.bitcoins.core.crypto.ExtPublicKey
+import org.bitcoins.core.hd.HDCoinType
+import org.bitcoins.core.hd.HDPurpose
+import org.bitcoins.core.hd.HDPath
+import org.bitcoins.core.hd.HDChain
+import org.bitcoins.core.protocol.BitcoinAddress
+import org.bitcoins.rpc.serializers.JsonSerializers._
+import play.api.libs.json.Reads
+import play.api.libs.json.JsResult
+import org.bitcoins.rpc.serializers.SerializerUtil
+import play.api.libs.json.JsError
+import play.api.libs.json.JsSuccess
+import org.bitcoins.core.hd.HDCoin
+import org.bitcoins.core.hd.HDChainType
+import org.bitcoins.core.hd.HDPurposes
+import org.bitcoins.wallet.config.WalletAppConfig
+import org.bitcoins.testkit.BitcoinSAppConfig
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import akka.actor.ActorSystem
+import scala.concurrent.Future
+import org.bitcoins.wallet.api.InitializeWalletSuccess
+import org.scalatest.AsyncFlatSpec
+import org.bitcoins.testkit.wallet.BitcoinSWalletTest
+import org.scalatest.FutureOutcome
+import org.bitcoins.testkit.fixtures.EmptyFixture
+import org.bitcoins.core.util.FutureUtil
+import org.bitcoins.core.hd.HDChainType.Change
+import org.bitcoins.core.hd.HDChainType.External
+import org.bitcoins.wallet.models.AddressDb
+import org.bitcoins.wallet.models.AccountDb
+import _root_.akka.actor.Address
+import org.scalatest.compatible.Assertion
+import scala.concurrent.ExecutionContext
+
+class TrezorAddressTest extends BitcoinSWalletTest with EmptyFixture {
+
+  val mnemonic = MnemonicCode.fromWords(
+    Vector("stage",
+           "boring",
+           "net",
+           "gather",
+           "radar",
+           "radio",
+           "arrest",
+           "eye",
+           "ask",
+           "risk",
+           "girl",
+           "country"))
+
+  lazy val json: JsValue = {
+    val rawText = Source
+      .fromResource("trezor-addresses.json")
+      .getLines
+      .drop(1) // first line is a comment
+      .mkString
+    Json.parse(rawText)
+  }
+
+  implicit val hdpathReads = new Reads[HDPath] {
+    override def reads(json: JsValue): JsResult[HDPath] =
+      json
+        .validate[String]
+        .flatMap(HDPath.fromString(_) match {
+          case None        => JsError(s"Could not read $json")
+          case Some(value) => JsSuccess(value)
+        })
+  }
+
+  implicit val hdcoinReads = new Reads[HDCoinType] {
+    override def reads(json: JsValue): JsResult[HDCoinType] =
+      json.validate[String].map(_.toLowerCase).map {
+        case "bitcoin" => HDCoinType.Bitcoin
+        case "testnet" => HDCoinType.Testnet
+      }
+  }
+
+  implicit val hdpurposeReads = new Reads[HDPurpose] {
+    override def reads(json: JsValue): JsResult[HDPurpose] =
+      json.validate[String].map {
+        case "legacy"      => HDPurposes.Legacy
+        case "segwit"      => HDPurposes.SegWit
+        case "p2sh-segwit" => HDPurposes.NestedSegWit
+      }
+  }
+
+  implicit val hdchainType = new Reads[HDChainType] {
+    override def reads(json: JsValue): JsResult[HDChainType] =
+      json.validate[String].map(_.toLowerCase).map {
+        case "change"   => HDChainType.Change
+        case "external" => HDChainType.External
+      }
+  }
+
+  private case class TestAddress(
+      path: HDPath,
+      chain: HDChainType,
+      addressIndex: Int,
+      address: BitcoinAddress)
+
+  private object TestAddress {
+    implicit val reads = Json.reads[TestAddress]
+  }
+
+  private case class TestVector(
+      xpub: ExtPublicKey,
+      coin: HDCoinType,
+      account: Int,
+      pathType: HDPurpose,
+      addresses: Vector[TestAddress])
+
+  private object TestVector {
+    implicit val reads = Json.reads[TestVector]
+  }
+
+  private lazy val vectors = json.validate[Vector[TestVector]] match {
+    case JsError(errors)     => fail(errors.head.toString)
+    case JsSuccess(value, _) => value
+  }
+
+  private lazy val legacyVectors =
+    vectors.filter(_.pathType == HDPurposes.Legacy)
+
+  private lazy val segwitVectors =
+    vectors.filter(_.pathType == HDPurposes.SegWit)
+
+  private lazy val nestedVectors =
+    vectors.filter(_.pathType == HDPurposes.NestedSegWit)
+
+  private def configForPurpose(purpose: HDPurpose): Config = {
+    val purposeStr = purpose match {
+      case HDPurposes.Legacy       => "legacy"
+      case HDPurposes.SegWit       => "segwit"
+      case HDPurposes.NestedSegWit => "nested-segwit"
+      case other                   => fail(s"unexpected purpose: $other")
+    }
+    val confStr = s"""bitcoin-s.wallet.defaultAccountType = $purposeStr
+                     |bitcoin-s.network = mainnet""".stripMargin
+    ConfigFactory.parseString(confStr)
+  }
+
+  private def getWallet(config: WalletAppConfig): Future[Wallet] =
+    Wallet
+      .initializeWithMnemonic(mnemonic)(
+        config, // to make sure we're not passing in the wrong conf by accident
+        implicitly[ExecutionContext])
+      .map {
+        case InitializeWalletSuccess(wallet: Wallet) =>
+          wallet
+        case err => fail(s"didn't get wallet: $err")
+      }
+
+  private case class AccountAndAddrsAndVector(
+      account: AccountDb,
+      addrs: Seq[AddressDb],
+      vector: TestVector)
+
+  private def testAccountType(purpose: HDPurpose): Future[Assertion] = {
+    val confOverride = configForPurpose(purpose)
+    implicit val conf: WalletAppConfig =
+      BitcoinSAppConfig.getTestConfig(confOverride)
+
+    val vectors = purpose match {
+      case HDPurposes.Legacy       => legacyVectors
+      case HDPurposes.SegWit       => segwitVectors
+      case HDPurposes.NestedSegWit => nestedVectors
+      case other                   => fail(s"unknown purpose: $other")
+    }
+
+    for {
+      wallet <- getWallet(conf)
+      existingAccounts <- wallet.listAccounts(purpose)
+      _ <- {
+        val accountsToCreate = existingAccounts.length until vectors.length
+
+        FutureUtil.sequentially(accountsToCreate) { _ =>
+          wallet.createNewAccount(purpose)
+        }
+      }
+      accounts <- wallet.listAccounts(purpose)
+
+      // we want to find all accounts for the given account type,
+      // and match it with its corresponding test vector
+      accountsWithVectors = {
+        assert(accounts.length == vectors.length)
+        val accountsWithVectors = vectors.map { vec =>
+          accounts.find(_.hdAccount.index == vec.account) match {
+            case None =>
+              fail(
+                s"Did not find account in wallet with index ${vec.account}. Accounts: ${accounts.mkString}")
+            case Some(account) =>
+              assert(account.xpub == vec.xpub)
+              account -> vec
+          }
+        }
+        accountsWithVectors
+      }
+
+      // here we generate addresses matching the ones found
+      // in the accompanying test vector for each account
+      // at the end we group them all together
+      accountsWithAddrsWithVecs <- {
+        FutureUtil.sequentially(accountsWithVectors) {
+          case (acc, vec) =>
+            val addrFutures: Future[Seq[AddressDb]] =
+              FutureUtil.sequentially(vec.addresses) { vector =>
+                val addrFut = vector.chain match {
+                  case Change => wallet.getNewChangeAddress(acc)
+                  case External =>
+                    wallet.getNewAddress(acc)
+                }
+                addrFut.flatMap(wallet.addressDAO.findAddress).map {
+                  case Some(addr) => addr
+                  case None =>
+                    fail(s"Did not find address we just generated in DAO!")
+                }
+              }
+            addrFutures.map(AccountAndAddrsAndVector(acc, _, vec))
+        }
+      }
+    } yield {
+      // lastly we loop over all accounts, addresses and vectors
+      // and verify that they are all the same
+      accountsWithAddrsWithVecs.foreach {
+        case AccountAndAddrsAndVector(account, addresses, vec) =>
+          val acctIdx = account.hdAccount.index
+          val vec = vectors.find(_.xpub == account.xpub) match {
+            case None =>
+              fail(s"Did not find test vector for account $acctIdx")
+            case Some(v) => v
+          }
+          assert(vec.addresses.length == addresses.length)
+
+          val sortedAddresses = addresses.sortBy(_.path.toString)
+          val sortedVectors = vec.addresses.sortBy(_.path.toString)
+          sortedAddresses.zip(sortedVectors).foreach {
+            case (foundAddress, expectedAddress) =>
+              assert(foundAddress.address == expectedAddress.address)
+          }
+      }
+      succeed
+    }
+  }
+
+  it must "act the same way as Trezor for legacy accounts" in { _ =>
+    testAccountType(HDPurposes.Legacy)
+  }
+
+  it must "act the same way as Trezor for segwit accounts" in { _ =>
+    testAccountType(HDPurposes.SegWit)
+  }
+
+  // TODO: implement this when nested segwit addresses are implemented
+  // in the wallet
+  it must "act the same way as Trezor for nested segwit accounts" ignore { _ =>
+    testAccountType(HDPurposes.NestedSegWit)
+  }
+}
