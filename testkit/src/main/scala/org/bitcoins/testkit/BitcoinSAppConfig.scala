05a89e1a7301047c4b77a2eadfbbc3e9256a7923
Torkel Rogstad <torkel@rogstad.io>
2019-06-14 18:10:36 +0200

    Default to file based databases in tests (#517)
    This is due to issues with deadlocks with
    in-memory based databases. We need DBs
    to stay alive between connections, but
    not across tests.
    
    In DB intensive chain validation tests we
    use in-memory databases instead. This
    seems like a reasonable tradeoff between
    simplicity and speed.

--- testkit/src/main/scala/org/bitcoins/testkit/BitcoinSAppConfig.scala
+++ testkit/src/main/scala/org/bitcoins/testkit/BitcoinSAppConfig.scala
@@ -67,37 +67,59 @@ object BitcoinSAppConfig {
 
   /**
     * App configuration suitable for test purposes:
-
+    *
     * 1) Data directory is set to user temp directory
-    * 2) All databases are in-memory
     */
   def getTestConfig(config: Config*) = {
     val tmpDir = Files.createTempDirectory("bitcoin-s-")
     val confStr = s"""
     | bitcoin-s {
     |   datadir = $tmpDir
-    |
-    |   wallet.db {
-    |     url = "jdbc:sqlite:file::memory:?cache=shared" 
-    |     connectionPool = disabled
-    |     keepAliveConnection = true 
-    |   }
-    |   node.db {
-    |     url = "jdbc:sqlite:file::memory:?cache=shared" 
-    |     connectionPool = disabled
-    |     keepAliveConnection = true 
-    |   }
-    |   chain.db {
-    |     url = "jdbc:sqlite:file::memory:?cache=shared" 
-    |     connectionPool = disabled
-    |     keepAliveConnection = true 
-    |   }
     | }
     |
     |""".stripMargin
     val conf = ConfigFactory.parseString(confStr)
     val allConfs = conf +: config
     BitcoinSAppConfig(allConfs: _*)
+  }
+
+  sealed trait ProjectType
+
+  object ProjectType {
+    case object Wallet extends ProjectType
+    case object Node extends ProjectType
+    case object Chain extends ProjectType
+
+    val all = List(Wallet, Node, Chain)
+  }
+
+  /** Generates a Typesafe config with DBs set to memory
+    * databases for the given project (or all, if no
+    * project is given). This configuration can then be
+    * given as a override to other configs.
+    */
+  def configWithMemoryDb(project: Option[ProjectType]): Config = {
+    def memConfigForProject(project: ProjectType): String = {
+      val name = project.toString().toLowerCase()
+      s"""
+        | $name.db {
+        |   url = "jdbc:sqlite:file:$name.db:?mode=memory&cache=shared"
+        |   connectionPool = disabled
+        |   keepAliveConnection = true
+        | }
+        |""".stripMargin
+    }
+
+    val confStr = project match {
+      case None    => ProjectType.all.map(memConfigForProject).mkString("\n")
+      case Some(p) => memConfigForProject(p)
+    }
+    val nestedConfStr = s"""
+    | bitcoin-s {
+    | $confStr
+    | }
+    |""".stripMargin
+    ConfigFactory.parseString(nestedConfStr)
   }
 
 }
