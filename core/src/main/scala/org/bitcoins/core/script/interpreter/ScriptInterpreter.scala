ec5280a0c786020f31fd0d46fce9ceaff34b4efd
Kai Brünnler <kai.bruennler@gmail.com>
2019-07-04 21:54:46 +0200

    remove duplicate duplicate check :) (#575)

--- core/src/main/scala/org/bitcoins/core/script/interpreter/ScriptInterpreter.scala
+++ core/src/main/scala/org/bitcoins/core/script/interpreter/ScriptInterpreter.scala
@@ -790,7 +790,7 @@ sealed abstract class ScriptInterpreter {
         //since this is not a coinbase tx we cannot have any empty previous outs inside of inputs
         !transaction.inputs.exists(_.previousOutput == EmptyTransactionOutPoint)
     }
-    inputOutputsNotZero && txNotLargerThanBlock && outputsSpendValidAmountsOfMoney && noDuplicateInputs &&
+    inputOutputsNotZero && txNotLargerThanBlock && outputsSpendValidAmountsOfMoney && 
     allOutputsValidMoneyRange && noDuplicateInputs && isValidScriptSigForCoinbaseTx
   }
 
