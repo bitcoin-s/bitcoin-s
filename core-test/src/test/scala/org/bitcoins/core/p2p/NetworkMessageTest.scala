00eafd9970b1eacd708a49d13b5dd7d20e3e17af
Chris Stewart <stewart.chris1234@gmail.com>
2019-10-06 09:48:53 -0500

    Use the fact that network headers specify the number of bytes in the … (#783)
    * Use the fact that network headers specify the number of bytes in the payload rather than just parsing from bytes.size, this should allow us to be more precise when parsing NetworkPayloads rather than _hoping_ that bytes.size does not land on pseudo-valid NetworkPayload. This hopefully resolves #782
    
    * Add safety check around HeadersMessage.toString()
    
    * Add invariant to NetworkMessage saying payloadSize in header must be the actual payload size

--- core-test/src/test/scala/org/bitcoins/core/p2p/NetworkMessageTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/p2p/NetworkMessageTest.scala
@@ -9,4 +9,21 @@ class NetworkMessageTest extends BitcoinSUnitTest {
     NetworkMessage(NodeTestUtil.rawNetworkMessage).hex must be(
       NodeTestUtil.rawNetworkMessage)
   }
+
+
+  it must "serialize and deserialize a version message example from the bitcoin wiki" in {
+    val hex = {
+      //taken from here with slight modifications
+      //https://en.bitcoin.it/wiki/Protocol_documentation#Message_structure
+      //this example uses an old protocol version WITHOUT the relay flag on the version message
+      //since we only support protocol version > 7, i added it manually
+      //this means the payload size is bumped by 1 byte in the NetworkHeader from 100 -> 101
+      //and a relay byte "00" is appended to the end of the payload
+      "F9BEB4D976657273696F6E000000000065000000358d4932" +
+        "62EA0000010000000000000011B2D05000000000010000000000000000000000000000000000FFFF000000000000010000000000000000000000000000000000FFFF0000000000003B2EB35D8CE617650F2F5361746F7368693A302E372E322FC03E0300" +
+        "00"
+    }.toLowerCase
+    val networkMsg = NetworkMessage.fromHex(hex)
+    networkMsg.hex must be (hex)
+  }
 }
