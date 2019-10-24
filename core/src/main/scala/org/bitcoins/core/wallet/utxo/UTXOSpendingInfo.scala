401aa10942ad2936d96cb14ec60ad49542414bbe
nkohen <nadavk25@gmail.com>
2019-10-24 15:30:28 -0500

    Added P2SH nested witness invariants and made BitcoinUTXOSpendingInfo backwards compatible, BitcoinTxBuilderSpec has one failing test

--- core/src/main/scala/org/bitcoins/core/wallet/utxo/UTXOSpendingInfo.scala
+++ core/src/main/scala/org/bitcoins/core/wallet/utxo/UTXOSpendingInfo.scala
@@ -2,9 +2,34 @@ package org.bitcoins.core.wallet.utxo
 
 import org.bitcoins.core.crypto.Sign
 import org.bitcoins.core.currency.CurrencyUnit
-import org.bitcoins.core.protocol.script.{P2SHScriptPubKey, ScriptPubKey, ScriptWitnessV0, WitnessScriptPubKeyV0}
-import org.bitcoins.core.protocol.transaction.{TransactionOutPoint, TransactionOutput}
+import org.bitcoins.core.protocol.script.{
+  CLTVScriptPubKey,
+  CSVScriptPubKey,
+  EmptyScriptPubKey,
+  EmptyScriptWitness,
+  MultiSignatureScriptPubKey,
+  NonStandardScriptPubKey,
+  P2PKHScriptPubKey,
+  P2PKScriptPubKey,
+  P2SHScriptPubKey,
+  P2WPKHWitnessSPKV0,
+  P2WPKHWitnessV0,
+  P2WSHWitnessSPKV0,
+  P2WSHWitnessV0,
+  ScriptPubKey,
+  ScriptWitness,
+  ScriptWitnessV0,
+  UnassignedWitnessScriptPubKey,
+  WitnessCommitment,
+  WitnessScriptPubKey,
+  WitnessScriptPubKeyV0
+}
+import org.bitcoins.core.protocol.transaction.{
+  TransactionOutPoint,
+  TransactionOutput
+}
 import org.bitcoins.core.script.crypto.HashType
+import org.bitcoins.core.util.CryptoUtil
 
 /**
   * Contains the information required to spend a unspent transaction output (UTXO)
@@ -21,65 +46,230 @@ sealed abstract class UTXOSpendingInfo {
 
   /** the actual output itself we are spending */
   def output: TransactionOutput = {
-    TransactionOutput(value = amount,
-      scriptPubKey = scriptPubKey)
+    TransactionOutput(value = amount, scriptPubKey = scriptPubKey)
   }
 
   /** the signers needed to spend from the output above */
   def signers: Seq[Sign]
 
   def hashType: HashType
+
+  def redeemScriptOpt: Option[ScriptPubKey]
+
+  def scriptWitnessOpt: Option[ScriptWitness]
 }
 
 sealed trait BitcoinUTXOSpendingInfo extends UTXOSpendingInfo
 
+object BitcoinUTXOSpendingInfo {
+
+  // TODO: Get rid of this and force all callers to directly call a subclass apply method
+  def apply(
+      outPoint: TransactionOutPoint,
+      output: TransactionOutput,
+      signers: Seq[Sign],
+      redeemScriptOpt: Option[ScriptPubKey],
+      scriptWitnessOpt: Option[ScriptWitness],
+      hashType: HashType): BitcoinUTXOSpendingInfo = {
+    output.scriptPubKey match {
+      case p2sh: P2SHScriptPubKey =>
+        redeemScriptOpt match {
+          case None =>
+            throw new IllegalArgumentException(
+              "Redeem Script must be defined for P2SH.")
+          case Some(redeemScript) =>
+            redeemScript match {
+              case wspk: WitnessScriptPubKeyV0 =>
+                val witnessOpt = scriptWitnessOpt match {
+                  case Some(witness: ScriptWitnessV0) => Some(witness)
+                  case None                           => None
+                  case Some(_: ScriptWitness) =>
+                    throw new UnsupportedOperationException(
+                      "Only v0 Segwit is currently supported")
+                }
+                P2SHNestedSegwitV0UTXOSpendingInfo(
+                  outPoint,
+                  output.value,
+                  p2sh,
+                  signers,
+                  hashType,
+                  wspk,
+                  witnessOpt.getOrElse(throw new IllegalArgumentException(
+                    "Script Witness must be defined for (nested) Segwit input"))
+                )
+              case _: ScriptPubKey =>
+                P2SHSpendingInfo(outPoint,
+                                 output.value,
+                                 p2sh,
+                                 signers,
+                                 hashType,
+                                 redeemScript)
+            }
+        }
+      case wspk: WitnessScriptPubKeyV0 =>
+        val witnessOpt = scriptWitnessOpt match {
+          case Some(witness: ScriptWitnessV0) => Some(witness)
+          case None                           => None
+          case Some(_: ScriptWitness) =>
+            throw new UnsupportedOperationException(
+              "Only v0 Segwit is currently supported")
+        }
+
+        SegwitV0NativeUTXOSpendingInfo(
+          outPoint,
+          output.value,
+          wspk,
+          signers,
+          hashType,
+          witnessOpt.getOrElse(
+            throw new IllegalArgumentException(
+              "Script Witness must be defined for Segwit input"))
+        )
+      case wspk: UnassignedWitnessScriptPubKey =>
+        UnassignedSegwitNativeUTXOSpendingInfo(
+          outPoint,
+          output.value,
+          wspk,
+          signers,
+          hashType,
+          scriptWitnessOpt.getOrElse(EmptyScriptWitness))
+      case _: P2PKScriptPubKey | _: P2PKHScriptPubKey |
+          _: MultiSignatureScriptPubKey | _: NonStandardScriptPubKey |
+          _: CLTVScriptPubKey | _: CSVScriptPubKey | _: WitnessCommitment |
+          EmptyScriptPubKey =>
+        RawScriptUTXOSpendingInfo(outPoint,
+                                  output.value,
+                                  output.scriptPubKey,
+                                  signers,
+                                  hashType)
+    }
+  }
+
+  // TODO: Get rid of this and force all matches to match on the ADT
+  def unapply(info: BitcoinUTXOSpendingInfo): Option[
+    (
+        TransactionOutPoint,
+        TransactionOutput,
+        Seq[Sign],
+        Option[ScriptPubKey],
+        Option[ScriptWitness],
+        HashType)] = {
+    Some(info.outPoint,
+         info.output,
+         info.signers,
+         info.redeemScriptOpt,
+         info.scriptWitnessOpt,
+         info.hashType)
+  }
+}
+
 /** This represents the information needed to be spend scripts like
   * [[org.bitcoins.core.protocol.script.P2PKHScriptPubKey p2pkh]] or [[org.bitcoins.core.protocol.script.P2PKScriptPubKey p2pk]]
   * scripts. Basically there is no nesting that requires a redeem script here*/
-case class RawScriptUTXOSpendingInfo(outPoint: TransactionOutPoint,
-                                     amount: CurrencyUnit,
-                                     scriptPubKey: ScriptPubKey,
-                                     signers: Seq[Sign],
-                                     hashType: HashType) extends BitcoinUTXOSpendingInfo
+case class RawScriptUTXOSpendingInfo(
+    outPoint: TransactionOutPoint,
+    amount: CurrencyUnit,
+    scriptPubKey: ScriptPubKey,
+    signers: Seq[Sign],
+    hashType: HashType)
+    extends BitcoinUTXOSpendingInfo {
+  override def redeemScriptOpt: Option[ScriptPubKey] = None
 
-
-/** This is the case where we are spending a [[org.bitcoins.core.protocol.script.WitnessScriptPubKeyV0 witness v0 script]]  */
-case class SegwitV0NativeUTXOSpendingInfo(outPoint: TransactionOutPoint,
-                                          amount: CurrencyUnit,
-                                          scriptPubKey: WitnessScriptPubKeyV0,
-                                          signers: Seq[Sign],
-                                          hashType: HashType,
-                                          scriptWitness: ScriptWitnessV0) extends BitcoinUTXOSpendingInfo
-
-/** This is the case were we are attempting to spend a [[org.bitcoins.core.protocol.script.P2SHScriptPubKey p2sh spk]] */
-case class P2SHSpendingInfo(outPoint: TransactionOutPoint,
-                            amount: CurrencyUnit,
-                            scriptPubKey: P2SHScriptPubKey,
-                            signers: Seq[Sign],
-                            hashType: HashType,
-                            redeemScript: ScriptPubKey) extends BitcoinUTXOSpendingInfo {
-  require(P2SHScriptPubKey(redeemScript) == output.scriptPubKey,
-    s"Given redeem script did not match hash in output script, " +
-      s"got=${P2SHScriptPubKey(redeemScript).scriptHash.hex}, " +
-      s"expected=${scriptPubKey.scriptHash.hex}")
+  override def scriptWitnessOpt: Option[ScriptWitnessV0] = None
 }
 
+/** This is the case where we are spending a [[org.bitcoins.core.protocol.script.WitnessScriptPubKeyV0 witness v0 script]]  */
+case class SegwitV0NativeUTXOSpendingInfo(
+    outPoint: TransactionOutPoint,
+    amount: CurrencyUnit,
+    scriptPubKey: WitnessScriptPubKeyV0,
+    signers: Seq[Sign],
+    hashType: HashType,
+    scriptWitness: ScriptWitnessV0)
+    extends BitcoinUTXOSpendingInfo {
+  override def redeemScriptOpt: Option[ScriptPubKey] = None
+
+  override def scriptWitnessOpt: Option[ScriptWitnessV0] = Some(scriptWitness)
+}
+
+/** This is the case where we are spending a [[org.bitcoins.core.protocol.script.WitnessScriptPubKeyV0 witness v0 script]]  */
+case class UnassignedSegwitNativeUTXOSpendingInfo(
+    outPoint: TransactionOutPoint,
+    amount: CurrencyUnit,
+    scriptPubKey: WitnessScriptPubKey,
+    signers: Seq[Sign],
+    hashType: HashType,
+    scriptWitness: ScriptWitness)
+    extends BitcoinUTXOSpendingInfo {
+  override def redeemScriptOpt: Option[ScriptPubKey] = None
+
+  override def scriptWitnessOpt: Option[ScriptWitness] = Some(scriptWitness)
+}
+
+/** This is the case were we are attempting to spend a [[org.bitcoins.core.protocol.script.P2SHScriptPubKey p2sh spk]] */
+case class P2SHSpendingInfo(
+    outPoint: TransactionOutPoint,
+    amount: CurrencyUnit,
+    scriptPubKey: P2SHScriptPubKey,
+    signers: Seq[Sign],
+    hashType: HashType,
+    redeemScript: ScriptPubKey)
+    extends BitcoinUTXOSpendingInfo {
+  /*require(
+    P2SHScriptPubKey(redeemScript) == output.scriptPubKey,
+    s"Given redeem script did not match hash in output script, " +
+      s"got=${P2SHScriptPubKey(redeemScript).scriptHash.hex}, " +
+      s"expected=${scriptPubKey.scriptHash.hex}"
+  )*/
+
+  override def redeemScriptOpt: Option[ScriptPubKey] = Some(redeemScript)
+
+  override def scriptWitnessOpt: Option[ScriptWitnessV0] = None
+}
 
 /** This is for the case we are spending a p2sh(p2w{pkh,sh}) script. This means that
   * we have nested a [[org.bitcoins.core.protocol.script.WitnessScriptPubKeyV0 witness spk]]
   * inside of a [[org.bitcoins.core.protocol.script.P2SHScriptPubKey p2sh spk]] */
-case class P2SHNestedSegwitV0UTXOSpendingInfo(outPoint: TransactionOutPoint,
-                                              amount: CurrencyUnit,
-                                                scriptPubKey: P2SHScriptPubKey,
-                                               signers: Seq[Sign],
-                                               hashType: HashType,
-                                               redeemScript: WitnessScriptPubKeyV0,
-                                               scriptWitness: ScriptWitnessV0
-) extends BitcoinUTXOSpendingInfo {
-  require(P2SHScriptPubKey(redeemScript) == output.scriptPubKey,
+case class P2SHNestedSegwitV0UTXOSpendingInfo(
+    outPoint: TransactionOutPoint,
+    amount: CurrencyUnit,
+    scriptPubKey: P2SHScriptPubKey,
+    signers: Seq[Sign],
+    hashType: HashType,
+    redeemScript: WitnessScriptPubKeyV0,
+    scriptWitness: ScriptWitnessV0)
+    extends BitcoinUTXOSpendingInfo {
+  /*require(
+    P2SHScriptPubKey(redeemScript) == output.scriptPubKey,
     s"Given redeem script did not match hash in output script, " +
       s"got=${P2SHScriptPubKey(redeemScript).scriptHash.hex}, " +
-      s"expected=${scriptPubKey.scriptHash.hex}")
+      s"expected=${scriptPubKey.scriptHash.hex}"
+  )
+  redeemScript match {
+    case p2wpkh: P2WPKHWitnessSPKV0 =>
+      scriptWitness match {
+        case witness: P2WPKHWitnessV0 =>
+          require(
+            CryptoUtil.sha256Hash160(witness.pubKey.bytes) == p2wpkh.pubKeyHash,
+            "P2WPKH witness public key must match SPK hash.")
+        case _: ScriptWitnessV0 =>
+          throw new IllegalArgumentException(
+            "P2WPKH ScriptPubKey must have P2WPKH witness.")
+      }
+    case p2wsh: P2WSHWitnessSPKV0 =>
+      scriptWitness match {
+        case witness: P2WSHWitnessV0 =>
+          require(CryptoUtil
+                    .sha256(witness.redeemScript.asmBytes) == p2wsh.scriptHash,
+                  "P2WSH witness script must match SPK hash.")
+        case _: ScriptWitnessV0 =>
+          throw new IllegalArgumentException(
+            "P2WSH ScriptPubKey must have P2WSH witness."
+          )
+      }
+  }*/
 
-  //can we put an invariant here with scriptWitness and the witness spk redeemScript?
+  override def redeemScriptOpt: Option[ScriptPubKey] = Some(redeemScript)
+
+  override def scriptWitnessOpt: Option[ScriptWitnessV0] = Some(scriptWitness)
 }
