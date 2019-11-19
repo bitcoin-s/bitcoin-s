b94230cb7449961fff06781a338309890a867724
nkohen <nadavk25@gmail.com>
2019-11-18 18:07:20 -0600

    Created BinaryOutcomeWithSelf, tests don't pass

---
+++ dlc/src/main/scala/org/bitcoins/dlc/BinaryOutcomeDLCWithSelf.scala
@@ -0,0 +1,289 @@
+package org.bitcoins.dlc
+
+import org.bitcoins.core.config.BitcoinNetwork
+import org.bitcoins.core.crypto.{
+  ECPrivateKey,
+  ECPublicKey,
+  Schnorr,
+  SchnorrDigitalSignature
+}
+import org.bitcoins.core.currency.{CurrencyUnit, Satoshis}
+import org.bitcoins.core.number.UInt32
+import org.bitcoins.core.protocol.script.{
+  CLTVScriptPubKey,
+  ConditionalScriptPubKey,
+  MultiSignatureScriptPubKey,
+  MultiSignatureWithTimeoutScriptPubKey,
+  P2PKHScriptPubKey,
+  ScriptPubKey
+}
+import org.bitcoins.core.protocol.transaction.{
+  Transaction,
+  TransactionOutPoint,
+  TransactionOutput
+}
+import org.bitcoins.core.script.constant.ScriptNumber
+import org.bitcoins.core.script.crypto.HashType
+import org.bitcoins.core.util.CryptoUtil
+import org.bitcoins.core.wallet.builder.BitcoinTxBuilder
+import org.bitcoins.core.wallet.fee.FeeUnit
+import org.bitcoins.core.wallet.utxo.{
+  BitcoinUTXOSpendingInfo,
+  ConditionalPath,
+  ConditionalSpendingInfo,
+  MultiSignatureSpendingInfo
+}
+import scodec.bits.ByteVector
+
+import scala.concurrent.{ExecutionContext, Future}
+
+// Two selfs: Local, Remote
+// Two outcomes: Win, Lose
+case class BinaryOutcomeDLCWithSelf(
+    outcomeWin: String,
+    outcomeLose: String,
+    oraclePubKey: ECPublicKey,
+    preCommittedR: ECPublicKey,
+    fundingLocalPrivKey: ECPrivateKey,
+    fundingRemotePrivKey: ECPrivateKey,
+    cetLocalPrivKey: ECPrivateKey,
+    cetRemotePrivKey: ECPrivateKey,
+    finalLocalPrivKey: ECPrivateKey,
+    finalRemotePrivKey: ECPrivateKey,
+    localInput: CurrencyUnit,
+    remoteInput: CurrencyUnit,
+    localFundingUtxos: Vector[BitcoinUTXOSpendingInfo],
+    remoteFundingUtxos: Vector[BitcoinUTXOSpendingInfo],
+    localWinPayout: CurrencyUnit,
+    remoteWinPayout: CurrencyUnit,
+    localLosePayout: CurrencyUnit,
+    remoteLosePayout: CurrencyUnit,
+    timeout: Int,
+    feeRate: FeeUnit,
+    changeSPK: ScriptPubKey,
+    network: BitcoinNetwork)(implicit ec: ExecutionContext) {
+
+  val messageWin: ByteVector =
+    CryptoUtil.sha256(ByteVector(outcomeWin.getBytes)).flip.bytes
+
+  val messageLose: ByteVector =
+    CryptoUtil.sha256(ByteVector(outcomeLose.getBytes)).flip.bytes
+
+  val sigPubKeyWin: ECPublicKey =
+    Schnorr.computePubKey(messageWin, preCommittedR, oraclePubKey)
+
+  val sigPubKeyLose: ECPublicKey =
+    Schnorr.computePubKey(messageLose, preCommittedR, oraclePubKey)
+
+  private val totalInput = localInput + remoteInput
+  private val fundingUtxos = localFundingUtxos ++ remoteFundingUtxos
+
+  val fundingLocalPubKey: ECPublicKey = fundingLocalPrivKey.publicKey
+  val fundingRemotePubKey: ECPublicKey = fundingRemotePrivKey.publicKey
+
+  val fundingSPK: MultiSignatureScriptPubKey = {
+    MultiSignatureScriptPubKey(2,
+                               Vector(fundingLocalPubKey, fundingRemotePubKey))
+  }
+
+  def createFundingTransaction: Future[Transaction] = {
+    println("CHECKPOINT 0")
+    val output: TransactionOutput =
+      TransactionOutput(totalInput + (Satoshis.one * 350), fundingSPK)
+
+    val outputs: Vector[TransactionOutput] = Vector(output)
+    val txBuilderF: Future[BitcoinTxBuilder] =
+      BitcoinTxBuilder(outputs, fundingUtxos, feeRate, changeSPK, network)
+    println("CHECKPOINT 1")
+    txBuilderF.flatMap(_.sign)
+  }
+
+  def toLocalSPK(
+      sigPubKey: ECPublicKey): MultiSignatureWithTimeoutScriptPubKey = {
+    val multiSig = MultiSignatureScriptPubKey(
+      requiredSigs = 2,
+      pubKeys = Vector(cetLocalPrivKey.publicKey, sigPubKey))
+    val timeoutSPK = CLTVScriptPubKey(
+      locktime = ScriptNumber(timeout),
+      scriptPubKey = P2PKHScriptPubKey(cetRemotePrivKey.publicKey))
+
+    MultiSignatureWithTimeoutScriptPubKey(multiSig, timeoutSPK)
+  }
+
+  def createCETLocal(sigPubKey: ECPublicKey,
+                     fundingSpendingInfo: BitcoinUTXOSpendingInfo,
+                     localPayout: CurrencyUnit,
+                     remotePayout: CurrencyUnit): Future[Transaction] = {
+    println("CHECKPOINT 4")
+    val multiSig = MultiSignatureScriptPubKey(
+      requiredSigs = 2,
+      pubKeys = Vector(cetLocalPrivKey.publicKey, sigPubKey))
+    val timeoutSPK = CLTVScriptPubKey(
+      locktime = ScriptNumber(timeout),
+      scriptPubKey = P2PKHScriptPubKey(cetRemotePrivKey.publicKey))
+
+    val toLocalSPK = MultiSignatureWithTimeoutScriptPubKey(multiSig, timeoutSPK)
+
+    val toLocal: TransactionOutput =
+      TransactionOutput(localPayout, toLocalSPK)
+    val toRemote: TransactionOutput =
+      TransactionOutput(remotePayout,
+                        P2PKHScriptPubKey(cetRemotePrivKey.publicKey))
+
+    val outputs: Vector[TransactionOutput] = Vector(toLocal, toRemote)
+    val txBuilderF =
+      BitcoinTxBuilder(outputs,
+                       Vector(fundingSpendingInfo),
+                       feeRate,
+                       changeSPK,
+                       network)
+
+    println("CHECKPOINT 5")
+    txBuilderF.flatMap(_.sign)
+  }
+
+  def createCETRemote(sigPubKey: ECPublicKey,
+                      fundingSpendingInfo: BitcoinUTXOSpendingInfo,
+                      localPayout: CurrencyUnit,
+                      remotePayout: CurrencyUnit): Future[Transaction] = {
+
+    val multiSig = MultiSignatureScriptPubKey(
+      requiredSigs = 2,
+      pubKeys = Vector(cetRemotePrivKey.publicKey, sigPubKey))
+    val timeoutSPK = CLTVScriptPubKey(
+      locktime = ScriptNumber(timeout),
+      scriptPubKey = P2PKHScriptPubKey(cetLocalPrivKey.publicKey))
+
+    val toLocalSPK = MultiSignatureWithTimeoutScriptPubKey(multiSig, timeoutSPK)
+
+    val toLocal: TransactionOutput =
+      TransactionOutput(remotePayout, toLocalSPK)
+    val toRemote: TransactionOutput =
+      TransactionOutput(localPayout,
+                        P2PKHScriptPubKey(cetLocalPrivKey.publicKey))
+
+    val outputs: Vector[TransactionOutput] = Vector(toLocal, toRemote)
+    val txBuilderF =
+      BitcoinTxBuilder(outputs,
+                       Vector(fundingSpendingInfo),
+                       feeRate,
+                       changeSPK,
+                       network)
+
+    txBuilderF.flatMap(_.sign)
+  }
+
+  def createCETWinLocal(
+      fundingSpendingInfo: BitcoinUTXOSpendingInfo): Future[Transaction] = {
+    createCETLocal(
+      sigPubKey = sigPubKeyWin,
+      fundingSpendingInfo = fundingSpendingInfo,
+      localPayout = localWinPayout,
+      remotePayout = remoteWinPayout
+    )
+  }
+
+  def createCETLoseLocal(
+      fundingSpendingInfo: BitcoinUTXOSpendingInfo): Future[Transaction] = {
+    createCETLocal(
+      sigPubKey = sigPubKeyLose,
+      fundingSpendingInfo = fundingSpendingInfo,
+      localPayout = localLosePayout,
+      remotePayout = remoteLosePayout
+    )
+  }
+
+  def createCETWinRemote(
+      fundingSpendingInfo: BitcoinUTXOSpendingInfo): Future[Transaction] = {
+    createCETRemote(
+      sigPubKey = sigPubKeyWin,
+      fundingSpendingInfo = fundingSpendingInfo,
+      localPayout = localWinPayout,
+      remotePayout = remoteWinPayout
+    )
+  }
+
+  def createCETLoseRemote(
+      fundingSpendingInfo: BitcoinUTXOSpendingInfo): Future[Transaction] = {
+    createCETRemote(
+      sigPubKey = sigPubKeyLose,
+      fundingSpendingInfo = fundingSpendingInfo,
+      localPayout = localLosePayout,
+      remotePayout = remoteLosePayout
+    )
+  }
+
+  def executeDLC(
+      oracleSigF: Future[SchnorrDigitalSignature]): Future[Transaction] = {
+    createFundingTransaction.flatMap { fundingTx =>
+      println("CHECKPOINT 2")
+      println(s"Funding Transaction: ${fundingTx.hex}\n")
+
+      val fundingTxId = fundingTx.txIdBE
+      val output = fundingTx.outputs.head
+      val fundingSpendingInfo = MultiSignatureSpendingInfo(
+        outPoint = TransactionOutPoint(fundingTxId, UInt32.zero),
+        amount = output.value,
+        scriptPubKey =
+          output.scriptPubKey.asInstanceOf[MultiSignatureScriptPubKey],
+        signers = Vector(fundingLocalPrivKey, fundingRemotePrivKey),
+        hashType = HashType.sigHashAll
+      )
+
+      println("CHECKPOINT 3")
+      val cetWinLocalF = createCETWinLocal(fundingSpendingInfo)
+      val cetLoseLocalF = createCETLoseLocal(fundingSpendingInfo)
+      val cetWinRemoteF = createCETWinRemote(fundingSpendingInfo)
+      val cetLoseRemoteF = createCETLoseRemote(fundingSpendingInfo)
+
+      cetWinLocalF.foreach(cet => println(s"CET Win Local: ${cet.hex}\n"))
+      cetLoseLocalF.foreach(cet => println(s"CET Lose Local: ${cet.hex}\n"))
+      cetWinRemoteF.foreach(cet => println(s"CET Win Remote: ${cet.hex}\n"))
+      cetLoseRemoteF.foreach(cet => println(s"CET Lose Remote: ${cet.hex}\n"))
+
+      // Publish funding tx
+
+      oracleSigF.flatMap { oracleSig =>
+        val cetLocalF =
+          if (Schnorr.verify(messageWin, oracleSig, oraclePubKey)) {
+            cetWinLocalF
+          } else if (Schnorr.verify(messageLose, oracleSig, oraclePubKey)) {
+            cetLoseLocalF
+          } else {
+            Future.failed(???)
+          }
+
+        cetLocalF.flatMap { cet =>
+          val output = cet.outputs.head
+          val cetSpendingInfo = ConditionalSpendingInfo(
+            TransactionOutPoint(cet.txIdBE, UInt32.zero),
+            output.value,
+            output.scriptPubKey.asInstanceOf[ConditionalScriptPubKey],
+            Vector(cetLocalPrivKey, ECPrivateKey(oracleSig.s)),
+            HashType.sigHashAll,
+            ConditionalPath.nonNestedTrue
+          )
+
+          val txBuilder = BitcoinTxBuilder(
+            Vector(
+              TransactionOutput(
+                localWinPayout - (Satoshis.one * 232),
+                P2PKHScriptPubKey(finalLocalPrivKey.publicKey))),
+            Vector(cetSpendingInfo),
+            feeRate,
+            changeSPK,
+            network
+          )
+
+          val spendingTxF = txBuilder.flatMap(_.sign)
+
+          spendingTxF.foreach(tx => println(s"Closing Tx: ${tx.hex}"))
+
+          // Publish tx
+
+          spendingTxF
+        }
+      }
+    }
+  }
+}
---
+++ dlc/src/test/scala/org/bitcoins/dlc/BinaryOutcomeDLCWithSelfTest.scala
@@ -0,0 +1,106 @@
+package org.bitcoins.dlc
+
+import org.bitcoins.core.config.RegTest
+import org.bitcoins.core.crypto.{DoubleSha256DigestBE, ECPrivateKey, Schnorr}
+import org.bitcoins.core.currency.{CurrencyUnits, Satoshis}
+import org.bitcoins.core.number.UInt32
+import org.bitcoins.core.protocol.script.P2PKHScriptPubKey
+import org.bitcoins.core.protocol.transaction.TransactionOutPoint
+import org.bitcoins.core.script.crypto.HashType
+import org.bitcoins.core.util.CryptoUtil
+import org.bitcoins.core.wallet.fee.SatoshisPerByte
+import org.bitcoins.core.wallet.utxo.P2PKHSpendingInfo
+import org.bitcoins.testkit.util.BitcoinSAsyncTest
+import scodec.bits.ByteVector
+
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+
+class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
+  behavior of "BinaryOutcomeDLCWithSelf"
+
+  it should "work" in {
+    val outcomeWin = "WIN"
+    val outcomeWinHash = CryptoUtil.sha256(ByteVector(outcomeWin.getBytes)).flip
+    val outcomeLose = "LOSE"
+    val outcomeLoseHash =
+      CryptoUtil.sha256(ByteVector(outcomeLose.getBytes)).flip
+    val oraclePrivKey = ECPrivateKey.freshPrivateKey
+    val oraclePubKey = oraclePrivKey.publicKey
+    val preCommittedK = ECPrivateKey.freshPrivateKey
+    val preCommittedR = preCommittedK.publicKey
+    val fundingLocalPrivKey = ECPrivateKey.freshPrivateKey
+    val fundingRemotePrivKey = ECPrivateKey.freshPrivateKey
+    val cetLocalPrivKey = ECPrivateKey.freshPrivateKey
+    val cetRemotePrivKey = ECPrivateKey.freshPrivateKey
+    val finalLocalPrivKey = ECPrivateKey.freshPrivateKey
+    val finalRemotePrivKey = ECPrivateKey.freshPrivateKey
+    val localInput = CurrencyUnits.oneBTC
+    val remoteInput = CurrencyUnits.oneBTC
+
+    val inputPrivKeyLocal = ECPrivateKey.freshPrivateKey
+    val inputPubKeyLocal = inputPrivKeyLocal.publicKey
+    val inputPrivKeyRemote = ECPrivateKey.freshPrivateKey
+    val inputPubKeyRemote = inputPrivKeyRemote.publicKey
+
+    val localFundingUtxos = Vector(
+      P2PKHSpendingInfo(
+        outPoint = TransactionOutPoint(DoubleSha256DigestBE.empty, UInt32.zero),
+        amount = localInput + (Satoshis.one * 375),
+        scriptPubKey = P2PKHScriptPubKey(inputPubKeyLocal),
+        signer = inputPrivKeyLocal,
+        hashType = HashType.sigHashAll
+      )
+    )
+
+    val remoteFundingUtxos = Vector(
+      P2PKHSpendingInfo(
+        outPoint = TransactionOutPoint(DoubleSha256DigestBE.empty, UInt32.zero),
+        amount = remoteInput + (Satoshis.one * 375),
+        scriptPubKey = P2PKHScriptPubKey(inputPubKeyRemote),
+        signer = inputPrivKeyRemote,
+        hashType = HashType.sigHashAll
+      )
+    )
+
+    val localWinPayout = localInput + CurrencyUnits.oneMBTC
+    val remoteWinPayout = remoteInput - CurrencyUnits.oneMBTC
+    val localLosePayout = localInput - CurrencyUnits.oneMBTC
+    val remoteLosePayout = remoteInput + CurrencyUnits.oneMBTC
+    val timeout = 1.day.toMillis.toInt
+    val feeRate = SatoshisPerByte(Satoshis.one)
+    val changePrivKey = ECPrivateKey.freshPrivateKey
+    val changePubKey = changePrivKey.publicKey
+    val changeSPK = P2PKHScriptPubKey(changePubKey)
+    val network = RegTest
+
+    val oracleSig = Schnorr.sign(outcomeWinHash.bytes, oraclePrivKey)
+
+    val dlc = BinaryOutcomeDLCWithSelf(
+      outcomeWin,
+      outcomeLose,
+      oraclePubKey,
+      preCommittedR,
+      fundingLocalPrivKey,
+      fundingRemotePrivKey,
+      cetLocalPrivKey,
+      cetRemotePrivKey,
+      finalLocalPrivKey,
+      finalRemotePrivKey,
+      localInput,
+      remoteInput,
+      localFundingUtxos,
+      remoteFundingUtxos,
+      localWinPayout,
+      remoteWinPayout,
+      localLosePayout,
+      remoteLosePayout,
+      timeout,
+      feeRate,
+      changeSPK,
+      network
+    )
+
+    dlc.executeDLC(Future.successful(oracleSig)).map(_ => succeed)
+  }
+}
