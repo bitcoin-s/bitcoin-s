daea0a0ef2a94e0750937237db8b8657081e4329
Ben Carman <benthecarman@live.com>
2019-11-20 10:07:00 -0600

    bitcoind v19 Update mempool RPCs and tests (#868)
    * Update mempool RPC calls to bitcoind v19 compatibility
    
    * Typo fix
    
    * Add parameter name to calls
    
    * Fix remaining rpc calls
    
    * Formatting
    
    * scaladoc for param
    
    * Change param to correct type
    
    * Clarify on scaladoc

--- testkit/src/main/scala/org/bitcoins/testkit/rpc/BitcoindRpcTestUtil.scala
+++ testkit/src/main/scala/org/bitcoins/testkit/rpc/BitcoindRpcTestUtil.scala
@@ -819,9 +819,9 @@ trait BitcoindRpcTestUtil extends BitcoinSLogger {
             v16.signRawTransaction(transaction, utxoDeps)
           case (_, Success(v17), _, _) =>
             v17.signRawTransactionWithWallet(transaction, utxoDeps)
-          case (_, _, Success(v18),_) =>
+          case (_, _, Success(v18), _) =>
             v18.signRawTransactionWithWallet(transaction, utxoDeps)
-          case (_, _,_,Success(v19)) =>
+          case (_, _, _, Success(v19)) =>
             v19.signRawTransactionWithWallet(transaction, utxoDeps)
         }
     }
@@ -863,8 +863,7 @@ trait BitcoindRpcTestUtil extends BitcoinSLogger {
       addr <- sender.getNewAddress
       _ <- sender.generateToAddress(100, addr)
       // Can't spend coinbase until depth 100
-      transactionHash <- sender.sendRawTransaction(signedtx.hex,
-                                                   allowHighFees = true)
+      transactionHash <- sender.sendRawTransaction(signedtx.hex, maxfeerate = 0)
       transaction <- sender.getTransaction(transactionHash)
     } yield transaction
   }
