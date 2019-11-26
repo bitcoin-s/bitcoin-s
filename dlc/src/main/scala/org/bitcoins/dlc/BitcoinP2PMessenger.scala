1ec4aeda85ba71217540cfdfad0f7482b3cdd033
Nadav Kohen <nadavk25@gmail.com>
2019-11-26 09:10:42 -0700

    Regtest Binary DLC (#886)
    * Introduced bitcoind regtest test for binary outcome DLC. Test currently fails when publishing CET
    
    * Fixed the regtest test!
    
    * Actually fixed it this time
    
    * Added coverage for all four cases on regtest
    
    * Made test name more descriptive
    
    * Created a BitcoinP2PMessenger interface so that DLC source code does not depend on RPC clients
    
    * Fixed things after the rebase

---
+++ dlc/src/main/scala/org/bitcoins/dlc/BitcoinP2PMessenger.scala
@@ -0,0 +1,34 @@
+package org.bitcoins.dlc
+
+import org.bitcoins.core.protocol.BitcoinAddress
+import org.bitcoins.core.protocol.transaction.Transaction
+import org.bitcoins.rpc.client.common.BitcoindRpcClient
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/** DLC client uses this to communicate transactions to the network and receive confirmations. */
+trait BitcoinP2PMessenger {
+
+  /** Sends the input Transaction to the Bitcoin network. */
+  def sendTransaction(tx: Transaction): Future[Unit]
+
+  /** Returns a Future that completes when the specified number of blocks have been mined. */
+  def waitForConfirmations(blocks: Int): Future[Unit]
+}
+
+/** For use in testing DLC client with Regtest bitcoind node. */
+case class BitcoindRpcMessengerRegtest(client: BitcoindRpcClient)(
+    implicit ec: ExecutionContext)
+    extends BitcoinP2PMessenger {
+  private val addressForMiningF: Future[BitcoinAddress] = client.getNewAddress
+
+  override def sendTransaction(tx: Transaction): Future[Unit] = {
+    client.sendRawTransaction(tx).map(_ => ())
+  }
+
+  override def waitForConfirmations(blocks: Int): Future[Unit] = {
+    addressForMiningF.flatMap { addressForMining =>
+      client.generateToAddress(blocks, addressForMining).map(_ => ())
+    }
+  }
+}
