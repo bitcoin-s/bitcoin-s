8edf55944fc190bd0814d0f0b0ad611eb0815e15
Nadav Kohen <nadavk25@gmail.com>
2019-11-27 07:17:16 -0700

    DLC Fee distribution (#892)
    * Fees are now evenly distributed amongst parties
    
    * Fixed scaladoc
    
    * Added property test for fee subtraction
    
    * Fixed comparison

--- dlc/src/test/scala/org/bitcoins/dlc/BinaryOutcomeDLCWithSelfTest.scala
+++ dlc/src/test/scala/org/bitcoins/dlc/BinaryOutcomeDLCWithSelfTest.scala
@@ -1,6 +1,6 @@
 package org.bitcoins.dlc
 
-import org.bitcoins.core.config.RegTest
+import org.bitcoins.core.config.{BitcoinNetwork, RegTest}
 import org.bitcoins.core.crypto.{
   DoubleSha256DigestBE,
   ECPrivateKey,
@@ -9,22 +9,92 @@ import org.bitcoins.core.crypto.{
   Sha256DigestBE
 }
 import org.bitcoins.core.currency.{CurrencyUnits, Satoshis}
-import org.bitcoins.core.number.UInt32
-import org.bitcoins.core.protocol.script.P2PKHScriptPubKey
-import org.bitcoins.core.protocol.transaction.TransactionOutPoint
+import org.bitcoins.core.number.{Int64, UInt32}
+import org.bitcoins.core.protocol.script.{EmptyScriptPubKey, P2PKHScriptPubKey}
+import org.bitcoins.core.protocol.transaction.{
+  TransactionOutPoint,
+  TransactionOutput
+}
 import org.bitcoins.core.script.crypto.HashType
 import org.bitcoins.core.util.{BitcoinScriptUtil, CryptoUtil}
-import org.bitcoins.core.wallet.fee.SatoshisPerByte
-import org.bitcoins.core.wallet.utxo.P2PKHSpendingInfo
+import org.bitcoins.core.wallet.builder.{BitcoinTxBuilder, TxBuilder}
+import org.bitcoins.core.wallet.fee.{FeeUnit, SatoshisPerByte}
+import org.bitcoins.core.wallet.utxo.{
+  BitcoinUTXOSpendingInfo,
+  P2PKHSpendingInfo
+}
+import org.bitcoins.testkit.core.gen.{
+  CurrencyUnitGenerator,
+  TransactionGenerators
+}
 import org.bitcoins.testkit.util.BitcoinSAsyncTest
+import org.scalacheck.Gen
 import org.scalatest.Assertion
 import scodec.bits.ByteVector
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
 
 class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
   behavior of "BinaryOutcomeDLCWithSelf"
+
+  it should "correctly subtract fees evenly amongst outputs" in {
+    // Can't use TransactionGenerators.realisiticOutputs as that can return List.empty
+    val nonEmptyRealisticOutputsGen = Gen
+      .choose(1, 5)
+      .flatMap(n => Gen.listOfN(n, TransactionGenerators.realisticOutput))
+      .suchThat(_.nonEmpty)
+
+    // CurrencyUnitGenerator.feeRate gives too high of fees
+    val feeRateGen = Gen.choose(0, CurrencyUnits.oneBTC.satoshis.toLong).map {
+      n =>
+        SatoshisPerByte(Satoshis(Int64(n)))
+    }
+
+    forAll(nonEmptyRealisticOutputsGen, feeRateGen) {
+      case (outputs, feeRate) =>
+        val totalInput = outputs.foldLeft(CurrencyUnits.zero) {
+          case (accum, output) =>
+            accum + output.value
+        }
+
+        val inputKey = ECPrivateKey.freshPrivateKey
+        val utxos: Vector[BitcoinUTXOSpendingInfo] = Vector(
+          P2PKHSpendingInfo(TransactionOutPoint(DoubleSha256DigestBE.empty,
+                                                UInt32.zero),
+                            totalInput,
+                            P2PKHScriptPubKey(inputKey.publicKey),
+                            inputKey,
+                            HashType.sigHashAll))
+        val changeSPK = EmptyScriptPubKey
+        val network: BitcoinNetwork = RegTest
+
+        val txBuilderF =
+          BitcoinTxBuilder(outputs, utxos, feeRate, changeSPK, network)
+
+        val badFeeF = txBuilderF.flatMap { txBuilder =>
+          recoverToSucceededIf[IllegalArgumentException](txBuilder.sign)
+        }
+
+        val resultF = for {
+          txBuilder <- txBuilderF
+          _ <- badFeeF
+          tx <- BinaryOutcomeDLCWithSelf.subtractFeeAndSign(txBuilder)
+        } yield {
+          val diffs = outputs.zip(tx.outputs).map {
+            case (before, after) =>
+              before.value - after.value
+          }
+
+          val firstDiff = diffs.head
+          // Fee has been evenly distributed (up to some remainder)
+          assert(diffs.forall(diff =>
+            diff - firstDiff < Satoshis(Int64(diffs.length))))
+        }
+
+        Await.result(resultF, 5.seconds)
+    }
+  }
 
   it should "be able to construct and verify with ScriptInterpreter every tx in a DLC for every case" in {
     val outcomeWin = "WIN"
@@ -68,9 +138,8 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
     val changePubKey = changePrivKey.publicKey
     val changeSPK = P2PKHScriptPubKey(changePubKey)
 
-    def executeForCase(
-        outcomeHash: Sha256DigestBE,
-        local: Boolean): Future[Assertion] = {
+    def executeForCase(outcomeHash: Sha256DigestBE,
+                       local: Boolean): Future[Assertion] = {
       val oracleSig =
         Schnorr.signWithNonce(outcomeHash.bytes, oraclePrivKey, preCommittedK)
 
