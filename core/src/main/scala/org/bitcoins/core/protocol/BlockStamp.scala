65c645c4e2ea986705b4a12de53ef4653a5dc4e8
Nadav Kohen <nadavk25@gmail.com>
2019-12-05 11:42:24 -0700

    DLC Refund Transaction Test (#915)
    * Implemented using BlockStamp in BinaryOutcomeDLC
    
    Implemented unclean but working refund execution case with positive tests that pass
    
    Expanded DLCOutcome to include remote's closing tx. Added tests for this
    
    Factored out comanalities between DLC execution cases
    
    * Added BlockStampWithFuture.toScriptNumber and separated setupDLC from executeDLC
    
    * scalafmt

--- core/src/main/scala/org/bitcoins/core/protocol/BlockStamp.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/BlockStamp.scala
@@ -6,12 +6,19 @@ import java.time.temporal.{ChronoField, TemporalAccessor}
 
 import org.bitcoins.core.crypto.DoubleSha256DigestBE
 import org.bitcoins.core.number.UInt32
+import org.bitcoins.core.script.constant.ScriptNumber
 
 import scala.util.{Failure, Try}
 
 /** This trait represents a point on blockchain, and is used to specify block ranges */
 sealed trait BlockStamp {
   def mkString: String
+}
+
+/** This trait represents a point on the blockchain, including future points */
+sealed trait BlockStampWithFuture extends BlockStamp {
+  def toUInt32: UInt32
+  def toScriptNumber: ScriptNumber
 }
 
 object BlockStamp {
@@ -21,14 +28,32 @@ object BlockStamp {
   case class BlockHash(hash: DoubleSha256DigestBE) extends BlockStamp {
     override def mkString: String = hash.hex
   }
-  case class BlockHeight(height: Int) extends BlockStamp {
+  case class BlockHeight(height: Int) extends BlockStampWithFuture {
     require(height >= 0, "block height must be a positive number")
     override def mkString: String = height.toString
+    override def toUInt32: UInt32 = UInt32(height)
+    override def toScriptNumber: ScriptNumber = ScriptNumber(height)
   }
-  case class BlockTime(time: UInt32) extends BlockStamp {
+  case class BlockTime(time: UInt32) extends BlockStampWithFuture {
     override def mkString: String = {
       val instant = Instant.ofEpochSecond(time.toLong)
       DateTimeFormatter.ISO_INSTANT.format(instant)
+    }
+    override def toUInt32: UInt32 = time
+    override def toScriptNumber: ScriptNumber = ScriptNumber(time.toLong)
+  }
+
+  /** This is Tue Nov  5 00:53:20 1985 UTC
+    * @see [[https://github.com/bitcoin/bitcoin/blob/master/src/script/script.h#L39]]
+    */
+  final val LOCKTIME_THRESHOLD: Int = 500000000
+
+  /** @see [[https://github.com/bitcoin/bips/blob/master/bip-0065.mediawiki#detailed-specification]] */
+  def apply(timelockNumber: Int): BlockStampWithFuture = {
+    if (timelockNumber < LOCKTIME_THRESHOLD) {
+      BlockHeight(timelockNumber)
+    } else {
+      BlockTime(UInt32(timelockNumber))
     }
   }
 
