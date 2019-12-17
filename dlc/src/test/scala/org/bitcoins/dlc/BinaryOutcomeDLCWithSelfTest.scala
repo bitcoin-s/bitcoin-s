e768f6f942a62dd1f2474da395314d3f8b0921fe
Nadav Kohen <nadavk25@gmail.com>
2019-12-17 12:06:40 -0700

    DLC: Two Change SPKs (#950)
    * Added change spk so both local and remote have one
    
    * Added invariant that no EmptyScriptPubKeys are allowed and added assertions for this in tests

--- dlc/src/test/scala/org/bitcoins/dlc/BinaryOutcomeDLCWithSelfTest.scala
+++ dlc/src/test/scala/org/bitcoins/dlc/BinaryOutcomeDLCWithSelfTest.scala
@@ -15,7 +15,7 @@ import org.bitcoins.core.currency.{CurrencyUnit, CurrencyUnits, Satoshis}
 import org.bitcoins.core.number.{Int64, UInt32}
 import org.bitcoins.core.protocol.BlockStamp.BlockTime
 import org.bitcoins.core.protocol.script.{EmptyScriptPubKey, P2PKHScriptPubKey}
-import org.bitcoins.core.protocol.transaction.TransactionOutPoint
+import org.bitcoins.core.protocol.transaction.{Transaction, TransactionOutPoint}
 import org.bitcoins.core.script.crypto.HashType
 import org.bitcoins.core.util.{BitcoinScriptUtil, CryptoUtil}
 import org.bitcoins.core.wallet.builder.BitcoinTxBuilder
@@ -24,7 +24,7 @@ import org.bitcoins.core.wallet.utxo.{
   BitcoinUTXOSpendingInfo,
   P2PKHSpendingInfo
 }
-import org.bitcoins.testkit.core.gen.TransactionGenerators
+import org.bitcoins.testkit.core.gen.{ScriptGenerators, TransactionGenerators}
 import org.bitcoins.testkit.util.BitcoinSAsyncTest
 import org.scalacheck.Gen
 import org.scalatest.Assertion
@@ -36,10 +36,14 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
   behavior of "BinaryOutcomeDLCWithSelf"
 
   it should "correctly subtract fees evenly amongst outputs" in {
+    // subtractFeeAndSign has an invariant that no EmptyScriptPubKeys are allowed
+    val realisticNonEmptyGen = TransactionGenerators.realisticOutput.suchThat(
+      _.scriptPubKey != EmptyScriptPubKey)
+
     // Can't use TransactionGenerators.realisiticOutputs as that can return List.empty
     val nonEmptyRealisticOutputsGen = Gen
       .choose(1, 5)
-      .flatMap(n => Gen.listOfN(n, TransactionGenerators.realisticOutput))
+      .flatMap(n => Gen.listOfN(n, realisticNonEmptyGen))
       .suchThat(_.nonEmpty)
 
     // CurrencyUnitGenerator.feeRate gives too high of fees
@@ -48,8 +52,10 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
         SatoshisPerByte(Satoshis(Int64(n)))
     }
 
-    forAllAsync(nonEmptyRealisticOutputsGen, feeRateGen) {
-      case (outputs, feeRate) =>
+    forAllAsync(nonEmptyRealisticOutputsGen,
+                feeRateGen,
+                ScriptGenerators.p2pkhScriptPubKey) {
+      case (outputs, feeRate, (changeSPK, _)) =>
         val totalInput = outputs.foldLeft(CurrencyUnits.zero) {
           case (accum, output) =>
             accum + output.value
@@ -65,7 +71,6 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
             signer = inputKey,
             hashType = HashType.sigHashAll
           ))
-        val changeSPK = EmptyScriptPubKey
         val network: BitcoinNetwork = RegTest
 
         val txBuilderF =
@@ -119,7 +124,7 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
   val localFundingUtxos = Vector(
     P2PKHSpendingInfo(
       outPoint = TransactionOutPoint(DoubleSha256DigestBE.empty, UInt32.zero),
-      amount = localInput,
+      amount = localInput * 2,
       scriptPubKey = P2PKHScriptPubKey(inputPubKeyLocal),
       signer = inputPrivKeyLocal,
       hashType = HashType.sigHashAll
@@ -129,16 +134,18 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
   val remoteFundingUtxos = Vector(
     P2PKHSpendingInfo(
       outPoint = TransactionOutPoint(DoubleSha256DigestBE.empty, UInt32.one),
-      amount = remoteInput,
+      amount = remoteInput * 2,
       scriptPubKey = P2PKHScriptPubKey(inputPubKeyRemote),
       signer = inputPrivKeyRemote,
       hashType = HashType.sigHashAll
     )
   )
 
-  val changePrivKey: ECPrivateKey = ECPrivateKey.freshPrivateKey
-  val changePubKey: ECPublicKey = changePrivKey.publicKey
-  val changeSPK: P2PKHScriptPubKey = P2PKHScriptPubKey(changePubKey)
+  val localChangeSPK: P2PKHScriptPubKey = P2PKHScriptPubKey(
+    ECPublicKey.freshPublicKey)
+
+  val remoteChangeSPK: P2PKHScriptPubKey = P2PKHScriptPubKey(
+    ECPublicKey.freshPublicKey)
 
   val dlc: BinaryOutcomeDLCWithSelf = BinaryOutcomeDLCWithSelf(
     outcomeWin = outcomeWin,
@@ -155,9 +162,14 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
     localLosePayout = localInput - CurrencyUnits.oneMBTC,
     timeout = blockTimeToday,
     feeRate = SatoshisPerByte(Satoshis.one),
-    changeSPK = changeSPK,
+    localChangeSPK = localChangeSPK,
+    remoteChangeSPK = remoteChangeSPK,
     network = RegTest
   )
+
+  def noEmptySPKOutputs(tx: Transaction): Boolean = {
+    tx.outputs.forall(_.scriptPubKey != EmptyScriptPubKey)
+  }
 
   def validateOutcome(outcome: DLCOutcome): Assertion = {
     val DLCOutcome(fundingTx,
@@ -168,6 +180,11 @@ class BinaryOutcomeDLCWithSelfTest extends BitcoinSAsyncTest {
                    fundingSpendingInfo,
                    localCetSpendingInfo,
                    remoteCetSpendingInfo) = outcome
+
+    assert(noEmptySPKOutputs(fundingTx))
+    assert(noEmptySPKOutputs(cet))
+    assert(noEmptySPKOutputs(localClosingTx))
+    assert(noEmptySPKOutputs(remoteClosingTx))
 
     assert(
       BitcoinScriptUtil.verifyScript(fundingTx, initialSpendingInfos)
