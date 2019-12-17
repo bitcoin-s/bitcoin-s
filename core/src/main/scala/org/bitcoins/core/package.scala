0d6d26e4e6ed1d1246492f96ab447491554f9f50
Ben Carman <benthecarman@live.com>
2019-12-17 07:59:43 -0600

    PSBT Updater

---
+++ core/src/main/scala/org/bitcoins/core/package.scala
@@ -0,0 +1,23 @@
+package org.bitcoins
+
+import scodec.bits._
+import scala.annotation.tailrec
+
+package object core {
+  implicit val byteVectorOrdering: Ordering[ByteVector] =
+    new Ordering[ByteVector] {
+
+      @tailrec
+      override def compare(x: ByteVector, y: ByteVector): Int = {
+        if (x.isEmpty) {
+          -1
+        } else if (y.isEmpty) {
+          1
+        } else if (x.head != y.head) {
+          x.head.compare(y.head)
+        } else {
+          compare(x.tail, y.tail)
+        }
+      }
+    }
+}
