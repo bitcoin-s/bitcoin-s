cd942d646d098f27301f08df0005b17b9a8141e1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-12-17 12:55:15 +0100

    Update slf4j-api to 1.7.30 (#953)

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
     val scalaTest = "3.1.0"
 
     val scalaTestPlus = "3.1.0.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
-    val slf4j = "1.7.29"
+    val slf4j = "1.7.30"
     val spray = "1.3.5"
     val zeromq = "0.5.1"
     val akkav = "10.1.11"
