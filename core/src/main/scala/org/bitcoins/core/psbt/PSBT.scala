940624272946f82a486977ab730b8aa7b904d501
Ben Carman <benthecarman@live.com>
2019-12-18 15:07:14 -0600

    PSBT: Fix for EmptyWitness changes (#968)

--- core/src/main/scala/org/bitcoins/core/psbt/PSBT.scala
+++ core/src/main/scala/org/bitcoins/core/psbt/PSBT.scala
@@ -207,7 +207,7 @@ object GlobalPSBTRecord {
       "All ScriptSignatures must be empty")
 
     private val witnessIsEmpty = transaction match {
-      case wtx: WitnessTransaction => wtx.witness == EmptyWitness
+      case wtx: WitnessTransaction => wtx.witness.isInstanceOf[EmptyWitness]
       case _: BaseTransaction      => true
     }
     require(witnessIsEmpty, "Witness must be empty")
