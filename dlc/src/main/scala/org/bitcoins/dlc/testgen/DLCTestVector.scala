0fc1e18d886b8becd49248e3fe331b1239ecb534
Nadav Kohen <nadavk25@gmail.com>
2020-01-15 14:46:48 -0700

    Reworked test vector code to use JsObjects instead of JsArrays (#1027)

--- dlc/src/main/scala/org/bitcoins/dlc/testgen/DLCTestVector.scala
+++ dlc/src/main/scala/org/bitcoins/dlc/testgen/DLCTestVector.scala
@@ -10,11 +10,16 @@ import org.bitcoins.core.crypto.{
   Sha256DigestBE
 }
 import org.bitcoins.core.currency.{CurrencyUnit, Satoshis}
-import org.bitcoins.core.number.UInt32
-import org.bitcoins.core.protocol.{BlockStamp, BlockStampWithFuture}
+import org.bitcoins.core.number.Int32
+import org.bitcoins.core.protocol.{
+  BlockStamp,
+  BlockStampWithFuture,
+  NetworkElement
+}
 import org.bitcoins.core.protocol.script.{
   ScriptPubKey,
   ScriptWitnessV0,
+  WitnessScriptPubKey,
   WitnessScriptPubKeyV0
 }
 import org.bitcoins.core.protocol.transaction.{
@@ -22,9 +27,15 @@ import org.bitcoins.core.protocol.transaction.{
   TransactionOutPoint,
   TransactionOutput
 }
+import org.bitcoins.core.script.constant.ScriptToken
 import org.bitcoins.core.script.crypto.HashType
 import org.bitcoins.core.serializers.script.RawScriptWitnessParser
-import org.bitcoins.core.util.{BitcoinSUtil, CryptoUtil}
+import org.bitcoins.core.util.{
+  BitcoinSUtil,
+  BitcoinScriptUtil,
+  CryptoUtil,
+  Factory
+}
 import org.bitcoins.core.wallet.fee.SatoshisPerByte
 import org.bitcoins.core.wallet.utxo.{
   ConditionalPath,
@@ -32,13 +43,13 @@ import org.bitcoins.core.wallet.utxo.{
 }
 import org.bitcoins.dlc.BinaryOutcomeDLCWithSelf
 import play.api.libs.json.{
-  JsArray,
+  JsNumber,
   JsResult,
   JsString,
-  JsSuccess,
   JsValue,
   Json,
-  Reads
+  Reads,
+  Writes
 }
 import scodec.bits.ByteVector
 
@@ -136,208 +147,17 @@ case class DLCTestVector(
   }
 
   def toJson: JsValue = {
-    val inputs = Vector(
-      localPayouts,
-      realOutcome,
-      oracleKey.hex,
-      oracleKValue.hex,
-      localExtPrivKey.toString,
-      localInput.satoshis.toLong,
-      localFundingUtxos,
-      localChangeSPK.hex,
-      remoteExtPrivKey.toString,
-      remoteInput.satoshis.toLong,
-      remoteFundingUtxos,
-      remoteChangeSPK.hex,
-      timeout.toUInt32.toLong,
-      feeRate.toLong
-    )
-
-    val localCets = Vector(localWinCet.hex, localLoseCet.hex)
-    val remoteCets = Vector(remoteWinCet.hex, remoteLoseCet.hex)
-
-    val jsOutputs = Vector(
-      JsString(fundingTx.hex),
-      Json.toJson(localCets),
-      Json.toJson(remoteCets),
-      JsString(refundTx.hex),
-      JsString(localClosingTx.hex),
-      JsString(remoteClosingTx.hex)
-    )
-
-    val jsInputs: Vector[JsValue] = inputs.map {
-      case map: Map[_, _] =>
-        val localPayoutsMap = map.asInstanceOf[Map[String, CurrencyUnit]]
-        val underlyingMap = localPayoutsMap.map {
-          case (outcome, payout) => (outcome, payout.satoshis.toLong)
-        }
-        Json.toJson(underlyingMap)
-      case vec: Vector[_] =>
-        val utxos = vec.asInstanceOf[Vector[SegwitV0NativeUTXOSpendingInfo]]
-        val jsUtxos = utxos.map { utxo =>
-          Vector(
-            JsString(utxo.outPoint.hex),
-            JsString(utxo.output.hex),
-            Json.toJson(utxo.signers.map(_.asInstanceOf[ECPrivateKey].hex)),
-            JsString(utxo.hashType.byte.toHexString),
-            JsString(utxo.scriptWitness.hex)
-          )
-        }
-
-        Json.toJson(jsUtxos)
-      case str: String => Json.toJson(str)
-      case num: Long   => Json.toJson(num)
-    }
-
-    Json.toJson(Vector(jsInputs, jsOutputs))
+    SerializedDLCTestVector.fromDLCTestVector(this).toJson
   }
 }
 
 object DLCTestVector {
 
   def fromJson(json: JsValue): JsResult[DLCTestVector] = {
-    import org.bitcoins.rpc.serializers.JsonSerializers._
-
-    def vectorReads[T](fromJson: JsValue => JsResult[T]): Reads[Vector[T]] = {
-      Reads[Vector[T]] { json =>
-        for {
-          arr <- json.validate[JsArray]
-          vec <- arr.value
-            .foldLeft[JsResult[Vector[T]]](JsSuccess(Vector.empty)) {
-              case (jsResultAccum, json) =>
-                jsResultAccum.flatMap { accum =>
-                  fromJson(json).map { t =>
-                    accum :+ t
-                  }
-                }
-            }
-        } yield vec
-      }
-    }
-
-    case class SerializedSegwitSpendingInfo(
-        outPoint: TransactionOutPoint,
-        output: TransactionOutput,
-        signers: Vector[ECPrivateKey],
-        hashType: HashType,
-        scriptWitness: ScriptWitnessV0) {
-      def toSpendingInfo: SegwitV0NativeUTXOSpendingInfo = {
-        SegwitV0NativeUTXOSpendingInfo(
-          outPoint = outPoint,
-          amount = output.value,
-          scriptPubKey = output.scriptPubKey.asInstanceOf[WitnessScriptPubKeyV0],
-          signers = signers,
-          hashType = hashType,
-          scriptWitness = scriptWitness,
-          conditionalPath = ConditionalPath.NoConditionsLeft
-        )
-      }
-    }
-
-    def serializedSegwitSpendingInfoFromJson(
-        json: JsValue): JsResult[SerializedSegwitSpendingInfo] = {
-      for {
-        arr <- json.validate[JsArray]
-        seq = arr.value
-        outPointStr <- seq.head.validate[String]
-        outPoint = TransactionOutPoint.fromHex(outPointStr)
-        outputStr <- seq(1).validate[String]
-        output = TransactionOutput.fromHex(outputStr)
-        signers <- seq(2).validate[Vector[ECPrivateKey]](
-          vectorReads(_.validate[String].map(ECPrivateKey.fromHex)))
-        hashTypeStr <- seq(3).validate[String]
-        hashType = HashType.fromHex(hashTypeStr)
-        scriptWitnessStr <- seq(4).validate[String]
-        scriptWitness = RawScriptWitnessParser
-          .read(scriptWitnessStr)
-          .asInstanceOf[ScriptWitnessV0]
-      } yield {
-        SerializedSegwitSpendingInfo(outPoint,
-                                     output,
-                                     signers,
-                                     hashType,
-                                     scriptWitness)
-      }
-    }
-
-    for {
-      arr <- json.validate[JsArray]
-      jsInputsArr <- arr.value.head.validate[JsArray]
-      jsInputs = jsInputsArr.value
-      jsOuptutsArr <- arr.value.last.validate[JsArray]
-      jsOutputs = jsOuptutsArr.value
-      localPayoutsLong <- jsInputs.head.validate[Map[String, Long]]
-      localPayouts = localPayoutsLong.map {
-        case (outcome, sats) => (outcome, Satoshis(sats))
-      }
-      realOutcome <- jsInputs(1).validate[String]
-      oracleKeyStr <- jsInputs(2).validate[String]
-      oracleKey = ECPrivateKey.fromHex(oracleKeyStr)
-      oracleKValueStr <- jsInputs(3).validate[String]
-      oracleKValue = SchnorrNonce.fromBytes(
-        BitcoinSUtil.decodeHex(oracleKValueStr))
-      localExtPrivKey <- jsInputs(4).validate[ExtPrivateKey]
-      localInputLong <- jsInputs(5).validate[Long]
-      localInput = Satoshis(localInputLong)
-      localFundingUtxosSerialized <- jsInputs(6)
-        .validate[Vector[SerializedSegwitSpendingInfo]](
-          vectorReads(serializedSegwitSpendingInfoFromJson))
-      localFundingUtxos = localFundingUtxosSerialized.map(_.toSpendingInfo)
-      localChangeSPKStr <- jsInputs(7).validate[String]
-      localChangeSPK = ScriptPubKey
-        .fromHex(localChangeSPKStr)
-        .asInstanceOf[WitnessScriptPubKeyV0]
-      remoteExtPrivKey <- jsInputs(8).validate[ExtPrivateKey]
-      remoteInputLong <- jsInputs(9).validate[Long]
-      remoteInput = Satoshis(remoteInputLong)
-      remoteFundingUtxosSerialized <- jsInputs(10)
-        .validate[Vector[SerializedSegwitSpendingInfo]](
-          vectorReads(serializedSegwitSpendingInfoFromJson))
-      remoteFundingUtxos = remoteFundingUtxosSerialized.map(_.toSpendingInfo)
-      remoteChangeSPKStr <- jsInputs(11).validate[String]
-      remoteChangeSPK = ScriptPubKey
-        .fromHex(remoteChangeSPKStr)
-        .asInstanceOf[WitnessScriptPubKeyV0]
-      timeoutUInt32 <- jsInputs(12).validate[UInt32]
-      timeout = BlockStamp(timeoutUInt32.toInt)
-      feeRateLong <- jsInputs(13).validate[Long]
-      feeRate = SatoshisPerByte(Satoshis(feeRateLong))
-      fundingTx <- jsOutputs.head.validate[Transaction]
-      localCets <- jsOutputs(1).validate[JsArray]
-      localCetWin <- localCets.value.head.validate[Transaction]
-      localCetLose <- localCets.value.last.validate[Transaction]
-      remoteCets <- jsOutputs(2).validate[JsArray]
-      remoteCetWin <- remoteCets.value.head.validate[Transaction]
-      remoteCetLose <- remoteCets.value.last.validate[Transaction]
-      refundTx <- jsOutputs(3).validate[Transaction]
-      localClosingTx <- jsOutputs(4).validate[Transaction]
-      remoteClosingTx <- jsOutputs(5).validate[Transaction]
-    } yield {
-      DLCTestVector(
-        localPayouts = localPayouts,
-        realOutcome = realOutcome,
-        oracleKey = oracleKey,
-        oracleKValue = oracleKValue,
-        localExtPrivKey = localExtPrivKey,
-        localInput = localInput,
-        localFundingUtxos = localFundingUtxos,
-        localChangeSPK = localChangeSPK,
-        remoteExtPrivKey = remoteExtPrivKey,
-        remoteInput = remoteInput,
-        remoteFundingUtxos = remoteFundingUtxos,
-        remoteChangeSPK = remoteChangeSPK,
-        timeout = timeout,
-        feeRate = feeRate,
-        fundingTx = fundingTx,
-        localWinCet = localCetWin,
-        localLoseCet = localCetLose,
-        remoteWinCet = remoteCetWin,
-        remoteLoseCet = remoteCetLose,
-        refundTx = refundTx,
-        localClosingTx = localClosingTx,
-        remoteClosingTx = remoteClosingTx
-      )
-    }
+    json
+      .validate[SerializedDLCTestVector](
+        SerializedDLCTestVectorSerializers.serializedDLCTestVectorReads)
+      .map(_.dlcTestVector)
   }
 
   def fromInputs(
@@ -414,4 +234,261 @@ object DLCTestVector {
       )
     }
   }
+}
+
+case class SerializedDLCTestVector(
+    inputs: SerializedDLCInputs,
+    outputs: SerializedDLCOutputs) {
+  lazy val dlcTestVector: DLCTestVector = {
+    DLCTestVector(
+      localPayouts = inputs.localPayouts,
+      realOutcome = inputs.realOutcome,
+      oracleKey = inputs.oracleKey,
+      oracleKValue = inputs.oracleKValue,
+      localExtPrivKey = inputs.localExtPrivKey,
+      localInput = inputs.localInput,
+      localFundingUtxos = inputs.localFundingUtxos.map(_.toSpendingInfo),
+      localChangeSPK = inputs.localChangeSPK,
+      remoteExtPrivKey = inputs.remoteExtPrivKey,
+      remoteInput = inputs.remoteInput,
+      remoteFundingUtxos = inputs.remoteFundingUtxos.map(_.toSpendingInfo),
+      remoteChangeSPK = inputs.remoteChangeSPK,
+      timeout = inputs.timeout,
+      feeRate = inputs.feeRate,
+      fundingTx = outputs.fundingTx,
+      localWinCet = outputs.localCets.head,
+      localLoseCet = outputs.localCets.last,
+      remoteWinCet = outputs.remoteCets.head,
+      remoteLoseCet = outputs.remoteCets.last,
+      refundTx = outputs.refundTx,
+      localClosingTx = outputs.localClosingTx,
+      remoteClosingTx = outputs.remoteClosingTx
+    )
+  }
+
+  def toJson: JsValue = {
+    Json.toJson(this)(
+      SerializedDLCTestVectorSerializers.serializedDLCTestVectorWrites)
+  }
+}
+
+object SerializedDLCTestVector {
+
+  def fromDLCTestVector(testVector: DLCTestVector): SerializedDLCTestVector = {
+    val inputs = SerializedDLCInputs(
+      localPayouts = testVector.localPayouts,
+      realOutcome = testVector.realOutcome,
+      oracleKey = testVector.oracleKey,
+      oracleKValue = testVector.oracleKValue,
+      localExtPrivKey = testVector.localExtPrivKey,
+      localInput = testVector.localInput,
+      localFundingUtxos = testVector.localFundingUtxos.map(
+        SerializedSegwitSpendingInfo.fromSpendingInfo),
+      localChangeSPK = testVector.localChangeSPK,
+      remoteExtPrivKey = testVector.remoteExtPrivKey,
+      remoteInput = testVector.remoteInput,
+      remoteFundingUtxos = testVector.remoteFundingUtxos.map(
+        SerializedSegwitSpendingInfo.fromSpendingInfo),
+      remoteChangeSPK = testVector.remoteChangeSPK,
+      timeout = testVector.timeout,
+      feeRate = testVector.feeRate
+    )
+    val outputs = SerializedDLCOutputs(
+      fundingTx = testVector.fundingTx,
+      localCets = Vector(testVector.localWinCet, testVector.localLoseCet),
+      remoteCets = Vector(testVector.remoteWinCet, testVector.remoteLoseCet),
+      refundTx = testVector.refundTx,
+      localClosingTx = testVector.localClosingTx,
+      remoteClosingTx = testVector.remoteClosingTx
+    )
+
+    SerializedDLCTestVector(inputs, outputs)
+  }
+}
+
+object SerializedDLCTestVectorSerializers {
+
+  def hexWrites[T <: NetworkElement]: Writes[T] = Writes[T] { element =>
+    JsString(element.hex)
+  }
+
+  implicit val transactionOutPointWrites: Writes[TransactionOutPoint] =
+    hexWrites[TransactionOutPoint]
+  implicit val transactionOutputWrites: Writes[TransactionOutput] =
+    hexWrites[TransactionOutput]
+  implicit val privKeyWrites: Writes[ECPrivateKey] = hexWrites[ECPrivateKey]
+  implicit val hashTypeWrites: Writes[HashType] = Writes[HashType] { hashType =>
+    JsNumber(hashType.num.toInt)
+  }
+  implicit val scriptWitnessWrites: Writes[ScriptWitnessV0] =
+    hexWrites[ScriptWitnessV0]
+  implicit val schnorrNonceWrites: Writes[SchnorrNonce] =
+    hexWrites[SchnorrNonce]
+  implicit val extPrivKeyWrites: Writes[ExtPrivateKey] =
+    hexWrites[ExtPrivateKey]
+  implicit val currencyUnitWrites: Writes[CurrencyUnit] = Writes[CurrencyUnit] {
+    currencyUnit =>
+      JsNumber(currencyUnit.satoshis.toLong)
+  }
+  implicit val scriptPubKeyWrites: Writes[ScriptPubKey] =
+    hexWrites[ScriptPubKey]
+  implicit val blockStampWithFutureWrites: Writes[BlockStampWithFuture] =
+    Writes[BlockStampWithFuture] { blockStamp =>
+      JsNumber(blockStamp.toUInt32.toLong)
+    }
+  implicit val satoshisPerByteWrites: Writes[SatoshisPerByte] =
+    Writes[SatoshisPerByte] { fee =>
+      JsNumber(fee.toLong)
+    }
+  implicit val transactionWrites: Writes[Transaction] = hexWrites[Transaction]
+
+  implicit val serializedSegwitSpendingInfoWrites: Writes[
+    SerializedSegwitSpendingInfo] =
+    Json.writes[SerializedSegwitSpendingInfo]
+  implicit val serializedDLCInputsWrites: Writes[SerializedDLCInputs] =
+    Json.writes[SerializedDLCInputs]
+  implicit val serializedDLCOutputsWrites: Writes[SerializedDLCOutputs] =
+    Json.writes[SerializedDLCOutputs]
+  implicit val serializedDLCTestVectorWrites: Writes[SerializedDLCTestVector] =
+    Json.writes[SerializedDLCTestVector]
+
+  def hexReads[T <: NetworkElement](factory: Factory[T]): Reads[T] = Reads[T] {
+    json =>
+      json.validate[String].map(factory.fromHex)
+  }
+
+  implicit val transactionOutPointReads: Reads[TransactionOutPoint] = hexReads(
+    TransactionOutPoint)
+  implicit val transactionOutputReads: Reads[TransactionOutput] = hexReads(
+    TransactionOutput)
+  implicit val extPrivKeyReads: Reads[ExtPrivateKey] = hexReads(ExtPrivateKey)
+  implicit val blockStampWithFutureReads: Reads[BlockStampWithFuture] =
+    Reads[BlockStampWithFuture] { json =>
+      json.validate[Int].map(BlockStamp.apply)
+    }
+  implicit val hashTypeReads: Reads[HashType] = Reads[HashType] { json =>
+    json.validate[Int].map(Int32.apply).map(HashType.fromNumber)
+  }
+  implicit val scriptWitnessReads: Reads[ScriptWitnessV0] =
+    Reads[ScriptWitnessV0] { json =>
+      json
+        .validate[String]
+        .map(BitcoinSUtil.decodeHex)
+        .map(RawScriptWitnessParser.read)
+        .map(_.asInstanceOf[ScriptWitnessV0])
+    }
+  implicit val currencyUnitReads: Reads[CurrencyUnit] = Reads[CurrencyUnit] {
+    json =>
+      json.validate[Long].map(Satoshis.apply)
+  }
+  implicit val privKeyReads: Reads[ECPrivateKey] = hexReads(ECPrivateKey)
+  implicit val schnorrNonceReads: Reads[SchnorrNonce] = Reads[SchnorrNonce] {
+    json =>
+      json
+        .validate[String]
+        .map(BitcoinSUtil.decodeHex)
+        .map(SchnorrNonce.fromBytes)
+  }
+  private val witnessScriptPubKeyV0FromAsm: Vector[ScriptToken] => WitnessScriptPubKeyV0 = {
+    asm =>
+      WitnessScriptPubKey.fromAsm(asm).get.asInstanceOf[WitnessScriptPubKeyV0]
+  }
+  implicit val scriptPubKeyReads: Reads[WitnessScriptPubKeyV0] =
+    Reads[WitnessScriptPubKeyV0] { json =>
+      json
+        .validate[String]
+        .map(BitcoinSUtil.decodeHex)
+        .map(BitcoinScriptUtil.parseScript(_, witnessScriptPubKeyV0FromAsm))
+    }
+  implicit val satoshisPerByteReads: Reads[SatoshisPerByte] =
+    Reads[SatoshisPerByte] { json =>
+      json.validate[Long].map(Satoshis.apply).map(SatoshisPerByte.apply)
+    }
+  implicit val transactionReads: Reads[Transaction] = hexReads(Transaction)
+
+  implicit val serializedSegwitSpendingInfoReads: Reads[
+    SerializedSegwitSpendingInfo] =
+    Json.reads[SerializedSegwitSpendingInfo]
+  implicit val serializedDLCInputsReads: Reads[SerializedDLCInputs] =
+    Json.reads[SerializedDLCInputs]
+  implicit val serializedDLCOutputsReads: Reads[SerializedDLCOutputs] =
+    Json.reads[SerializedDLCOutputs]
+  implicit val serializedDLCTestVectorReads: Reads[SerializedDLCTestVector] =
+    Json.reads[SerializedDLCTestVector]
+}
+
+case class SerializedDLCInputs(
+    localPayouts: Map[String, CurrencyUnit],
+    realOutcome: String,
+    oracleKey: ECPrivateKey,
+    oracleKValue: SchnorrNonce,
+    localExtPrivKey: ExtPrivateKey,
+    localInput: CurrencyUnit,
+    localFundingUtxos: Vector[SerializedSegwitSpendingInfo],
+    localChangeSPK: WitnessScriptPubKeyV0,
+    remoteExtPrivKey: ExtPrivateKey,
+    remoteInput: CurrencyUnit,
+    remoteFundingUtxos: Vector[SerializedSegwitSpendingInfo],
+    remoteChangeSPK: WitnessScriptPubKeyV0,
+    timeout: BlockStampWithFuture,
+    feeRate: SatoshisPerByte) {
+  require(localPayouts.keySet.contains(realOutcome), "Outcome must be possible")
+  require(localPayouts.size == 2, "Currently only Binary DLCs are supported")
+  require(localPayouts.values.forall(_ <= localInput + remoteInput),
+          "Payouts must be less than total input")
+  require(localFundingUtxos
+            .map(_.output.value.satoshis.toLong)
+            .sum > localInput.satoshis.toLong,
+          "Local does not have enough to fund")
+  require(remoteFundingUtxos
+            .map(_.output.value.satoshis.toLong)
+            .sum > remoteInput.satoshis.toLong,
+          "Remote does not have enough to fund")
+}
+
+case class SerializedSegwitSpendingInfo(
+    outPoint: TransactionOutPoint,
+    output: TransactionOutput,
+    keys: Vector[ECPrivateKey],
+    hashType: HashType,
+    scriptWitness: ScriptWitnessV0) {
+
+  def toSpendingInfo: SegwitV0NativeUTXOSpendingInfo = {
+    SegwitV0NativeUTXOSpendingInfo(
+      outPoint = outPoint,
+      amount = output.value,
+      scriptPubKey = output.scriptPubKey.asInstanceOf[WitnessScriptPubKeyV0],
+      signers = keys,
+      hashType = hashType,
+      scriptWitness = scriptWitness,
+      conditionalPath = ConditionalPath.NoConditionsLeft
+    )
+  }
+}
+
+object SerializedSegwitSpendingInfo {
+
+  def fromSpendingInfo(
+      spendingInfo: SegwitV0NativeUTXOSpendingInfo): SerializedSegwitSpendingInfo = {
+    SerializedSegwitSpendingInfo(
+      outPoint = spendingInfo.outPoint,
+      output = spendingInfo.output,
+      keys = spendingInfo.signers.toVector.map(_.asInstanceOf[ECPrivateKey]),
+      hashType = spendingInfo.hashType,
+      scriptWitness = spendingInfo.scriptWitness
+    )
+  }
+}
+
+case class SerializedDLCOutputs(
+    fundingTx: Transaction,
+    localCets: Vector[Transaction],
+    remoteCets: Vector[Transaction],
+    refundTx: Transaction,
+    localClosingTx: Transaction,
+    remoteClosingTx: Transaction) {
+  require(localCets.length == 2,
+          s"There must be two local CETs, got $localCets")
+  require(remoteCets.length == 2,
+          s"There must be two remote CETs, got $remoteCets")
 }
