5f0023a663f7b201162dd1214ccea88e04ca0e18
Chris Stewart <stewart.chris1234@gmail.com>
2020-01-20 06:27:48 -0600

    Make different project's migrations independent of each other

--- db-commons-test/src/test/scala/org/bitcoins/db/DbManagementTest.scala
+++ db-commons-test/src/test/scala/org/bitcoins/db/DbManagementTest.scala
@@ -9,6 +9,7 @@ import org.bitcoins.testkit.BitcoinSTestAppConfig
 import org.bitcoins.testkit.BitcoinSTestAppConfig.ProjectType
 import org.bitcoins.testkit.util.BitcoinSUnitTest
 import org.bitcoins.wallet.config.WalletAppConfig
+import org.bitcoins.wallet.db.WalletDbManagement
 
 class DbManagementTest extends BitcoinSUnitTest {
   def dbConfig(project: ProjectType): Config = {
@@ -17,22 +18,22 @@ class DbManagementTest extends BitcoinSUnitTest {
   it must "run migrations for chain db" in {
     val chainAppConfig = ChainAppConfig(BitcoinSTestAppConfig.tmpDir(),
       dbConfig(ProjectType.Chain))
-    val result = ChainDbManagement.migrate()(chainAppConfig)
-    assert(result == 3)
+    val result = ChainDbManagement.migrate(chainAppConfig)
+    assert(result == 1)
   }
 
   it must "run migrations for wallet db" in {
     val walletAppConfig = WalletAppConfig(BitcoinSTestAppConfig.tmpDir(),
       dbConfig(ProjectType.Wallet))
-    val result = ChainDbManagement.migrate()(walletAppConfig)
-    assert(result == 3)
+    val result = WalletDbManagement.migrate(walletAppConfig)
+    assert(result == 1)
   }
 
 
   it must "run migrations for node db" in {
     val nodeAppConfig = NodeAppConfig(BitcoinSTestAppConfig.tmpDir(),
       dbConfig(ProjectType.Node))
-    val result = NodeDbManagement.migrate()(nodeAppConfig)
-    assert(result == 3)
+    val result = NodeDbManagement.migrate(nodeAppConfig)
+    assert(result == 1)
   }
 }
