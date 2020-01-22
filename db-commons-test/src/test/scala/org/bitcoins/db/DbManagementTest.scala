d70d38686852bd08ffb30d4a8ccc694ce488ee07
Ben Carman <benthecarman@live.com>
2020-01-22 10:26:34 -0600

    Move to new file, drop old column

--- db-commons-test/src/test/scala/org/bitcoins/db/DbManagementTest.scala
+++ db-commons-test/src/test/scala/org/bitcoins/db/DbManagementTest.scala
@@ -26,7 +26,7 @@ class DbManagementTest extends BitcoinSUnitTest {
     val walletAppConfig = WalletAppConfig(BitcoinSTestAppConfig.tmpDir(),
       dbConfig(ProjectType.Wallet))
     val result = WalletDbManagement.migrate(walletAppConfig)
-    assert(result == 1)
+    assert(result == 2)
   }
 
 
