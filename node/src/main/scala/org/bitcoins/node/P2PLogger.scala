099465f72a3a85d6ed0a07a20dc02fa50c04d9d6
Nadav Kohen <nadavk25@gmail.com>
2020-02-11 10:35:28 -0700

    Multiple Logger Fix (#1086)
    * An attempt to fix logging to file using MDC
    
    * Responded to review

--- node/src/main/scala/org/bitcoins/node/P2PLogger.scala
+++ node/src/main/scala/org/bitcoins/node/P2PLogger.scala
@@ -1,24 +1,25 @@
 package org.bitcoins.node
 
-import org.slf4j.Logger
 import org.bitcoins.node.config.NodeAppConfig
-import org.bitcoins.db.AppLoggers
+import org.bitcoins.db.{AppLoggers, MarkedLogger}
 
 /** Exposes access to the P2P submodule logger */
 private[bitcoins] trait P2PLogger {
-  private var _logger: Logger = _
+  private var _logger: MarkedLogger = _
   protected def logger(implicit config: NodeAppConfig) = {
     if (_logger == null) {
-      _logger = P2PLogger.getLogger
+      _logger = P2PLoggerImpl(config).getLogger
     }
     _logger
   }
 }
 
-private[bitcoins] object P2PLogger extends AppLoggers {
+private[node] case class P2PLoggerImpl(override val conf: NodeAppConfig)
+    extends AppLoggers {
 
   /**
     * @return the peer-to-peer submobule logger
     */
-  def getLogger(implicit conf: NodeAppConfig) = getLoggerImpl(LoggerKind.P2P)
+  def getLogger: MarkedLogger =
+    getLoggerImpl(LoggerKind.P2P)
 }
