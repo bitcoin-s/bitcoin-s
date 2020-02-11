a4adea098e541c083d0163ace80c4954d2ca5bea
Ben Carman <benthecarman@live.com>
2020-02-11 11:33:36 -0600

    Reserved TxoState

--- wallet/src/main/scala/org/bitcoins/wallet/LockedWallet.scala
+++ wallet/src/main/scala/org/bitcoins/wallet/LockedWallet.scala
@@ -48,7 +48,7 @@ abstract class LockedWallet
                 case TxoState.PendingConfirmationsReceived |
                     TxoState.ConfirmedReceived =>
                   txo.output.value
-                case TxoState.PendingConfirmationsSpent |
+                case TxoState.Reserved | TxoState.PendingConfirmationsSpent |
                     TxoState.ConfirmedSpent | TxoState.DoesNotExist =>
                   CurrencyUnits.zero
               }
