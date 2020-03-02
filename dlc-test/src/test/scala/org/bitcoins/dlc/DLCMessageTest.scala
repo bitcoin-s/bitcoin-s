540fff0812b6a502a5ce3108c46e57e9891c1381
Ben Carman <benthecarman@live.com>
2020-03-02 17:17:06 -0600

    Add unit tests for new requirements

---
+++ dlc-test/src/test/scala/org/bitcoins/dlc/DLCMessageTest.scala
@@ -0,0 +1,74 @@
+package org.bitcoins.dlc
+
+import org.bitcoins.core.crypto.{
+  DummyECDigitalSignature,
+  ECPublicKey,
+  Sha256DigestBE
+}
+import org.bitcoins.core.currency.Satoshis
+import org.bitcoins.core.number.UInt32
+import org.bitcoins.core.protocol.BitcoinAddress
+import org.bitcoins.core.protocol.BlockStamp.{BlockHeight, BlockTime}
+import org.bitcoins.core.psbt.InputPSBTRecord.PartialSignature
+import org.bitcoins.core.wallet.fee.SatoshisPerVirtualByte
+import org.bitcoins.dlc.DLCMessage.{
+  ContractInfo,
+  DLCAccept,
+  DLCOffer,
+  OracleInfo
+}
+import org.bitcoins.testkit.util.BitcoinSAsyncTest
+import scodec.bits.ByteVector
+
+class DLCMessageTest extends BitcoinSAsyncTest {
+  behavior of "DLCMessage"
+
+  it must "not allow a DLCTimeout where the contract times out before it matures" in {
+    assertThrows[IllegalArgumentException](
+      DLCTimeouts(UInt32(5), BlockHeight(4), BlockHeight(2)))
+    assertThrows[IllegalArgumentException](
+      DLCTimeouts(UInt32(5), BlockTime(UInt32(4)), BlockTime(UInt32(2))))
+  }
+
+  val dummyPubKey: ECPublicKey = ECPublicKey.freshPublicKey
+  val dummyPubKey2: ECPublicKey = ECPublicKey.freshPublicKey
+
+  val dummyAddress: BitcoinAddress = BitcoinAddress(
+    "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa").get
+
+  val dummySig: PartialSignature =
+    PartialSignature(dummyPubKey, DummyECDigitalSignature)
+
+  it must "not allow a negative collateral for a DLCOffer" in {
+    assertThrows[IllegalArgumentException](
+      DLCOffer(
+        ContractInfo.empty,
+        OracleInfo.empty,
+        DLCPublicKeys(dummyPubKey, dummyPubKey2, dummyAddress),
+        Satoshis(-1),
+        Vector.empty,
+        dummyAddress,
+        SatoshisPerVirtualByte.one,
+        DLCTimeouts(UInt32(5), BlockHeight(1), BlockHeight(2))
+      ))
+  }
+
+  it must "not allow a negative collateral for a DLCAccept" in {
+    assertThrows[IllegalArgumentException](
+      DLCAccept(
+        Satoshis(-1),
+        DLCPublicKeys(dummyPubKey, dummyPubKey2, dummyAddress),
+        Vector.empty,
+        dummyAddress,
+        CETSignatures(dummySig, dummySig, dummySig),
+        Sha256DigestBE(ByteVector.low(32))
+      )
+    )
+  }
+
+  it must "not allow duplicate keys in a DLCPublicKeys" in {
+    assertThrows[IllegalArgumentException](
+      DLCPublicKeys(dummyPubKey, dummyPubKey, dummyAddress)
+    )
+  }
+}
