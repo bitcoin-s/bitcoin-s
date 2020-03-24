7f634702728c64e4fe069e58b5517f78cb5f4299
Ben Carman <benthecarman@live.com>
2020-03-24 08:07:21 -0500

    Increase code coverage

---
+++ core-test/src/test/scala/org/bitcoins/core/wallet/fee/FeeUnitTest.scala
@@ -0,0 +1,22 @@
+package org.bitcoins.core.wallet.fee
+
+import org.bitcoins.testkit.util.BitcoinSUnitTest
+
+class FeeUnitTest extends BitcoinSUnitTest {
+
+  "SatoshisPerByte" must "have the correct FeeUnit" in {
+    assert(SatoshisPerByte.one.units == FeeUnit.PerByte)
+  }
+
+  "SatoshisPerVirtualByte" must "have the correct FeeUnit" in {
+    assert(SatoshisPerVirtualByte.one.units == FeeUnit.PerVirtualByte)
+  }
+
+  "SatoshisPerKiloByte" must "have the correct FeeUnit" in {
+    assert(SatoshisPerKiloByte.one.units == FeeUnit.PerKiloByte)
+  }
+
+  "FlatSatoshis" must "have the correct FeeUnit" in {
+    assert(FlatSatoshis(1000).units == FeeUnit.Flat)
+  }
+}
