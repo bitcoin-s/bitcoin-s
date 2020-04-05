6b7da63ef095e27f2c2ea0017896df61ba91f937
nkohen <nadavk25@gmail.com>
2020-04-05 11:37:26 -0500

    Added util methods for adaptor sig serialization

--- core/src/main/scala/org/bitcoins/core/crypto/ECAdaptorSignature.scala
+++ core/src/main/scala/org/bitcoins/core/crypto/ECAdaptorSignature.scala
@@ -1,8 +1,20 @@
 package org.bitcoins.core.crypto
 
+import org.bitcoins.core.protocol.NetworkElement
 import scodec.bits.ByteVector
 
-case class ECAdaptorSignature(adaptedSig: ByteVector, dleqProof: ByteVector) {
+case class ECAdaptorSignature(adaptedSig: ByteVector, dleqProof: ByteVector)
+    extends NetworkElement {
   require(adaptedSig.length == 65)
   require(dleqProof.length == 97)
+
+  def bytes: ByteVector = adaptedSig ++ dleqProof
+}
+
+object ECAdaptorSignature {
+
+  def fromBytes(bytes: ByteVector): ECAdaptorSignature = {
+    require(bytes.length == 65 + 97)
+    ECAdaptorSignature(bytes.take(65), bytes.drop(65))
+  }
 }
