6842927bfdd7458592690a020df51e560702ea05
Ben Carman <benthecarman@live.com>
2020-04-07 11:12:08 -0500

    Make adaptorScalar nullable

--- wallet/src/main/resources/walletdb/migration/V5__add_ptlc_tables.sql
+++ wallet/src/main/resources/walletdb/migration/V5__add_ptlc_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "wallet_ptlcs" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"invoiceId" VARCHAR(254) NOT NULL UNIQUE,"network" VARCHAR(254) NOT NULL,"isInitiator" INTEGER NOT NULL, "account" VARCHAR(254) NOT NULL, "keyIndex" INTEGER NOT NULL,"refundSig" VARCHAR(254));
 CREATE INDEX "wallet_ptlcs_invoiceId_index" on "wallet_ptlcs" ("invoiceId");
 
-CREATE TABLE "wallet_ptlc_invoices" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"invoiceId" VARCHAR(254) NOT NULL UNIQUE,"adaptorScalar" VARCHAR(254) NOT NULL,"adaptorPoint" VARCHAR(254) NOT NULL,"amount" INTEGER NOT NULL,"pubkey" VARCHAR(254) NOT NULL,"finalAddress" VARCHAR(254) NOT NULL,"timeout" INTEGER NOT NULL,constraint "fk_invoiceId" foreign key("invoiceId") references "wallet_ptlcs"("invoiceId") on update NO ACTION on delete NO ACTION);
+CREATE TABLE "wallet_ptlc_invoices" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"invoiceId" VARCHAR(254) NOT NULL UNIQUE,"adaptorScalar" VARCHAR(254),"adaptorPoint" VARCHAR(254) NOT NULL,"amount" INTEGER NOT NULL,"pubkey" VARCHAR(254) NOT NULL,"finalAddress" VARCHAR(254) NOT NULL,"timeout" INTEGER NOT NULL,constraint "fk_invoiceId" foreign key("invoiceId") references "wallet_ptlcs"("invoiceId") on update NO ACTION on delete NO ACTION);
 CREATE INDEX "wallet_ptlc_invoices_invoiceId_index" on "wallet_ptlc_invoices" ("invoiceId");
 
 CREATE TABLE "wallet_ptlc_accepts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"invoiceId" VARCHAR(254) NOT NULL UNIQUE,"pubkey" VARCHAR(254) NOT NULL,"unsignedTx" VARCHAR(254) NOT NULL,"adaptorSignature" VARCHAR(254) NOT NULL,"refundAddress" VARCHAR(254) NOT NULL, constraint "fk_invoiceId" foreign key("invoiceId") references "wallet_ptlcs"("invoiceId") on update NO ACTION on delete NO ACTION);
