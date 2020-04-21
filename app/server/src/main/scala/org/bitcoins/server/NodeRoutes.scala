b16b44bb8d6d861519b69daba374a4e5e71fa699
Ben Carman <benthecarman@live.com>
2020-04-21 13:23:07 -0500

    Past tense

--- app/server/src/main/scala/org/bitcoins/server/NodeRoutes.scala
+++ app/server/src/main/scala/org/bitcoins/server/NodeRoutes.scala
@@ -35,7 +35,7 @@ case class NodeRoutes(node: Node)(implicit system: ActorSystem)
         case Success(SendRawTransaction(tx)) =>
           complete {
             node.broadcastTransaction(tx).map { _ =>
-              Server.httpSuccess(s"Broadcast ${tx.txIdBE}")
+              Server.httpSuccess(s"Broadcasted ${tx.txIdBE}")
             }
           }
       }
