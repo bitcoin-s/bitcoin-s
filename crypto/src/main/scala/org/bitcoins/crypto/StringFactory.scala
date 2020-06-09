5c2fb8f5801b8a4ed0188613f30efa01d4a2ea20
Chris Stewart <stewart.chris1234@gmail.com>
2020-06-09 15:03:56 -0500

    Add StringFactory trait (#1537)

---
+++ crypto/src/main/scala/org/bitcoins/crypto/StringFactory.scala
@@ -0,0 +1,20 @@
+package org.bitcoins.crypto
+
+import scala.util.Try
+
+/** A common factory trait that can be re-used to deserialize a string to a type t */
+trait StringFactory[T] {
+
+  /** Tries to parse a string to type t, throws an exception if fails */
+  def fromString(string: String): T
+
+  /** Treis to parse a string to type t, returns None if failure */
+  def fromStringOpt(string: String): Option[T] = {
+    fromStringT(string).toOption
+  }
+
+  /** Tries to parsea  string to type t, returns [[scala.util.Failure]] if the fails */
+  def fromStringT(string: String): Try[T] = {
+    Try(fromString(string))
+  }
+}
