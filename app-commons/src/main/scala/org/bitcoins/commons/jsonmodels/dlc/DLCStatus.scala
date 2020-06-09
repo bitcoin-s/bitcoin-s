00c0364df2363d117c0b6e71c29aa067aedbf5ed
nkohen <nadavk25@gmail.com>
2020-06-09 10:07:03 -0500

    Added scaladocs to DLCStatuses

--- app-commons/src/main/scala/org/bitcoins/commons/jsonmodels/dlc/DLCStatus.scala
+++ app-commons/src/main/scala/org/bitcoins/commons/jsonmodels/dlc/DLCStatus.scala
@@ -9,6 +9,11 @@ import org.bitcoins.commons.jsonmodels.dlc.DLCMessage.{
 import org.bitcoins.core.protocol.transaction.Transaction
 import org.bitcoins.crypto.{SchnorrDigitalSignature, Sha256DigestBE}
 
+/** Represents the state of specific DLC for a given party.
+  * This state is made up of all messages that have been
+  * passed back and forth as well as any relevant on-chain
+  * transactions and oracle signatures.
+  */
 sealed trait DLCStatus {
   def eventId: Sha256DigestBE
   def isInitiator: Boolean
@@ -16,12 +21,16 @@ sealed trait DLCStatus {
   def statusString: String
 }
 
+/** All states other than Offered contain an accept message. */
 sealed trait AcceptedDLCStatus extends DLCStatus {
   def accept: DLCAccept
 }
 
 object DLCStatus {
 
+  /** The state where an offer has been created but no
+    * accept message has yet been created/received.
+    */
   case class Offered(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -34,6 +43,9 @@ object DLCStatus {
     }
   }
 
+  /** The state where an offer has been accepted but
+    * no sign message has yet been created/received.
+    */
   case class Accepted(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -47,6 +59,11 @@ object DLCStatus {
     }
   }
 
+  /** The state where the initiating party has created
+    * a sign message in response to an accept message
+    * but the DLC funding transaction has not yet been
+    * broadcasted to the network.
+    */
   case class Signed(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -65,6 +82,10 @@ object DLCStatus {
     }
   }
 
+  /** The state where the accepting (non-initiating)
+    * party has broadcasted the DLC funding transaction
+    * to the blockchain, and it has not yet been confirmed.
+    */
   case class Broadcasted(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -80,6 +101,10 @@ object DLCStatus {
     }
   }
 
+  /** The state where the DLC funding transaction has been
+    * confirmed on-chain and no execution paths have yet been
+    * initiated.
+    */
   case class Confirmed(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -118,6 +143,11 @@ object DLCStatus {
     }
   }
 
+  /** The state where a party has initiated a mutual close
+    * with a DLCMutualCloseSig message (containing an
+    * oracle signature and their signature of the close tx)
+    * but the mutual close tx has not yet been broadcasted.
+    */
   case class CloseOffered(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -145,6 +175,9 @@ object DLCStatus {
     }
   }
 
+  /** The state where a mutual close has been successfully
+    * executed with an on-chain close transaction.
+    */
   case class Closed(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -158,6 +191,10 @@ object DLCStatus {
     override val statusString: String = "CLOSED"
   }
 
+  /** The state where this party has initiated a unilateral
+    * execution by broadcasting a CET to the network. In this
+    * state the to_local output on the CET has not yet been spent.
+    */
   case class Claiming(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -195,6 +232,10 @@ object DLCStatus {
     }
   }
 
+  /** The state where this party has successfully executed
+    * a unilateral execution by broadcasting a CET and successfully
+    * spending the to_local output of that CET.
+    */
   case class Claimed(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -209,6 +250,12 @@ object DLCStatus {
     override val statusString: String = "CLAIMED"
   }
 
+  /** The state where this party has broadcasted a CET
+    * to the network and failed to spend the to_local output
+    * in time, so that the counterparty has successfully
+    * spent the to_local output to themselves with a
+    * penalty transaction.
+    */
   case class Penalized(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -223,6 +270,10 @@ object DLCStatus {
     override val statusString: String = "PENALIZED"
   }
 
+  /** The state where the remote party has initiated a
+    * unilateral execution by broadcasting one of their
+    * CETs but has not yet spent their to_local output
+    */
   case class RemoteClaiming(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -257,6 +308,10 @@ object DLCStatus {
     }
   }
 
+  /** The state where the remote party has successfully
+    * executed a unilateral execution by broadcasting both
+    * a CET and successfully spending their to_local output.
+    */
   case class RemoteClaimed(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -270,6 +325,11 @@ object DLCStatus {
     override val statusString: String = "REMOTE CLAIMED"
   }
 
+  /** The state where the remote party has published a
+    * CET but failed to spend their to_local output
+    * and this party has successfully spent that to_local
+    * output with a penalty transaction.
+    */
   case class RemotePenalized(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
@@ -283,6 +343,9 @@ object DLCStatus {
     override val statusString: String = "REMOTE PENALIZED"
   }
 
+  /** The state where the DLC refund transaction has been
+    * accepted by the network.
+    */
   case class Refunded(
       eventId: Sha256DigestBE,
       isInitiator: Boolean,
