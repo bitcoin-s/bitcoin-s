fb99307867839e975e8685152cb5195402556cf7
Nadav Kohen <nadavk25@gmail.com>
2020-06-16 10:54:37 -0500

    Added BlockTimeStamp abstraction for BlockStamps that aren't hashes (#1573)
    * Added BlockTimeStamp abstraction for BlockStamps that aren't hashes
    
    * Made BlockStamp and BlockTimeStamp extend StringFactory

--- core/src/main/scala/org/bitcoins/core/protocol/BlockStamp.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/BlockStamp.scala
@@ -5,34 +5,46 @@ import java.time.format.DateTimeFormatter
 import java.time.temporal.{ChronoField, TemporalAccessor}
 
 import org.bitcoins.core.number.UInt32
-import org.bitcoins.crypto.DoubleSha256DigestBE
+import org.bitcoins.core.protocol.transaction.TransactionConstants
+import org.bitcoins.core.script.constant.ScriptNumber
+import org.bitcoins.crypto.{DoubleSha256DigestBE, StringFactory}
 
-import scala.util.{Failure, Try}
+import scala.util.{Failure, Success, Try}
 
 /** This trait represents a point on blockchain, and is used to specify block ranges */
 sealed trait BlockStamp {
   def mkString: String
 }
 
-object BlockStamp {
-  val height0 = BlockHeight(0)
-  val height0Opt = Some(height0)
+/** This trait represents a point in time on the blockchain, including future times */
+sealed trait BlockTimeStamp extends BlockStamp {
+  def toUInt32: UInt32
+  def toScriptNumber: ScriptNumber
+}
+
+object BlockStamp extends StringFactory[BlockStamp] {
+  val height0: BlockHeight = BlockHeight(0)
+  val height0Opt: Option[BlockHeight] = Some(height0)
 
   case class InvalidBlockStamp(blockStamp: String)
-      extends RuntimeException(s"Invalid blockstamp: ${blockStamp}")
+      extends RuntimeException(s"Invalid blockstamp: $blockStamp")
 
   case class BlockHash(hash: DoubleSha256DigestBE) extends BlockStamp {
     override def mkString: String = hash.hex
   }
-  case class BlockHeight(height: Int) extends BlockStamp {
+  case class BlockHeight(height: Int) extends BlockTimeStamp {
     require(height >= 0, "block height must be a positive number")
     override def mkString: String = height.toString
+    override def toUInt32: UInt32 = UInt32(height)
+    override def toScriptNumber: ScriptNumber = ScriptNumber(height)
   }
-  case class BlockTime(time: UInt32) extends BlockStamp {
+  case class BlockTime(time: UInt32) extends BlockTimeStamp {
     override def mkString: String = {
       val instant = Instant.ofEpochSecond(time.toLong)
       DateTimeFormatter.ISO_INSTANT.format(instant)
     }
+    override def toUInt32: UInt32 = time
+    override def toScriptNumber: ScriptNumber = ScriptNumber(time.toLong)
   }
 
   object BlockTime {
@@ -45,10 +57,10 @@ object BlockStamp {
 
   }
 
-  def fromString(s: String): Try[BlockStamp] = {
-    lazy val blockHash = Try(DoubleSha256DigestBE.fromHex(s)).map(BlockHash(_))
+  override def fromStringT(s: String): Try[BlockStamp] = {
+    lazy val blockHash = Try(DoubleSha256DigestBE.fromHex(s)).map(BlockHash)
 
-    lazy val blockHeight = Try(s.toInt).map(BlockHeight(_))
+    lazy val blockHeight = Try(s.toInt).map(BlockHeight)
 
     lazy val blockTime =
       Try(DateTimeFormatter.ISO_INSTANT.parse(s)).map(BlockTime(_))
@@ -56,5 +68,48 @@ object BlockStamp {
     lazy val error = Failure(InvalidBlockStamp(s))
 
     blockHash orElse blockHeight orElse blockTime orElse error
+  }
+
+  override def fromString(string: String): BlockStamp = {
+    fromStringT(string) match {
+      case Failure(exception) => throw exception
+      case Success(stamp)     => stamp
+    }
+  }
+}
+
+object BlockTimeStamp extends StringFactory[BlockTimeStamp] {
+
+  override def fromStringT(s: String): Try[BlockTimeStamp] = {
+    lazy val blockHeight = Try(s.toInt).map(BlockStamp.BlockHeight)
+
+    lazy val blockTime =
+      Try(DateTimeFormatter.ISO_INSTANT.parse(s)).map(BlockStamp.BlockTime(_))
+
+    lazy val error = Failure(BlockStamp.InvalidBlockStamp(s))
+
+    blockHeight orElse blockTime orElse error
+  }
+
+  override def fromString(string: String): BlockTimeStamp = {
+    fromStringT(string) match {
+      case Failure(exception) => throw exception
+      case Success(stamp)     => stamp
+    }
+  }
+
+  def apply(timeLockNumber: UInt32): BlockTimeStamp =
+    fromUInt32(timeLockNumber)
+
+  def apply(timeLockNumber: Int): BlockTimeStamp =
+    fromUInt32(UInt32(timeLockNumber))
+
+  /** @see [[https://github.com/bitcoin/bips/blob/master/bip-0065.mediawiki#detailed-specification]] */
+  def fromUInt32(uInt32: UInt32): BlockTimeStamp = {
+    if (uInt32 < TransactionConstants.locktimeThreshold) {
+      BlockStamp.BlockHeight(uInt32.toInt)
+    } else {
+      BlockStamp.BlockTime(uInt32)
+    }
   }
 }
