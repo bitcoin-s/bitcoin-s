e504d8dfb7d7e41b0387db14168c35380fb83301
Nadav Kohen <nadavk25@gmail.com>
2020-06-16 15:21:33 -0500

    Added DLC json data structures (#1575)

---
+++ app-commons/src/main/scala/org/bitcoins/commons/jsonmodels/dlc/DLCSignatures.scala
@@ -0,0 +1,22 @@
+package org.bitcoins.commons.jsonmodels.dlc
+
+import org.bitcoins.core.protocol.transaction.TransactionOutPoint
+import org.bitcoins.core.psbt.InputPSBTRecord.PartialSignature
+import org.bitcoins.core.util.MapWrapper
+import org.bitcoins.crypto.Sha256DigestBE
+
+sealed trait DLCSignatures
+
+case class FundingSignatures(
+    sigs: Map[TransactionOutPoint, Vector[PartialSignature]])
+    extends MapWrapper[TransactionOutPoint, Vector[PartialSignature]]
+    with DLCSignatures {
+  override protected def wrapped: Map[
+    TransactionOutPoint,
+    Vector[PartialSignature]] = sigs
+}
+
+case class CETSignatures(
+    outcomeSigs: Map[Sha256DigestBE, PartialSignature],
+    refundSig: PartialSignature)
+    extends DLCSignatures
--- core/src/main/scala/org/bitcoins/core/util/Wrappers.scala
+++ core/src/main/scala/org/bitcoins/core/util/Wrappers.scala
@@ -7,15 +7,6 @@ trait SeqWrapper[+T] extends IndexedSeq[T] {
   override def apply(idx: Int): T = wrapped(idx)
 }
 
-trait MapWrapper[K, +T] extends Map[K, T] {
-  protected def wrapped: Map[K, T]
-  override def +[B1 >: T](kv: (K, B1)): Map[K, B1] = wrapped.+(kv)
-  override def get(key: K): Option[T] = wrapped.get(key)
-  override def iterator: Iterator[(K, T)] = wrapped.iterator
-  override def updated[V1 >: T](key: K, value: V1): Map[K, V1] =
-    wrapped.updated(key, value)
-}
-
 class Mutable[A](initialValue: A) {
   private val lock = new java.util.concurrent.locks.ReentrantReadWriteLock()
 
