6e278f66da54adf5b4adf4c7e3dcda00ca3f4c46
Ben Carman <benthecarman@live.com>
2020-07-06 15:55:07 -0500

    Drop AutoInc col for Broadcastable Transaction Table

--- node/src/main/scala/org/bitcoins/node/models/BroadcastAbleTransactionDAO.scala
+++ node/src/main/scala/org/bitcoins/node/models/BroadcastAbleTransactionDAO.scala
@@ -1,20 +1,19 @@
 package org.bitcoins.node.models
 
-import org.bitcoins.db.CRUDAutoInc
-import org.bitcoins.node.config.NodeAppConfig
-
-import scala.concurrent.ExecutionContext
-import slick.lifted.ProvenShape
-
-import scala.concurrent.Future
 import org.bitcoins.core.protocol.transaction.Transaction
 import org.bitcoins.crypto.{DoubleSha256Digest, DoubleSha256DigestBE}
+import org.bitcoins.db.{CRUD, SlickUtil}
+import org.bitcoins.node.config.NodeAppConfig
 import scodec.bits.ByteVector
+import slick.lifted.ProvenShape
+
+import scala.concurrent.{ExecutionContext, Future}
 
 final case class BroadcastAbleTransactionDAO()(implicit
     override val appConfig: NodeAppConfig,
     val ec: ExecutionContext)
-    extends CRUDAutoInc[BroadcastAbleTransaction] {
+    extends CRUD[BroadcastAbleTransaction, DoubleSha256DigestBE]
+    with SlickUtil[BroadcastAbleTransaction, DoubleSha256DigestBE] {
 
   import profile.api._
   val mappers = new org.bitcoins.db.DbCommonsColumnMappers(profile)
@@ -22,6 +21,27 @@ final case class BroadcastAbleTransactionDAO()(implicit
 
   override val table: profile.api.TableQuery[BroadcastAbleTransactionTable] =
     profile.api.TableQuery[BroadcastAbleTransactionTable]
+
+  override def createAll(ts: Vector[BroadcastAbleTransaction]): Future[
+    Vector[BroadcastAbleTransaction]] = createAllNoAutoInc(ts, safeDatabase)
+
+  /** Finds the rows that correlate to the given primary keys */
+  override protected def findByPrimaryKeys(
+      txIds: Vector[DoubleSha256DigestBE]): Query[
+    BroadcastAbleTransactionTable,
+    BroadcastAbleTransaction,
+    Seq] = {
+    table.filter(_.txid.inSet(txIds))
+  }
+
+  override protected def findAll(ts: Vector[BroadcastAbleTransaction]): Query[
+    BroadcastAbleTransactionTable,
+    BroadcastAbleTransaction,
+    Seq] = findByPrimaryKeys(ts.map(_.transaction.txIdBE))
+
+  def findByHash(
+      hash: DoubleSha256DigestBE): Future[Option[BroadcastAbleTransaction]] =
+    findByHash(hash.flip)
 
   /** Searches for a TX by its TXID */
   def findByHash(
@@ -35,26 +55,23 @@ final case class BroadcastAbleTransactionDAO()(implicit
 
   /** Table over TXs we can broadcast over the P2P network */
   class BroadcastAbleTransactionTable(tag: Tag)
-      extends TableAutoInc[BroadcastAbleTransaction](tag,
-                                                     "broadcast_elements") {
-    private type Tuple = (DoubleSha256DigestBE, ByteVector, Option[Long])
+      extends Table[BroadcastAbleTransaction](tag, "broadcast_elements") {
+    private type Tuple = (DoubleSha256DigestBE, ByteVector)
 
-    private val fromTuple: (Tuple => BroadcastAbleTransaction) = {
-      case (txid, bytes, id) =>
+    private val fromTuple: Tuple => BroadcastAbleTransaction = {
+      case (txid, bytes) =>
         val tx = Transaction.fromBytes(bytes)
         require(tx.txId == txid.flip)
-        BroadcastAbleTransaction(tx, id)
+        BroadcastAbleTransaction(tx)
     }
 
     private val toTuple: BroadcastAbleTransaction => Option[Tuple] = tx =>
-      Some(tx.transaction.txId.flip, tx.transaction.bytes, tx.id)
+      Some(tx.transaction.txId.flip, tx.transaction.bytes)
 
-    def txid: Rep[DoubleSha256DigestBE] = column("txid", O.Unique)
+    def txid: Rep[DoubleSha256DigestBE] = column("txid", O.PrimaryKey)
     def bytes: Rep[ByteVector] = column("tx_bytes")
 
     def * : ProvenShape[BroadcastAbleTransaction] =
-      (txid, bytes, id.?) <>
-        (fromTuple, toTuple)
+      (txid, bytes) <> (fromTuple, toTuple)
   }
-
 }
