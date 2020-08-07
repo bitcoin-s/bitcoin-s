c15d3eadb442a846fddfff8a182b242b3616e02a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-07 14:03:42 +0200

    Update scalatest to 3.2.1 (#1778)

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
     val bouncyCastle = "1.66"
     val logback = "1.2.3"
     val scalacheck = "1.14.3"
-    val scalaTest = "3.2.0"
+    val scalaTest = "3.2.1"
 
     val scalaTestPlus = "3.2.1.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
     val slf4j = "1.7.30"
