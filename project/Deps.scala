465fa6f68bf1e9130896dce50f9e34cd87c38c60
Ben Carman <benthecarman@live.com>
2020-08-12 17:30:45 -0500

    Revert scalaTestPlus 3.2.1.0 -> 3.2.0.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
     val scalaTest = "3.2.0"
 
     val scalaTestPlus =
-      "3.2.1.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
+      "3.2.0.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
     val slf4j = "1.7.30"
     val spray = "1.3.5"
     val zeromq = "0.5.2"
