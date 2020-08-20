2a1c3998385d001587f6cdffe7ee3a927b41cdbc
Nadav Kohen <nadavk25@gmail.com>
2020-08-20 13:42:16 -0600

    Implement basic TLV functionality (#1847)
    * Defined BigSizeUInt as in BOLT 1
    
    * Introduced UInt16 and TLV types
    
    * Responded to review
    
    * Responded to review

---
+++ core-test/src/test/scala/org/bitcoins/core/protocol/tlv/TLVTest.scala
@@ -0,0 +1,44 @@
+package org.bitcoins.core.protocol.tlv
+
+import org.bitcoins.testkit.core.gen.TLVGen
+import org.bitcoins.testkit.util.BitcoinSUnitTest
+
+class TLVTest extends BitcoinSUnitTest {
+
+  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
+    generatorDrivenConfigNewCode
+
+  "TLV" must "have serizliation symmetry" in {
+    forAll(TLVGen.tlv) { tlv =>
+      assert(TLV(tlv.bytes) == tlv)
+    }
+  }
+
+  "UnknownTLV" must "have serialization symmetry" in {
+    forAll(TLVGen.unknownTLV) { unknown =>
+      assert(UnknownTLV(unknown.bytes) == unknown)
+      assert(TLV(unknown.bytes) == unknown)
+    }
+  }
+
+  "ErrorTLV" must "have serialization symmetry" in {
+    forAll(TLVGen.errorTLV) { error =>
+      assert(ErrorTLV(error.bytes) == error)
+      assert(TLV(error.bytes) == error)
+    }
+  }
+
+  "PingTLV" must "have serialization symmetry" in {
+    forAll(TLVGen.pingTLV) { ping =>
+      assert(PingTLV(ping.bytes) == ping)
+      assert(TLV(ping.bytes) == ping)
+    }
+  }
+
+  "PongTLV" must "have serialization symmetry" in {
+    forAll(TLVGen.pongTLV) { pong =>
+      assert(PongTLV(pong.bytes) == pong)
+      assert(TLV(pong.bytes) == pong)
+    }
+  }
+}
---
+++ core/src/main/scala/org/bitcoins/core/protocol/tlv/TLV.scala
@@ -0,0 +1,148 @@
+package org.bitcoins.core.protocol.tlv
+
+import org.bitcoins.core.number.UInt16
+import org.bitcoins.core.protocol.BigSizeUInt
+import org.bitcoins.core.protocol.tlv.TLV.DecodeTLVResult
+import org.bitcoins.crypto.{Factory, NetworkElement}
+import scodec.bits.ByteVector
+
+sealed trait TLV extends NetworkElement {
+  def tpe: BigSizeUInt
+  def value: ByteVector
+
+  def length: BigSizeUInt = {
+    BigSizeUInt.calcFor(value)
+  }
+
+  override def bytes: ByteVector = {
+    tpe.bytes ++ length.bytes ++ value
+  }
+}
+
+object TLV extends Factory[TLV] {
+
+  case class DecodeTLVResult(
+      tpe: BigSizeUInt,
+      length: BigSizeUInt,
+      value: ByteVector)
+
+  def decodeTLV(bytes: ByteVector): DecodeTLVResult = {
+    val tpe = BigSizeUInt(bytes)
+    val length = BigSizeUInt(bytes.drop(tpe.byteSize))
+    val prefixSize = tpe.byteSize + length.byteSize
+
+    require(
+      bytes.length >= prefixSize + length.num.toLong,
+      s"Length specified was $length but not enough bytes in ${bytes.drop(prefixSize)}")
+
+    val value = bytes.drop(prefixSize).take(length.num.toLong)
+
+    DecodeTLVResult(tpe, length, value)
+  }
+
+  private val allFactories: Vector[TLVFactory[TLV]] =
+    Vector(ErrorTLV, PingTLV, PongTLV)
+
+  val knownTypes: Vector[BigSizeUInt] = allFactories.map(_.tpe)
+
+  def fromBytes(bytes: ByteVector): TLV = {
+    val DecodeTLVResult(tpe, _, value) = decodeTLV(bytes)
+
+    allFactories.find(_.tpe == tpe) match {
+      case Some(tlvFactory) => tlvFactory.fromTLVValue(value)
+      case None             => UnknownTLV(tpe, value)
+    }
+  }
+}
+
+sealed trait TLVFactory[+T <: TLV] extends Factory[T] {
+  def tpe: BigSizeUInt
+  def fromTLVValue(value: ByteVector): T
+
+  override def fromBytes(bytes: ByteVector): T = {
+    val DecodeTLVResult(tpe, _, value) = TLV.decodeTLV(bytes)
+
+    require(tpe == this.tpe, s"Invalid type $tpe when expecting ${this.tpe}")
+
+    fromTLVValue(value)
+  }
+}
+
+case class UnknownTLV(tpe: BigSizeUInt, value: ByteVector) extends TLV {
+  require(!TLV.knownTypes.contains(tpe), s"Type $tpe is known")
+}
+
+object UnknownTLV extends Factory[UnknownTLV] {
+
+  override def fromBytes(bytes: ByteVector): UnknownTLV = {
+    val DecodeTLVResult(tpe, _, value) = TLV.decodeTLV(bytes)
+
+    UnknownTLV(tpe, value)
+  }
+}
+
+/** @see [[https://github.com/lightningnetwork/lightning-rfc/blob/master/01-messaging.md#the-error-message]] */
+case class ErrorTLV(id: ByteVector, data: ByteVector) extends TLV {
+  require(id.length == 32, s"ID associated with error is incorrect length: $id")
+
+  override val tpe: BigSizeUInt = ErrorTLV.tpe
+
+  override val value: ByteVector = {
+    id ++ UInt16(data.length).bytes ++ data
+  }
+}
+
+object ErrorTLV extends TLVFactory[ErrorTLV] {
+  override val tpe: BigSizeUInt = BigSizeUInt(17)
+
+  override def fromTLVValue(value: ByteVector): ErrorTLV = {
+    val id = value.take(32)
+    val len = UInt16(value.drop(32).take(2))
+    val data = value.drop(32 + 2).take(len.toInt)
+
+    ErrorTLV(id, data)
+  }
+}
+
+case class PingTLV(numPongBytes: UInt16, ignored: ByteVector) extends TLV {
+  override val tpe: BigSizeUInt = PingTLV.tpe
+
+  override val value: ByteVector = {
+    numPongBytes.bytes ++ UInt16(ignored.length).bytes ++ ignored
+  }
+}
+
+object PingTLV extends TLVFactory[PingTLV] {
+  override val tpe: BigSizeUInt = BigSizeUInt(18)
+
+  override def fromTLVValue(value: ByteVector): PingTLV = {
+    val numPongBytes = UInt16(value.take(2))
+    val numIgnored = UInt16(value.slice(2, 4))
+    val ignored = value.drop(4).take(numIgnored.toLong)
+
+    PingTLV(numPongBytes, ignored)
+  }
+}
+
+case class PongTLV(ignored: ByteVector) extends TLV {
+  override val tpe: BigSizeUInt = PongTLV.tpe
+
+  override val value: ByteVector = {
+    UInt16(ignored.length).bytes ++ ignored
+  }
+}
+
+object PongTLV extends TLVFactory[PongTLV] {
+  override val tpe: BigSizeUInt = BigSizeUInt(19)
+
+  override def fromTLVValue(value: ByteVector): PongTLV = {
+    val numIgnored = UInt16(value.take(2))
+    val ignored = value.drop(2).take(numIgnored.toLong)
+
+    PongTLV.forIgnored(ignored)
+  }
+
+  def forIgnored(ignored: ByteVector): PongTLV = {
+    new PongTLV(ignored)
+  }
+}
