c3130429d5bdaeda36fbabb53ffe76f5a76eabaf
christewart <stewart.chris1234@gmail.com>
2020-08-21 16:33:51 -0500

    Make MerkleBuffersTest use CachedAppConfig

--- testkit/src/main/scala/org/bitcoins/testkit/node/CachedAppConfig.scala
+++ testkit/src/main/scala/org/bitcoins/testkit/node/CachedAppConfig.scala
@@ -1,5 +1,6 @@
 package org.bitcoins.testkit.node
 
+import org.bitcoins.node.config.NodeAppConfig
 import org.bitcoins.server.BitcoinSAppConfig
 import org.bitcoins.testkit.BitcoinSTestAppConfig
 import org.bitcoins.testkit.util.BitcoinSAsyncTest
@@ -10,6 +11,10 @@ trait CachedAppConfig { _: BitcoinSAsyncTest =>
 
   implicit protected lazy val config: BitcoinSAppConfig =
     BitcoinSTestAppConfig.getSpvTestConfig()
+
+  implicit protected lazy val nodeConf: NodeAppConfig = {
+    config.nodeConf
+  }
 
   override def afterAll(): Unit = {
     Await.result(config.stop(), akkaTimeout.duration)
