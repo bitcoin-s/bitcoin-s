74807e867beae9240062bd7c31d8f9cd264303eb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-10 13:44:02 +0200

    Update play-json to 2.9.1 (#1994)

--- project/Deps.scala
+++ project/Deps.scala
@@ -14,7 +14,7 @@ object Deps {
     val spray = "1.3.5"
     val zeromq = "0.5.2"
     val akkav = "10.1.12"
-    val playv = "2.9.0"
+    val playv = "2.9.1"
     val akkaStreamv = "2.6.8"
     val scodecV = "1.1.18"
     val junitV = "0.11"
