519793f3603356b0b1fac979f3b1cc9f3e93c34a
Scala Steward <me@scala-steward.org>
2020-09-10 09:02:06 +0200

    Update akka-actor, akka-slf4j, akka-stream, ... to 2.6.9

--- project/Deps.scala
+++ project/Deps.scala
@@ -15,7 +15,7 @@ object Deps {
     val zeromq = "0.5.2"
     val akkav = "10.1.12"
     val playv = "2.9.0"
-    val akkaStreamv = "2.6.8"
+    val akkaStreamv = "2.6.9"
     val scodecV = "1.1.18"
     val junitV = "0.11"
     val nativeLoaderV = "2.3.4"
