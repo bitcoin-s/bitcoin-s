a00729b8a8c7e24ef589e3326c9f78ce4ae01096
Ben Carman <benthecarman@live.com>
2020-09-17 13:31:29 -0500

    Add test to use llyod's info (#2027)

--- dlc-wallet-test/src/test/scala/org/bitcoins/dlc/wallet/WalletDLCSetupTest.scala
+++ dlc-wallet-test/src/test/scala/org/bitcoins/dlc/wallet/WalletDLCSetupTest.scala
@@ -1,14 +1,27 @@
 package org.bitcoins.dlc.wallet
 
 import org.bitcoins.commons.jsonmodels.dlc.DLCMessage.{
+  ContractInfo,
   DLCAccept,
   DLCOffer,
-  DLCSign
+  DLCSign,
+  OracleInfo
 }
 import org.bitcoins.commons.jsonmodels.dlc.{CETSignatures, DLCMessage}
+import org.bitcoins.core.currency.Satoshis
+import org.bitcoins.core.wallet.fee.SatoshisPerVirtualByte
+import org.bitcoins.crypto.{
+  CryptoUtil,
+  FieldElement,
+  SchnorrDigitalSignature,
+  SchnorrNonce,
+  SchnorrPublicKey
+}
+import org.bitcoins.testkit.wallet.DLCWalletUtil._
 import org.bitcoins.testkit.wallet.FundWalletUtil.FundedDLCWallet
 import org.bitcoins.testkit.wallet.{BitcoinSDualWalletTest, DLCWalletUtil}
 import org.scalatest.{Assertion, FutureOutcome}
+import scodec.bits.ByteVector
 
 import scala.concurrent.Future
 
@@ -218,5 +231,118 @@ class WalletDLCSetupTest extends BitcoinSDualWalletTest {
           accept.copy(
             cetSigs = CETSignatures(accept.cetSigs.outcomeSigs,
                                     DLCWalletUtil.dummyPartialSig)))
+  }
+
+  it must "setup and execute with lloyd's example" in {
+    FundedDLCWallets: (FundedDLCWallet, FundedDLCWallet) =>
+      val walletA = FundedDLCWallets._1.wallet
+      val walletB = FundedDLCWallets._2.wallet
+
+      val winHash = CryptoUtil.sha256(ByteVector("Democrat_win".getBytes))
+      val loseHash = CryptoUtil.sha256(ByteVector("Republican_win".getBytes))
+      val drawHash = CryptoUtil.sha256(ByteVector("other".getBytes))
+      val contractInfo = ContractInfo(winHash.bytes ++ Satoshis(
+        10000).bytes ++ loseHash.bytes ++ Satoshis.zero.bytes ++ drawHash.bytes ++ Satoshis(
+        5000).bytes)
+
+      val oraclePubKey = SchnorrPublicKey(
+        "156c7d1c7922f0aa1168d9e21ac77ea88bbbe05e24e70a08bbe0519778f2e5da")
+      val oracleNonce = SchnorrNonce(
+        "ea3a68d8749b81682513b0479418d289d17e24d4820df2ce979f1a56a63ca525")
+      val attestation = FieldElement(
+        "77a5aabd716936411bbe19219bd0b261fae8f0524367268feb264e0a3b215766")
+
+      val oracleInfo = OracleInfo(oraclePubKey, oracleNonce)
+
+      val offerData = DLCOffer(
+        contractInfo,
+        oracleInfo,
+        dummyDLCKeys,
+        Satoshis(5000),
+        Vector(dummyOutputRefs.head),
+        dummyAddress,
+        SatoshisPerVirtualByte(Satoshis(3)),
+        dummyTimeouts
+      )
+
+      val oracleSig = SchnorrDigitalSignature(oracleNonce, attestation)
+
+      val paramHash = DLCMessage.calcParamHash(offerData.oracleInfo,
+                                               offerData.contractInfo,
+                                               offerData.timeouts)
+
+      for {
+        offer <- walletA.createDLCOffer(
+          offerData.oracleInfo,
+          offerData.contractInfo,
+          offerData.totalCollateral,
+          Some(offerData.feeRate),
+          offerData.timeouts.contractMaturity.toUInt32,
+          offerData.timeouts.contractTimeout.toUInt32
+        )
+        _ = {
+          assert(offer.oracleInfo == offerData.oracleInfo)
+          assert(offer.contractInfo == offerData.contractInfo)
+          assert(offer.totalCollateral == offerData.totalCollateral)
+          assert(offer.feeRate == offerData.feeRate)
+          assert(offer.timeouts == offerData.timeouts)
+          assert(offer.fundingInputs.nonEmpty)
+          assert(offer.changeAddress.value.nonEmpty)
+        }
+
+        accept <- walletB.acceptDLCOffer(offer)
+        _ = {
+          assert(accept.fundingInputs.nonEmpty)
+          assert(
+            accept.totalCollateral == offer.contractInfo.values.max - offer.totalCollateral)
+          assert(accept.changeAddress.value.nonEmpty)
+        }
+
+        sign <- walletA.signDLC(accept)
+        _ = {
+          assert(sign.fundingSigs.keys.size == offerData.fundingInputs.size)
+        }
+
+        dlcDb <- walletB.addDLCSigs(sign)
+        outcomeSigs <- walletB.dlcSigsDAO.findByParamHash(offer.paramHash)
+
+        refundSigsA <-
+          walletA.dlcRefundSigDAO
+            .findByParamHash(paramHash)
+            .map(_.map(_.refundSig))
+        refundSigsB <-
+          walletB.dlcRefundSigDAO
+            .findByParamHash(paramHash)
+            .map(_.map(_.refundSig))
+
+        walletAChange <- walletA.addressDAO.read(offer.changeAddress)
+        walletAFinal <- walletA.addressDAO.read(offer.pubKeys.payoutAddress)
+
+        walletBChange <- walletB.addressDAO.read(accept.changeAddress)
+        walletBFinal <- walletB.addressDAO.read(accept.pubKeys.payoutAddress)
+
+        _ = {
+          assert(dlcDb.contractIdOpt.get == sign.contractId)
+
+          assert(refundSigsA.size == 2)
+          assert(refundSigsA.forall(refundSigsB.contains))
+
+          assert(sign.cetSigs.outcomeSigs.forall(sig =>
+            outcomeSigs.exists(_.toTuple == sig)))
+          // Test that the Addresses are in the wallet's database
+          assert(walletAChange.isDefined)
+          assert(walletAFinal.isDefined)
+          assert(walletBChange.isDefined)
+          assert(walletBFinal.isDefined)
+        }
+
+        func =
+          (wallet: DLCWallet) => wallet.executeDLC(sign.contractId, oracleSig)
+        result <- dlcExecutionTest(dlcA = walletA,
+                                   dlcB = walletB,
+                                   asInitiator = true,
+                                   func = func,
+                                   expectedOutputs = 1)
+      } yield assert(result)
   }
 }
