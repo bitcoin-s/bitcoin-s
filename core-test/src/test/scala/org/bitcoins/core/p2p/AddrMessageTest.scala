fa31609daca744d21ea217692d3a4661d923c30f
Ben Carman <benthecarman@live.com>
2020-10-02 10:36:15 -0500

    Enable AddrMessageTest (#2106)

--- core-test/src/test/scala/org/bitcoins/core/p2p/AddrMessageTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/p2p/AddrMessageTest.scala
@@ -1,15 +1,13 @@
 package org.bitcoins.core.p2p
 
-import org.bitcoins.testkit.util.BitcoinSUnitTest
-import org.bitcoins.testkit.core.gen.p2p.DataMessageGenerator
 import org.bitcoins.testkit.core.gen.p2p.ControlMessageGenerator
+import org.bitcoins.testkit.util.BitcoinSUnitTest
 
 class AddrMessageTest extends BitcoinSUnitTest {
   it must "have serialization symmetry" in {
     forAll(ControlMessageGenerator.addrMessage) { addr =>
       val fromBytes = AddrMessage.fromBytes(addr.bytes)
-      // assert(fromBytes == addr) todo
-      assert(true)
+      assert(fromBytes == addr)
     }
   }
 }
