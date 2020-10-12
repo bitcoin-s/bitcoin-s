11bf536287bdfc328362c2d89220aef03ef366a5
Ben Carman <benthecarman@live.com>
2020-10-12 16:59:57 -0500

    Have api endpoints return json

--- app/server/src/main/scala/org/bitcoins/server/NodeRoutes.scala
+++ app/server/src/main/scala/org/bitcoins/server/NodeRoutes.scala
@@ -3,6 +3,7 @@ package org.bitcoins.server
 import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
+import org.bitcoins.commons.serializers.Picklers._
 import org.bitcoins.core.api.node.NodeApi
 import org.bitcoins.node.Node
 import org.bitcoins.rpc.client.common.BitcoindRpcClient
@@ -49,7 +50,7 @@ case class NodeRoutes(nodeApi: NodeApi)(implicit system: ActorSystem)
         case Success(SendRawTransaction(tx)) =>
           complete {
             nodeApi.broadcastTransaction(tx).map { _ =>
-              Server.httpSuccess(s"${tx.txIdBE.hex}")
+              Server.httpSuccess(tx.txIdBE)
             }
           }
       }
