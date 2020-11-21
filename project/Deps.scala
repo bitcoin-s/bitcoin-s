912e3cd0c76be3437279b14bdf22f8b1aa1ec314
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-21 14:51:46 +0100

    Update akka-http, akka-http-testkit to 10.1.13 (#2276)

--- project/Deps.scala
+++ project/Deps.scala
@@ -14,7 +14,7 @@ object Deps {
     val slf4j = "1.7.30"
     val spray = "1.3.6"
     val zeromq = "0.5.2"
-    val akkav = "10.1.12"
+    val akkav = "10.1.13"
     val playv = "2.9.1"
     val akkaStreamv = "2.6.10"
     val scodecV = "1.1.21"
