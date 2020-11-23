b638c6837e6c9a145b4e0318fcb06878684d07f3
Chris Stewart <stewart.chris1234@gmail.com>
2020-11-23 10:27:22 -0600

    Move english word list to be represented by a Vector in memory (#2287)
    * Move english word list to be represented by a Vector in memory
    
    * rename to EnglishWordsBip39

---
+++ core-test/src/test/scala/org/bitcoins/core/crypto/words/EnglishWordsBip39Test.scala
@@ -0,0 +1,21 @@
+package org.bitcoins.core.crypto.words
+
+import org.bitcoins.testkit.util.BitcoinSUnitTest
+
+import scala.io.Source
+
+class EnglishWordsBip39Test extends BitcoinSUnitTest {
+  behavior of "EnglishWords"
+
+  private val ENGLISH_WORDS_FILE = "/bip39-wordlists/english.txt"
+
+  it must "have the same in memory and file contents" in {
+    val resourceStream = getClass.getResourceAsStream(ENGLISH_WORDS_FILE)
+    val source = Source.fromInputStream(resourceStream)
+
+    val lines = source.getLines()
+    val linesVec = lines.toVector
+    source.close()
+    assert(EnglishWordsBip39.getWords == linesVec)
+  }
+}
