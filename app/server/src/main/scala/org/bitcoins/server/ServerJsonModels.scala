f621f0c4876c28a0020bb9c069986f28fcc0a94e
Ben Carman <benthecarman@live.com>
2020-12-22 12:48:42 -0600

    Remove need for params in RPC request

--- app/server/src/main/scala/org/bitcoins/server/ServerJsonModels.scala
+++ app/server/src/main/scala/org/bitcoins/server/ServerJsonModels.scala
@@ -22,7 +22,22 @@ import scala.util.{Failure, Try}
 case class ServerCommand(method: String, params: ujson.Arr)
 
 object ServerCommand {
-  implicit val rw: ReadWriter[ServerCommand] = macroRW
+
+  implicit val rw: ReadWriter[ServerCommand] =
+    readwriter[ujson.Value].bimap[ServerCommand](
+      cmd => {
+        if (cmd.params.arr.isEmpty)
+          Obj("method" -> Str(cmd.method))
+        else Obj("method" -> Str(cmd.method), "params" -> cmd.params)
+      },
+      json => {
+        val obj = json.obj
+        val method = obj("method").str
+        if (obj.contains("params"))
+          ServerCommand(method, obj("params").arr)
+        else ServerCommand(method, Arr())
+      }
+    )
 }
 
 case class GetNewAddress(labelOpt: Option[AddressLabelTag])
