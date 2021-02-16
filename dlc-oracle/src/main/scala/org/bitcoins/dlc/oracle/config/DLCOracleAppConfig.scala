7a23a1cb3e02a424a9e4e3584c64b93490856a28
Ben Carman <benthecarman@live.com>
2021-02-16 10:35:22 -0600

    Fix exists function

--- dlc-oracle/src/main/scala/org/bitcoins/dlc/oracle/config/DLCOracleAppConfig.scala
+++ dlc-oracle/src/main/scala/org/bitcoins/dlc/oracle/config/DLCOracleAppConfig.scala
@@ -15,7 +15,7 @@ import org.bitcoins.keymanager.WalletStorage
 import org.bitcoins.keymanager.bip39.BIP39KeyManager
 import org.bitcoins.keymanager.config.KeyManagerAppConfig
 
-import java.nio.file.{Files, Path}
+import java.nio.file.{Files, Path, Paths}
 import scala.concurrent.{ExecutionContext, Future}
 
 case class DLCOracleAppConfig(
@@ -121,10 +121,11 @@ case class DLCOracleAppConfig(
   def seedExists(): Boolean = kmConf.seedExists()
 
   def exists(): Boolean = {
+    val dbPath = config.getString("bitcoin-s.oracle.db.path")
     lazy val hasDb = this.driver match {
       case PostgreSQL => true
       case SQLite =>
-        Files.exists(datadir.resolve("oracle.sqlite"))
+        Files.exists(Paths.get(dbPath))
     }
     seedExists() && hasDb
   }
