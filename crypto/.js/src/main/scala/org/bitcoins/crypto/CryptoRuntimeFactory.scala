a55e1734e133ade918b4b1b085e5304135c11c67
rorp <rorp@users.noreply.github.com>
2021-02-24 08:31:05 -0800

    some more changes

--- crypto/.js/src/main/scala/org/bitcoins/crypto/CryptoRuntimeFactory.scala
+++ crypto/.js/src/main/scala/org/bitcoins/crypto/CryptoRuntimeFactory.scala
@@ -3,3 +3,5 @@ package org.bitcoins.crypto
 sealed protected trait CryptoRuntimeFactory {
   def newCryptoRuntime: CryptoRuntime = BCryptoCryptoRuntime
 }
+
+protected object CryptoRuntimeFactory extends CryptoRuntimeFactory
