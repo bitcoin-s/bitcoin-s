8c008b8a40154b299b1cdd9e264f2b7404146c99
Chris Stewart <stewart.chris1234@gmail.com>
2021-03-09 08:08:37 -0600

    Try bumping threadpool for scheduler

--- async-utils/src/main/scala/org/bitcoins/asyncutil/AsyncUtil.scala
+++ async-utils/src/main/scala/org/bitcoins/asyncutil/AsyncUtil.scala
@@ -145,7 +145,8 @@ abstract class AsyncUtil extends BitcoinSLogger {
 
 object AsyncUtil extends AsyncUtil {
 
-  private[bitcoins] val scheduler = Executors.newScheduledThreadPool(2)
+  private[bitcoins] val scheduler = Executors.newScheduledThreadPool(
+    Runtime.getRuntime.availableProcessors() * 2)
 
   /** The default interval between async attempts
     */
