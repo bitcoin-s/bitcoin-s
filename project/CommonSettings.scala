d315b5028b43dc1c6d810d505526f6be39d00f88
Chris Stewart <stewart.chris1234@gmail.com>
2021-03-15 15:33:17 -0500

    update Base docker image to a ubuntu buster (#2799)

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -164,7 +164,7 @@ object CommonSettings {
   lazy val dockerSettings: Seq[Setting[_]] = {
     Vector(
       //https://sbt-native-packager.readthedocs.io/en/latest/formats/docker.html
-      dockerBaseImage := "openjdk",
+      dockerBaseImage := "openjdk:15.0.2-jdk-buster",
       dockerRepository := Some("bitcoinscala"),
       //set the user to be 'bitcoin-s' rather than
       //the default provided by sbt native packager
