15cc7600be49632439782eed31e9e86709c28c77
rorp <rorp@users.noreply.github.com>
2021-03-15 09:56:18 -0700

    add some more tests

--- crypto-test/src/test/scala/org/bitcoins/crypto/SignTest.scala
+++ crypto-test/src/test/scala/org/bitcoins/crypto/SignTest.scala
@@ -1,5 +1,7 @@
 package org.bitcoins.crypto
 
+import scodec.bits.ByteVector
+
 class SignTest extends BitcoinSCryptoAsyncTest {
 
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
@@ -11,6 +13,21 @@ class SignTest extends BitcoinSCryptoAsyncTest {
   val pubKey: ECPublicKey = privKey.publicKey
 
   behavior of "Sign"
+
+  it must "identify DER encoded signatures" in {
+    val privateKey = ECPrivateKey.fromHex(
+      "109cc9befbae8ff3e8b342f08091bf6e6a36d2b6e7acfa9b477d9abc71eb94b4")
+    val publicKey = privateKey.publicKey
+    val data = ByteVector.fromValidHex(
+      "258592575c6bd6d489c38662199f3d469fa9296b56d5873159eb66775035919e")
+    val sigCompact = ECDigitalSignature.fromHex(
+      "b4b07c3373ec271ccf0c51e7491f6d82cdc7b0c50f8ea11130fa266348824a2a2e3f190a4d139faa3e17c677cd8fdcc96c51aee7c1cb2e6edb1d6f8637063f20")
+    val sigDER = ECDigitalSignature.fromHex(
+      "3045022100b4b07c3373ec271ccf0c51e7491f6d82cdc7b0c50f8ea11130fa266348824a2a02202e3f190a4d139faa3e17c677cd8fdcc96c51aee7c1cb2e6edb1d6f8637063f20")
+    assert(DERSignatureUtil.isDEREncoded(sigDER))
+    assert(!DERSignatureUtil.isDEREncoded(sigCompact))
+    assert(publicKey.verify(data, sigDER))
+  }
 
   it must "sign arbitrary pieces of data correctly" in {
     forAllAsync(CryptoGenerators.sha256Digest) { hash =>
