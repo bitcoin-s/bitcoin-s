50d4e1f9698642a100a7dd870569242a04fdfe16
benthecarman <benthecarman@live.com>
2021-03-23 12:06:18 -0500

    Move hard coded test vectors from resource files into scala files (#2818)
    * Remove sighash.json resource
    
    * Remove trezor_bip39_vectors.json
    
    * Hardcode transaction json test vectors
    
    * Hardcode big size uint json test vectors
    
    * Hardcode base58 json test vectors
    
    * Hardcode script tests json test vectors
    
    * Make BlockTest JVM only

--- core-test/src/test/scala/org/bitcoins/core/protocol/BigSizeUIntTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/protocol/BigSizeUIntTest.scala
@@ -27,51 +27,183 @@ class BigSizeUIntTest extends BitcoinSUnitTest {
   }
 
   it must "pass encoding tests" in {
-    val bufferedSource =
-      io.Source.fromURL(getClass.getResource("/bigsize_encoding.json"))
-    try {
-      val builder = new StringBuilder
-      bufferedSource.getLines().foreach(builder.append)
-      val tests = ujson.read(builder.result()).arr.toVector
-      tests.foreach { test =>
-        val obj = test.obj
-        val name = obj("name").str
-        val num = BigInt(obj("value").str)
-        val bytes = ByteVector.fromValidHex(obj("bytes").str)
-        assert(BigSizeUInt(num).bytes == bytes, name)
-      }
-    } finally {
-      bufferedSource.close()
+    val tests = ujson.read(BigSizeJsonTestVectors.encode).arr.toVector
+    tests.foreach { test =>
+      val obj = test.obj
+      val name = obj("name").str
+      val num = BigInt(obj("value").str)
+      val bytes = ByteVector.fromValidHex(obj("bytes").str)
+      assert(BigSizeUInt(num).bytes == bytes, name)
     }
   }
 
   it must "pass decoding tests" in {
-    val bufferedSource =
-      io.Source.fromURL(getClass.getResource("/bigsize_decoding.json"))
-    try {
-      val builder = new StringBuilder
-      bufferedSource.getLines().foreach(builder.append)
-      val tests = ujson.read(builder.result()).arr.toVector
-      tests.foreach { test =>
-        val obj = test.obj
-        val name = obj("name").str
-        val numStr = obj("value").str
-        val bytes = ByteVector.fromValidHex(obj("bytes").str)
-        if (numStr.nonEmpty) {
-          assert(BigSizeUInt(bytes).num == UInt64(BigInt(numStr)), name)
-        } else {
-          Try {
-            assertThrows[IllegalArgumentException] {
-              BigSizeUInt(bytes)
-            }
-          } match {
-            case Failure(err)     => fail(obj("exp_error").str, err)
-            case Success(success) => success
+    val tests = ujson.read(BigSizeJsonTestVectors.decode).arr.toVector
+    tests.foreach { test =>
+      val obj = test.obj
+      val name = obj("name").str
+      val numStr = obj("value").str
+      val bytes = ByteVector.fromValidHex(obj("bytes").str)
+      if (numStr.nonEmpty) {
+        assert(BigSizeUInt(bytes).num == UInt64(BigInt(numStr)), name)
+      } else {
+        Try {
+          assertThrows[IllegalArgumentException] {
+            BigSizeUInt(bytes)
           }
+        } match {
+          case Failure(err)     => fail(obj("exp_error").str, err)
+          case Success(success) => success
         }
       }
-    } finally {
-      bufferedSource.close()
     }
   }
+}
+
+object BigSizeJsonTestVectors {
+  val encode: String = """[
+                         |  {
+                         |    "name": "zero",
+                         |    "value": "0",
+                         |    "bytes": "00"
+                         |  },
+                         |  {
+                         |    "name": "one byte high",
+                         |    "value": "252",
+                         |    "bytes": "fc"
+                         |  },
+                         |  {
+                         |    "name": "two byte low",
+                         |    "value": "253",
+                         |    "bytes": "fd00fd"
+                         |  },
+                         |  {
+                         |    "name": "two byte high",
+                         |    "value": "65535",
+                         |    "bytes": "fdffff"
+                         |  },
+                         |  {
+                         |    "name": "four byte low",
+                         |    "value": "65536",
+                         |    "bytes": "fe00010000"
+                         |  },
+                         |  {
+                         |    "name": "four byte high",
+                         |    "value": "4294967295",
+                         |    "bytes": "feffffffff"
+                         |  },
+                         |  {
+                         |    "name": "eight byte low",
+                         |    "value": "4294967296",
+                         |    "bytes": "ff0000000100000000"
+                         |  },
+                         |  {
+                         |    "name": "eight byte high",
+                         |    "value": "18446744073709551615",
+                         |    "bytes": "ffffffffffffffffff"
+                         |  }
+                         |]""".stripMargin
+
+  val decode: String = """[
+                         |  {
+                         |    "name": "zero",
+                         |    "value": "0",
+                         |    "bytes": "00"
+                         |  },
+                         |  {
+                         |    "name": "one byte high",
+                         |    "value": "252",
+                         |    "bytes": "fc"
+                         |  },
+                         |  {
+                         |    "name": "two byte low",
+                         |    "value": "253",
+                         |    "bytes": "fd00fd"
+                         |  },
+                         |  {
+                         |    "name": "two byte high",
+                         |    "value": "65535",
+                         |    "bytes": "fdffff"
+                         |  },
+                         |  {
+                         |    "name": "four byte low",
+                         |    "value": "65536",
+                         |    "bytes": "fe00010000"
+                         |  },
+                         |  {
+                         |    "name": "four byte high",
+                         |    "value": "4294967295",
+                         |    "bytes": "feffffffff"
+                         |  },
+                         |  {
+                         |    "name": "eight byte low",
+                         |    "value": "4294967296",
+                         |    "bytes": "ff0000000100000000"
+                         |  },
+                         |  {
+                         |    "name": "eight byte high",
+                         |    "value": "18446744073709551615",
+                         |    "bytes": "ffffffffffffffffff"
+                         |  },
+                         |  {
+                         |    "name": "two byte not canonical",
+                         |    "value": "",
+                         |    "bytes": "fd00fc",
+                         |    "exp_error": "decoded bigsize is not canonical"
+                         |  },
+                         |  {
+                         |    "name": "four byte not canonical",
+                         |    "value": "",
+                         |    "bytes": "fe0000ffff",
+                         |    "exp_error": "decoded bigsize is not canonical"
+                         |  },
+                         |  {
+                         |    "name": "eight byte not canonical",
+                         |    "value": "",
+                         |    "bytes": "ff00000000ffffffff",
+                         |    "exp_error": "decoded bigsize is not canonical"
+                         |  },
+                         |  {
+                         |    "name": "two byte short read",
+                         |    "value": "",
+                         |    "bytes": "fd00",
+                         |    "exp_error": "unexpected EOF"
+                         |  },
+                         |  {
+                         |    "name": "four byte short read",
+                         |    "value": "",
+                         |    "bytes": "feffff",
+                         |    "exp_error": "unexpected EOF"
+                         |  },
+                         |  {
+                         |    "name": "eight byte short read",
+                         |    "value": "",
+                         |    "bytes": "ffffffffff",
+                         |    "exp_error": "unexpected EOF"
+                         |  },
+                         |  {
+                         |    "name": "one byte no read",
+                         |    "value": "",
+                         |    "bytes": "",
+                         |    "exp_error": "EOF"
+                         |  },
+                         |  {
+                         |    "name": "two byte no read",
+                         |    "value": "",
+                         |    "bytes": "fd",
+                         |    "exp_error": "unexpected EOF"
+                         |  },
+                         |  {
+                         |    "name": "four byte no read",
+                         |    "value": "",
+                         |    "bytes": "fe",
+                         |    "exp_error": "unexpected EOF"
+                         |  },
+                         |  {
+                         |    "name": "eight byte no read",
+                         |    "value": "",
+                         |    "bytes": "ff",
+                         |    "exp_error": "unexpected EOF"
+                         |  }
+                         |]""".stripMargin
 }
