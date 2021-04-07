adf31d9a728a9a074cba367cd4f2a405a9f3c14b
benthecarman <benthecarman@live.com>
2021-04-07 15:11:45 -0500

    Add website url to ExplorerEnv (#2868)
    * Add website url to ExplorerEnv
    
    * l -> 1

--- oracle-explorer-client/src/main/scala/org/bitcoins/explorer/env/ExplorerEnv.scala
+++ oracle-explorer-client/src/main/scala/org/bitcoins/explorer/env/ExplorerEnv.scala
@@ -3,22 +3,26 @@ package org.bitcoins.explorer.env
 import org.bitcoins.crypto.StringFactory
 
 sealed trait ExplorerEnv {
+  def siteUrl: String
   def baseUri: String
 }
 
 object ExplorerEnv extends StringFactory[ExplorerEnv] {
 
   case object Production extends ExplorerEnv {
-    override val baseUri: String = "https://oracle.suredbits.com/v1/"
+    override val siteUrl: String = "https://oracle.suredbits.com/"
+    override val baseUri: String = s"${siteUrl}v1/"
   }
 
   case object Test extends ExplorerEnv {
-    override val baseUri: String = "https://test.oracle.suredbits.com/v1/"
+    override val siteUrl: String = "https://test.oracle.suredbits.com/"
+    override val baseUri: String = s"${siteUrl}v1/"
   }
 
   /** For local testing purposes */
   case object Local extends ExplorerEnv {
-    override val baseUri: String = "http://localhost:9000/v1/"
+    override val siteUrl: String = "http://localhost:9000/"
+    override val baseUri: String = s"${siteUrl}v1/"
   }
 
   val all: Vector[ExplorerEnv] = Vector(Production, Test, Local)
