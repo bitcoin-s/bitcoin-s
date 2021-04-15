41a2104b5f04e9656540ea54726519626fc2ce38
Ben Carman <benthecarman@live.com>
2021-04-15 10:50:42 -0500

    Make offer dialog enum only

--- app/gui/src/main/scala/org/bitcoins/gui/util/GUIUtil.scala
+++ app/gui/src/main/scala/org/bitcoins/gui/util/GUIUtil.scala
@@ -1,7 +1,10 @@
 package org.bitcoins.gui.util
 
-import scalafx.Includes._
-import scalafx.scene.control.TextField
+import scalafx.scene.control.{TextField, TextFormatter}
+
+import java.text.NumberFormat
+import scala.util.control.NonFatal
+import scala.util.matching.Regex
 
 import java.text.NumberFormat
 import scala.util.matching.Regex
@@ -12,13 +15,34 @@ object GUIUtil {
   val numberFormatter: NumberFormat = java.text.NumberFormat.getIntegerInstance
 
   def setNumericInput(textField: TextField): Unit = {
-    textField.text.addListener {
-      (
-          _: javafx.beans.value.ObservableValue[_ <: String],
-          _: String,
-          newVal: String) =>
-        if (!newVal.matches("-?\\d*"))
-          textField.setText(newVal.replaceAll("[^-?\\d]", ""))
-    }
+    textField.textFormatter =
+      new TextFormatter[String]((change: TextFormatter.Change) => {
+        if (change.isContentChange) {
+          val newText = change.getControlNewText
+          if (
+            newText.isEmpty || (!numericRegex.pattern.matcher(newText).matches)
+          ) {
+            change
+          } else {
+            val formatted = {
+              try {
+                val num = numberFormatter.parse(newText)
+                numberFormatter.format(num)
+              } catch {
+                case NonFatal(_) => newText // allow input if error
+              }
+            }
+
+            // replace with modified text
+            change.setRange(0, change.getRangeEnd)
+            change.setText(formatted)
+            change.setCaretPosition(formatted.length)
+            change.setAnchor(formatted.length)
+            change
+          }
+        } else {
+          change // no need for modification, if only the selection changes
+        }
+      })
   }
 }
