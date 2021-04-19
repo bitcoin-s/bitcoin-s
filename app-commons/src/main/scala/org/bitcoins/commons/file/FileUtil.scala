1757aae6dd03651a34612692a2d2617bc0eb34f2
Chris Stewart <stewart.chris1234@gmail.com>
2021-04-19 15:53:25 -0500

    Fix bug where we weren't closing zip file

---
+++ app-commons/src/main/scala/org/bitcoins/commons/file/FileUtil.scala
@@ -0,0 +1,41 @@
+package org.bitcoins.commons.file
+
+import grizzled.slf4j.Logging
+
+import java.io.{FileOutputStream, IOException}
+import java.nio.file.Path
+import java.util.zip.ZipOutputStream
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.zip.ZipEntry
+
+object FileUtil extends Logging {
+
+  /** Zips the [[directory]] into a zip file and then stores it at [[target]]
+    * @see https://www.quickprogrammingtips.com/java/how-to-zip-a-folder-in-java.html
+    */
+  def zipDirectory(source: Path, target: Path): Path = {
+    val zos = new ZipOutputStream(new FileOutputStream(target.toFile()))
+    Files.walkFileTree(
+      source,
+      new SimpleFileVisitor[Path]() {
+        @throws[IOException]
+        override def visitFile(
+            file: Path,
+            attrs: BasicFileAttributes): FileVisitResult = {
+          logger.info(
+            s"Zipping file=${file.toAbsolutePath} to ${target.toAbsolutePath}")
+          zos.putNextEntry(new ZipEntry(source.relativize(file).toString))
+          Files.copy(file, zos)
+          zos.closeEntry()
+          logger.info(s"Done zipping file=${file.toAbsolutePath}")
+          FileVisitResult.CONTINUE
+        }
+      }
+    )
+    zos.close()
+    target
+  }
+}
