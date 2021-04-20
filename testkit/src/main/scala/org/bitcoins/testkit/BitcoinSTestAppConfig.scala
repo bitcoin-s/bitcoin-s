de5f7fc7f9db6ad07bce280af6f3694368be35de
Chris Stewart <stewart.chris1234@gmail.com>
2021-04-20 12:47:28 -0500

    Reduce number of threads in postgres connection pool for tests  (#2931)
    * Reduce number of threads in postgres connection pool from 10 -> 2
    
    * Try reducing to just 1 thread

--- testkit/src/main/scala/org/bitcoins/testkit/BitcoinSTestAppConfig.scala
+++ testkit/src/main/scala/org/bitcoins/testkit/BitcoinSTestAppConfig.scala
@@ -181,7 +181,7 @@ object BitcoinSTestAppConfig {
          |   user = "postgres"
          |   password = "postgres"
          |   port = $port
-         |   numThreads = 10
+         |   numThreads = 1
          |   keepAliveConnection = true
          | }""".stripMargin
     }
