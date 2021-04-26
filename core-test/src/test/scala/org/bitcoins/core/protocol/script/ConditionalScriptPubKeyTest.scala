279b93f9e0d1607daebd0448dc4737e9d927102d
Chris Stewart <stewart.chris1234@gmail.com>
2021-04-26 12:46:50 -0500

    Rework P2SHScriptSignature.isStandardNonP2SH() (#2963)
    * Take ben's suggestion for parsing the entire tx hex
    
    * Add assertion that the secondSPK is NOT a P2SHScriptPubKey, revert the classification of P2SHScriptPubKey in P2SHScriptSignature.isStandardNonP2SH()
    
    * Revert conditionalSPK match logic
    
    * Fixed ConditionalScriptPubKeyTest.scala (#32)
    
    Co-authored-by: Nadav Kohen <nadavk25@gmail.com>

--- core-test/src/test/scala/org/bitcoins/core/protocol/script/ConditionalScriptPubKeyTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/protocol/script/ConditionalScriptPubKeyTest.scala
@@ -1,5 +1,6 @@
 package org.bitcoins.core.protocol.script
 
+import org.bitcoins.core.protocol.transaction.Transaction
 import org.bitcoins.core.script.constant.ScriptConstant
 import org.bitcoins.testkitcore.util.BitcoinSJvmTest
 
@@ -27,10 +28,22 @@ class ConditionalScriptPubKeyTest extends BitcoinSJvmTest {
       "632103184b16f5d6c01e2d6ded3f8a292e5b81608318ecb8e93aa3747bc88b8dbf256cac67a914f5862841f254a1483eab66909ae588e45d617c5e8768"
     val scriptPubKey = RawScriptPubKey.fromAsmHex(hex)
 
-    assert(scriptPubKey.isInstanceOf[IfConditionalScriptPubKey])
-
+    scriptPubKey match {
+      case conditional: IfConditionalScriptPubKey =>
+        assert(
+          conditional.secondSPK.isInstanceOf[NonStandardScriptPubKey],
+          s"Although we have same op codes as p2sh spk, " +
+            s"this combination isn't used in a standalone output, rather a redeemScript"
+        )
+      case x => fail(s"Incorrect type for the spk, got=$x")
+    }
     val constant = ScriptConstant.fromHex(hex)
 
     assert(P2SHScriptSignature.isRedeemScript(constant))
+
+    //we should be able to parse the tx that contains it with no problem
+    val txHex =
+      "010000000193983cb2f1f3d83615f10e1ca33f49c72250acbebd840793aaa8bc7a6b28b76b000000008848304502207ffb30631d837895ac1b415408b70a809090b25d4070198043299fe247f62d2602210089b52f0d243dea1e4313ef67723b0d0642ff77bb6ca05ea85296b2539c797f5c81513d632103184b16f5d6c01e2d6ded3f8a292e5b81608318ecb8e93aa3747bc88b8dbf256cac67a914f5862841f254a1483eab66909ae588e45d617c5e8768ffffffff01a0860100000000001976a914a07aa8415d34d0db44f220ff9522609641c0bfbf88ac00000000"
+    assert(Transaction.fromHexT(txHex).isSuccess)
   }
 }
