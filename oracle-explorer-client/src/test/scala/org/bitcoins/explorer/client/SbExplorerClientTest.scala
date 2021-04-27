5a5213e946caed5cb329fb165036ae6afa95941d
Chris Stewart <stewart.chris1234@gmail.com>
2021-04-27 07:30:52 -0500

    Switch explorer over to prod for now as test is down to get CI passing

--- oracle-explorer-client/src/test/scala/org/bitcoins/explorer/client/SbExplorerClientTest.scala
+++ oracle-explorer-client/src/test/scala/org/bitcoins/explorer/client/SbExplorerClientTest.scala
@@ -19,7 +19,7 @@ class SbExplorerClientTest extends BitcoinSAsyncTest {
 
   behavior of "SbExplorerClient"
 
-  val explorerClient = SbExplorerClient(ExplorerEnv.Test)
+  val explorerClient = SbExplorerClient(ExplorerEnv.Production)
 
   //https://test.oracle.suredbits.com/event/57505dcdfe8746d9adf3454df538244a425f302c07642d9dc4a4f635fbf08d30
   private val announcementHex: String =
