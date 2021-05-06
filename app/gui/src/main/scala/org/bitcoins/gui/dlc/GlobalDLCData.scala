f011e60efc49b95821f6cfde6fbf15e03ebe7486
benthecarman <benthecarman@live.com>
2021-05-06 10:44:27 -0500

    Show DLC details when signing DLC, reoder accept details (#3028)

--- app/gui/src/main/scala/org/bitcoins/gui/dlc/GlobalDLCData.scala
+++ app/gui/src/main/scala/org/bitcoins/gui/dlc/GlobalDLCData.scala
@@ -1,8 +1,14 @@
 package org.bitcoins.gui.dlc
 
+import org.bitcoins.core.protocol.dlc.DLCStatus
+import scalafx.collections.ObservableBuffer
+
 object GlobalDLCData {
   var lastContractId: String = ""
   var lastOracleSig: String = ""
   var lastOracleAnnouncement: String = ""
   var lastContractInfo: String = ""
+
+  val dlcs: ObservableBuffer[DLCStatus] =
+    new ObservableBuffer[DLCStatus]()
 }
