ee5ebb7f6719717437701f72005c368839dda983
Chris Stewart <stewart.chris1234@gmail.com>
2021-05-26 15:04:42 -0500

    Add more unit tests for u64 mapper

--- db-commons-test/src/test/scala/org/bitcoins/db/UInt64MapperTest.scala
+++ db-commons-test/src/test/scala/org/bitcoins/db/UInt64MapperTest.scala
@@ -11,25 +11,39 @@ class UInt64MapperTest extends BitcoinSUnitTest {
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration = {
     generatorDrivenConfigNewCode
   }
-  val sqlite = new DbCommonsColumnMappers(SQLiteProfile)
+  private val sqlite = new DbCommonsColumnMappers(SQLiteProfile)
 
-  val postgres = new DbCommonsColumnMappers(PostgresProfile)
+  private val postgres = new DbCommonsColumnMappers(PostgresProfile)
+
   it must "map a large uint64 to bytes and back" in {
     val u64 = UInt64.max
-    val sqliteHex = sqlite.uInt64ToHex(UInt64.max)
+    val sqliteHex = sqlite.uInt64ToHex(u64)
     val sqliteu64 = UInt64.fromHex(sqliteHex)
     assert(sqliteu64 == u64)
 
-    val pgHex = postgres.uInt64ToHex(UInt64.max - UInt64.one)
+    val pgHex = postgres.uInt64ToHex(u64)
     val pgu64 = UInt64.fromHex(pgHex)
     assert(pgu64 == u64)
-
   }
+
+  it must "map UInt64.zero to bytes and back" in {
+    val u64 = UInt64.zero
+    val sqliteHex = sqlite.uInt64ToHex(u64)
+    val sqliteu64 = UInt64.fromHex(sqliteHex)
+    assert(sqliteu64 == u64)
+
+    val pgHex = postgres.uInt64ToHex(u64)
+    val pgu64 = UInt64.fromHex(pgHex)
+    assert(pgu64 == u64)
+  }
+
   it must "map uint64 to bytes and back" in {
     forAll(NumberGenerator.uInt64) { u64 =>
-      val sqliteu64 = sqlite.uInt64ToHex(u64)
+      val sqliteHex = sqlite.uInt64ToHex(u64)
+      val sqliteu64 = UInt64.fromHex(sqliteHex)
       assert(sqliteu64 == u64)
-      val pgu64 = postgres.uInt64ToHex(u64)
+      val pgHex = postgres.uInt64ToHex(u64)
+      val pgu64 = UInt64.fromHex(pgHex)
       assert(pgu64 == u64)
     }
   }
