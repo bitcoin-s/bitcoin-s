9baea88d04f4cf52a4c7ff47a298fa461bc792c4
Ben Carman <benthecarman@live.com>
2021-05-27 04:16:13 -0500

    Fix logging in bundle

--- app/bundle/src/main/scala/org/bitcoins/bundle/gui/BundleGUI.scala
+++ app/bundle/src/main/scala/org/bitcoins/bundle/gui/BundleGUI.scala
@@ -1,5 +1,9 @@
 package org.bitcoins.bundle.gui
 
+import com.typesafe.config.Config
+import org.bitcoins.core.config._
+import org.bitcoins.db.AppConfig
+import org.bitcoins.db.AppConfig.DEFAULT_BITCOIN_S_DATADIR
 import org.bitcoins.gui._
 import org.bitcoins.gui.util.GUIUtil
 import scalafx.application.{JFXApp, Platform}
@@ -8,6 +12,8 @@ import scalafx.scene.Scene
 import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.control._
 import scalafx.scene.layout.VBox
+
+import java.nio.file.{Path, Paths}
 
 object BundleGUI extends WalletGUI with JFXApp {
 
@@ -25,6 +31,43 @@ object BundleGUI extends WalletGUI with JFXApp {
     })
 
   lazy val args = parameters.raw
+
+  // Set log location
+  {
+    val baseConfig: Config = AppConfig
+      .getBaseConfig(DEFAULT_BITCOIN_S_DATADIR)
+      .resolve()
+
+    val datadir: Path =
+      Paths.get(baseConfig.getString("bitcoin-s.datadir"))
+
+    val networkStr: String =
+      baseConfig.getString("bitcoin-s.network")
+
+    val network: BitcoinNetwork = networkStr.toLowerCase match {
+      case "mainnet"  => MainNet
+      case "main"     => MainNet
+      case "testnet3" => TestNet3
+      case "testnet"  => TestNet3
+      case "test"     => TestNet3
+      case "regtest"  => RegTest
+      case "signet"   => SigNet
+      case "sig"      => SigNet
+      case _: String =>
+        throw new IllegalArgumentException(s"Invalid network $networkStr")
+    }
+
+    val lastDirname = network match {
+      case MainNet  => "mainnet"
+      case TestNet3 => "testnet3"
+      case RegTest  => "regtest"
+      case SigNet   => "signet"
+    }
+
+    val usedDir = datadir.resolve(lastDirname)
+
+    System.setProperty("bitcoins.log.location", usedDir.toAbsolutePath.toString)
+  }
 
   val landingPane = new LandingPane(glassPane)
   rootView.children = Vector(landingPane.view, glassPane)
