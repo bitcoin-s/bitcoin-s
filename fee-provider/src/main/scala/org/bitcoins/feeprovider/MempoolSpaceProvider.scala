a98a26c9296a8e6f8a0e19c7fd36bafb4b39fa22
benthecarman <benthecarman@live.com>
2021-06-22 12:07:44 -0500

    Make mempool.space fee provider network specific (#3316)

--- fee-provider/src/main/scala/org/bitcoins/feeprovider/MempoolSpaceProvider.scala
+++ fee-provider/src/main/scala/org/bitcoins/feeprovider/MempoolSpaceProvider.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.Uri
 import org.bitcoins.commons.jsonmodels.wallet.MempoolSpaceResult
 import org.bitcoins.commons.serializers.JsonSerializers._
+import org.bitcoins.core.config._
 import org.bitcoins.core.wallet.fee.SatoshisPerVirtualByte
 import org.bitcoins.feeprovider.MempoolSpaceTarget._
 import play.api.libs.json.{JsError, JsSuccess, Json}
@@ -13,12 +14,22 @@ import scala.util.{Failure, Success, Try}
 /** Fetches fee rate from mempool.space's API
   * Documentation found here: https://mempool.space/about
   */
-case class MempoolSpaceProvider(target: MempoolSpaceTarget)(implicit
-    override val system: ActorSystem)
+case class MempoolSpaceProvider(
+    target: MempoolSpaceTarget,
+    network: BitcoinNetwork)(implicit override val system: ActorSystem)
     extends CachedHttpFeeRateProvider[SatoshisPerVirtualByte] {
 
-  override val uri: Uri =
-    Uri("https://mempool.space/api/v1/fees/recommended")
+  override val uri: Uri = network match {
+    case MainNet =>
+      Uri("https://mempool.space/api/v1/fees/recommended")
+    case TestNet3 =>
+      Uri("https://mempool.space/testnet/api/v1/fees/recommended")
+    case SigNet =>
+      Uri("https://mempool.space/signet/api/v1/fees/recommended")
+    case RegTest =>
+      // just use testnet here, shouldn't matter
+      Uri("https://mempool.space/testnet/api/v1/fees/recommended")
+  }
 
   override def converter(str: String): Try[SatoshisPerVirtualByte] = {
     val json = Json.parse(str)
@@ -45,7 +56,13 @@ object MempoolSpaceProvider extends FeeProviderFactory[MempoolSpaceProvider] {
   override def fromBlockTarget(blocks: Int)(implicit
       system: ActorSystem): MempoolSpaceProvider = {
     val target = MempoolSpaceTarget.fromBlockTarget(blocks)
-    MempoolSpaceProvider(target)
+    MempoolSpaceProvider(target, MainNet)
+  }
+
+  def fromBlockTarget(blocks: Int, network: BitcoinNetwork)(implicit
+      system: ActorSystem): MempoolSpaceProvider = {
+    val target = MempoolSpaceTarget.fromBlockTarget(blocks)
+    MempoolSpaceProvider(target, network)
   }
 }
 
