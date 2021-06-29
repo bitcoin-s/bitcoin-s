81fe7d76eec852a9452b3ff278b29c92c081adbd
benthecarman <benthecarman@live.com>
2021-06-29 13:30:32 -0500

    Remove canceled DLCs from table (#3349)

--- app/gui/src/main/scala/org/bitcoins/gui/dlc/DLCPaneModel.scala
+++ app/gui/src/main/scala/org/bitcoins/gui/dlc/DLCPaneModel.scala
@@ -81,6 +81,8 @@ class DLCPaneModel(pane: DLCPane)(implicit ec: ExecutionContext)
   def updateDLCs(): Unit = {
     getDLCs.map { dlcs =>
       dlcs.foreach(updateDLCInList)
+      val toRemove = GlobalDLCData.dlcs.diff(dlcs)
+      GlobalDLCData.dlcs --= toRemove
       pane.sortTable()
     }
 
