ae68dd45623b00fca70d9eb2834e033d5bad2109
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-02 13:55:22 +0200

    Update sqlite-jdbc to 3.36.0.1 (#3359)

--- project/Deps.scala
+++ project/Deps.scala
@@ -33,7 +33,7 @@ object Deps {
     val postgresV = "42.2.22"
     val akkaActorV = akkaStreamv
     val slickV = "3.3.3"
-    val sqliteV = "3.36.0"
+    val sqliteV = "3.36.0.1"
 
     val scalameterV = "0.17"
     val scalamockV = "5.1.0"
