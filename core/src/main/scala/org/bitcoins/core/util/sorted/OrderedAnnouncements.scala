418e37c2bf24b9c873f107af714b6f6a2562615e
benthecarman <benthecarman@live.com>
2021-07-05 13:55:43 -0500

    Update scaladoc

--- core/src/main/scala/org/bitcoins/core/util/sorted/OrderedAnnouncements.scala
+++ core/src/main/scala/org/bitcoins/core/util/sorted/OrderedAnnouncements.scala
@@ -2,13 +2,17 @@ package org.bitcoins.core.util.sorted
 
 import org.bitcoins.core.protocol.tlv._
 
-/** Represents an ordered set of OracleAnnouncementTLVs */
+/** Represents an ordered set of OracleAnnouncementTLVs
+  * The ordering represents the ranked preference of the user
+  */
 case class OrderedAnnouncements(vec: Vector[OracleAnnouncementTLV])
     extends SortedVec[OracleAnnouncementTLV, OracleAnnouncementTLV](
       vec,
       SortedVec.forOrdered(vec))
 
-/** Represents an ordered set of OracleAnnouncementV0TLV */
+/** Represents an ordered set of OracleAnnouncementV0TLV
+  * The ordering represents the ranked preference of the user
+  */
 case class OrderedAnnouncementV0s(vec: Vector[OracleAnnouncementV0TLV])
     extends SortedVec[OracleAnnouncementV0TLV, OracleAnnouncementV0TLV](
       vec,
