46b8ff2841c0d59a9033461950a04ab3c66b5f32
Scala Steward <me@scala-steward.org>
2021-07-09 06:23:56 +0200

    Update scala-collection-compat to 2.5.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -37,7 +37,7 @@ object Deps {
 
     val scalameterV = "0.17"
     val scalamockV = "5.1.0"
-    val scalaCollectionCompatV = "2.4.4"
+    val scalaCollectionCompatV = "2.5.0"
     val pgEmbeddedV = "0.13.4"
 
     val breezeV = "1.2"
