406988ef9180adf6dcc6ae18bad4df71f2b13b34
benthecarman <benthecarman@live.com>
2021-07-27 10:27:03 -0500

    Respond to some review

--- dlc-node-test/src/test/scala/org/bitcoins/dlc/node/DLCConnectionHandlerTest.scala
+++ dlc-node-test/src/test/scala/org/bitcoins/dlc/node/DLCConnectionHandlerTest.scala
@@ -3,7 +3,6 @@ package org.bitcoins.dlc.node
 import org.bitcoins.core.protocol.tlv._
 import org.bitcoins.testkit.node._
 import org.bitcoins.testkit.util.BitcoinSAsyncTest
-import org.bitcoins.testkitcore.Implicits.GeneratorOps
 import org.bitcoins.testkitcore.gen.LnMessageGen
 
 class DLCConnectionHandlerTest
@@ -13,43 +12,44 @@ class DLCConnectionHandlerTest
   behavior of "parseIndividualMessages"
 
   it must "DLC Accept message that is not aligned with a tcp frame" in {
-    val accept: LnMessage[DLCAcceptTLV] =
-      LnMessageGen.dlcAcceptMessage.sampleSome
+    forAllAsync(LnMessageGen.dlcAcceptMessage) { accept =>
+      //split the msg at a random index to simulate a tcp frame not being aligned
+      val randomIndex = scala.util.Random.nextInt().abs % accept.bytes.size
+      val (firstHalf, secondHalf) = accept.bytes.splitAt(randomIndex)
+      val (firstHalfParseHeaders, remainingBytes) =
+        DLCConnectionHandler.parseIndividualMessages(firstHalf)
+      firstHalfParseHeaders must be(empty)
 
-    //split the msg at a random index to simulate a tcp frame not being aligned
-    val randomIndex = scala.util.Random.nextInt().abs % accept.bytes.size
-    val (firstHalf, secondHalf) = accept.bytes.splitAt(randomIndex)
-    val (firstHalfParseHeaders, remainingBytes) =
-      DLCConnectionHandler.parseIndividualMessages(firstHalf)
-    firstHalfParseHeaders must be(empty)
+      val (secondHalfParsedHeaders, _) =
+        DLCConnectionHandler.parseIndividualMessages(
+          remainingBytes ++ secondHalf)
+      val parsedLnMessage = secondHalfParsedHeaders.head
+      val parsedLnAcceptMessage =
+        parsedLnMessage.asInstanceOf[LnMessage[DLCAcceptTLV]]
 
-    val (secondHalfParsedHeaders, _) =
-      DLCConnectionHandler.parseIndividualMessages(remainingBytes ++ secondHalf)
-    val parsedLnMessage = secondHalfParsedHeaders.head
-    val parsedLnAcceptMessage =
-      parsedLnMessage.asInstanceOf[LnMessage[DLCAcceptTLV]]
-
-    parsedLnAcceptMessage.bytes must be(accept.bytes)
+      parsedLnAcceptMessage.bytes must be(accept.bytes)
+    }
   }
 
   it must "return the entire byte array if a message is not aligned to a byte frame" in {
-    val message = LnMessageGen.knownLnMessage.sampleSome
-    // remove last byte so the message is not aligned
-    val bytes = message.bytes.dropRight(1)
-    val (parsedMessages, unAlignedBytes) =
-      DLCConnectionHandler.parseIndividualMessages(bytes)
+    forAllAsync(LnMessageGen.dlcAcceptMessage) { accept =>
+      // remove last byte so the message is not aligned
+      val bytes = accept.bytes.dropRight(1)
+      val (parsedMessages, unAlignedBytes) =
+        DLCConnectionHandler.parseIndividualMessages(bytes)
 
-    assert(parsedMessages.isEmpty)
-    assert(unAlignedBytes == bytes)
+      assert(parsedMessages.isEmpty)
+      assert(unAlignedBytes == bytes)
+    }
   }
 
   // todo figure out how to properly handle unknown messages
   it must "parse an unknown message" ignore {
-    val unknown = LnMessageGen.unknownMessage.sampleSome
-
-    val (messages, leftover) =
-      DLCConnectionHandler.parseIndividualMessages(unknown.bytes)
-    assert(messages == Vector(unknown))
-    assert(leftover.isEmpty)
+    forAllAsync(LnMessageGen.unknownMessage) { unknown =>
+      val (messages, leftover) =
+        DLCConnectionHandler.parseIndividualMessages(unknown.bytes)
+      assert(messages == Vector(unknown))
+      assert(leftover.isEmpty)
+    }
   }
 }
