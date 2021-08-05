e315fb0563d2886f8209384bcfa4aa9e681f7415
user411 <erickson.ivan@gmail.com>
2021-08-05 16:42:04 -0600

    Move to GridPane wallet, dynamic sizing (#3492)

--- core/src/main/scala/org/bitcoins/core/dlc/accounting/RateOfReturnUtil.scala
+++ core/src/main/scala/org/bitcoins/core/dlc/accounting/RateOfReturnUtil.scala
@@ -4,7 +4,12 @@ object RateOfReturnUtil {
 
   /** @see https://alvinalexander.com/scala/how-to-format-numbers-commas-international-currency-in-scala/ */
   def prettyPrint(ror: BigDecimal): String = {
-    val percent = ror * 100
-    f"${percent}%1.2f" + "%"
+    toPercentage(ror, 2) + "%"
   }
+
+  /** Convert to default two decimal percentage representation without % label */
+  def toPercentage(ror: BigDecimal, decimals: Int = 2): String = {
+    s"%1.${decimals}f".format(ror * 100)
+  }
+
 }
