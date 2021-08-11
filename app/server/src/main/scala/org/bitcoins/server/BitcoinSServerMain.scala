610df760a3bca356195b87c3752aac704723ad85
Chris Stewart <stewart.chris1234@gmail.com>
2021-08-11 10:31:33 -0500

    Reduce logs for inv messages and wallet callbacks to DEBUG

--- app/server/src/main/scala/org/bitcoins/server/BitcoinSServerMain.scala
+++ app/server/src/main/scala/org/bitcoins/server/BitcoinSServerMain.scala
@@ -247,7 +247,7 @@ class BitcoinSServerMain(override val serverArgParser: ServerArgParser)(implicit
       nodeConf: NodeAppConfig,
       ec: ExecutionContext): Future[NodeCallbacks] = {
     lazy val onTx: OnTxReceived = { tx =>
-      logger.info(s"Receiving transaction txid=${tx.txIdBE.hex} as a callback")
+      logger.debug(s"Receiving transaction txid=${tx.txIdBE.hex} as a callback")
       wallet.processTransaction(tx, blockHashOpt = None).map(_ => ())
     }
     lazy val onCompactFilters: OnCompactFiltersReceived = { blockFilters =>
