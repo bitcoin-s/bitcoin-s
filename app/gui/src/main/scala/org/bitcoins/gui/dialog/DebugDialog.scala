1e9e105d4b280e715edb1865aee9880c6d590a66
Ivan <erickson.ivan@gmail.com>
2021-08-12 14:17:04 -0600

    Use Runtime.exec() to open unix file

--- app/gui/src/main/scala/org/bitcoins/gui/dialog/DebugDialog.scala
+++ app/gui/src/main/scala/org/bitcoins/gui/dialog/DebugDialog.scala
@@ -9,6 +9,7 @@ import scalafx.stage.{Modality, Window}
 import java.awt.Desktop
 import java.io.File
 import java.nio.file.{Files, Paths}
+import scala.util.Properties
 
 object DebugDialog {
 
@@ -29,16 +30,24 @@ object DebugDialog {
         // Get root active network directory
         val location = System.getProperty("bitcoins.log.location")
         val path = Paths.get(location, LOGFILE_NAME)
-        if (
-          Files.exists(path) && Desktop.isDesktopSupported && Desktop
-            .getDesktop()
-            .isSupported(Desktop.Action.OPEN)
-        ) {
-          // Open file in default log reader per OS
-          Desktop.getDesktop().open(new File(path.toString))
-        } else {
-          println(
-            "File is missing or Desktop operations are not supported on this platform")
+        if (Files.exists(path)) {
+          val file = new File(path.toString)
+          // Ubuntu seems to support Desktop and Desktop.open(), but hangs on opening file
+          if (Properties.isLinux) {
+            val _ = Runtime
+              .getRuntime()
+              .exec("/usr/bin/evince \"" + file.getAbsolutePath() + "\"")
+          } else if (
+            Desktop.isDesktopSupported && Desktop
+              .getDesktop()
+              .isSupported(Desktop.Action.OPEN)
+          ) {
+            // Open file in default log reader per OS
+            Desktop.getDesktop().open(file)
+          } else {
+            println(
+              "File is missing or Desktop operations are not supported on this platform")
+          }
         }
       }
     }
