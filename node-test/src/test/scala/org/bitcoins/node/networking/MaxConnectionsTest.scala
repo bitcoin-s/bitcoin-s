00c80b100d71150c68772761a11c8c6369a9dde7
Chris Stewart <stewart.chris1234@gmail.com>
2021-08-18 13:24:09 -0500

    Add unit test

--- node-test/src/test/scala/org/bitcoins/node/networking/MaxConnectionsTest.scala
+++ node-test/src/test/scala/org/bitcoins/node/networking/MaxConnectionsTest.scala
@@ -1,27 +1,30 @@
 package org.bitcoins.node.networking
 
 import org.bitcoins.asyncutil.AsyncUtil
+import org.bitcoins.asyncutil.AsyncUtil.RpcRetryException
 import org.bitcoins.node.models.Peer
 import org.bitcoins.node.networking.peer.PeerHandler
-import org.bitcoins.rpc.config.{BitcoindConfig, BitcoindInstance}
+import org.bitcoins.rpc.config.BitcoindInstance
 import org.bitcoins.testkit.node.{
   CachedBitcoinSAppConfig,
   NodeTestUtil,
   NodeUnitTest
 }
 import org.bitcoins.testkit.rpc.BitcoindRpcTestUtil
-import org.bitcoins.testkit.util.BitcoindRpcTest
+import org.bitcoins.testkit.util.{AkkaUtil, BitcoindRpcTest}
 
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 class MaxConnectionsTest extends BitcoindRpcTest with CachedBitcoinSAppConfig {
 
-  //we need a custom bitcoind config with maxconnections=0
-  def writeNormalConfig: BitcoindConfig = BitcoindRpcTestUtil.standardConfig
+  val standardConfig = BitcoindRpcTestUtil.standardConfig
+
+  val datadir = standardConfig.datadir
 
   //need to start with maxconnections=0
   val maxConnections0Config =
-    writeNormalConfig.withOption("maxconnections", "0")
+    standardConfig.withOption("maxconnections", "0")
 
   val instance = BitcoindInstance.fromConfig(maxConnections0Config)
 
@@ -42,10 +45,33 @@ class MaxConnectionsTest extends BitcoindRpcTest with CachedBitcoinSAppConfig {
 
     val connectedF = for {
       peerHandler <- peerHandlerF
+      bitcoindRpc <- bitcoindRpcF
+
       _ = peerHandler.peerMsgSender.connect()
-      _ <- AsyncUtil.retryUntilSatisfiedF(() =>
-        peerHandler.p2pClient.isConnected())
+      _ <- AsyncUtil
+        .retryUntilSatisfiedF(() => peerHandler.p2pClient.isConnected())
+        .recover { case _: RpcRetryException =>
+          //expect this to fail, we cannot connect
+          //because maxconnections=0
+          ()
+        }
+      //we need to wait for the re-connection attempts
+      //to at least a 16 second delay before reconnecting
+      //so we have time to restart bitcoind
+      _ = AkkaUtil.nonBlockingSleep(10.seconds)
+      _ <- bitcoindRpc.stop()
+      //write updated configuration, this sets maxconnections=1
+      //which allows us to connect
+      _ = standardConfig.withOption("maxconnections", "125")
+      _ <- bitcoindRpc.start()
+      _ = logger.error(s"Done starting bitcoind again")
+      //now we should eventually automatically reconnect
+      _ <- AsyncUtil.retryUntilSatisfiedF(
+        conditionF = () => peerHandler.p2pClient.isConnected(),
+        interval = 500.millis,
+        maxTries = 60)
     } yield succeed
+
     connectedF
   }
 }
