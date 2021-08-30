bc927bb552c8e6694518f0363142e5c08566315e
benthecarman <benthecarman@live.com>
2021-08-30 15:03:10 -0500

    Add comments

--- core-test/src/test/scala/org/bitcoins/core/crypto/ExtKeyTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/crypto/ExtKeyTest.scala
@@ -278,96 +278,112 @@ class ExtKeyTest extends BitcoinSUnitTest {
   }
 
   it must "pass test vector 5 in BIP32" in {
+    // pubkey version / prvkey mismatch
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xpub661MyMwAqRbcEYS8w7XLSVeEsBXy79zSzH1J8vCdxAZningWLdN3zgtU6LBpB85b3D2yc8sfvZU521AAwdZafEz7mnzBBsz4wKY5fTtTQBm")
         .key)
 
+    // prvkey version / pubkey mismatch
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s21ZrQH143K24Mfq5zL5MhWK9hUhhGbd45hLXo2Pq2oqzMMo63oStZzFGTQQD3dC4H2D5GBj7vWvSQaaBv5cxi9gafk7NF3pnBju6dwKvH")
         .key)
 
+    // invalid pubkey prefix 04
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xpub661MyMwAqRbcEYS8w7XLSVeEsBXy79zSzH1J8vCdxAZningWLdN3zgtU6Txnt3siSujt9RCVYsx4qHZGc62TG4McvMGcAUjeuwZdduYEvFn")
         .key)
 
+    // invalid prvkey prefix 04
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s21ZrQH143K24Mfq5zL5MhWK9hUhhGbd45hLXo2Pq2oqzMMo63oStZzFGpWnsj83BHtEy5Zt8CcDr1UiRXuWCmTQLxEK9vbz5gPstX92JQ")
         .key)
 
+    // invalid pubkey prefix 01
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xpub661MyMwAqRbcEYS8w7XLSVeEsBXy79zSzH1J8vCdxAZningWLdN3zgtU6N8ZMMXctdiCjxTNq964yKkwrkBJJwpzZS4HS2fxvyYUA4q2Xe4")
         .key)
 
+    // invalid prvkey prefix 01
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s21ZrQH143K24Mfq5zL5MhWK9hUhhGbd45hLXo2Pq2oqzMMo63oStZzFAzHGBP2UuGCqWLTAPLcMtD9y5gkZ6Eq3Rjuahrv17fEQ3Qen6J")
         .key)
 
+    // zero depth with non-zero parent fingerprint
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s2SPatNQ9Vc6GTbVMFPFo7jsaZySyzk7L8n2uqKXJen3KUmvQNTuLh3fhZMBoG3G4ZW1N2kZuHEPY53qmbZzCHshoQnNf4GvELZfqTUrcv")
         .key)
 
+    // zero depth with non-zero parent fingerprint
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xpub661no6RGEX3uJkY4bNnPcw4URcQTrSibUZ4NqJEw5eBkv7ovTwgiT91XX27VbEXGENhYRCf7hyEbWrR3FewATdCEebj6znwMfQkhRYHRLpJ")
         .key)
 
+    // zero depth with non-zero index
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s21ZrQH4r4TsiLvyLXqM9P7k1K3EYhA1kkD6xuquB5i39AU8KF42acDyL3qsDbU9NmZn6MsGSUYZEsuoePmjzsB3eFKSUEh3Gu1N3cqVUN")
         .key)
 
+    // zero depth with non-zero index
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xpub661MyMwAuDcm6CRQ5N4qiHKrJ39Xe1R1NyfouMKTTWcguwVcfrZJaNvhpebzGerh7gucBvzEQWRugZDuDXjNDRmXzSZe4c7mnTK97pTvGS8")
         .key)
 
+    // unknown extended key version
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "DMwo58pR1QLEFihHiXPVykYB6fJmsTeHvyTp7hRThAtCX8CvYzgPcn8XnmdfHGMQzT7ayAmfo4z3gY5KfbrZWZ6St24UVf2Qgo6oujFktLHdHY4")
         .key)
 
+    // unknown extended key version
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "DMwo58pR1QLEFihHiXPVykYB6fJmsTeHvyTp7hRThAtCX8CvYzgPcn8XnmdfHPmHJiEDXkTiJTVV9rHEBUem2mwVbbNfvT2MTcAqj3nesx8uBf9")
         .key)
 
+    // private key 0 not in 1..n-1
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s21ZrQH143K24Mfq5zL5MhWK9hUhhGbd45hLXo2Pq2oqzMMo63oStZzF93Y5wvzdUayhgkkFoicQZcP3y52uPPxFnfoLZB21Teqt1VvEHx")
         .key)
 
+    // private key n not in 1..n-1
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xprv9s21ZrQH143K24Mfq5zL5MhWK9hUhhGbd45hLXo2Pq2oqzMMo63oStZzFAzHGBP2UuGCqWLTAPLcMtD5SDKr24z3aiUvKr9bJpdrcLg1y3G")
         .key)
 
+    // invalid pubkey 020000000000000000000000000000000000000000000000000000000000000007
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
           "xpub661MyMwAqRbcEYS8w7XLSVeEsBXy79zSzH1J8vCdxAZningWLdN3zgtU6Q5JXayek4PRsn35jii4veMimro1xefsM58PgBMrvdYre8QyULY")
         .key)
 
+    // invalid checksum
     assertThrows[IllegalArgumentException](
       ExtKey
         .fromString(
