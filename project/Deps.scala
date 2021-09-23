f9cfd054146e6966bfdc9c393b4e3d0b5eaa8857
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-23 20:50:00 +0200

    Update postgresql to 42.2.24 (#3687)

--- project/Deps.scala
+++ project/Deps.scala
@@ -30,7 +30,7 @@ object Deps {
     val asyncNewScalaV = "1.0.1"
 
     val flywayV = "6.4.2"
-    val postgresV = "42.2.23"
+    val postgresV = "42.2.24"
     val akkaActorV = akkaStreamv
     val slickV = "3.3.3"
     val sqliteV = "3.36.0.3"
