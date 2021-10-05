6e26a03d43562e09b5db3e8f30938c4da8b332ff
Scala Steward <me@scala-steward.org>
2021-10-05 04:16:51 +0200

    Update scalacheck-1-14 to 3.2.2.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -12,7 +12,7 @@ object Deps {
     val scalaTest = "3.2.10"
 
     val scalaTestPlus =
-      "3.2.1.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
+      "3.2.2.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
     val slf4j = "1.7.32"
     val spray = "1.3.6"
     val zeromq = "0.5.2"
