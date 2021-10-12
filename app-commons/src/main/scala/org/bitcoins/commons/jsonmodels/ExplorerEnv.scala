e5648b7edd24bee33dc55b0fa5d6465fa75ec46e
Chris Stewart <stewart.chris1234@gmail.com>
2021-10-12 13:46:10 -0500

    update oracle explorer client to use v2 of the API

--- app-commons/src/main/scala/org/bitcoins/commons/jsonmodels/ExplorerEnv.scala
+++ app-commons/src/main/scala/org/bitcoins/commons/jsonmodels/ExplorerEnv.scala
@@ -18,18 +18,18 @@ object ExplorerEnv extends StringFactory[ExplorerEnv] {
 
   case object Production extends ExplorerEnv {
     override val siteUrl: String = "https://oracle.suredbits.com/"
-    override val baseUri: String = s"${siteUrl}v1/"
+    override val baseUri: String = s"${siteUrl}v2/"
   }
 
   case object Test extends ExplorerEnv {
     override val siteUrl: String = "https://test.oracle.suredbits.com/"
-    override val baseUri: String = s"${siteUrl}v1/"
+    override val baseUri: String = s"${siteUrl}v2/"
   }
 
   /** For local testing purposes */
   case object Local extends ExplorerEnv {
     override val siteUrl: String = "http://localhost:9000/"
-    override val baseUri: String = s"${siteUrl}v1/"
+    override val baseUri: String = s"${siteUrl}v2/"
   }
 
   val all: Vector[ExplorerEnv] = Vector(Production, Test, Local)
