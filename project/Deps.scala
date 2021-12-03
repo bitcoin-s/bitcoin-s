c9b5d585f70f87157fa2301a22f08d0372527aaf
Scala Steward <me@scala-steward.org>
2021-12-04 00:18:23 +0100

    Update scalafx to 17.0.1-R26

--- project/Deps.scala
+++ project/Deps.scala
@@ -25,7 +25,7 @@ object Deps {
     val nativeLoaderV = "2.4.0"
     val typesafeConfigV = "1.4.1"
 
-    val scalaFxV = "16.0.0-R25"
+    val scalaFxV = "17.0.1-R26"
     val javaFxV = "17-ea+8"
 
     val asyncNewScalaV = "1.0.1"
