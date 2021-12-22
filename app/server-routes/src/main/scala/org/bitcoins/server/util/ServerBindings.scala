0a79795ae3b5dc92b617a11050602af39c32661b
Chris Stewart <stewart.chris1234@gmail.com>
2021-12-22 13:37:40 -0600

    Properly close the server with .terminate() rather than .unbind()

--- app/server-routes/src/main/scala/org/bitcoins/server/util/ServerBindings.scala
+++ app/server-routes/src/main/scala/org/bitcoins/server/util/ServerBindings.scala
@@ -2,15 +2,18 @@ package org.bitcoins.server.util
 
 import akka.http.scaladsl.Http
 
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 
 case class ServerBindings(
     httpServer: Http.ServerBinding,
     webSocketServerOpt: Option[Http.ServerBinding]) {
 
+  private val terminateTimeout = 5.seconds
+
   def stop()(implicit ec: ExecutionContext): Future[Unit] = {
-    val stopHttp = httpServer.unbind()
-    val stopWsFOpt = webSocketServerOpt.map(_.unbind())
+    val stopHttp = httpServer.terminate(terminateTimeout)
+    val stopWsFOpt = webSocketServerOpt.map(_.terminate(terminateTimeout))
     for {
       _ <- stopHttp
       _ <- stopWsFOpt match {
