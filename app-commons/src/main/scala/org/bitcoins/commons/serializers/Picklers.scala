d0b84013553380bd1cd41e3762ca8330cae6d09e
rorp <rorp@users.noreply.github.com>
2022-02-14 10:24:05 -0800

    Sort outcomes in `decodeoffer` response

--- app-commons/src/main/scala/org/bitcoins/commons/serializers/Picklers.scala
+++ app-commons/src/main/scala/org/bitcoins/commons/serializers/Picklers.scala
@@ -625,7 +625,8 @@ object Picklers {
           Vector(writeJs(hyperbolaPiece))
       }
 
-      Obj(PicklerKeys.pointsKey -> (endPointsJs ++ midPointJs))
+      val points = (endPointsJs ++ midPointJs).sortBy(_.obj(PicklerKeys.outcomeKey).num)
+      Obj(PicklerKeys.pointsKey -> points)
     }
   }
 
