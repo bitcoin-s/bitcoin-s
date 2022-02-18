f94459ba1f6004f07961c9a74c6c1cd83af444c1
Chris Stewart <stewart.chris1234@gmail.com>
2022-02-18 12:44:01 -0600

    Break receive and spent tests into two test cases

--- wallet-test/src/test/scala/org/bitcoins/wallet/UTXOHandlingTest.scala
+++ wallet-test/src/test/scala/org/bitcoins/wallet/UTXOHandlingTest.scala
@@ -18,131 +18,156 @@ class UTXOHandlingTest extends BitcoinSWalletTest {
     withNewWallet(test, getBIP39PasswordOpt())(getFreshWalletAppConfig)
   }
 
-  it must "correctly update txo state based on confirmations" in { wallet =>
-    val utxo = sampleSegwitUTXO(EmptyScriptPubKey,
-                                state = TxoState.Reserved
-    ) //state doesn't matter here
-    val requiredConfs = 6
-    assert(wallet.walletConfig.requiredConfirmations == requiredConfs)
+  val utxo = sampleSegwitUTXO(EmptyScriptPubKey,
+                              state = TxoState.Reserved
+  ) //state doesn't matter here
+  val requiredConfs = 6
+  val reserved = utxo.copyWithState(Reserved)
 
-    val immatureCoinbase = utxo.copyWithState(ImmatureCoinbase)
-    val pendingConfReceived = utxo.copyWithState(PendingConfirmationsReceived)
-    val spendingTxId = randomTXID
-    val pendingConfSpent = utxo
-      .copyWithSpendingTxId(spendingTxId)
-      .copyWithState(PendingConfirmationsSpent)
-    val confReceived = utxo
-      .copyWithState(ConfirmedReceived)
-    val confSpent = utxo
-      .copyWithSpendingTxId(spendingTxId)
-      .copyWithState(ConfirmedSpent)
-    val reserved = utxo.copyWithState(Reserved)
+  val immatureCoinbase = utxo.copyWithState(ImmatureCoinbase)
 
-    //it must stay reserved if we are receiving confirmations
-    assert(wallet.updateReceivedTxoWithConfs(reserved, 1) == reserved)
-    val withSpendingTxId =
-      reserved.copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+  val confReceived = utxo
+    .copyWithState(ConfirmedReceived)
 
-    //it must transition from reserved to spent
-    assert(
-      wallet
-        .updateSpentTxoWithConfs(withSpendingTxId, 1)
-        .state == TxoState.PendingConfirmationsSpent)
+  val pendingConfReceived = utxo.copyWithState(PendingConfirmationsReceived)
 
-    //it must stay immature coinbase if we don't have > 101 confirmations
-    assert(
-      wallet.updateReceivedTxoWithConfs(immatureCoinbase,
-                                        10) == immatureCoinbase)
-    assert(
-      wallet.updateReceivedTxoWithConfs(immatureCoinbase, 101) == confReceived)
+  it must "correct update receive txo state based on confirmations" in {
+    wallet =>
+      //it must stay reserved if we are receiving confirmations
+      assert(wallet.updateReceivedTxoWithConfs(reserved, 1) == reserved)
 
-    //cannot spend an immature coinbase output
-    assertThrows[RuntimeException] {
-      //cannot have utxo spent from coinbase before 101 blocks
-      wallet.updateSpentTxoWithConfs(immatureCoinbase, 100)
-    }
+      //it must stay immature coinbase if we don't have > 101 confirmations
+      assert(
+        wallet.updateReceivedTxoWithConfs(immatureCoinbase,
+                                          10) == immatureCoinbase)
+      assert(
+        wallet.updateReceivedTxoWithConfs(immatureCoinbase,
+                                          101) == confReceived)
 
-    //we must stay pending confirmations received with only 1 confirmation
-    assert(
-      wallet.updateReceivedTxoWithConfs(pendingConfReceived,
-                                        1) == pendingConfReceived)
+      //we must stay pending confirmations received with only 1 confirmation
+      assert(
+        wallet.updateReceivedTxoWithConfs(pendingConfReceived,
+                                          1) == pendingConfReceived)
 
-    val pendingConfReceivedWithTxId = pendingConfReceived
-      .copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+      //must be able to go back to broadcast received if we don't have confirmations (re-org scenario)
+      assert(
+        wallet
+          .updateReceivedTxoWithConfs(pendingConfReceived, 0)
+          .state == TxoState.BroadcastReceived)
 
-    val expectedConfSpent = pendingConfReceived
-      .copyWithSpendingTxId(DoubleSha256DigestBE.empty)
-      .copyWithState(TxoState.ConfirmedSpent)
+      //it must transition from DoesNotExist -> PendingConfirmationsReceived
+      assert(
+        wallet.updateReceivedTxoWithConfs(dne, 1) == dne.copyWithState(
+          TxoState.PendingConfirmationsReceived))
 
-    val updated =
-      wallet.updateSpentTxoWithConfs(pendingConfReceivedWithTxId, requiredConfs)
-    //it must transition from Pending Confirmation Received -> Pending Confirmation Spent
-    assert(updated == expectedConfSpent)
+      //transition from TxoState.ConfirmedReceived -> TxoState.PendingConfirmationsReceived (reorg scenario)
+      assert(
+        wallet.updateReceivedTxoWithConfs(confReceived, 1) == confReceived
+          .copyWithState(PendingConfirmationsReceived))
 
-    //must stay at pending confirmations spent with only 1 confirmation
-    assert(
-      wallet.updateSpentTxoWithConfs(pendingConfSpent, 1) == pendingConfSpent)
+      //it must stay TxoState.ConfirmedReceived if we keep receiving confirmations
+      assert(
+        wallet.updateReceivedTxoWithConfs(confReceived,
+                                          requiredConfs) == confReceived)
 
-    //must transition from PendingConfirmationsSpent -> ConfirmedSpent
-    assert(
-      wallet.updateSpentTxoWithConfs(pendingConfSpent,
-                                     requiredConfs) == confSpent)
+      //it must stay broadcast received with 0 confirmations
+      val broadcastReceived = utxo
+        .copyWithState(TxoState.BroadcastReceived)
+      assert(
+        wallet.updateReceivedTxoWithConfs(broadcastReceived,
+                                          0) == broadcastReceived)
+  }
 
-    //it must transition from DoesNotExist -> PendingConfirmationsReceived
-    assert(
-      wallet.updateReceivedTxoWithConfs(dne, 1) == dne.copyWithState(
-        TxoState.PendingConfirmationsReceived))
+  it must "correctly update spent txo state based on confirmations" in {
+    wallet =>
+      assert(wallet.walletConfig.requiredConfirmations == requiredConfs)
 
-    val expectedDNEWithTxId =
-      dne.copyWithSpendingTxId(DoubleSha256DigestBE.empty)
-    val expectedDNEConfirmedSpent =
-      expectedDNEWithTxId.copyWithState(TxoState.ConfirmedSpent)
-    //transition from TxoState.DoesNotExist -> TxoState.ConfirmedSpent
-    assert(
-      wallet.updateSpentTxoWithConfs(
-        expectedDNEWithTxId,
-        requiredConfs) == expectedDNEConfirmedSpent)
+      val spendingTxId = randomTXID
+      val pendingConfSpent = utxo
+        .copyWithSpendingTxId(spendingTxId)
+        .copyWithState(PendingConfirmationsSpent)
 
-    //transition form TxoState.ConfirmedSpent -> TxoState.PendingConfirmationSpent (reorg scenario)
-    assert(
-      wallet.updateSpentTxoWithConfs(confSpent, 1) == confSpent.copyWithState(
-        PendingConfirmationsSpent))
+      val confSpent = utxo
+        .copyWithSpendingTxId(spendingTxId)
+        .copyWithState(ConfirmedSpent)
 
-    //stay confirmed if we are already confirmed
-    assert(
-      wallet.updateSpentTxoWithConfs(confSpent, requiredConfs) == confSpent)
+      val withSpendingTxId =
+        reserved.copyWithSpendingTxId(DoubleSha256DigestBE.empty)
 
-    //transition from TxoState.ConfirmedReceived -> TxoState.PendingConfirmationsReceived (reorg scenario)
-    assert(
-      wallet.updateReceivedTxoWithConfs(confReceived, 1) == confReceived
-        .copyWithState(PendingConfirmationsReceived))
+      //it must transition from reserved to broacast spent
+      assert(
+        wallet
+          .updateSpentTxoWithConfs(withSpendingTxId, 0)
+          .state == TxoState.BroadcastSpent)
 
-    //it must stay TxoState.ConfirmedReceived if we keep receiving confirmations
-    assert(
-      wallet.updateReceivedTxoWithConfs(confReceived,
-                                        requiredConfs) == confReceived)
+      //it must transition from reserved to spent
+      assert(
+        wallet
+          .updateSpentTxoWithConfs(withSpendingTxId, 1)
+          .state == TxoState.PendingConfirmationsSpent)
 
-    val expectedConfReceivedWithTxid =
-      confReceived.copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+      //cannot spend an immature coinbase output
+      assertThrows[RuntimeException] {
+        //cannot have utxo spent from coinbase before 101 blocks
+        wallet.updateSpentTxoWithConfs(immatureCoinbase, 100)
+      }
 
-    //TxoState.ConfirmedReceived -> TxoState.ConfirmedSpent
-    assert(
-      wallet.updateSpentTxoWithConfs(
-        expectedConfReceivedWithTxid,
-        requiredConfs) == expectedConfReceivedWithTxid.copyWithState(
-        TxoState.ConfirmedSpent))
+      val pendingConfReceivedWithTxId = pendingConfReceived
+        .copyWithSpendingTxId(DoubleSha256DigestBE.empty)
 
-    //it must stay BroadcastSpent with 0 confirmations
-    val broadcastSpent = utxo
-      .copyWithSpendingTxId(DoubleSha256DigestBE.empty)
-      .copyWithState(TxoState.BroadcastSpent)
-    assert(wallet.updateSpentTxoWithConfs(broadcastSpent, 0) == broadcastSpent)
+      val expectedConfSpent = pendingConfReceived
+        .copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+        .copyWithState(TxoState.ConfirmedSpent)
 
-    //it must stay broadcast received with 0 confirmations
-    val broadcastReceived = utxo
-      .copyWithState(TxoState.BroadcastReceived)
-    assert(
-      wallet.updateReceivedTxoWithConfs(broadcastReceived,
-                                        0) == broadcastReceived)
+      val updated =
+        wallet.updateSpentTxoWithConfs(pendingConfReceivedWithTxId,
+                                       requiredConfs)
+      //it must transition from Pending Confirmation Received -> Pending Confirmation Spent
+      assert(updated == expectedConfSpent)
+
+      //must stay at pending confirmations spent with only 1 confirmation
+      assert(
+        wallet.updateSpentTxoWithConfs(pendingConfSpent, 1) == pendingConfSpent)
+
+      //must transition from PendingConfirmationsSpent -> ConfirmedSpent
+      assert(
+        wallet.updateSpentTxoWithConfs(pendingConfSpent,
+                                       requiredConfs) == confSpent)
+
+      val expectedDNEWithTxId =
+        dne.copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+      val expectedDNEConfirmedSpent =
+        expectedDNEWithTxId.copyWithState(TxoState.ConfirmedSpent)
+      //transition from TxoState.DoesNotExist -> TxoState.ConfirmedSpent
+      assert(
+        wallet.updateSpentTxoWithConfs(
+          expectedDNEWithTxId,
+          requiredConfs) == expectedDNEConfirmedSpent)
+
+      //transition form TxoState.ConfirmedSpent -> TxoState.PendingConfirmationSpent (reorg scenario)
+      assert(
+        wallet.updateSpentTxoWithConfs(confSpent, 1) == confSpent.copyWithState(
+          PendingConfirmationsSpent))
+
+      //stay confirmed if we are already confirmed
+      assert(
+        wallet.updateSpentTxoWithConfs(confSpent, requiredConfs) == confSpent)
+
+      val expectedConfReceivedWithTxid =
+        confReceived.copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+
+      //TxoState.ConfirmedReceived -> TxoState.ConfirmedSpent
+      assert(
+        wallet.updateSpentTxoWithConfs(
+          expectedConfReceivedWithTxid,
+          requiredConfs) == expectedConfReceivedWithTxid.copyWithState(
+          TxoState.ConfirmedSpent))
+
+      //it must stay BroadcastSpent with 0 confirmations
+      val broadcastSpent = utxo
+        .copyWithSpendingTxId(DoubleSha256DigestBE.empty)
+        .copyWithState(TxoState.BroadcastSpent)
+      assert(
+        wallet.updateSpentTxoWithConfs(broadcastSpent, 0) == broadcastSpent)
   }
 }
