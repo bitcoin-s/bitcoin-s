26c71d08c02097c65a64ec6e701957ca662b5c74
Chris Stewart <stewart.chris1234@gmail.com>
2022-03-02 13:46:11 -0600

    revert wallet

--- wallet/src/main/scala/org/bitcoins/wallet/internal/RescanHandling.scala
+++ wallet/src/main/scala/org/bitcoins/wallet/internal/RescanHandling.scala
@@ -250,14 +250,10 @@ private[wallet] trait RescanHandling extends WalletLogger {
           }
       spksDb <- scriptPubKeyDAO.findAll()
     } yield {
-      val addrSpks = {
+      val addrSpks =
         addresses.map(_.scriptPubKey) ++ changeAddresses.map(_.scriptPubKey)
-      }
-
-      require(addrSpks.length == count * 2)
       val otherSpks = spksDb.map(_.scriptPubKey)
-      require(
-        addrSpks.forall(addrSpk => spksDb.exists(_.scriptPubKey == addrSpk)))
+
       (addrSpks ++ otherSpks).distinct
     }
   }
