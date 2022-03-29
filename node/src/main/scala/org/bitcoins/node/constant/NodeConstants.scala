a3fb13314ba08eae082bcd2d79606894c80d5a45
Chris Stewart <stewart.chris1234@gmail.com>
2022-03-29 18:29:20 -0500

    Revert to static version

--- node/src/main/scala/org/bitcoins/node/constant/NodeConstants.scala
+++ node/src/main/scala/org/bitcoins/node/constant/NodeConstants.scala
@@ -1,7 +1,5 @@
 package org.bitcoins.node.constant
 
-import org.bitcoins.core.util.EnvUtil
-
 case object NodeConstants {
-  lazy val userAgent = s"/bitcoin-s:${EnvUtil.parseVersion(EnvUtil.getVersion)}/"
+  val userAgent = "/bitcoin-s:1.9.1/"
 }
