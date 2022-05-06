ee1e201e0890564ed7ea97c8f2c5408e7297be06
Chris Stewart <stewart.chris1234@gmail.com>
2022-05-06 09:56:37 -0500

    Do chmod regardless of os

--- app/oracle-server/src/universal/mac-jlink-xattr-jre-bin-java.sh
+++ app/oracle-server/src/universal/mac-jlink-xattr-jre-bin-java.sh
@@ -4,6 +4,7 @@ if [[ "$OSTYPE" == "darwin21" ]]; then
   echo "RUNNING MAC"
   chmod +x jre/bin/java
   xattr -d com.apple.quarantine jre/bin/java
-  chmod +x bin/bitcoin-s-oracle-server
 fi
+
+chmod +x bin/bitcoin-s-oracle-server
 
