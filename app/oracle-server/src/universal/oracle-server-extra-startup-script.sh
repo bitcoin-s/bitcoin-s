f8b209c4ea4cc8f9ad37ddd2b5065df99613d0a6
Chris Stewart <stewart.chris1234@gmail.com>
2022-05-07 08:23:27 -0500

    Make bash scripts more generic on mac

--- app/oracle-server/src/universal/oracle-server-extra-startup-script.sh
+++ app/oracle-server/src/universal/oracle-server-extra-startup-script.sh
@@ -1,5 +1,5 @@
 chmod +x jre/bin/java #make sure java is executable
-if [[ "$OSTYPE" == "darwin21" ]]; then
+if [[ "$OS" == "OSX" ]]; then
   #mac doesn't allow random binaries to be executable
   #remove the quarantine attribute so java is executable on mac
   xattr -d com.apple.quarantine jre/bin/java
