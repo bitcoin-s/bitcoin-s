7da785696a7c89a4f97ee7900adb9b5915f9d265
Chris Stewart <stewart.chris1234@gmail.com>
2022-06-04 17:43:10 -0500

    Switch base docker image to ubuntu to get docker working again

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -201,7 +201,7 @@ object CommonSettings {
   lazy val dockerSettings: Seq[Setting[_]] = {
     Vector(
       //https://sbt-native-packager.readthedocs.io/en/latest/formats/docker.html
-      dockerBaseImage := "openjdk:17-slim",
+      dockerBaseImage := "ubuntu",
       dockerRepository := Some("bitcoinscala"),
       //set the user to be 'bitcoin-s' rather than
       //the default provided by sbt native packager
