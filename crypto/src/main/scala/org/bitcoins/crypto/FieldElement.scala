33d095adc503bd618bd7820879f18da28ec6c696
Nadav Kohen <nadavk25@gmail.com>
2022-06-14 12:14:20 -0500

    Removed G from FieldElement altogether

--- crypto/src/main/scala/org/bitcoins/crypto/FieldElement.scala
+++ crypto/src/main/scala/org/bitcoins/crypto/FieldElement.scala
@@ -36,9 +36,6 @@ object FieldElement
     new FieldElement(bytes)
   }
 
-  // CryptoParams.curve.getG (but decompressed)
-  private val G: ECPublicKey = ECPublicKey(
-    "0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8")
-
-  def computePoint(fe: FieldElement): ECPublicKey = G.multiply(fe)
+  def computePoint(fe: FieldElement): ECPublicKey =
+    CryptoParams.getG.multiply(fe)
 }
