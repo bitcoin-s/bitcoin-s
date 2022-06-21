14ad3aa2a1694bf5dc11d9164fe77e569ffbf461
Nadav Kohen <nadavk25@gmail.com>
2022-06-20 22:30:38 -0500

    An initial (not yet working) implementation with test

---
+++ crypto-test/src/test/scala/org/bitcoins/crypto/MuSig2UtilTest.scala
@@ -0,0 +1,31 @@
+package org.bitcoins.crypto
+
+import org.bitcoins.crypto.MuSig2Util._
+
+class MuSig2UtilTest extends BitcoinSCryptoTest {
+  behavior of "MuSig2Util"
+
+  it should "work for two parties" in {
+    val priv1 = ECPrivateKey.freshPrivateKey
+    val pub1 = priv1.publicKey
+    val (noncePub1: MultiNoncePub, noncePriv1: MultiNoncePriv) = genMultiNonce()
+    val priv2 = ECPrivateKey.freshPrivateKey
+    val pub2 = priv2.publicKey
+    val (noncePub2: MultiNoncePub, noncePriv2: MultiNoncePriv) = genMultiNonce()
+    val keySet: KeySet = Vector(pub1, pub2)
+    val msg = CryptoUtil.sha256(keySetSerialize(keySet)).bytes
+    val aggMultiNoncePub = aggNonces(Vector(noncePub1, noncePub2))
+    val (aggNonce1, s1) = sign(noncePriv1, aggMultiNoncePub, priv1, msg, keySet)
+    val (aggNonce2, s2) = sign(noncePriv2, aggMultiNoncePub, priv2, msg, keySet)
+
+    assert(aggNonce1 == aggNonce2)
+
+    val sig = signAgg(Vector(s1, s2), aggNonce1)
+    val aggPub = keyAgg(keySet)
+
+    // This currently fails, need to debug, starting with
+    // TODO check all of the ECPublicKey vs SchnorrPubKey vs SchnorrNonce stuff
+    // TODO should be using the actual BIP340 signature hash instead of a custom one
+    assert(aggPub.schnorrPublicKey.verify(msg, sig))
+  }
+}
---
+++ crypto/src/main/scala/org/bitcoins/crypto/MuSig2Util.scala
@@ -0,0 +1,107 @@
+package org.bitcoins.crypto
+
+import scodec.bits.ByteVector
+
+object MuSig2Util {
+
+  private val nonceNum: Int = 2 // 4
+
+  type MultiNoncePriv = Vector[ECPrivateKey]
+  type MultiNoncePub = Vector[ECPublicKey]
+  type KeySet = Vector[ECPublicKey]
+
+  def keySetSerialize(keySet: KeySet): ByteVector = {
+    keySet.map(_.schnorrPublicKey.bytes).reduce(_ ++ _)
+  }
+
+  def aggHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/agg").bytes
+  }
+
+  def nonHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/non").bytes
+  }
+
+  def sigHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/sig").bytes
+  }
+
+  def keyAggCoef(keySet: KeySet, key: ECPublicKey): FieldElement = {
+    val bytes = aggHash(keySetSerialize(keySet) ++ key.schnorrPublicKey.bytes)
+
+    FieldElement(bytes)
+  }
+
+  def keyAgg(keySet: KeySet): ECPublicKey = {
+    keySet
+      .map { key =>
+        val coef = keyAggCoef(keySet, key)
+        key.multiply(coef)
+      }
+      .reduce(_.add(_))
+  }
+
+  def genMultiNonce(): (MultiNoncePub, MultiNoncePriv) = {
+    val privNonces = Vector.fill(nonceNum)(ECPrivateKey.freshPrivateKey)
+    val pubNonces = privNonces.map(_.publicKey)
+
+    (pubNonces, privNonces)
+  }
+
+  def aggNonces(nonces: Vector[MultiNoncePub]): MultiNoncePub = {
+    0.until(nonceNum).toVector.map { j =>
+      nonces.map(multiNonce => multiNonce(j)).reduce(_.add(_))
+    }
+  }
+
+  def sign(
+      noncePriv: MultiNoncePriv,
+      aggMultiNoncePub: MultiNoncePub,
+      privKey: ECPrivateKey,
+      message: ByteVector,
+      keySet: KeySet): (ECPublicKey, FieldElement) = {
+    val pubKey = privKey.publicKey
+    val coef = keyAggCoef(keySet, pubKey)
+    val aggPubKey = keyAgg(keySet)
+    val bBytes = nonHash(
+      aggPubKey.schnorrPublicKey.bytes ++ aggMultiNoncePub
+        .map(_.schnorrPublicKey.bytes)
+        .reduce(_ ++ _) ++ message)
+    val b = FieldElement(bBytes)
+    val aggNonce = aggMultiNoncePub.tail
+      .foldLeft((FieldElement.one, aggMultiNoncePub.head)) {
+        case ((prevPow, sumSoFar), nonce) =>
+          val pow = prevPow.multiply(b)
+          val point = nonce.multiply(pow)
+
+          (pow, sumSoFar.add(point))
+      }
+      ._2
+
+    val cBytes = sigHash(
+      aggPubKey.schnorrPublicKey.bytes ++ aggNonce.schnorrNonce.bytes ++ message)
+    val c = FieldElement(cBytes)
+
+    val privNonceSum = noncePriv
+      .foldLeft((FieldElement.one, FieldElement.zero)) {
+        case ((pow, sumSoFar), privNonce) =>
+          val scalar = privNonce.fieldElement.multiply(pow)
+          val nextPow = pow.multiply(b)
+
+          (nextPow, sumSoFar.add(scalar))
+      }
+      ._2
+
+    val s = privKey.fieldElement.multiply(c).multiply(coef).add(privNonceSum)
+
+    (aggNonce, s)
+  }
+
+  def signAgg(
+      sVals: Vector[FieldElement],
+      aggPubNonce: ECPublicKey): SchnorrDigitalSignature = {
+    val s = sVals.reduce(_.add(_))
+
+    SchnorrDigitalSignature(aggPubNonce.schnorrNonce, s)
+  }
+}
