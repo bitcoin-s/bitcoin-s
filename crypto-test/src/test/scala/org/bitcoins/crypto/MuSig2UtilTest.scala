ed83d298249a34509744e949052ab53506836925
Nadav Kohen <nadavk25@gmail.com>
2022-06-23 15:22:41 -0500

    Got MuSig2 working with BIP340 verification passing

--- crypto-test/src/test/scala/org/bitcoins/crypto/MuSig2UtilTest.scala
+++ crypto-test/src/test/scala/org/bitcoins/crypto/MuSig2UtilTest.scala
@@ -5,32 +5,32 @@ import org.bitcoins.crypto.MuSig2Util._
 class MuSig2UtilTest extends BitcoinSCryptoTest {
   behavior of "MuSig2Util"
 
+  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
+    generatorDrivenConfigNewCode
+
   it should "work for two parties" in {
-    val priv1 = ECPrivateKey.freshPrivateKey
-    val pub1 = priv1.publicKey
-    val (noncePub1: MultiNoncePub, noncePriv1: MultiNoncePriv) = genMultiNonce()
-    val priv2 = ECPrivateKey.freshPrivateKey
-    val pub2 = priv2.publicKey
-    val (noncePub2: MultiNoncePub, noncePriv2: MultiNoncePriv) = genMultiNonce()
-    val keySet: KeySet = KeySet(pub1, pub2)
-    val msg = CryptoUtil.sha256(keySet.serialize).bytes
-    val aggMultiNoncePub = aggNonces(Vector(noncePub1, noncePub2))
-    val (aggNonce1, s1) = sign(noncePriv1, aggMultiNoncePub, priv1, msg, keySet)
-    val (aggNonce2, s2) = sign(noncePriv2, aggMultiNoncePub, priv2, msg, keySet)
+    forAll(CryptoGenerators.privateKey,
+           CryptoGenerators.privateKey,
+           NumberGenerator.bytevector(32)) { case (priv1, priv2, msg) =>
+      val pub1 = priv1.publicKey
+      val (noncePub1: MultiNoncePub, noncePriv1: MultiNoncePriv) =
+        genMultiNonce()
+      val pub2 = priv2.publicKey
+      val (noncePub2: MultiNoncePub, noncePriv2: MultiNoncePriv) =
+        genMultiNonce()
+      val keySet: KeySet = KeySet(pub1.schnorrPublicKey, pub2.schnorrPublicKey)
+      val aggMultiNoncePub = aggNonces(Vector(noncePub1, noncePub2))
+      val (aggNonce1, s1) =
+        sign(noncePriv1, aggMultiNoncePub, priv1, msg, keySet)
+      val (aggNonce2, s2) =
+        sign(noncePriv2, aggMultiNoncePub, priv2, msg, keySet)
 
-    assert(aggNonce1 == aggNonce2)
+      assert(aggNonce1 == aggNonce2)
 
-    val sig = signAgg(Vector(s1, s2), aggNonce1)
-    val aggPub = keySet.aggPubKey
+      val sig = signAgg(Vector(s1, s2), aggNonce1)
+      val aggPub = keySet.aggPubKey
 
-    // This currently fails, need to debug, starting with
-    // TODO check all of the ECPublicKey vs SchnorrPubKey vs SchnorrNonce stuff
-    // TODO should be using the actual BIP340 signature hash instead of a custom one
-    // TODO key ordering before aggregation
-    // TODO partial signature verification
-    // TODO MuSig2* optimization
-    // TODO SessionContext and renaming of defs
-    // TODO implement tweaking
-    assert(verify(aggPub, msg, sig))
+      assert(aggPub.schnorrPublicKey.verify(msg, sig))
+    }
   }
 }
--- crypto/src/main/scala/org/bitcoins/crypto/MuSig2Util.scala
+++ crypto/src/main/scala/org/bitcoins/crypto/MuSig2Util.scala
@@ -2,6 +2,8 @@ package org.bitcoins.crypto
 
 import scodec.bits.ByteVector
 
+// TODO partial signature verification
+// TODO implement tweaking
 object MuSig2Util {
 
   private val nonceNum: Int = 2
@@ -9,67 +11,166 @@ object MuSig2Util {
   type MultiNoncePriv = Vector[ECPrivateKey]
   type MultiNoncePub = Vector[ECPublicKey]
 
-  case class KeySet(keys: Vector[ECPublicKey]) {
+  case class KeyGenContext(
+      aggPubKey: ECPublicKey,
+      tacc: FieldElement,
+      gacc: Boolean) {
+    def getPubKey: ByteVector = aggPubKey.bytes.tail
+    def getPubKey33: ByteVector = aggPubKey.bytes
+  }
+
+  case class KeySet(keys: Vector[SchnorrPublicKey]) {
     keys.init.zip(keys.tail).foreach { case (key1, key2) =>
       require(key1.hex.compareTo(key2.hex) <= 0,
               "Keys must be sorted lexicographically")
     }
 
     lazy val serialize: ByteVector = {
-      keys.map(_.schnorrPublicKey.bytes).reduce(_ ++ _)
+      keys.map(_.bytes).reduce(_ ++ _)
     }
 
-    def keyAggCoef(key: ECPublicKey): FieldElement = {
-      val bytes = aggHash(this.serialize ++ key.schnorrPublicKey.bytes)
+    def keyAggCoef(key: SchnorrPublicKey): FieldElement = {
+      if (key == secondKey) FieldElement.one
+      else {
+        val listHashBytes = aggListHash(serialize)
+        val bytes = aggCoefHash(listHashBytes ++ key.bytes)
 
-      FieldElement(bytes)
+        FieldElement(new java.math.BigInteger(1, bytes.toArray))
+      }
     }
 
     lazy val aggPubKey: ECPublicKey = {
       keys
         .map { key =>
           val coef = keyAggCoef(key)
-          key.multiply(coef)
+          key.publicKey.multiply(coef)
         }
         .reduce(_.add(_))
     }
+
+    lazy val secondKey: SchnorrPublicKey = keys.tail.head
   }
 
   object KeySet {
 
-    def apply(keys: Vector[ECPublicKey]): KeySet = {
-      val sortedKeys = keys.sorted(Ordering.by[ECPublicKey, String](_.hex))
+    def apply(keys: Vector[SchnorrPublicKey]): KeySet = {
+      val sortedKeys = keys.sorted(Ordering.by[SchnorrPublicKey, String](_.hex))
       new KeySet(sortedKeys)
     }
 
-    def apply(keys: ECPublicKey*): KeySet = {
+    def apply(keys: SchnorrPublicKey*): KeySet = {
       KeySet(keys.toVector)
     }
   }
 
-  def aggHash(bytes: ByteVector): ByteVector = {
-    CryptoUtil.taggedSha256(bytes, "MuSig2/agg").bytes
+  def aggListHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/agg_list").bytes
+  }
+
+  def aggCoefHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/agg_coef").bytes
   }
 
   def nonHash(bytes: ByteVector): ByteVector = {
     CryptoUtil.taggedSha256(bytes, "MuSig2/non").bytes
   }
 
+  def nonCoefHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/non_coef").bytes
+  }
+
   def sigHash(bytes: ByteVector): ByteVector = {
     CryptoUtil.taggedSha256(bytes, "MuSig2/sig").bytes
   }
 
-  def genMultiNonce(): (MultiNoncePub, MultiNoncePriv) = {
-    val privNonces = Vector.fill(nonceNum)(ECPrivateKey.freshPrivateKey)
+  def auxHash(bytes: ByteVector): ByteVector = {
+    CryptoUtil.taggedSha256(bytes, "MuSig2/aux").bytes
+  }
+
+  def genMultiNonce(
+      privKeyOpt: Option[ECPrivateKey] = None,
+      aggPubKeyOpt: Option[SchnorrPublicKey] = None,
+      msgOpt: Option[ByteVector] = None,
+      extraInOpt: Option[ByteVector] = None): (
+      MultiNoncePub,
+      MultiNoncePriv) = {
+    msgOpt.foreach(msg => require(msg.length == 32))
+    extraInOpt.foreach(extraIn => require(extraIn.length <= 4294967295L))
+
+    val preRand =
+      ECPrivateKey.freshPrivateKey.bytes // TODO this needs to draw 32 bytes uniformily at random
+    val rand = privKeyOpt match {
+      case Some(privKey) => auxHash(preRand).xor(privKey.bytes)
+      case None          => preRand
+    }
+
+    val aggPubKeyBytes = aggPubKeyOpt match {
+      case Some(aggPubKey) => aggPubKey.bytes.+:(aggPubKey.bytes.length.toByte)
+      case None            => ByteVector.fromByte(0)
+    }
+
+    val msgBytes = msgOpt match {
+      case Some(msg) => msg.+:(msg.length.toByte)
+      case None      => ByteVector.fromByte(0)
+    }
+
+    val extraInBytes = extraInOpt match {
+      case Some(extraIn) =>
+        ByteVector.fromLong(extraIn.length).padLeft(4) ++ extraIn
+      case None => ByteVector.fromByte(0)
+    }
+
+    val privNonces = 0.until(nonceNum).toVector.map { index =>
+      val indexByte = ByteVector.fromByte(index.toByte)
+
+      val noncePreBytes =
+        nonHash(rand ++ aggPubKeyBytes ++ msgBytes ++ extraInBytes ++ indexByte)
+
+      val noncePreNum = new java.math.BigInteger(1, noncePreBytes.toArray)
+
+      FieldElement(noncePreNum).toPrivateKey
+    }
     val pubNonces = privNonces.map(_.publicKey)
 
     (pubNonces, privNonces)
   }
 
   def aggNonces(nonces: Vector[MultiNoncePub]): MultiNoncePub = {
-    0.until(nonceNum).toVector.map { j =>
-      nonces.map(multiNonce => multiNonce(j)).reduce(_.add(_))
+    0.until(nonceNum).toVector.map { i =>
+      nonces.map(multiNonce => multiNonce(i)).reduce(_.add(_))
     }
+  }
+
+  def getSessionValues(
+      aggMultiNoncePub: MultiNoncePub,
+      keySet: KeySet,
+      message: ByteVector): (FieldElement, ECPublicKey, FieldElement) = {
+    require(aggMultiNoncePub.length == nonceNum)
+
+    val aggPubKey = keySet.aggPubKey.schnorrPublicKey
+
+    val bHash = nonCoefHash(
+      aggMultiNoncePub
+        .map(_.bytes)
+        .reduce(_ ++ _) ++ aggPubKey.bytes ++ message)
+    val b = FieldElement(new java.math.BigInteger(1, bHash.toArray))
+
+    val aggNonce = aggMultiNoncePub.tail
+      .foldLeft((FieldElement.one, aggMultiNoncePub.head)) {
+        case ((prevPow, sumSoFar), nonce) =>
+          val pow = prevPow.multiply(b)
+          val point = nonce.multiply(pow)
+
+          (pow, sumSoFar.add(point)) // TODO Deal with infinity here
+      }
+      ._2
+    val eBytes = CryptoUtil
+      .sha256SchnorrChallenge(
+        aggNonce.schnorrNonce.bytes ++ aggPubKey.bytes ++ message)
+      .bytes
+    val e = FieldElement(new java.math.BigInteger(1, eBytes.toArray))
+
+    (b, aggNonce, e)
   }
 
   def sign(
@@ -79,28 +180,27 @@ object MuSig2Util {
       message: ByteVector,
       keySet: KeySet): (ECPublicKey, FieldElement) = {
     val pubKey = privKey.publicKey
-    val coef = keySet.keyAggCoef(pubKey)
-    val aggPubKey = keySet.aggPubKey
-    val bBytes = nonHash(
-      aggPubKey.schnorrPublicKey.bytes ++ aggMultiNoncePub
-        .map(_.schnorrPublicKey.bytes)
-        .reduce(_ ++ _) ++ message)
-    val b = FieldElement(bBytes)
-    val aggNonce = aggMultiNoncePub.tail
-      .foldLeft((FieldElement.one, aggMultiNoncePub.head)) {
-        case ((prevPow, sumSoFar), nonce) =>
-          val pow = prevPow.multiply(b)
-          val point = nonce.multiply(pow)
+    val coef = keySet.keyAggCoef(pubKey.schnorrPublicKey)
+    val (b, aggNonce, e) = getSessionValues(aggMultiNoncePub, keySet, message)
 
-          (pow, sumSoFar.add(point))
-      }
-      ._2
+    val adjustedNoncePriv = aggNonce.parity match {
+      case EvenParity => noncePriv
+      case OddParity  => noncePriv.map(_.negate)
+    }
 
-    val cBytes = sigHash(
-      aggPubKey.schnorrPublicKey.bytes ++ aggNonce.schnorrNonce.bytes ++ message)
-    val c = FieldElement(cBytes)
+    val gp = pubKey.parity match {
+      case EvenParity => FieldElement.one
+      case OddParity  => FieldElement.orderMinusOne
+    }
 
-    val privNonceSum = noncePriv
+    val g = keySet.aggPubKey.parity match {
+      case EvenParity => FieldElement.one
+      case OddParity  => FieldElement.orderMinusOne
+    }
+
+    val adjustedPrivKey = privKey.fieldElement.multiply(gp).multiply(g)
+
+    val privNonceSum = adjustedNoncePriv
       .foldLeft((FieldElement.one, FieldElement.zero)) {
         case ((pow, sumSoFar), privNonce) =>
           val scalar = privNonce.fieldElement.multiply(pow)
@@ -110,9 +210,9 @@ object MuSig2Util {
       }
       ._2
 
-    val s = privKey.fieldElement.multiply(c).multiply(coef).add(privNonceSum)
+    val s = adjustedPrivKey.multiply(e).multiply(coef).add(privNonceSum)
 
-    (aggNonce, s)
+    (aggNonce, s) // TODO require passes internal verification
   }
 
   def signAgg(
@@ -121,18 +221,5 @@ object MuSig2Util {
     val s = sVals.reduce(_.add(_))
 
     SchnorrDigitalSignature(aggPubNonce.schnorrNonce, s)
-  }
-
-  def verify(
-      aggPubKey: ECPublicKey,
-      message: ByteVector,
-      sig: SchnorrDigitalSignature): Boolean = {
-    val aggPubNonce = sig.rx
-    val s = sig.sig
-    val cBytes = sigHash(
-      aggPubKey.schnorrPublicKey.bytes ++ aggPubNonce.bytes ++ message)
-    val c = FieldElement(cBytes)
-
-    s.getPublicKey == aggPubNonce.publicKey.add(aggPubKey.multiply(c))
   }
 }
