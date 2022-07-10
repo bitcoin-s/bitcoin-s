af85f9e2966eed9c2298e8d9967092bdeaf8fb2d
Chris Stewart <stewart.chris1234@gmail.com>
2022-07-10 15:21:49 -0500

    Address ben's code review

--- core/src/main/scala/org/bitcoins/core/crypto/TransactionSignatureChecker.scala
+++ core/src/main/scala/org/bitcoins/core/crypto/TransactionSignatureChecker.scala
@@ -98,7 +98,7 @@ trait TransactionSignatureChecker {
                                                       taprootOptions)
     //bip341 restricts valid hash types: https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki#common-signature-message
     val invalidHashType = {
-      !(hashType.byte <= 3 || (hashType.byte >= 81.toByte && hashType.byte <= 0x83.toByte))
+      !(hashType.byte <= 0x03.toByte || (hashType.byte >= 0x81.toByte && hashType.byte <= 0x83.toByte))
     }
     if (invalidHashType) {
       ScriptErrorSchnorrSigHashType
