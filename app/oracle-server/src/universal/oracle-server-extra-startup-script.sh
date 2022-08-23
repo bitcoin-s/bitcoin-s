2001e86ab94019dd468aaa658695620626e19e98
Chris Stewart <stewart.chris1234@gmail.com>
2022-08-23 13:16:42 -0500

    If `DISABLE_JLINK` set, don't try to chmod `jre/bin/java` (#4651)
    * If DISABLE_JLINK set, don't try to chmod jre/bin/java
    
    * Do it for oracle server as well

--- app/oracle-server/src/universal/oracle-server-extra-startup-script.sh
+++ app/oracle-server/src/universal/oracle-server-extra-startup-script.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-if test -f "jre/bin/java"; then
-  if [[ "$OS" == "OSX" ]]; then
-    #mac doesn't allow random binaries to be executable
-    #remove the quarantine attribute so java is executable on mac
-    xattr -d com.apple.quarantine jre/bin/java
+if [[ -z "$DISABLE_JLINK" ]]; then
+  if test -f "jre/bin/java"; then
+    if [[ "$OS" == "OSX" ]]; then
+      #mac doesn't allow random binaries to be executable
+      #remove the quarantine attribute so java is executable on mac
+      xattr -d com.apple.quarantine jre/bin/java
+    fi
+    chmod +x jre/bin/java #make sure java is executable
   fi
-  chmod +x jre/bin/java #make sure java is executable
-fi
 
-if test -f "../jre/bin/java" ; then
-  if [[ "$OS" == "OSX" ]]; then
-    #mac doesn't allow random binaries to be executable
-    #remove the quarantine attribute so java is executable on mac
-    xattr -d com.apple.quarantine ../jre/bin/java
+
+  if test -f "../jre/bin/java" ; then
+    if [[ "$OS" == "OSX" ]]; then
+      #mac doesn't allow random binaries to be executable
+      #remove the quarantine attribute so java is executable on mac
+      xattr -d com.apple.quarantine ../jre/bin/java
+    fi
+    chmod +x ../jre/bin/java #make sure java is executable
   fi
-  chmod +x ../jre/bin/java #make sure java is executable
 fi
 
 get_java_no_jlink() {
