7322186b5692bb25ea3f2595f59a64e5574c0f79
rorp <rorp@users.noreply.github.com>
2022-09-01 14:54:39 -0700

    Fix Tor hidden service config (#4689)
    * Fix Tor hidden service config
    
    * default the target list to an empty list
    
    * Scala 2.12 compatibility
    
    * update docs
    
    * Fix typo
    
    Co-authored-by: Chris Stewart <stewart.chris1234@gmail.com>

--- docs/config/configuration.md
+++ docs/config/configuration.md
@@ -211,6 +211,9 @@ bitcoin-s {
 
         # The path to the private key of the onion service being created
         # privateKeyPath = /path/to/priv/key
+        
+        # Optonal Tor targets. If empty all hidden serices will be created at localhost. 
+        targets = []
     }
     
     # settings for the chain module
--- tor/src/main/scala/org/bitcoins/tor/config/TorAppConfig.scala
+++ tor/src/main/scala/org/bitcoins/tor/config/TorAppConfig.scala
@@ -45,6 +45,9 @@ case class TorAppConfig(
 
   lazy val useRandomPorts = getBoolean("tor.use-random-ports")
 
+  lazy val targets = getStringList("tor.targets")
+    .map(NetworkUtil.parseInetSocketAddress(_, -1))
+
   lazy val socks5ProxyParams: Option[Socks5ProxyParams] = {
     if (getBoolean("proxy.enabled")) {
       val address = if (torProvided) {
@@ -221,6 +224,15 @@ case class TorAppConfig(
 
   private def getStringOrNone(key: String): Option[String] =
     getConfigValue(config.getStringOrNone)(key)
+
+  private def getStringList(key: String): Vector[String] = try {
+    val list = getConfigValue(config.getStringList)(key)
+    0.until(list.size())
+      .foldLeft(Vector.empty[String])((acc, i) => acc :+ list.get(i))
+      .flatMap(_.split(","))
+  } catch {
+    case _: com.typesafe.config.ConfigException.Missing => Vector()
+  }
 
   private def getConfigValue[V](getValue: String => V)(key: String): V = {
     subModuleNameOpt match {
