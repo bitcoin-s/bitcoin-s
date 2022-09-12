6655bf7dd8d9780ee09ecb0a29e3d560b7d16508
benthecarman <benthecarman@live.com>
2022-09-11 19:30:28 -0500

    Use postrgres when flagged in TrezorAddressTest

--- wallet-test/src/test/scala/org/bitcoins/wallet/TrezorAddressTest.scala
+++ wallet-test/src/test/scala/org/bitcoins/wallet/TrezorAddressTest.scala
@@ -220,7 +220,9 @@ class TrezorAddressTest extends BitcoinSWalletTest with EmptyFixture {
   private def testAccountType(purpose: HDPurpose): Future[Assertion] = {
     val confOverride = configForPurposeAndSeed(purpose)
     implicit val conf: WalletAppConfig =
-      BitcoinSTestAppConfig.getNeutrinoTestConfig(confOverride).walletConf
+      BitcoinSTestAppConfig
+        .getNeutrinoWithEmbeddedDbTestConfig(() => pgUrl(), confOverride)
+        .walletConf
 
     val testVectors = purpose match {
       case HDPurposes.Legacy       => legacyVectors
