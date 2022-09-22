7383fa8c0177d8dc519e4cbf54b41e0ea210d905
benthecarman <benthecarman@live.com>
2022-09-22 13:35:57 -0500

    Add blockheight to bitcoind's ListTransactionsResult

--- bitcoind-rpc-test/src/test/scala/org/bitcoins/rpc/common/WalletRpcTest.scala
+++ bitcoind-rpc-test/src/test/scala/org/bitcoins/rpc/common/WalletRpcTest.scala
@@ -358,6 +358,24 @@ class WalletRpcTest extends BitcoindFixturesCachedPairV21 {
       }
   }
 
+  it should "be able to list transactions" in { nodePair: FixtureParam =>
+    val client = nodePair.node1
+    val otherClient = nodePair.node2
+    for {
+      address <- otherClient.getNewAddress
+      txid <-
+        BitcoindRpcTestUtil
+          .fundBlockChainTransaction(client,
+                                     otherClient,
+                                     address,
+                                     Bitcoins(1.5))
+      txs <- otherClient.listTransactions()
+    } yield {
+      assert(txs.nonEmpty)
+      assert(txs.exists(_.txid.contains(txid)))
+    }
+  }
+
   it should "be able to import an address" in { nodePair: FixtureParam =>
     val client = nodePair.node1
     val otherClient = nodePair.node2
