483323edd184f5af7959763251d7452d21394ccc
Chris Stewart <stewart.chris1234@gmail.com>
2022-11-01 13:07:05 -0500

    Remove legacy GUI, this fixes CI merges to master as we no longer use the deprecated notary tool from apple

--- app/bundle/src/main/scala/org/bitcoins/bundle/util/BitcoinSAppJFX3.scala
+++
@@ -1,6 +0,0 @@
-package org.bitcoins.bundle.util
-
-import org.bitcoins.server.util.BitcoinSApp
-import scalafx.application.JFXApp3
-
-trait BitcoinSAppJFX3 extends BitcoinSApp with JFXApp3
--- app/gui/src/main/scala/org/bitcoins/gui/dlc/dialog/CliCommandProducer.scala
+++
@@ -1,8 +0,0 @@
-package org.bitcoins.gui.dlc.dialog
-
-import org.bitcoins.commons.rpc.CliCommand
-
-trait CliCommandProducer[T <: CliCommand] {
-  def getCliCommand(): T
-//  def buildView(params vary per dialog)
-}
