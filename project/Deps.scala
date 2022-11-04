f381941b1da3f9286d6b3632036096d14b405ff6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-11-04 18:51:43 +0100

    Update junixsocket-core to 2.6.1 (#4876)

--- project/Deps.scala
+++ project/Deps.scala
@@ -26,7 +26,7 @@ object Deps {
     val akkav = "10.2.10"
     val playv = "2.9.3" //https://github.com/playframework/play-json/releases
     val akkaStreamv = "2.6.20"
-    val jUnixSocketV = "2.6.0"
+    val jUnixSocketV = "2.6.1"
     val scodecV = "1.1.34"
     val junitV = "0.13.3"
     val nativeLoaderV = "2.4.0"
