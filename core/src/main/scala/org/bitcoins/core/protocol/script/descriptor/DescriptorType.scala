720fe27e44642de1d707773c015c8fca9fc9cdc2
Chris Stewart <stewart.chris1234@gmail.com>
2022-12-07 09:27:17 -0600

    Add DescriptorType, OutputDescriptor

---
+++ core/src/main/scala/org/bitcoins/core/protocol/script/descriptor/DescriptorType.scala
@@ -0,0 +1,74 @@
+package org.bitcoins.core.protocol.script.descriptor
+
+import org.bitcoins.core.script.ScriptType
+import org.bitcoins.crypto.StringFactory
+
+/** @see [[https://github.com/bitcoin/bitcoin/blob/master/doc/descriptors.md#features]] */
+sealed abstract class DescriptorType {
+  def scriptType: ScriptType
+}
+
+object DescriptorType extends StringFactory[DescriptorType] {
+
+  case object PK extends DescriptorType {
+    override val scriptType = ScriptType.PUBKEY
+    override def toString: String = "pk"
+  }
+
+  case object PKH extends DescriptorType {
+    override val scriptType = ScriptType.PUBKEYHASH
+    override def toString: String = "pkh"
+  }
+
+  case object SH extends DescriptorType {
+    override val scriptType = ScriptType.SCRIPTHASH
+    override def toString: String = "sh"
+  }
+
+  case object WPKH extends DescriptorType {
+    override val scriptType = ScriptType.WITNESS_V0_KEYHASH
+    override def toString: String = "wpkh"
+  }
+
+  case object WSH extends DescriptorType {
+    override val scriptType = ScriptType.WITNESS_V0_SCRIPTHASH
+    override val toString: String = "wsh"
+  }
+
+  case object TR extends DescriptorType {
+    override val scriptType = ScriptType.WITNESS_V1_TAPROOT
+    override val toString: String = "tr"
+  }
+
+  case object Multi extends DescriptorType {
+    override val scriptType = ScriptType.MULTISIG
+    override val toString: String = "multi"
+  }
+
+  case object SortedMulti extends DescriptorType {
+    override val scriptType = ScriptType.MULTISIG
+    override val toString: String = "sortedmulti"
+  }
+
+  private val all: Vector[DescriptorType] = Vector(
+    PK,
+    PKH,
+    WPKH,
+    WSH,
+    TR,
+    Multi,
+    SortedMulti
+  )
+
+  override def fromStringOpt(string: String): Option[DescriptorType] = {
+    all.find(_.toString.toLowerCase == string.toLowerCase)
+  }
+
+  override def fromString(string: String): DescriptorType = {
+    fromStringOpt(string) match {
+      case Some(d) => d
+      case None =>
+        sys.error(s"Could not find descriptor type for string=$string")
+    }
+  }
+}
