09150e5a1f1d4ac67af0ef4eaa4938b7e050e446
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-05-08 16:54:10 +0200

    Update scalacheck-1-15 to 3.2.12.0 (#5068)

--- project/Deps.scala
+++ project/Deps.scala
@@ -18,7 +18,7 @@ object Deps {
     val scalaTest = "3.2.15" //https://www.scalatest.org/
 
     val scalaTestPlus =
-      "3.2.12.0-RC2" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
+      "3.2.12.0" //super annoying... https://oss.sonatype.org/content/groups/public/org/scalatestplus/
     val slf4j = "2.0.7"
     val spray = "1.3.6"
     val zeromq = "0.5.3"
@@ -225,7 +225,7 @@ object Deps {
         "org.scalatest" %%% "scalatest" % V.scalaTest withSources () withJavadoc ())
 
     val scalaTestPlus = Def.setting(
-      "org.scalatestplus" %%% "scalacheck-1-15" % V.scalaTestPlus withSources () withJavadoc ())
+      "org.scalatestplus" %%% "scalacheck-1-16" % V.scalaTestPlus withSources () withJavadoc ())
 
     val pgEmbedded =
       "com.opentable.components" % "otj-pg-embedded" % V.pgEmbeddedV withSources () withJavadoc ()
