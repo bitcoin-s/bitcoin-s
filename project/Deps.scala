1014c47bd9382e328d6b4da5d88544780362a2c0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-05-13 15:23:38 +0200

    Update bcprov-jdk18on to 1.73 (#5044)

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ import sbt._
 object Deps {
 
   object V {
-    val bouncyCastle = "1.72"
+    val bouncyCastle = "1.73"
     val dropwizardMetricsV = "4.2.18" //https://github.com/dropwizard/metrics
 
     val logback = "1.4.7" //https://github.com/qos-ch/logback
