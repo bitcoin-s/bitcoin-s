25055ab1ab17ab3b814724bc29bcfe57f4dec975
Chris Stewart <stewart.chris1234@gmail.com>
2023-06-05 09:51:21 -0500

    Re-add PeerMessageReceiverTest

--- node-test/src/test/scala/org/bitcoins/node/networking/peer/PeerMessageReceiverTest.scala
+++ node-test/src/test/scala/org/bitcoins/node/networking/peer/PeerMessageReceiverTest.scala
@@ -1,12 +1,24 @@
 package org.bitcoins.node.networking.peer
 
+import akka.actor.ActorRef
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import org.bitcoins.core.config.RegTest
+import org.bitcoins.core.number.Int32
+import org.bitcoins.core.p2p.{InetAddress, VerAckMessage, VersionMessage}
+import org.bitcoins.node.constant.NodeConstants
+import org.bitcoins.node.models.Peer
+import org.bitcoins.node.networking.P2PClient
 import org.bitcoins.testkit.util.BitcoinSAsyncTest
+
+import java.net.InetSocketAddress
+import scala.concurrent.Promise
 
 class PeerMessageReceiverTest extends BitcoinSAsyncTest {
 
   behavior of "PeerMessageReceiverTest"
 
-  /*  it must "change a peer message receiver to be disconnected" in {
+  it must "change a peer message receiver to be disconnected" in {
     val socket = InetSocketAddress.createUnresolved("google.com", 12345)
     val peer = Peer(socket, None, None)
     val client = P2PClient(ActorRef.noSender, peer)
@@ -31,8 +43,11 @@ class PeerMessageReceiverTest extends BitcoinSAsyncTest {
                                                    Promise[Unit](),
                                                  versionMsgP = versionMsgP,
                                                  verackMsgP = verackMsgP)
-
-    val newMsgReceiverStateF = normal.disconnect(peer, Source., P2PClientCallbacks.empty)
+    val queue = Source
+      .queue[StreamDataMessageWrapper](1, OverflowStrategy.backpressure)
+      .toMat(Sink.ignore)(Keep.left)
+      .run()
+    val newMsgReceiverStateF = normal.disconnect(peer, queue)
 
     newMsgReceiverStateF.map { newMsgReceiverState =>
       assert(
@@ -68,7 +83,10 @@ class PeerMessageReceiverTest extends BitcoinSAsyncTest {
                                                    Promise[Unit](),
                                                  versionMsgP = versionMsgP,
                                                  verackMsgP = verackMsgP)
-
+    val queue = Source
+      .queue[StreamDataMessageWrapper](1, OverflowStrategy.backpressure)
+      .toMat(Sink.ignore)(Keep.left)
+      .run()
     val newMsgReceiverState = normal.initializeDisconnect(peer)
 
     assert(
@@ -76,13 +94,12 @@ class PeerMessageReceiverTest extends BitcoinSAsyncTest {
         .isInstanceOf[PeerMessageReceiverState.InitializedDisconnect])
     assert(!newMsgReceiverState.isDisconnected)
 
-    newMsgReceiverState.disconnect(peer, P2PClientCallbacks.empty).map {
-      disconnectRecv =>
-        assert(
-          disconnectRecv
-            .isInstanceOf[PeerMessageReceiverState.InitializedDisconnectDone])
-        assert(disconnectRecv.isDisconnected)
-        assert(disconnectRecv.clientDisconnectP.isCompleted)
+    newMsgReceiverState.disconnect(peer, queue).map { disconnectRecv =>
+      assert(
+        disconnectRecv
+          .isInstanceOf[PeerMessageReceiverState.InitializedDisconnectDone])
+      assert(disconnectRecv.isDisconnected)
+      assert(disconnectRecv.clientDisconnectP.isCompleted)
     }
-  }*/
+  }
 }
