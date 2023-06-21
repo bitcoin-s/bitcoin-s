478f38e0b44bf425a9f00312b7beea2f5786a730
Chris Stewart <stewart.chris1234@gmail.com>
2023-06-21 09:52:23 -0500

    Refactor things out of PeerMessageSender.connect(), add some flow logs

--- node/src/main/scala/org/bitcoins/node/networking/peer/PeerMessageSender.scala
+++ node/src/main/scala/org/bitcoins/node/networking/peer/PeerMessageSender.scala
@@ -25,7 +25,6 @@ import org.bitcoins.node.P2PLogger
 import org.bitcoins.node.config.NodeAppConfig
 import org.bitcoins.node.constant.NodeConstants
 import org.bitcoins.node.networking.P2PClient
-import org.bitcoins.node.networking.P2PClient.ExpectResponseCommand
 import org.bitcoins.node.networking.peer.PeerMessageReceiver.NetworkMessageReceived
 import org.bitcoins.node.util.PeerMessageSenderApi
 import scodec.bits.ByteVector
@@ -42,7 +41,10 @@ case class PeerMessageSender(
     system: ActorSystem)
     extends P2PLogger {
   import system.dispatcher
-  private val socket = client.peer.socket
+
+  private val peer = client.peer
+  private val socket = peer.socket
+
   implicit private val timeout: Timeout = Timeout(30.seconds)
 
   private val options = Vector(KeepAlive(true))
@@ -85,16 +87,22 @@ case class PeerMessageSender(
     Vector[NetworkMessage],
     NotUsed] = {
     Flow[ByteString]
-      .log("hello", { case bytes => logger.info(s"Flow.bytes=$bytes") })
       .statefulMap(() => ByteString.empty)(parseHelper,
                                            { _: ByteString => None })
+      .log("parseToNetworkMsgFlow",
+           { case msgs: Vector[NetworkMessage] =>
+             msgs
+               .map(msg =>
+                 s"received msg=${msg.payload.commandName} from peer=$peer")
+               .mkString("\n")
+           })
   }
 
   private val writeNetworkMsgFlow: Flow[NetworkMessage, ByteString, NotUsed] = {
     Flow[NetworkMessage].map(msg => ByteString(msg.bytes.toArray))
   }
 
-  val bidiFlow: BidiFlow[
+  private val bidiFlow: BidiFlow[
     ByteString,
     Vector[NetworkMessage],
     NetworkMessage,
@@ -103,9 +111,38 @@ case class PeerMessageSender(
     BidiFlow.fromFlows(parseToNetworkMsgFlow, writeNetworkMsgFlow)
   }
 
+  private val mergeHubSource: Source[
+    NetworkMessage,
+    Sink[NetworkMessage, NotUsed]] =
+    MergeHub
+      .source[NetworkMessage](16)
+      .log("mergehub",
+           { case msg: NetworkMessage =>
+             s"sending msg=${msg.payload.commandName} to peer=$peer"
+           })
+
+  private val connectionFlow: Flow[
+    NetworkMessage,
+    Vector[NetworkMessage],
+    Future[Tcp.OutgoingConnection]] =
+    connection.joinMat(bidiFlow)(Keep.left)
+
+  private def connectionGraph(
+      handleNetworkMsgSink: Sink[
+        Vector[NetworkMessage],
+        NotUsed]): RunnableGraph[
+    (Sink[NetworkMessage, NotUsed], Future[Tcp.OutgoingConnection])] = {
+    mergeHubSource
+      .viaMat(connectionFlow)(Keep.both)
+      .toMat(handleNetworkMsgSink)(Keep.left)
+  }
+
   @volatile private[this] var connectionP: Option[
     Promise[Option[NetworkMessage]]] =
     None
+
+  @volatile private[this] var connectionSinkOpt: Option[
+    Sink[NetworkMessage, NotUsed]] = None
 
   /** Initiates a connection with the given peer */
   def connect(): Future[Unit] = {
@@ -115,18 +152,6 @@ case class PeerMessageSender(
         Future.unit
       case None =>
         logger.info(s"Attempting to connect to peer=${client.peer}")
-        val connectionFlow: Flow[
-          NetworkMessage,
-          Vector[NetworkMessage],
-          Future[Tcp.OutgoingConnection]] =
-          connection.joinMat(bidiFlow)(Keep.left)
-
-        val mergeHubSource: Source[
-          NetworkMessage,
-          Sink[NetworkMessage, NotUsed]] =
-          MergeHub
-            .source[NetworkMessage](16)
-            .log("mergehub")
 
         val initializing =
           initPeerMessageRecv.connect(client, peerMessageSenderApi)
@@ -143,17 +168,12 @@ case class PeerMessageSender(
             .to(Sink.ignore)
         }
 
-        val graph: RunnableGraph[
-          (Sink[NetworkMessage, NotUsed], Future[Tcp.OutgoingConnection])] = {
-          mergeHubSource
-            .viaMat(connectionFlow)(Keep.both)
-            .toMat(handleNetworkMsgSink)(Keep.left)
-        }
-
         val (mergeHubSink: Sink[NetworkMessage, NotUsed],
              outgoingConnectionF: Future[Tcp.OutgoingConnection]) = {
-          graph.run()
+          connectionGraph(handleNetworkMsgSink).run()
         }
+
+        connectionSinkOpt = Some(mergeHubSink)
 
         val resultF: Future[NotUsed] = for {
           _ <- outgoingConnectionF
@@ -233,12 +253,22 @@ case class PeerMessageSender(
 
   private[node] def sendMsg(msg: NetworkMessage): Future[Unit] = {
     logger.debug(s"Sending msg=${msg.header.commandName} to peer=${socket}")
-    val wrap = msg match {
+    /*    val wrap = msg match {
       case _: ExpectsResponse => ExpectResponseCommand(msg.payload)
       case _                  => msg
+    }*/
+    connectionSinkOpt match {
+      case Some(connectionSink) =>
+        val sendMsgF = Future {
+          Source.single(msg).to(connectionSink).run()
+        }.map(_ => ())
+
+        sendMsgF
+      case None =>
+        val exn = new RuntimeException(
+          s"Could not send msg=${msg.payload.commandName} because we do not have an active connection to peer=${peer}")
+        Future.failed(exn)
     }
-    client.actor ! wrap
-    Future.unit
   }
 }
 
