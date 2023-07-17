1bd21ffcbc7ffc49633de81be4eafe989dde9ae6
Chris Stewart <stewart.chris1234@gmail.com>
2023-07-17 10:09:18 -0500

    Refactor transaction tests in coreTest

--- core-test/src/test/scala/org/bitcoins/core/protocol/transaction/TransactionOutputTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/protocol/transaction/TransactionOutputTest.scala
@@ -2,6 +2,7 @@ package org.bitcoins.core.protocol.transaction
 
 import org.bitcoins.core.currency.CurrencyUnits
 import org.bitcoins.core.protocol.script.EmptyScriptPubKey
+import org.bitcoins.testkitcore.gen.TransactionGenerators
 import org.bitcoins.testkitcore.util.BitcoinSUnitTest
 
 /** Created by chris on 3/30/16.
@@ -11,5 +12,12 @@ class TransactionOutputTest extends BitcoinSUnitTest {
   "TransactionOutput" must "define an empty transaction output" in {
     EmptyTransactionOutput.scriptPubKey must be(EmptyScriptPubKey)
     EmptyTransactionOutput.value must be(CurrencyUnits.negativeSatoshi)
+  }
+
+  it must "serialization symmetry" in {
+    forAll(TransactionGenerators.output) { output =>
+      assert(TransactionOutput(output.hex) == output)
+      assert(output.hex == TransactionOutput(output.hex).hex)
+    }
   }
 }
