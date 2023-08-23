e8ab293fa0c368684c178d3745081cc2122c7183
Chris Stewart <stewart.chris1234@gmail.com>
2023-08-23 08:34:37 -0500

    Remove `Initialized` from `NodeStreamMessage`, return `Initialized `from `ControlMessageHandler` (#5199)
    * Remove Initialized from NodeStreamMessage, return Initialized from ControlMessageHandler to avoid deadlocking queue with backpressure
    
    * Revert a few things
    
    * scalafmt

--- node/src/main/scala/org/bitcoins/node/NodeStreamMessage.scala
+++ node/src/main/scala/org/bitcoins/node/NodeStreamMessage.scala
@@ -24,8 +24,6 @@ object NodeStreamMessage {
   case class DisconnectedPeer(peer: Peer, forceReconnect: Boolean)
       extends NodeStreamMessage
 
-  case class Initialized(peer: Peer) extends NodeStreamMessage
-
   case class InitializationTimeout(peer: Peer) extends NodeStreamMessage
 
   case class QueryTimeout(peer: Peer, payload: ExpectsResponse)
@@ -35,4 +33,6 @@ object NodeStreamMessage {
       extends NodeStreamMessage
 
   case class SendToPeer(msg: NetworkMessage, peerOpt: Option[Peer])
+
+  case class Initialized(peer: Peer)
 }
