984a2344bc23a6cff4b3e4f6bf12eae0ac033487
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-10-24 17:47:47 +0200

    Update scodec-bits to 1.1.38 (#5273)

--- project/Deps.scala
+++ project/Deps.scala
@@ -28,7 +28,7 @@ object Deps {
     val playv = "2.10.2" //https://github.com/playframework/play-json/releases
     val akkaStreamv = "2.6.20"
     val jUnixSocketV = "2.8.1"
-    val scodecV = "1.1.37"
+    val scodecV = "1.1.38"
     val junitV = "0.13.3"
     val nativeLoaderV = "2.4.0"
     val typesafeConfigV = "1.4.3"
