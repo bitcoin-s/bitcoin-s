e6ceda44f2ab54d3cc9cfd45303010ed6e95660a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-01-19 13:37:56 +0100

    Update play-json to 2.10.4 (#5356)

--- project/Deps.scala
+++ project/Deps.scala
@@ -25,7 +25,7 @@ object Deps {
     val zeromq = "0.5.4"
     val scalapb = "0.11.14"
     val akkav = "10.2.10"
-    val playv = "2.10.3" //https://github.com/playframework/play-json/releases
+    val playv = "2.10.4" //https://github.com/playframework/play-json/releases
     val akkaStreamv = "2.6.20"
     val jUnixSocketV = "2.8.3"
     val scodecV = "1.1.38"
