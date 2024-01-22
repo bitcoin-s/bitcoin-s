6da3f37bf8ecdb126d2e5aee7bccd3237c2be6a8
Scala Steward <scala_steward@virtuslab.com>
2024-01-22 14:55:26 +0000

    Update scalapb-runtime to 0.11.15

--- project/Deps.scala
+++ project/Deps.scala
@@ -23,7 +23,7 @@ object Deps {
     val slf4j = "2.0.11"
     val spray = "1.3.6"
     val zeromq = "0.5.4"
-    val scalapb = "0.11.14"
+    val scalapb = "0.11.15"
     val akkav = "10.2.10"
     val playv = "2.10.4" //https://github.com/playframework/play-json/releases
     val akkaStreamv = "2.6.20"
