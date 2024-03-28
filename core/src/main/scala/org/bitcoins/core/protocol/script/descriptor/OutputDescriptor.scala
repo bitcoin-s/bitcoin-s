e7e686893e0994da505634a263a45424ac727684
Chris Stewart <stewart.chris1234@gmail.com>
2024-03-28 07:27:37 -0500

    WIP: Implement polyMod

--- core/src/main/scala/org/bitcoins/core/protocol/script/descriptor/OutputDescriptor.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/script/descriptor/OutputDescriptor.scala
@@ -1,6 +1,6 @@
 package org.bitcoins.core.protocol.script.descriptor
 
-import org.bitcoins.core.number.{UInt5, UInt64}
+import org.bitcoins.core.number.{UInt64, UInt8}
 import org.bitcoins.core.protocol.script.{P2WPKHWitnessSPKV0, ScriptPubKey}
 import org.bitcoins.core.util.Bech32
 import org.bitcoins.crypto.StringFactory
@@ -43,6 +43,10 @@ object OutputDescriptor extends StringFactory[OutputDescriptor] {
       "ijklmnopqrstuvwxyzABCDEFGH`#\"\\ ").toVector
   }
 
+  private val charsetWithIdx: Vector[(Char, Int)] = {
+    charset.zipWithIndex
+  }
+
   private val map: Map[DescriptorType, StringFactory[OutputDescriptor]] = {
     Map(
       DescriptorType.WPKH -> P2WPKHDescriptor
@@ -66,25 +70,29 @@ object OutputDescriptor extends StringFactory[OutputDescriptor] {
     var c = UInt64.one
     var cls = 0
     var clsCount = 0
-    val u5s = Bech32.decodeStringToU5s(string, charset)
-    u5s.zipWithIndex.foreach { case (u5, pos) =>
-      c = polyMod(c, u5)
+
+    string.foreach { case ch =>
+      val pos = charsetWithIdx
+        .find(_._1 == ch)
+        .map(_._2)
+        .get
+      c = polyMod(c, pos)
       cls = cls * 3 + (pos >> 5)
       clsCount += 1
       if (clsCount == 3) {
         // Emit an extra symbol representing the group numbers, for every 3 characters.
-        c = polyMod(c, UInt5(cls))
+        c = polyMod(c, cls)
         cls = 0
         clsCount = 0
       }
     }
 
     if (clsCount > 0) {
-      c = polyMod(c, UInt5(cls))
+      c = polyMod(c, cls)
     }
 
     1.until(8).foreach { _ =>
-      c = polyMod(c, UInt5.zero)
+      c = polyMod(c, 0)
     }
 
     //c = c ^ 1 // Prevent appending zeroes from not affecting the checksum.
@@ -92,12 +100,49 @@ object OutputDescriptor extends StringFactory[OutputDescriptor] {
     val builder = new StringBuilder()
     1.until(8).foreach { j =>
       //ret[j] = CHECKSUM_CHARSET[(c >> (5 * (7 - j))) & 31]
-      val char = charset((c.toInt >> (5 * (7 - j))) & 31)
+      val char = Bech32.charset((c.toInt >> (5 * (7 - j))) & 31)
       builder.append(char)
     }
 
     builder.result()
   }
 
-  def polyMod(c: UInt64, pos: UInt5): UInt64 = ???
+  def polyMod(c: UInt64, idx: Int): UInt64 = {
+    //uint64_t PolyMod(uint64_t c, int val)
+    //{
+    //    uint8_t c0 = c >> 35;
+    //    c = ((c & 0x7ffffffff) << 5) ^ val;
+    //    if (c0 & 1) c ^= 0xf5dee51989;
+    //    if (c0 & 2) c ^= 0xa9fdca3312;
+    //    if (c0 & 4) c ^= 0x1bab10e32d;
+    //    if (c0 & 8) c ^= 0x3706b1677a;
+    //    if (c0 & 16) c ^= 0x644d626ffd;
+    //    return c;
+    //}
+
+    var res = c
+    val c0: UInt8 = UInt8((c >> 35).toInt)
+    res = (c & UInt64(0x7ffffffffL) << 5) ^ idx
+    if ((c0 & UInt8.one) != UInt8.zero) {
+      res = res ^ 0xf5dee51989L
+    }
+
+    if ((c0 & UInt8(2)) != UInt8.zero) {
+      res = res ^ 0xa9fdca3312L
+    }
+
+    if ((c0 & UInt8(4)) != UInt8.zero) {
+      res = res ^ 0x1bab10e32dL
+    }
+
+    if ((c0 & UInt8(8)) != UInt8.zero) {
+      res = res ^ 0x3706b1677aL
+    }
+
+    if ((c0 & UInt8(16)) != UInt8.zero) {
+      res = res ^ 0x644d626ffdL
+    }
+
+    res
+  }
 }
