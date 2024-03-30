950f30623cbc78b65d17e59d684cb7b6b064aad2
Chris Stewart <stewart.chris1234@gmail.com>
2024-03-30 14:03:33 -0500

    Get basic KeyExpression test case passing

--- core-test/src/test/scala/org/bitcoins/core/protocol/script/descriptor/KeyExpressionTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/protocol/script/descriptor/KeyExpressionTest.scala
@@ -7,7 +7,7 @@ class KeyExpressionTest extends BitcoinSUnitTest {
   behavior of "KeyExpression"
 
   it must "serialize and deserialize key origin examples in BIP380" in {
-    val str0 = "[deadbeef/0h/0h/0h]"
+    val str0 = "[deadbeef/0'/0'/0']"
     val keyOrigin = KeyOriginExpression.fromString(str0)
     assert(str0 == keyOrigin.toString)
   }
--- core/src/main/scala/org/bitcoins/core/protocol/script/descriptor/KeyOriginExpression.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/script/descriptor/KeyOriginExpression.scala
@@ -1,12 +1,12 @@
 package org.bitcoins.core.protocol.script.descriptor
 
-import org.bitcoins.core.hd.HDPath
+import org.bitcoins.core.hd.{BIP32Path}
 import org.bitcoins.crypto.StringFactory
 
-case class KeyOriginExpression(fingerprint: String, hdPath: HDPath) {
+case class KeyOriginExpression(fingerprint: String, path: BIP32Path) {
 
   override def toString: String = {
-    s"[${fingerprint + hdPath.toString}]"
+    s"[${fingerprint + path.toString.drop(1)}]"
   }
 }
 
@@ -18,7 +18,7 @@ object KeyOriginExpression extends StringFactory[KeyOriginExpression] {
     val payload = string.drop(1).dropRight(1)
     val fingerprint = payload.take(8)
     val pathElements = payload.drop(8)
-    val path = HDPath.fromString("m" + pathElements)
+    val path = BIP32Path.fromString("m" + pathElements)
     KeyOriginExpression(fingerprint, path)
   }
 }
