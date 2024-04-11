c823f4fc1d024b2e75dbcd2a1d8e5ff320fed42c
Chris Stewart <stewart.chris1234@gmail.com>
2024-04-11 09:24:45 -0500

    Get first test case passing

--- core-test/.jvm/src/test/scala/org/bitcoins/core/protocol/transaction/TaprootWalletTestVectors.scala
+++ core-test/.jvm/src/test/scala/org/bitcoins/core/protocol/transaction/TaprootWalletTestVectors.scala
@@ -1,5 +1,8 @@
 package org.bitcoins.core.protocol.transaction
 
+import org.bitcoins.core.config.MainNet
+import org.bitcoins.core.protocol.Bech32mAddress
+import org.bitcoins.core.protocol.script.TaprootScriptPubKey
 import org.bitcoins.testkitcore.util.BitcoinSUnitTest
 
 class TaprootWalletTestVectors extends BitcoinSUnitTest {
@@ -20,8 +23,28 @@ class TaprootWalletTestVectors extends BitcoinSUnitTest {
   lazy val tests = testCase.tests
 
   it must "build correct taproot spks" in {
-    tests.foreach(t => println(t))
-    succeed
+    tests.foreach { test =>
+      val `given` = test.`given`
+      val intermediary = test.intermediary
+      val expected = test.expected
+      if (`given`.scriptTrees.isEmpty) {
+        //val expectedAddr = test.expected.bip350Address
+        //val expectedSPK = test.expected.scriptPubKey
+        val internal = `given`.internalPubkey
+        val tweakHash =
+          internal.computeTapTweakHash(intermediary.merkleRootOpt)
+        assert(tweakHash == test.intermediary.tweak)
+        val (_, tweakedKey) =
+          internal.createTapTweak(intermediary.merkleRootOpt)
+        assert(tweakedKey == intermediary.tweakedPubkey)
+        val spk = TaprootScriptPubKey.fromPubKey(tweakedKey)
+        val addr = Bech32mAddress.fromScriptPubKey(spk, MainNet)
+        assert(spk == expected.scriptPubKey)
+        assert(addr == expected.bip350Address)
+      } else {
+        fail(s"test=$test")
+      }
+    }
 
   }
 }
