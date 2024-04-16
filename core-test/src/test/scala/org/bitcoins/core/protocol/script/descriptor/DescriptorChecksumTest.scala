6613d22121787e2c9781db61dacec3e1154d8e4c
Chris Stewart <stewart.chris1234@gmail.com>
2024-04-16 15:56:58 -0500

    Add descriptor.py in comments

--- core-test/src/test/scala/org/bitcoins/core/protocol/script/descriptor/DescriptorChecksumTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/protocol/script/descriptor/DescriptorChecksumTest.scala
@@ -47,3 +47,90 @@ class DescriptorChecksumTest extends BitcoinSUnitTest {
     assert(!Descriptor.isValidChecksum(descriptor0, Some("#9f8spxm")))
   }
 }
+
+//descriptor.py
+//INPUT_CHARSET = "0123456789()[],'/*abcdefgh@:$%{}IJKLMNOPQRSTUVWXYZ&+-.;<=>?!^_|~ijklmnopqrstuvwxyzABCDEFGH`#\"\\ "
+//CHECKSUM_CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+//GENERATOR = [0xf5dee51989, 0xa9fdca3312, 0x1bab10e32d, 0x3706b1677a, 0x644d626ffd]
+//
+//def descsum_polymod(symbols):
+//    """Internal function that computes the descriptor checksum."""
+//    chk = 1
+//    for value in symbols:
+//        top = chk >> 35
+//        chk = (chk & 0x7ffffffff) << 5 ^ value
+//        for i in range(5):
+//            chk ^= GENERATOR[i] if ((top >> i) & 1) else 0
+//    return chk
+//
+//def descsum_expand(s):
+//    """Internal function that does the character to symbol expansion"""
+//    groups = []
+//    symbols = []
+//    for c in s:
+//        if not c in INPUT_CHARSET:
+//            return None
+//        v = INPUT_CHARSET.find(c)
+//        symbols.append(v & 31)
+//        groups.append(v >> 5)
+//        if len(groups) == 3:
+//            symbols.append(groups[0] * 9 + groups[1] * 3 + groups[2])
+//            groups = []
+//    if len(groups) == 1:
+//        symbols.append(groups[0])
+//    elif len(groups) == 2:
+//        symbols.append(groups[0] * 3 + groups[1])
+//    return symbols
+//
+//def descsum_check(s):
+//    """Verify that the checksum is correct in a descriptor"""
+//    if s[-9] != '#':
+//        return False
+//    if not all(x in CHECKSUM_CHARSET for x in s[-8:]):
+//        return False
+//    symbols = descsum_expand(s[:-9]) + [CHECKSUM_CHARSET.find(x) for x in s[-8:]]
+//    return descsum_polymod(symbols) == 1
+//INPUT_CHARSET = "0123456789()[],'/*abcdefgh@:$%{}IJKLMNOPQRSTUVWXYZ&+-.;<=>?!^_|~ijklmnopqrstuvwxyzABCDEFGH`#\"\\ "
+//CHECKSUM_CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+//GENERATOR = [0xf5dee51989, 0xa9fdca3312, 0x1bab10e32d, 0x3706b1677a, 0x644d626ffd]
+//
+//def descsum_polymod(symbols):
+//    """Internal function that computes the descriptor checksum."""
+//    chk = 1
+//    for value in symbols:
+//        top = chk >> 35
+//        chk = (chk & 0x7ffffffff) << 5 ^ value
+//        for i in range(5):
+//            chk ^= GENERATOR[i] if ((top >> i) & 1) else 0
+//    return chk
+//
+//def descsum_expand(s):
+//    """Internal function that does the character to symbol expansion"""
+//    groups = []
+//    symbols = []
+//    for c in s:
+//        if not c in INPUT_CHARSET:
+//            return None
+//        v = INPUT_CHARSET.find(c)
+//        symbols.append(v & 31)
+//        groups.append(v >> 5)
+//        if len(groups) == 3:
+//            symbols.append(groups[0] * 9 + groups[1] * 3 + groups[2])
+//            groups = []
+//    if len(groups) == 1:
+//        symbols.append(groups[0])
+//    elif len(groups) == 2:
+//        symbols.append(groups[0] * 3 + groups[1])
+//    return symbols
+//
+//def descsum_check(s):
+//    """Verify that the checksum is correct in a descriptor"""
+//    if s[-9] != '#':
+//        return False
+//    if not all(x in CHECKSUM_CHARSET for x in s[-8:]):
+//        return False
+//    symbols = descsum_expand(s[:-9]) + [CHECKSUM_CHARSET.find(x) for x in s[-8:]]
+//    return descsum_polymod(symbols) == 1
+//
+//result = descsum_check("wpkh([d34db33f/84h/0h/0h]xpub6DJ2dNUysrn5Vt36jH2KLBT2i1auw1tTSSomg8PhqNiUtx8QX2SvC9nrHu81fT41fvDUnhMjEzQgXnQjKEu3oaqMSzhSrHMxyyoEAmUHQbY/0/*)#cjjspncu")
+//print(result)
