ccc05c953ba97ed0fb80922b1cb4fc397a469668
Chris Stewart <stewart.chris1234@gmail.com>
2024-04-20 15:02:00 -0500

    Delete MultisigRpcTest that depends on legacy wallet

--- bitcoind-rpc-test/src/test/scala/org/bitcoins/rpc/common/MultisigRpcTest.scala
+++ bitcoind-rpc-test/src/test/scala/org/bitcoins/rpc/common/MultisigRpcTest.scala
@@ -1,7 +1,6 @@
 package org.bitcoins.rpc.common
 
 import org.bitcoins.commons.jsonmodels.bitcoind.RpcOpts.AddressType
-import org.bitcoins.core.protocol.P2PKHAddress
 import org.bitcoins.crypto.ECPrivateKey
 import org.bitcoins.rpc.client.common.{BitcoindRpcClient, BitcoindVersion}
 import org.bitcoins.rpc.config.BitcoindInstanceLocal
@@ -33,22 +32,6 @@ class MultisigRpcTest extends BitcoindRpcTest {
       _ <- client.createMultiSig(2,
                                  Vector(pubKey1, pubKey2),
                                  AddressType.Bech32)
-    } yield succeed
-  }
-
-  it should "be able to add a multi sig address to the wallet" in {
-    val ecPrivKey1 = ECPrivateKey.freshPrivateKey
-    val pubKey1 = ecPrivKey1.publicKey
-
-    for {
-      client <- clientF
-      address <- client.getNewAddress(addressType = AddressType.Legacy)
-      _ <- {
-        val pubkey = Left(pubKey1)
-        val p2pkh = Right(address.asInstanceOf[P2PKHAddress])
-        client
-          .addMultiSigAddress(2, Vector(pubkey, p2pkh))
-      }
     } yield succeed
   }
 
