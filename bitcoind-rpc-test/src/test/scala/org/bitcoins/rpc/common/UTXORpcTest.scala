bd34a6a01600b268e33ac015979e1b6661ae7d14
Chris Stewart <stewart.chris1234@gmail.com>
2024-04-21 13:41:39 -0500

    Upgrade UTXORpcTest to use test fixtures

--- bitcoind-rpc-test/src/test/scala/org/bitcoins/rpc/common/UTXORpcTest.scala
+++ bitcoind-rpc-test/src/test/scala/org/bitcoins/rpc/common/UTXORpcTest.scala
@@ -2,70 +2,65 @@ package org.bitcoins.rpc.common
 
 import org.bitcoins.commons.jsonmodels.bitcoind.RpcOpts
 import org.bitcoins.crypto.DoubleSha256DigestBE
-import org.bitcoins.rpc.client.common.BitcoindRpcClient
-import org.bitcoins.testkit.rpc.BitcoindRpcTestUtil
-import org.bitcoins.testkit.util.BitcoindRpcTest
+import org.bitcoins.testkit.rpc.{
+  BitcoindFixturesFundedCachedNewest,
+  BitcoindRpcTestUtil
+}
 
 import java.io.File
 import java.nio.file.Files
-import scala.concurrent.Future
 
-class UTXORpcTest extends BitcoindRpcTest {
-
-  lazy val clientF: Future[BitcoindRpcClient] =
-    BitcoindRpcTestUtil.startedBitcoindRpcClient(clientAccum = clientAccum)
+class UTXORpcTest extends BitcoindFixturesFundedCachedNewest {
 
   behavior of "UTXORpc"
 
-  it should "be able to list utxos" in {
+  it should "be able to list utxos" in { case client =>
     for {
-      client <- clientF
       unspent <- client.listUnspent
     } yield assert(unspent.nonEmpty)
 
   }
 
   it should "be able to lock and unlock utxos as well as list locked utxos" in {
-    for {
-      client <- clientF
-      unspent <- client.listUnspent
-      txid1 = unspent(0).txid
-      txid2 = unspent(1).txid
-      param = {
+    case client =>
+      for {
+        _ <- client.generate(1)
+        unspent <- client.listUnspent
+        txid1 = unspent(0).txid
+        txid2 = unspent(1).txid
+        param = {
 
-        val vout1 = unspent(0).vout
-        val vout2 = unspent(1).vout
-        Vector(
-          RpcOpts.LockUnspentOutputParameter(txid1, vout1),
-          RpcOpts.LockUnspentOutputParameter(txid2, vout2)
-        )
+          val vout1 = unspent(0).vout
+          val vout2 = unspent(1).vout
+          Vector(
+            RpcOpts.LockUnspentOutputParameter(txid1, vout1),
+            RpcOpts.LockUnspentOutputParameter(txid2, vout2)
+          )
+        }
+        firstSuccess <- client.lockUnspent(unlock = false, param)
+        locked <- client.listLockUnspent
+        secondSuccess <- client.lockUnspent(unlock = true, param)
+        newLocked <- client.listLockUnspent
+      } yield {
+        val txids = Vector(txid1, txid2)
+        assert(firstSuccess)
+        assert(locked.length == 2)
+        assert(txids.exists(_ == locked(0).txIdBE))
+        assert(txids.exists(_ == locked(1).txIdBE))
+        assert(secondSuccess)
+        assert(newLocked.isEmpty)
       }
-      firstSuccess <- client.lockUnspent(unlock = false, param)
-      locked <- client.listLockUnspent
-      secondSuccess <- client.lockUnspent(unlock = true, param)
-      newLocked <- client.listLockUnspent
-    } yield {
-      val txids = Vector(txid1, txid2)
-      assert(firstSuccess)
-      assert(locked.length == 2)
-      assert(txids.exists(_ == locked(0).txId.flip))
-      assert(txids.exists(_ == locked(1).txId.flip))
-      assert(secondSuccess)
-      assert(newLocked.isEmpty)
-    }
   }
 
-  it should "be able to get utxo info" in {
+  it should "be able to get utxo info" in { case client =>
     for {
-      client <- clientF
       block <- BitcoindRpcTestUtil.getFirstBlock(client)
       info1 <- client.getTxOut(block.tx.head.txid, 0)
     } yield assert(info1.coinbase)
   }
 
-  it should "be able to fail to get utxo info" in {
+  it should "be able to fail to get utxo info" in { case client =>
     for {
-      client <- clientF
       block <- BitcoindRpcTestUtil.getFirstBlock(client)
       info1 <- client.getTxOutOpt(block.tx.head.txid, 0)
       info2 <- client.getTxOutOpt(DoubleSha256DigestBE.empty, 0)
@@ -75,9 +70,8 @@ class UTXORpcTest extends BitcoindRpcTest {
     }
   }
 
-  it should "correctly dump tx out set" in {
+  it should "correctly dump tx out set" in { case client =>
     for {
-      client <- clientF
       hash <- client.getBestBlockHash()
       height <- client.getBestHashBlockHeight()
       result <- client.dumpTxOutSet(new File("utxo.dat").toPath)
