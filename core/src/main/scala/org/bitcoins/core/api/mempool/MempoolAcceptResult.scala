b871d06d93b69528a42065b5029b7597a8494b59
Chris Stewart <stewart.chris1234@gmail.com>
2024-05-04 09:59:16 -0500

    Introduce MempoolApi, MempoolAcceptResult

---
+++ core/src/main/scala/org/bitcoins/core/api/mempool/MempoolAcceptResult.scala
@@ -0,0 +1,33 @@
+package org.bitcoins.core.api.mempool
+
+import org.bitcoins.core.currency.Satoshis
+
+sealed abstract class MempoolAcceptResult {
+
+  /** Raw base fees in satoshis. */
+  def vsize: Int
+
+  /** The feerate at which this transaction was considered. This includes any
+    * fee delta added using prioritisetransaction (i.e. modified fees). If this
+    * transaction was submitted as a package, this is the package feerate, which
+    * may also include its descendants and/or ancestors (see
+    * m_wtxids_fee_calculations below).
+    */
+  def baseFee: Satoshis
+}
+
+object MempoolAcceptResult {
+
+  case class Valid(vsize: Int, baseFee: Satoshis) extends MempoolAcceptResult
+  case class Invalid(vsize: Int, baseFee: Satoshis) extends MempoolAcceptResult
+
+  /** A tx that we already have in our mempool */
+  case class DuplicateEntry(vsize: Int, baseFee: Satoshis)
+      extends MempoolAcceptResult
+
+  /** Not validated. A same-txid-different-witness tx (see m_other_wtxid)
+    * already exists in the mempool and was not replaced.
+    */
+  case class DifferentWitness(vsize: Int, baseFee: Satoshis)
+      extends MempoolAcceptResult
+}
