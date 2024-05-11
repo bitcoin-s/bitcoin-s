2f0a8541ea1df4db3350d7e85bbddfb3434c502b
Chris Stewart <stewart.chris1234@gmail.com>
2024-05-11 14:21:42 -0500

    Get db-commons/ compiling
    # Conflicts:
    #	db-commons/src/main/scala/org/bitcoins/db/DbCommonsColumnMappers.scala
    
    # Conflicts:
    #	db-commons/src/main/scala/org/bitcoins/db/CRUDAutoInc.scala

--- db-commons/src/main/scala/org/bitcoins/db/SlickUtil.scala
+++ db-commons/src/main/scala/org/bitcoins/db/SlickUtil.scala
@@ -1,19 +1,16 @@
 package org.bitcoins.db
 
-import slick.jdbc.JdbcProfile
+import slick.dbio.{DBIOAction, Effect, NoStream}
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
-trait SlickUtilAction[T, PrimaryKeyType] { _: CRUDAction[T, PrimaryKeyType] =>
-
-  def profile: JdbcProfile
-
-  import profile.api._
+trait SlickUtilAction[T, PrimaryKeyType] {
+  this: CRUDAction[T, PrimaryKeyType] =>
 
   def createAllAction(
-      ts: Vector[T]
-  ): DBIOAction[Vector[T], NoStream, Effect.Write] = {
+      ts: Vector[T]): DBIOAction[Vector[T], NoStream, Effect.Write] = {
+    import profile.api._
     val fixedSqlAction = table ++= ts
 
     fixedSqlAction.map(_ => ts)
@@ -21,15 +18,13 @@ trait SlickUtilAction[T, PrimaryKeyType] { _: CRUDAction[T, PrimaryKeyType] =>
 }
 
 trait SlickUtil[T, PrimaryKeyType] extends SlickUtilAction[T, PrimaryKeyType] {
-  _: CRUD[T, PrimaryKeyType] =>
-  def profile: JdbcProfile
+  this: CRUD[T, PrimaryKeyType] =>
 
   import profile.api._
 
   /** Creates rows in a database that are not auto incremented */
   def createAllNoAutoInc(ts: Vector[T], database: SafeDatabase)(implicit
-      ec: ExecutionContext
-  ): Future[Vector[T]] = {
+      ec: ExecutionContext): Future[Vector[T]] = {
     val actions = (table ++= ts).andThen(DBIO.successful(ts))
     val result = database.run(actions)
     result
