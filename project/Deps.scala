fa97dd55ecad608144d139ace42c2d07e111ec4c
Chris Stewart <stewart.chris1234@gmail.com>
2024-05-11 12:33:03 -0500

    Get scala3 crossbuild working upto db-commons/

--- project/Deps.scala
+++ project/Deps.scala
@@ -219,7 +219,7 @@ object Deps {
         "org.scalacheck" %%% "scalacheck" % V.scalacheck withSources () withJavadoc ())
 
     val scalaJsStubs =
-      "org.scala-js" %% "scalajs-stubs" % V.scalaJsStubsV % "provided"
+      "org.scala-js" % "scalajs-stubs_2.13" % V.scalaJsStubsV % "provided"
 
     val scalaJsTime =
       Def.setting(
@@ -230,7 +230,7 @@ object Deps {
         "org.scalatest" %%% "scalatest" % V.scalaTest withSources () withJavadoc ())
 
     val scalaTestPlus = Def.setting(
-      "org.scalatestplus" %%% "scalacheck-1-17" % V.scalaTestPlus withSources () withJavadoc ())
+      "org.scalatestplus" %%% "scalacheck-1-17" % V.scalaTestPlus withSources () /*withJavadoc ()*/)
 
     val pgEmbedded =
       "com.opentable.components" % "otj-pg-embedded" % V.pgEmbeddedV withSources () withJavadoc ()
