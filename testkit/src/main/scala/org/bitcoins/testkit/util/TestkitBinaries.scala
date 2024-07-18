df6be4e5277ee29af4a4934b0e1bfd16231e2dd9
Chris Stewart <stewart.chris1234@gmail.com>
2024-07-18 12:12:09 -0500

    Simplify TestKitBinaries

--- testkit/src/main/scala/org/bitcoins/testkit/util/TestkitBinaries.scala
+++ testkit/src/main/scala/org/bitcoins/testkit/util/TestkitBinaries.scala
@@ -2,8 +2,7 @@ package org.bitcoins.testkit.util
 
 import org.bitcoins.commons.config.AppConfig
 
-import java.nio.file.{Path, Paths}
-import scala.util.Properties
+import java.nio.file.{Path}
 
 object TestkitBinaries {
 
@@ -12,15 +11,5 @@ object TestkitBinaries {
 
   /** The base directory where binaries needed in tests are located.
     */
-  lazy val baseBinaryDirectory: Path = {
-    val home = Paths.get(Properties.userHome)
-    fromRoot(home)
-  }
-
-  /** Gives you an arbitrary root path, and then tacks on .bitcoin-s/binaries/
-    * onto the end of it
-    */
-  private def fromRoot(path: Path): Path = {
-    path.resolve(base)
-  }
+  lazy val baseBinaryDirectory: Path = base
 }
