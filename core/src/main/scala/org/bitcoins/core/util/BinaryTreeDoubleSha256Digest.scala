dca2146647e474d60b550106238f818c9bd9eea3
Chris Stewart <stewart.chris1234@gmail.com>
2024-10-19 15:13:01 -0500

    2024 10 19 rm generic btree (#5725)
    * Remove BinaryTree.scala
    
    * Enable -Xlint:type-parameter-shadow
    
    * Remove implicit usage in BinaryTreeDoubleSha256Digest

--- core/src/main/scala/org/bitcoins/core/util/BinaryTreeDoubleSha256Digest.scala
+++ core/src/main/scala/org/bitcoins/core/util/BinaryTreeDoubleSha256Digest.scala
@@ -4,23 +4,22 @@ import org.bitcoins.crypto.DoubleSha256Digest
 
 import scala.annotation.tailrec
 
-sealed trait BinaryTreeDoubleSha256Digest
-    extends BinaryTree[DoubleSha256Digest] {
-  override def value: Option[DoubleSha256Digest] =
+sealed trait BinaryTreeDoubleSha256Digest {
+  def value: Option[DoubleSha256Digest] =
     this match {
       case n: NodeDoubleSha256Digest   => Some(n.v)
       case l: LeafDoubleSha256Digest   => Some(l.v)
       case EmptyTreeDoubleSha256Digest => None
     }
 
-  override def left: Option[BinaryTreeDoubleSha256Digest] =
+  def left: Option[BinaryTreeDoubleSha256Digest] =
     this match {
       case n: NodeDoubleSha256Digest   => Some(n.l)
       case _: LeafDoubleSha256Digest   => None
       case EmptyTreeDoubleSha256Digest => None
     }
 
-  override def right: Option[BinaryTreeDoubleSha256Digest] =
+  def right: Option[BinaryTreeDoubleSha256Digest] =
     this match {
       case n: NodeDoubleSha256Digest   => Some(n.r)
       case _: LeafDoubleSha256Digest   => None
@@ -30,9 +29,8 @@ sealed trait BinaryTreeDoubleSha256Digest
   /** A function to find the first occurrence of a predicate inside a
     * [[org.bitcoins.core.util.BinaryTree BinaryTree]].
     */
-  def findFirstDFS(t: DoubleSha256Digest)(implicit
-      tree: BinaryTreeDoubleSha256Digest)
-      : Option[BinaryTreeDoubleSha256Digest] = {
+  def findFirstDFS(
+      t: DoubleSha256Digest): Option[BinaryTreeDoubleSha256Digest] = {
     val f = { (x: DoubleSha256Digest) => x == t }
     @tailrec
     def loop(
@@ -51,24 +49,22 @@ sealed trait BinaryTreeDoubleSha256Digest
           else loop(l, r :: remainder)
       }
     }
-    loop(tree, List())
+    loop(this, List.empty)
   }
 
   /** Checks if the [[org.bitcoins.core.util.BinaryTree BinaryTree]] contains a
     * certain element.
     */
-  def contains(t: DoubleSha256Digest)(implicit
-      tree: BinaryTreeDoubleSha256Digest): Boolean =
-    findFirstDFS(t)(tree).isDefined
+  def contains(t: DoubleSha256Digest): Boolean =
+    findFirstDFS(t).isDefined
 
   /** Inserts an element into one of the two branches in a
     * [[org.bitcoins.core.util.BinaryTree BinaryTree]]. If it cannot insert it
     * because the branches are not empty, it throws a
     * [[scala.RuntimeException RuntimeException]].
     */
-  def insert(t: DoubleSha256Digest)(implicit
-      tree: BinaryTreeDoubleSha256Digest): BinaryTreeDoubleSha256Digest = {
-    insert(LeafDoubleSha256Digest(t))(tree)
+  def insert(t: DoubleSha256Digest): BinaryTreeDoubleSha256Digest = {
+    insert(LeafDoubleSha256Digest(t))
   }
 
   /** Inserts a tree into one of the two branches in a
@@ -76,9 +72,9 @@ sealed trait BinaryTreeDoubleSha256Digest
     * because the branches are not empty, it throws a
     * [[scala.RuntimeException RuntimeException]].
     */
-  def insert(subTree: BinaryTreeDoubleSha256Digest)(implicit
-      parentTree: BinaryTreeDoubleSha256Digest): BinaryTreeDoubleSha256Digest =
-    parentTree match {
+  def insert(
+      subTree: BinaryTreeDoubleSha256Digest): BinaryTreeDoubleSha256Digest =
+    this match {
       case n: NodeDoubleSha256Digest =>
         if (n.l == EmptyTreeDoubleSha256Digest)
           NodeDoubleSha256Digest(n.v, subTree, n.r)
@@ -86,42 +82,36 @@ sealed trait BinaryTreeDoubleSha256Digest
           NodeDoubleSha256Digest(n.v, n.l, subTree)
         else
           throw new RuntimeException(
-            "There was no empty branch to insert the new t: " + subTree + "inside of tree: " + parentTree)
+            "There was no empty branch to insert the new t: " + subTree + "inside of tree: " + this)
       case l: LeafDoubleSha256Digest =>
-        NodeDoubleSha256Digest(l.v,
-                               subTree,
-                               Empty.asInstanceOf[BinaryTreeDoubleSha256Digest])
+        NodeDoubleSha256Digest(l.v, subTree, EmptyTreeDoubleSha256Digest)
       case EmptyTreeDoubleSha256Digest => subTree
     }
 
   /** Removes the subTree from the parentTree. */
-  def remove(subTree: BinaryTreeDoubleSha256Digest)(
-      parentTree: BinaryTreeDoubleSha256Digest)
-      : BinaryTreeDoubleSha256Digest = {
+  def remove(
+      subTree: BinaryTreeDoubleSha256Digest): BinaryTreeDoubleSha256Digest = {
     // TODO: Optimize into a tail recursive function
-    parentTree match {
+    this match {
       case EmptyTreeDoubleSha256Digest =>
-        Empty.asInstanceOf[BinaryTreeDoubleSha256Digest]
+        EmptyTreeDoubleSha256Digest
       case l: LeafDoubleSha256Digest =>
-        if (l == subTree) Empty.asInstanceOf[BinaryTreeDoubleSha256Digest]
+        if (l == subTree) EmptyTreeDoubleSha256Digest
         else l
       case n: NodeDoubleSha256Digest =>
-        if (n == subTree) Empty.asInstanceOf[BinaryTreeDoubleSha256Digest]
+        if (n == subTree) EmptyTreeDoubleSha256Digest
         else
-          NodeDoubleSha256Digest(n.v,
-                                 remove(subTree)(n.l),
-                                 remove(subTree)(n.r))
+          NodeDoubleSha256Digest(n.v, remove(subTree), remove(subTree))
     }
   }
 
   /** Replaces all instances of the original tree with the replacement tree. */
   def replace(
       originalTree: BinaryTreeDoubleSha256Digest,
-      replacementTree: BinaryTreeDoubleSha256Digest)(implicit
-      parentTree: BinaryTreeDoubleSha256Digest)
+      replacementTree: BinaryTreeDoubleSha256Digest)
       : BinaryTreeDoubleSha256Digest = {
     // TODO: Optimize this into a tail recursive function
-    parentTree match {
+    this match {
       case EmptyTreeDoubleSha256Digest =>
         if (originalTree == EmptyTreeDoubleSha256Digest) replacementTree
         else EmptyTreeDoubleSha256Digest
@@ -131,8 +121,8 @@ sealed trait BinaryTreeDoubleSha256Digest
         if (n == originalTree) replacementTree
         else
           NodeDoubleSha256Digest(n.v,
-                                 replace(originalTree, replacementTree)(n.l),
-                                 replace(originalTree, replacementTree)(n.r))
+                                 replace(originalTree, replacementTree),
+                                 replace(originalTree, replacementTree))
     }
   }
 }
