e6cf850014bf5e5061e9653998e8c5c7a43d00b9
Scala Steward <scala_steward@virtuslab.com>
2025-01-16 14:37:02 +0000

    Update bcprov-jdk18on to 1.80

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
 
   object V {
     val antV = "1.10.15"
-    val bouncyCastle = "1.79"
+    val bouncyCastle = "1.80"
     val dropwizardMetricsV = "4.2.29" // https://github.com/dropwizard/metrics
 
     val logback = "1.5.16" // https://github.com/qos-ch/logback
