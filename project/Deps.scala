64a3abaa1ebc400a7996ec8088e8e664ae74e250
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-02-07 22:33:08 +0100

    Update scalamock to 6.2.0 (#5911)

--- project/Deps.scala
+++ project/Deps.scala
@@ -43,7 +43,7 @@ object Deps {
     val sqliteV = "3.49.0.0" // https://github.com/xerial/sqlite-jdbc
 
     val scalameterV = "0.17"
-    val scalamockV = "6.1.1"
+    val scalamockV = "6.2.0"
     val scalaCollectionCompatV = "2.13.0"
     val pgEmbeddedV = "1.1.0"
 
