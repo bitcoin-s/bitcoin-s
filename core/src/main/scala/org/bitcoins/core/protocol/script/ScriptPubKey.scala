12c1bdfe09ba85961e5a886555691a85708beae9
Chris Stewart <stewart.chris1234@gmail.com>
2025-02-12 13:28:22 -0600

    core: Tighten `MultiSignatureScriptPubKey.isValidAsm()` check (#5928)
    * core: Tighten MultiSignatureScriptPubKey.isValidAsm() check
    
    * Use string interpolation

--- core/src/main/scala/org/bitcoins/core/protocol/script/ScriptPubKey.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/script/ScriptPubKey.scala
@@ -245,7 +245,7 @@ object MultiSignatureScriptPubKey
   def fromAsm(asm: Seq[ScriptToken]): MultiSignatureScriptPubKey = {
     buildScript(asm.toVector,
                 MultiSignatureScriptPubKeyImpl.apply,
-                "Given asm was not a MultSignatureScriptPubKey, got: " + asm)
+                s"Given asm was not a MultSignatureScriptPubKey, got: $asm")
   }
 
   def apply(asm: Seq[ScriptToken]): MultiSignatureScriptPubKey = fromAsm(asm)
@@ -255,7 +255,7 @@ object MultiSignatureScriptPubKey
   /** Determines if the given script tokens are a multisignature `scriptPubKey`
     */
   override def isValidAsm(asm: Seq[ScriptToken]): Boolean = {
-    if (asm.nonEmpty && opCmsOPs.exists(_ == asm.last)) {
+    if (asm.length > 2 && opCmsOPs.exists(_ == asm.last)) {
       val cmsIdx = asm.size - 1
       // we need either the first or second asm operation to indicate how many signatures are required
       val hasRequiredSignaturesOpt: Option[Int] = {
