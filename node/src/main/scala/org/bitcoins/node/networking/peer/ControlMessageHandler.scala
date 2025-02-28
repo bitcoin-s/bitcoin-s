512edacab4d5478e8511719c2d236a5bb56ff4d5
Chris Stewart <stewart.chris1234@gmail.com>
2025-02-28 16:14:30 -0600

    scalafmt

--- node/src/main/scala/org/bitcoins/node/networking/peer/ControlMessageHandler.scala
+++ node/src/main/scala/org/bitcoins/node/networking/peer/ControlMessageHandler.scala
@@ -21,7 +21,8 @@ case class ControlMessageHandler(peerFinder: PeerFinder)(implicit
       peerMsgSenderApi: PeerMessageSenderApi
   ): Future[Option[ControlMessageHandlerState]] = {
     val peer = peerMsgSenderApi.peer
-    logger.debug(s"Received control message=${payload.commandName} from peer=$peer")
+    logger.debug(
+      s"Received control message=${payload.commandName} from peer=$peer")
     payload match {
       case versionMsg: VersionMessage =>
         peerFinder.onVersionMessage(peer, versionMsg)
