26a2034163a01c271d9c88d25312be68b7f17e4f
Scala Steward <scala_steward@virtuslab.com>
2025-03-31 11:15:46 +0000

    Update scalamock to 7.3.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -45,7 +45,7 @@ object Deps {
     val sqliteV = "3.49.1.0" // https://github.com/xerial/sqlite-jdbc
 
     val scalameterV = "0.17"
-    val scalamockV = "6.2.0"
+    val scalamockV = "7.3.0"
     val scalaCollectionCompatV = "2.13.0"
     val pgEmbeddedV = "1.1.0"
 
@@ -181,7 +181,8 @@ object Deps {
       "logkit" % "logkit" % V.logkitV // https://github.com/apache/commons-logging/blob/0d4f2604ada038fd95e714d504d2278f1bd5814a/pom.xml#L492
     val avalonLogging =
       "avalon-framework" % "avalon-framework" % V.avalonLoggingV // https://github.com/apache/commons-logging/blob/0d4f2604ada038fd95e714d504d2278f1bd5814a/pom.xml#L498
-    val xz = "org.tukaani" % "xz" % V.xzV // https://github.com/tukaani-project/xz-java
+    val xz =
+      "org.tukaani" % "xz" % V.xzV // https://github.com/tukaani-project/xz-java
     val codehaus = "org.codehaus.janino" % "janino" % V.codehausV
 
     // for loading secp256k1 natively
@@ -452,7 +453,7 @@ object Deps {
       Compile.logback,
       Compile.codehaus,
       Compile.ant,
-      Compile.xz //transitive
+      Compile.xz // transitive
     )
   }
 
