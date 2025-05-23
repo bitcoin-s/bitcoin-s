9b8b917ff19fe3835d0a4e66227f583d7ccaa3a0
Chris Stewart <stewart.chris1234@gmail.com>
2025-05-23 12:40:06 -0500

    Add scaladoc

--- core/src/main/scala/org/bitcoins/core/protocol/script/Annex.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/script/Annex.scala
@@ -2,6 +2,9 @@ package org.bitcoins.core.protocol.script
 import org.bitcoins.crypto.{Factory, NetworkElement}
 import scodec.bits.ByteVector
 
+/** A type to represent the Taproot annex as per BIP341
+  * [[https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki#cite_note-5]]
+  */
 sealed trait Annex extends NetworkElement {
   require(bytes.head == TaprootScriptPath.annex,
           s"Annex did not have correct first byte, got=${bytes.head}")
