00b639bd3722985d9d3c163b1810ee9851405be6
Scala Steward <scala_steward@virtuslab.com>
2025-05-26 13:36:01 +0000

    Update metrics-core, metrics-healthchecks, ... to 4.2.32

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
     val antV = "1.10.15"
 
     val bouncyCastle = "1.80"
-    val dropwizardMetricsV = "4.2.30" // https://github.com/dropwizard/metrics
+    val dropwizardMetricsV = "4.2.32" // https://github.com/dropwizard/metrics
     val dropWizardMetrics5V = "5.0.0"
 
     val logback = "1.5.18" // https://github.com/qos-ch/logback
