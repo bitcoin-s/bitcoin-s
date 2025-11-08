500ffb20e0fbfe4e5cb3480d9998f0a27d710bcd
Scala Steward <scala_steward@virtuslab.com>
2025-11-08 15:00:30 +0000

    Update scalamock to 7.5.2

--- project/Deps.scala
+++ project/Deps.scala
@@ -46,7 +46,7 @@ object Deps {
     val sqliteV = "3.51.0.0" // https://github.com/xerial/sqlite-jdbc
 
     val scalameterV = "0.17"
-    val scalamockV = "7.5.1"
+    val scalamockV = "7.5.2"
     val scalaCollectionCompatV = "2.14.0"
     val pgEmbeddedV = "1.1.1"
 
