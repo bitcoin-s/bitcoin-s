3e4eaca57205264856ddd2c816b05ec4327b671c
Chris Stewart <stewart.chris1234@gmail.com>
2025-11-27 12:29:45 -0600

    Add BIP54 test cases

---
+++ core-test/.jvm/src/test/scala/org/bitcoins/core/protocol/blockchain/Bip54SigopsTest.scala
@@ -0,0 +1,39 @@
+package org.bitcoins.core.protocol.blockchain
+import org.bitcoins.commons.serializers.SerializerUtil
+import org.bitcoins.core.protocol.transaction.{Transaction, TransactionOutput}
+import org.bitcoins.testkitcore.util.BitcoinSJvmTest
+import play.api.libs.json.{JsResult, JsValue, Json, Reads}
+
+import scala.io.Source
+import scala.util.Using
+
+class Bip54SigopsTest extends BitcoinSJvmTest {
+
+  behavior of "BIP54 sigops"
+
+  import org.bitcoins.commons.serializers.JsonSerializers.transactionReads
+  implicit object TransactionOutputReads extends Reads[TransactionOutput] {
+    override def reads(json: JsValue): JsResult[TransactionOutput] =
+      SerializerUtil.processJsString(TransactionOutput.fromHex)(json)
+  }
+  case class Bip54SigOpsTestCase(
+      spent_outputs: Vector[TransactionOutput],
+      tx: Transaction,
+      valid: Boolean,
+      comment: String)
+  implicit val bip54SigOpsTestCaseReader: Reads[Bip54SigOpsTestCase] =
+    Json.reads[Bip54SigOpsTestCase]
+  it must "pass all bip54 sigops test vectors" in {
+    val fileName =
+      "/sigops.json"
+    val lines = Using(Source.fromURL(getClass.getResource(fileName))) {
+      source => source.mkString
+    }.get
+    val json = Json.parse(lines)
+    val testCases = json.validate[Vector[Bip54SigOpsTestCase]]
+
+    succeed
+
+  }
+
+}
