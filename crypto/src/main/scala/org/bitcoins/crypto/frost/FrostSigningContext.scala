61a93f4c3f15d2a6eb7b612ca0334f456ff09439
Chris Stewart <stewart.chris1234@gmail.com>
2026-01-21 16:44:53 -0600

    WIP: sign_verify_vectors.json

--- crypto/src/main/scala/org/bitcoins/crypto/frost/FrostSigningContext.scala
+++ crypto/src/main/scala/org/bitcoins/crypto/frost/FrostSigningContext.scala
@@ -4,8 +4,7 @@ import org.bitcoins.crypto.{
   ECPublicKey,
   SecpPoint,
   SecpPointFinite,
-  SecpPointInfinity,
-  XOnlyPubKey
+  SecpPointInfinity
 }
 
 case class FrostSigningContext(
@@ -14,7 +13,7 @@ case class FrostSigningContext(
     u: Long,
     ids: Vector[Int],
     pubshares: Vector[ECPublicKey],
-    thresholdPubKey: XOnlyPubKey) {
+    thresholdPubKey: ECPublicKey) {
   require(n >= 2 && n < 4294967296L,
           s"n must be in the range [2, 2^32 - 1], got: $n")
   require(1 <= t && t <= n,
@@ -22,17 +21,17 @@ case class FrostSigningContext(
   require(u >= t && u <= n,
           s"u must be in the range [t, n], got: $u with t: $t and n: $n")
   require(
-    u == ids.length,
-    s"ids length must be equal to u, got ids length: ${ids.length} with u: $u")
+    n == ids.length,
+    s"ids length must be equal to u, got ids length: ${ids.length} with n: $n")
   require(
-    u == pubshares.length,
-    s"pubshare length must be equal to u, got pubshare length: ${pubshares.length} with u: $u")
+    n == pubshares.length,
+    s"pubshare length must be equal to u, got pubshare length: ${pubshares.length} with n: $u")
   require(ids.toSet.size == ids.length, s"All ids must be unique, got: $ids")
 
-  private def pk: XOnlyPubKey = {
+  private def pk: ECPublicKey = {
     val pointSum = SecpPoint.sum(pubshares.map(_.toPoint))
     pointSum match {
-      case s: SecpPointFinite => s.toPublicKey.toXOnly
+      case s: SecpPointFinite => s.toPublicKey
       case SecpPointInfinity =>
         throw new IllegalArgumentException(
           s"Computed aggregate public key is point at infinity, invalid pubshares: $pubshares")
