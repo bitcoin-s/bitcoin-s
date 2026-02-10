62450c635896ce38e6ce297a8d8f6d986b4d19e2
Scala Steward <scala_steward@virtuslab.com>
2026-02-10 15:56:09 +0000

    Update logback-classic to 1.5.29

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,7 +11,7 @@ object Deps {
 
     val dropwizardMetricsV = "4.2.38" // https://github.com/dropwizard/metrics
 
-    val logback = "1.5.28" // https://github.com/qos-ch/logback
+    val logback = "1.5.29" // https://github.com/qos-ch/logback
     val log4jV = "1.2.17"
 
     val logkitV = "20020529"
