f95a325944e42fec5e25d1010dc1034e442ad383
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-08-30 00:39:53 +0200

    Update metrics-core, metrics-healthchecks, ... to 4.2.40 (#6479)

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
     val bouncyCastle = "1.85"
     val dropWizardMetrics5V = "5.0.8"
 
-    val dropwizardMetricsV = "4.2.39" // https://github.com/dropwizard/metrics
+    val dropwizardMetricsV = "4.2.40" // https://github.com/dropwizard/metrics
 
     val logback = "1.6.3" // https://github.com/qos-ch/logback
     val log4jV = "1.2.17"
